2099f455212f49cfd6bceebb39f1829828346db0
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-07-06 23:56:55 +0000

    Whoops
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3devel/MMOCoreORB@428 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/config.lua
+++ bin/config.lua
@@ -49,11 +49,11 @@ MakePing = 1
 ORB = ""
 
 
-DBHost = "192.168.1.180"
+DBHost = "127.0.0.1"
 DBPort = 3306
-DBName = "swgemu2"
-DBUser = "root"
-DBPass = "351windsor"
+DBName = "swgemu"
+DBUser = "swgemu"
+DBPass = "swgemu"
 
 LoginPort = 44453
 LoginAllowedConnections = 3000
