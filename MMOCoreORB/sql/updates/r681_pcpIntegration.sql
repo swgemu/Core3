36cdd109c1efbd7a3522605b71984fabd56fd5ca
cRush <cRush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-09-10 22:17:44 +0000

    [database] Updated to reflect integration for PCP. -Does not require a wipe and these changes will affect nothing in game. No offsets need to be changed because they are respectively appended to their tables. [renamed] 674_deeds.sql to r674_deeds.sql
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@681 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/sql/updates/r681_pcpIntegration.sql
@@ -0,0 +1,7 @@
+﻿/*
+  Alters account and characters with two new fields for pcp integration.
+  - These fields will not impact anything in game, and no code changes are needed at this time, nor is a database wipe.
+*/
+
+ALTER TABLE `account` ADD COLUMN `adminLevel` tinyint(1) unsigned NOT NULL default '4' AFTER `active`;
+ALTER TABLE `characters` ADD COLUMN `public` tinyint(1) unsigned NOT NULL default '0' AFTER `adminLevel`;
