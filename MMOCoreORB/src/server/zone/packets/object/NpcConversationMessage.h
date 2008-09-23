ec8add0da70bb6f89b355c2c560a00be7d8ffc1e
Bobius <Bobius@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-09-23 02:38:55 +0000

    [added] StfParameter class to allow full use of all variables in stf file strings (%TO, %TT, %DI, etc...) [updated] NpcConversationMessage to utilize StfParamaters
    More to come.
    
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@726 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/packets/object/NpcConversationMessage.h
+++ MMOCoreORB/src/server/zone/packets/object/NpcConversationMessage.h
@@ -1,58 +1,187 @@
 /*
 Copyright (C) 2007 <SWGEmu>
- 
+
 This File is part of Core3.
- 
-This program is free software; you can redistribute 
-it and/or modify it under the terms of the GNU Lesser 
+
+This program is free software; you can redistribute
+it and/or modify it under the terms of the GNU Lesser
 General Public License as published by the Free Software
-Foundation; either version 2 of the License, 
+Foundation; either version 2 of the License,
 or (at your option) any later version.
- 
-This program is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Lesser General Public License for
 more details.
- 
-You should have received a copy of the GNU Lesser General 
+
+You should have received a copy of the GNU Lesser General
 Public License along with this program; if not, write to
 the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- 
-Linking Engine3 statically or dynamically with other modules 
-is making a combined work based on Engine3. 
-Thus, the terms and conditions of the GNU Lesser General Public License 
+
+Linking Engine3 statically or dynamically with other modules
+is making a combined work based on Engine3.
+Thus, the terms and conditions of the GNU Lesser General Public License
 cover the whole combination.
- 
-In addition, as a special exception, the copyright holders of Engine3 
-give you permission to combine Engine3 program with free software 
-programs or libraries that are released under the GNU LGPL and with 
-code included in the standard release of Core3 under the GNU LGPL 
-license (or modified versions of such code, with unchanged license). 
-You may copy and distribute such a system following the terms of the 
-GNU LGPL for Engine3 and the licenses of the other code concerned, 
-provided that you include the source code of that other code when 
+
+In addition, as a special exception, the copyright holders of Engine3
+give you permission to combine Engine3 program with free software
+programs or libraries that are released under the GNU LGPL and with
+code included in the standard release of Core3 under the GNU LGPL
+license (or modified versions of such code, with unchanged license).
+You may copy and distribute such a system following the terms of the
+GNU LGPL for Engine3 and the licenses of the other code concerned,
+provided that you include the source code of that other code when
 and as the GNU LGPL requires distribution of source code.
- 
-Note that people who make modified versions of Engine3 are not obligated 
-to grant this special exception for their modified versions; 
-it is their choice whether to do so. The GNU Lesser General Public License 
-gives permission to release a modified version without this exception; 
-this exception also makes it possible to release a modified version 
+
+Note that people who make modified versions of Engine3 are not obligated
+to grant this special exception for their modified versions;
+it is their choice whether to do so. The GNU Lesser General Public License
+gives permission to release a modified version without this exception;
+this exception also makes it possible to release a modified version
 which carries forward this exception.
 */
 
 #ifndef NPCCONVERSATIONMESSAGE_H_
 #define NPCCONVERSATIONMESSAGE_H_
- 
+
+#include "ObjectControllerMessage.h"
+#include "../../objects/creature/CreatureObject.h"
+#include "StfParameter.h"
+
 class NpcConversationMessage : public ObjectControllerMessage {
 public:
 
-	NpcConversationMessage(CreatureObject* creo, unicode& message) 
+	NpcConversationMessage(CreatureObject* creo, unicode& message)
 		: ObjectControllerMessage(creo->getObjectID(), 0x0B, 0xDF) {
-			
+
 		insertUnicode(message);
 	}
+
+	NpcConversationMessage(CreatureObject* creo, const string& file, const string& str, uint64 TO = 0, uint64 TU = 0, uint64 TT = 0, int DI = 0, float DF = 0) :
+		ObjectControllerMessage(creo->getObjectID(), 0x0B, 0xDF) {
+
+		int size = 0x56 + file.size() + str.size();
+		bool odd = (size & 1);
+
+		if (odd)
+			insertInt((size + 1) / 2);
+		else
+			insertInt(size / 2);
+
+		insertShort(0);
+		insertShort(0);
+		insertByte(1);
+		insertInt(0xFFFFFFFF);
+
+		insertAscii(file.c_str());
+		insertInt(0);
+		insertAscii(str.c_str());
+
+		insertLong(TU); // %TU
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertInt(0);
+
+		insertLong(TT); //%TT
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertInt(0);
+
+		insertLong(TO); //%TO
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertInt(0);
+
+		insertInt(DI); //%DI
+		insertFloat(DF); //%DF
+		insertByte(0);
+		insertShort(0);
+
+		if (odd)
+			insertByte(0);
+
+		insertInt(0);
+	}
+	NpcConversationMessage(CreatureObject* creo, const string& file, const string& str, unicode TO, uint64 TU = 0, uint64 TT = 0, int DI = 0, float DF = 0)
+		: ObjectControllerMessage(creo->getObjectID(), 0x0B, 0xDF) {
+
+		int size = 0x56 + file.size() + str.size() + (2 * TO.size());
+		bool odd = (size & 1);
+
+		if (odd)
+			insertInt((size + 1) / 2);
+		else
+			insertInt(size / 2);
+
+		insertShort(0);
+		insertShort(0);
+		insertByte(1);
+		insertInt(0xFFFFFFFF);
+
+		insertAscii(file.c_str());
+		insertInt(0);
+		insertAscii(str.c_str());
+
+		insertLong(TU); // %TU
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertInt(0);
+
+		insertLong(TT); //%TT
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertInt(0);
+
+		insertLong(0); //%TO
+		insertAscii("");
+		insertInt(0);
+		insertAscii("");
+		insertUnicode(TO);
+
+		insertInt(DI); //%DI
+		insertFloat(DF); //%DF
+		insertByte(0);
+		insertShort(0);
+
+		if (odd)
+			insertByte(0);
+
+		insertInt(0);
+	}
+
+	NpcConversationMessage(CreatureObject* creo, const string& file, const string& str, StfParameter * params)
+			: ObjectControllerMessage(creo->getObjectID(), 0x0B, 0xDF) {
+		int size = 0x0F + file.size() + str.size() + params->size();
+		bool odd = (size & 1);
+
+		if (odd)
+			insertInt((size + 1) / 2);
+		else
+			insertInt(size / 2);
+
+		insertShort(0);
+		insertShort(0);
+		insertByte(1);
+		insertInt(0xFFFFFFFF);
+
+		insertAscii(file.c_str());
+		insertInt(0);
+		insertAscii(str.c_str());
+
+		insertStream(params);
+
+		if (odd)
+			insertByte(0);
+
+		insertInt(0);
+	}
+
 };
 
 #endif
