4204ff495327684320c63e565807acf2160bd214
Anakis <Anakis@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-01-04 08:00:13 +0000

    [Fixed] New powerboost lua stuff was overwritten with old stuff during combat merge. [Removed] PowerboostEventEnd.h (no longer used).
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@1103 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/skills/playerSkills/combat/terasKasiArtist.lua
+++ bin/scripts/skills/playerSkills/combat/terasKasiArtist.lua
@@ -84,23 +84,13 @@ AddMeditateSkill(skill)
 
 -------------------------------------------------------------
 
-boost = {
-	skillname = "powerboost",
-	effect = "",
-	
-	requiredState = MEDITATE,
-	
-	duration = 300.0, -- seconds
-	
-	healthBonus = 500,
-	actionBonus = 500,
-	mindBonus = 500,
-	
-	StartFlyText = "",
-	FinishFlyText = ""
+powerboost = {
+ 	skillname = "powerboost",
+ 	effect = "",
+ 	bonus = 0.5 --multiple of base mind to add to HAM  	
 }
-
-AddEnhanceSelfSkill(boost)
+ 
+AddPowerboostSkill(powerboost)
 
 -------------------------------------------------------------
 
