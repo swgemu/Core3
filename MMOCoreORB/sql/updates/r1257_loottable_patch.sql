7ec5c2dd96953301c45794fdab63adaeb946140d
farmer_john <farmer_john@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-02-06 16:06:23 +0000

    [fixed] Tusken Robe cannot be worn with headwear together Obv. clothes of type „Robe“ are occupying the head-slot as well (client) while the server handles them correct (codewise).
    Can some1 confirm that Pre-CU/Live Robes cannot be worn with headwear together?
    
    Temp.Fix for now: Changing Robes to type „BODYSUIT“ (162777219)
    Apply the included sql-patch.
    
    
    
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@1257 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/sql/updates/r1257_loottable_patch.sql
@@ -0,0 +1,4 @@
+update loottable set template_type=16777219 where template_type=16777229;
+update character_items set template_type=16777219 where template_type=16777229;
+update player_storage set template_type=16777219 where template_type=16777229;
+
