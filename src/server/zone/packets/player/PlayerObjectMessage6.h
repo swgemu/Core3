e400504d4018356261e15d9681b362be392df20f
Anakis <Anakis@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-03-14 07:43:17 +0000

    [Fixed] Possible crash with @setAdminLevel command. [Fixed] CSRJR staff will now display (CSR) title in game.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@1371 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/packets/player/PlayerObjectMessage6.h
+++ src/server/zone/packets/player/PlayerObjectMessage6.h
@@ -55,7 +55,12 @@ public:
 			: BaseLineMessage(play->getObjectID(), 0x504C4159, 6, 0x03) {
 
 		insertInt(0);
-		insertByte(play->getAdminLevel());  //Developer/CSR Flag
+
+		int adminFlag = play->getAdminLevel();
+		if (adminFlag = PlayerImplementation::CSRJR)
+			adminFlag = PlayerImplementation::CSR; //Allow CSRJR to have CSR title.
+
+		insertByte(adminFlag);  //Developer/CSR Flag
 
 		setSize();
 	}
