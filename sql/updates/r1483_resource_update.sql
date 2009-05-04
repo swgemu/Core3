a002c635b3c2e44118a949888303a0df9fca43b1
mrtopas <mrtopas@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-05-04 20:16:38 +0000

    [fixed] sql scripts errors
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@1486 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- sql/updates/r1483_resource_update.sql
+++ sql/updates/r1483_resource_update.sql
@@ -1,1 +1,6 @@
-alter table resource_data change INDEX objectID;
+/*alter table resource_data change INDEX objectID;*/
+
+ALTER TABLE `swgemu`.`resource_data` CHANGE COLUMN `INDEX` `objectID` INTEGER  NOT NULL DEFAULT NULL AUTO_INCREMENT,
+ DROP PRIMARY KEY,
+ ADD PRIMARY KEY  USING BTREE(`objectID`);
+
