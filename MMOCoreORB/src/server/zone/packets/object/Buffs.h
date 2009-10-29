1f749b061017bec79ceba5ca78dd58d4018e3122
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-10-29 21:50:03 +0000

    Buff packet update
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@1767 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/packets/object/Buffs.h
+++ MMOCoreORB/src/server/zone/packets/object/Buffs.h
@@ -47,15 +47,36 @@ which carries forward this exception.
 
 #include "ObjectControllerMessage.h"
 
-class Buffs : public ObjectControllerMessage {
+class AddBuffMessage : public ObjectControllerMessage {
 public:
-	Buffs(CreatureObject* creo, uint32 buffcrc, float duration) 
-			: ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x229, false) {
-		
-		insertInt(buffcrc);
-		insertFloat(duration);		
-	}
+	/**
+	 * Adds the buff to the player's buff window.
+	 * @param player {PlayerCreature} The player to add this buff to.
+	 * @param buffcrc {uint32} The crc of the buff to add.
+	 * @param duration {float} The duration of the buff in seconds.
+	 * @return {AddBuffMessage} The add buff message..
+	 */
+	AddBuffMessage(PlayerCreature* player, uint32 buffcrc, float duration)
+			: ObjectControllerMessage(player->getObjectID(), 0x1B, 0x229, false) {
 
+		insertInt(buffcrc);
+		insertFloat(duration);
+	}
+};
+
+class RemoveBuffMessage : public ObjectControllerMessage {
+public:
+	/**
+	 * Removes a buff from a player's buff window.
+	 * @param player {PlayerCreature} The player whom's buff should be removed.
+	 * @param buffcrc {uint32} The crc of the buff to remove.
+	 * @return {RemoveBuffMessage} The remove buff message.
+	 */
+	RemoveBuffMessage(PlayerCreature* player, uint32 buffcrc)
+			: ObjectControllerMessage(player->getObjectID(), 0x1B, 0x22A, false) {
+
+		insertInt(buffcrc);
+	}
 };
 
 #endif /*BUFFS_H_*/
