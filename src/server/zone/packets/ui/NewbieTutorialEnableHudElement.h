d2ea46646ecb14ba39efd7bd88ef30737c978214
Bobius <Bobius@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-11-10 20:34:19 +0000

    [added][SWGEMU-113] NewbieTutorialEnableHugElement packet implementation
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/testing@1799 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/packets/ui/NewbieTutorialEnableHudElement.h
+++ src/server/zone/packets/ui/NewbieTutorialEnableHudElement.h
@@ -8,17 +8,32 @@
 #ifndef NEWBIETUTORIALENABLEHUDELEMENT_H_
 #define NEWBIETUTORIALENABLEHUDELEMENT_H_
 
+/* Valid elements found:
+ * radar
+ * toolbar
+ * buttonbar
+ * hambar
+ * chatbox
+ * all
+ */
 class NewbieTutorialEnableHudElement : public BaseMessage {
 public:
-	NewbieTutorialEnableHudElement(const String& element) : BaseMessage() {
+
+	/**
+	 * Enables or disables a HUD element on the client.
+	 * @param element String of element
+	 * @param disable If true, disables element, otherwise enables element
+	 */
+	NewbieTutorialEnableHudElement(const String& element, bool disable = false) : BaseMessage() {
 		insertShort(0x04);
 		insertInt(0xCA375124);  // CRC
 
-		insertAscii(element); //enable all.
-		insertInt(1); //??
+		insertAscii(element); //see elements above.
+		if (disable)
+			insertInt(0);
+		else
+			insertInt(1);
 		insertByte(0); //??
-
-
    }
 
 };
