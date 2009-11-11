87b1e82f320660c82a9c05cd5e7f29b6ef6bf3e7
Bobius <Bobius@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2009-11-11 02:08:06 +0000

    [fixed][SWGEMU-113] Struct for NewbieTutorialEnableHudElement
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/testing@1800 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/packets/ui/NewbieTutorialEnableHudElement.h
+++ src/server/zone/packets/ui/NewbieTutorialEnableHudElement.h
@@ -22,18 +22,15 @@ public:
 	/**
 	 * Enables or disables a HUD element on the client.
 	 * @param element String of element
-	 * @param disable If true, disables element, otherwise enables element
+	 * @param enable If true, element is visible, otherwise disables element
 	 */
-	NewbieTutorialEnableHudElement(const String& element, bool disable = false) : BaseMessage() {
+	NewbieTutorialEnableHudElement(const String& element, bool enable = true) : BaseMessage() {
 		insertShort(0x04);
 		insertInt(0xCA375124);  // CRC
 
 		insertAscii(element); //see elements above.
-		if (disable)
-			insertInt(0);
-		else
-			insertInt(1);
-		insertByte(0); //??
+		insertBoolean(enable);
+		insertFloat(0); // unknown
    }
 
 };
