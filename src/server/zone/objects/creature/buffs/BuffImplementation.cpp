48e1c91a940f39834a3d46527d87f63711aa57e2
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-03 21:28:41 +0000

    [converted] Buff class
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2057 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ src/server/zone/objects/creature/buffs/BuffImplementation.cpp
@@ -0,0 +1,299 @@
+/*
+Copyright (C) 2007 <SWGEmu>
+
+This File is part of Core3.
+
+This program is free software; you can redistribute
+it and/or modify it under the terms of the GNU Lesser
+General Public License as published by the Free Software
+Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with this program; if not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Linking Engine3 statically or dynamically with other modules
+is making a combined work based on Engine3.
+Thus, the terms and conditions of the GNU Lesser General Public License
+cover the whole combination.
+
+In addition, as a special exception, the copyright holders of Engine3
+give you permission to combine Engine3 program with free software
+programs or libraries that are released under the GNU LGPL and with
+code included in the standard release of Core3 under the GNU LGPL
+license (or modified versions of such code, with unchanged license).
+You may copy and distribute such a system following the terms of the
+GNU LGPL for Engine3 and the licenses of the other code concerned,
+provided that you include the source code of that other code when
+and as the GNU LGPL requires distribution of source code.
+
+Note that people who make modified versions of Engine3 are not obligated
+to grant this special exception for their modified versions;
+it is their choice whether to do so. The GNU Lesser General Public License
+gives permission to release a modified version without this exception;
+this exception also makes it possible to release a modified version
+which carries forward this exception.
+*/
+
+#include "engine/engine.h"
+
+#include "Buff.h"
+#include "BuffDurationEvent.h"
+
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/creature/CreatureAttribute.h"
+#include "server/zone/objects/player/PlayerCreature.h"
+#include "server/zone/packets/object/Buffs.h"
+
+void BuffImplementation::initializeTransientMembers() {
+	init();
+
+	if (buffEvent != NULL)
+		return;
+
+	if (nextExecutionTime.isPast()) {
+		buffEvent = new BuffDurationEvent(creature, _this);
+		buffEvent->schedule(50);
+	} else {
+		buffEvent = new BuffDurationEvent(creature, _this);
+		buffEvent->schedule(nextExecutionTime);
+	}
+}
+
+void BuffImplementation::sendTo(PlayerCreature* player) {
+	if (buffCRC != 0) {
+		AddBuffMessage* abm = new AddBuffMessage(player, buffCRC, getTimeLeft());
+		player->sendMessage(abm);
+	}
+}
+
+void BuffImplementation::setBuffEventNull() {
+	buffEvent = NULL;
+}
+
+void BuffImplementation::sendDestroyTo(PlayerCreature* player) {
+	RemoveBuffMessage* rbm = new RemoveBuffMessage(player, buffCRC);
+	player->sendMessage(rbm);
+}
+
+void BuffImplementation::activate() {
+	applyAttributeModifiers();
+	applySkillModifiers();
+
+	buffEvent = new BuffDurationEvent(creature, _this);
+	buffEvent->schedule(buffDuration * 1000);
+	nextExecutionTime = buffEvent->getNextExecutionTime();
+
+	if (creature->isPlayerCreature())
+		sendTo(((PlayerCreature*) creature.get()));
+
+	if (!startMessage.isEmpty())
+		creature->sendSystemMessage(startMessage);
+}
+
+void BuffImplementation::deactivate() {
+	removeAttributeModifiers();
+	removeSkillModifiers();
+
+	if (creature->isPlayerCreature())
+		sendDestroyTo((PlayerCreature*) creature.get());
+
+	if (!endMessage.isEmpty())
+		creature->sendSystemMessage(endMessage);
+
+	clearBuffEvent();
+}
+
+void BuffImplementation::parseAttributeModifierString(const String& modifierstring) {
+	StringTokenizer tokenizer(modifierstring);
+	tokenizer.setDelimeter(";");
+
+	while (tokenizer.hasMoreTokens()) {
+		String token;
+		tokenizer.getStringToken(token);
+
+		int tokpos = token.indexOf("=");
+
+		if (tokpos == -1)
+			continue;
+
+		uint8 attribute = CreatureAttribute::getAttribute(token.subString(0, tokpos));
+		int value = Integer::valueOf(token.subString(tokpos + 1, token.length()));
+
+		attributeModifiers.put(attribute, value);
+	}
+}
+
+void BuffImplementation::parseSkillModifierString(const String& modifierstring) {
+	StringTokenizer tokenizer(modifierstring);
+	tokenizer.setDelimeter(";");
+
+	while (tokenizer.hasMoreTokens()) {
+		String token;
+		tokenizer.getStringToken(token);
+
+		int tokpos = token.indexOf("=");
+
+		if (tokpos == -1)
+			continue;
+
+		String modname = token.subString(0, tokpos);
+		float value = Float::valueOf(token.subString(tokpos + 1, token.length()));
+
+		skillModifiers.put(modname, value);
+	}
+}
+
+String BuffImplementation::getAttributeModifierString() {
+	return String("");
+}
+
+String BuffImplementation::getSkillModifierString() {
+	return String("");
+}
+
+void BuffImplementation::scheduleBuffEvent() {
+	//buffEvent = new BuffDurationEvent(creature, _this);
+}
+
+float BuffImplementation::getTimeLeft() {
+	if (buffEvent == NULL || !buffEvent->isQueued())
+		return 0.0f;
+
+	float timeleft = round(Time().miliDifference(buffEvent->getNextExecutionTime()) / 1000.0f);
+
+	return MAX(0.0f, timeleft);
+}
+
+
+void BuffImplementation::applyAttributeModifiers() {
+	if (creature == NULL)
+		return;
+
+	if (speedModifier != 0) {
+		float originalSpeed = creature->getRunSpeed();
+		float newSpeed = originalSpeed + speedModifier;
+
+		creature->setRunSpeed(newSpeed, true);
+	}
+
+	int size = attributeModifiers.size();
+
+	if (size <= 0)
+		return;
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<uint8, int>* entry = &attributeModifiers.elementAt(i);
+
+		uint8 attribute = entry->getKey();
+		int value = entry->getValue();
+
+		if (value == 0)
+			continue;
+
+		int attributemax = creature->getMaxHAM(attribute) + value;
+		int attributeval = MAX(attributemax, creature->getHAM(attribute) + value);
+
+		creature->setMaxHAM(attribute, attributemax);
+		creature->setHAM(attribute, attributeval - creature->getWounds(attribute));
+	}
+
+}
+
+void BuffImplementation::applySkillModifiers() {
+	if (creature == NULL)
+		return;
+
+	int size = skillModifiers.size();
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<String, int>* entry = &skillModifiers.elementAt(i);
+
+		String key = entry->getKey();
+		int value = entry->getValue();
+
+		creature->addSkillMod(key, value, true);
+	}
+}
+
+void BuffImplementation::removeAttributeModifiers() {
+	if (creature == NULL)
+		return;
+
+	if (speedModifier != 0) {
+		float originalSpeed = creature->getRunSpeed();
+		float newSpeed = originalSpeed - speedModifier;
+
+		creature->setRunSpeed(newSpeed, true);
+	}
+
+	int size = attributeModifiers.size();
+
+	if (size <= 0)
+		return;
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<uint8, int>* entry = &attributeModifiers.elementAt(i);
+
+		uint8 attribute = entry->getKey();
+		int value = entry->getValue();
+
+		if (value == 0)
+			continue;
+
+		int attributemax = creature->getMaxHAM(attribute) - value;
+		int attributeval = MIN(attributemax, creature->getHAM(attribute) - value);
+
+		creature->setMaxHAM(attribute, attributemax);
+		creature->setHAM(attribute, attributeval);
+	}
+
+}
+
+void BuffImplementation::removeSkillModifiers() {
+	if (creature == NULL)
+		return;
+
+	int size = skillModifiers.size();
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<String, int>* entry = &skillModifiers.elementAt(i);
+
+		String key = entry->getKey();
+		int value = entry->getValue();
+
+		creature->addSkillMod(key, -value, true);
+
+	}
+
+}
+
+void BuffImplementation::clearBuffEvent() {
+	if (buffEvent != NULL) {
+		if (buffEvent->isQueued())
+			buffEvent->cancel();
+
+		buffEvent->setBuffObject(NULL);
+		buffEvent = NULL;
+		nextExecutionTime.updateToCurrentTime();
+	}
+}
+
+bool BuffImplementation::isActive() {
+	return (buffEvent != NULL && buffEvent->isQueued());
+}
+
+void BuffImplementation::setStartMessage(ParameterizedStringId& start) {
+	startMessage = start;
+}
+
+void BuffImplementation::setEndMessage(ParameterizedStringId& end) {
+	endMessage = end;
+}
---
+++ src/server/zone/objects/creature/buffs/BuffList.cpp
@@ -0,0 +1,166 @@
+/*
+Copyright (C) 2007 <SWGEmu>
+
+This File is part of Core3.
+
+This program is free software; you can redistribute
+it and/or modify it under the terms of the GNU Lesser
+General Public License as published by the Free Software
+Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General
+Public License along with this program; if not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Linking Engine3 statically or dynamically with other modules
+is making a combined work based on Engine3.
+Thus, the terms and conditions of the GNU Lesser General Public License
+cover the whole combination.
+
+In addition, as a special exception, the copyright holders of Engine3
+give you permission to combine Engine3 program with free software
+programs or libraries that are released under the GNU LGPL and with
+code included in the standard release of Core3 under the GNU LGPL
+license (or modified versions of such code, with unchanged license).
+You may copy and distribute such a system following the terms of the
+GNU LGPL for Engine3 and the licenses of the other code concerned,
+provided that you include the source code of that other code when
+and as the GNU LGPL requires distribution of source code.
+
+Note that people who make modified versions of Engine3 are not obligated
+to grant this special exception for their modified versions;
+it is their choice whether to do so. The GNU Lesser General Public License
+gives permission to release a modified version without this exception;
+this exception also makes it possible to release a modified version
+which carries forward this exception.
+*/
+
+#include "BuffList.h"
+#include "../CreatureObject.h"
+#include "../../player/PlayerCreature.h"
+#include "server/zone/managers/object/ObjectManager.h"
+
+BuffList::BuffList() {
+	spiceActive = false;
+
+	buffList.setNullValue(NULL);
+	buffList.setNoDuplicateInsertPlan();
+
+	addSerializableVariable("spiceActive", &spiceActive);
+	addSerializableVariable("buffList", &buffList);
+}
+
+void BuffList::updateBuffsToDatabase() {
+	for (int i = 0; i < getBuffListSize(); ++i) {
+		ManagedReference<Buff*> buff = getBuffByIndex(i);
+
+		if (!buff->isPersistent())
+			ObjectManager::instance()->persistObject(buff, 1, "buffs");
+		else
+			buff->updateToDatabase();
+	}
+
+	for (int i = 0; i < removedBuffs.size(); ++i) {
+		ManagedReference<Buff*> buff = removedBuffs.get(i);
+
+		if (buff->isPersistent())
+			ObjectManager::instance()->destroyObjectFromDatabase(buff->_getObjectID());
+	}
+
+	removedBuffs.removeAll();
+}
+
+void BuffList::sendTo(PlayerCreature* player) {
+	for (int i = 0; i < buffList.size(); ++i) {
+		Buff* buff = buffList.get(i);
+
+		buff->sendTo(player);
+	}
+}
+
+void BuffList::sendDestroyTo(PlayerCreature* player) {
+	for (int i = 0; i < buffList.size(); ++i) {
+		Buff* buff = buffList.get(i);
+
+		buff->sendDestroyTo(player);
+	}
+}
+
+void BuffList::addBuff(CreatureObject* creature, Buff* buff) {
+	if (buff == NULL)
+		return;
+
+	uint32 buffcrc = buff->getBuffCRC();
+
+	//Remove the old buff if it exists. (Exists checked in removeBuff)
+	removeBuff(creature, buffcrc);
+
+	buffList.put(buffcrc, buff);
+
+	if (buff->isSpiceBuff())
+		spiceActive = true;
+
+	if (!buff->isActive())
+		buff->activate();
+}
+
+void BuffList::removeBuff(CreatureObject* creature, uint32 buffcrc) {
+	ManagedReference<Buff*> buff = buffList.get(buffcrc);
+
+	if (buff == NULL)
+		return;
+
+	removeBuff(creature, buff);
+}
+
+void BuffList::removeBuff(CreatureObject* creature, Buff* buff) {
+	if (buff == NULL)
+		return;
+
+	uint32 buffcrc = buff->getBuffCRC();
+
+	if (buffList.contains(buffcrc)) {
+		//This only gets called if the event has been scheduled and is not executing.
+		if (buff->isActive())
+			buff->clearBuffEvent();
+
+		if (buff->isSpiceBuff())
+			spiceActive = false;
+
+		//Already null checked the buff.
+		buff->deactivate();
+
+		buff->clearBuffEvent();
+
+		removedBuffs.add(buff);
+
+		buffList.drop(buffcrc);
+	}
+}
+
+void BuffList::clearBuffs(CreatureObject* creature, bool updateclient) {
+	for (int i = 0; i < buffList.size(); ++i) {
+		ManagedReference<Buff*> buff = buffList.get(i);
+
+		if (buff != NULL) {
+			if (buff->isActive()) {
+				//This can only get called if the buff event is scheduled and not currently being executed.
+				buff->clearBuffEvent();
+				buff->deactivate();
+
+				removedBuffs.add(buff);
+			}
+
+		}
+	}
+
+	spiceActive = false;
+	buffList.removeAll();
+}
