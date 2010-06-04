537e664675965ed649c82b1bd7a3a471a1d7da73
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-04 18:16:58 +0000

    [added] City building deeds server templates [added] Guild deeds server templates
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2068 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ bin/scripts/object/tangible/deed/city_deed/corellia_city_deeds.lua
@@ -0,0 +1,183 @@
+object_tangible_deed_city_deed_bank_corellia_deed = object_tangible_deed_city_deed_shared_bank_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/bank_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_bank_corellia_deed, "object/tangible/deed/city_deed/bank_corellia_deed.iff")
+
+object_tangible_deed_city_deed_cantina_corellia_deed = object_tangible_deed_city_deed_shared_cantina_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cantina_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cantina_corellia_deed, "object/tangible/deed/city_deed/cantina_corellia_deed.iff")
+
+object_tangible_deed_city_deed_cityhall_corellia_deed = object_tangible_deed_city_deed_shared_cityhall_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cityhall_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cityhall_corellia_deed, "object/tangible/deed/city_deed/cityhall_corellia_deed.iff")
+
+object_tangible_deed_city_deed_cloning_corellia_deed = object_tangible_deed_city_deed_shared_cloning_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cloning_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cloning_corellia_deed, "object/tangible/deed/city_deed/cloning_corellia_deed.iff")
+
+object_tangible_deed_city_deed_garage_corellia_deed = object_tangible_deed_city_deed_shared_garage_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garage_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garage_corellia_deed, "object/tangible/deed/city_deed/garage_corellia_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_lrg_01_deed = object_tangible_deed_city_deed_shared_garden_corellia_lrg_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_lrg_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_lrg_01_deed, "object/tangible/deed/city_deed/garden_corellia_lrg_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_lrg_02_deed = object_tangible_deed_city_deed_shared_garden_corellia_lrg_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_lrg_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_lrg_02_deed, "object/tangible/deed/city_deed/garden_corellia_lrg_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_lrg_03_deed = object_tangible_deed_city_deed_shared_garden_corellia_lrg_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_lrg_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_lrg_03_deed, "object/tangible/deed/city_deed/garden_corellia_lrg_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_lrg_04_deed = object_tangible_deed_city_deed_shared_garden_corellia_lrg_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_lrg_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_lrg_04_deed, "object/tangible/deed/city_deed/garden_corellia_lrg_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_lrg_05_deed = object_tangible_deed_city_deed_shared_garden_corellia_lrg_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_lrg_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_lrg_05_deed, "object/tangible/deed/city_deed/garden_corellia_lrg_05_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_med_01_deed = object_tangible_deed_city_deed_shared_garden_corellia_med_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_med_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_med_01_deed, "object/tangible/deed/city_deed/garden_corellia_med_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_med_02_deed = object_tangible_deed_city_deed_shared_garden_corellia_med_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_med_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_med_02_deed, "object/tangible/deed/city_deed/garden_corellia_med_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_med_03_deed = object_tangible_deed_city_deed_shared_garden_corellia_med_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_med_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_med_03_deed, "object/tangible/deed/city_deed/garden_corellia_med_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_med_04_deed = object_tangible_deed_city_deed_shared_garden_corellia_med_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_med_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_med_04_deed, "object/tangible/deed/city_deed/garden_corellia_med_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_med_05_deed = object_tangible_deed_city_deed_shared_garden_corellia_med_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_med_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_med_05_deed, "object/tangible/deed/city_deed/garden_corellia_med_05_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_sml_01_deed = object_tangible_deed_city_deed_shared_garden_corellia_sml_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_sml_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_sml_01_deed, "object/tangible/deed/city_deed/garden_corellia_sml_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_sml_02_deed = object_tangible_deed_city_deed_shared_garden_corellia_sml_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_sml_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_sml_02_deed, "object/tangible/deed/city_deed/garden_corellia_sml_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_sml_03_deed = object_tangible_deed_city_deed_shared_garden_corellia_sml_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_sml_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_sml_03_deed, "object/tangible/deed/city_deed/garden_corellia_sml_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_sml_04_deed = object_tangible_deed_city_deed_shared_garden_corellia_sml_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_sml_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_sml_04_deed, "object/tangible/deed/city_deed/garden_corellia_sml_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_corellia_sml_05_deed = object_tangible_deed_city_deed_shared_garden_corellia_sml_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_corellia_sml_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_corellia_sml_05_deed, "object/tangible/deed/city_deed/garden_corellia_sml_05_deed.iff")
+
+object_tangible_deed_city_deed_hospital_corellia_deed = object_tangible_deed_city_deed_shared_hospital_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/hospital_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_hospital_corellia_deed, "object/tangible/deed/city_deed/hospital_corellia_deed.iff")
+
+object_tangible_deed_city_deed_shuttleport_corellia_deed = object_tangible_deed_city_deed_shared_shuttleport_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/shuttleport_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_shuttleport_corellia_deed, "object/tangible/deed/city_deed/shuttleport_corellia_deed.iff")
+
+object_tangible_deed_city_deed_theater_corellia_deed = object_tangible_deed_city_deed_shared_theater_corellia_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/theater_corellia.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_theater_corellia_deed, "object/tangible/deed/city_deed/theater_corellia_deed.iff")
