1a5272a51302fa8d10b78c50742c8a56cb3f080a
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-04 16:35:25 +0000

    Player House Server Templates
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2065 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/bin/scripts/object/building/player/player_house_corellia.lua
@@ -0,0 +1,71 @@
+object_building_player_player_guildhall_corellia_style_01 = object_building_player_shared_player_guildhall_corellia_style_01:new {
+	lotSize = 5,
+	baseMaintenanceRate = 100,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_guildhall_corellia_style_01, "object/building/player/player_guildhall_corellia_style_01.iff")
+
+object_building_player_player_house_corellia_large_style_01 = object_building_player_shared_player_house_corellia_large_style_01:new {
+	lotSize = 5,
+	baseMaintenanceRate = 50,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_large_style_01, "object/building/player/player_house_corellia_large_style_01.iff")
+
+object_building_player_player_house_corellia_large_style_02 = object_building_player_shared_player_house_corellia_large_style_02:new {
+	lotSize = 5,
+	baseMaintenanceRate = 50,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_large_style_02, "object/building/player/player_house_corellia_large_style_02.iff")
+
+object_building_player_player_house_corellia_medium_style_01 = object_building_player_shared_player_house_corellia_medium_style_01:new {
+	lotSize = 2,
+	baseMaintenanceRate = 34,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_medium_style_01, "object/building/player/player_house_corellia_medium_style_01.iff")
+
+object_building_player_player_house_corellia_medium_style_02 = object_building_player_shared_player_house_corellia_medium_style_02:new {
+	lotSize = 2,
+	baseMaintenanceRate = 34,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_medium_style_02, "object/building/player/player_house_corellia_medium_style_02.iff")
+
+object_building_player_player_house_corellia_small_style_01 = object_building_player_shared_player_house_corellia_small_style_01:new {
+	lotSize = 2,
+	baseMaintenanceRate = 16,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_small_style_01, "object/building/player/player_house_corellia_small_style_01.iff")
+
+object_building_player_player_house_corellia_small_style_01_floorplan_02 = object_building_player_shared_player_house_corellia_small_style_01_floorplan_02:new {
+	lotSize = 2,
+	baseMaintenanceRate = 16,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_small_style_01_floorplan_02, "object/building/player/player_house_corellia_small_style_01_floorplan_02.iff")
+
+object_building_player_player_house_corellia_small_style_02 = object_building_player_shared_player_house_corellia_small_style_02:new {
+	lotSize = 2,
+	baseMaintenanceRate = 16,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_small_style_02, "object/building/player/player_house_corellia_small_style_02.iff")
+
+object_building_player_player_house_corellia_small_style_02_floorplan_02 = object_building_player_shared_player_house_corellia_small_style_02_floorplan_02:new {
+	lotSize = 2,
+	baseMaintenanceRate = 16,
+	allowedZones = {0,7}
+}
+
+ObjectTemplates:addTemplate(object_building_player_player_house_corellia_small_style_02_floorplan_02, "object/building/player/player_house_corellia_small_style_02_floorplan_02.iff")
