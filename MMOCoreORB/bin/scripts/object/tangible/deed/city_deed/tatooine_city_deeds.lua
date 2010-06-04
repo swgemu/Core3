b0542960a2d48c7a79ec1b2b949f26e602d7005a
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-04 18:16:58 +0000

    [added] City building deeds server templates [added] Guild deeds server templates
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2068 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/bin/scripts/object/tangible/deed/city_deed/tatooine_city_deeds.lua
@@ -0,0 +1,183 @@
+object_tangible_deed_city_deed_bank_tatooine_deed = object_tangible_deed_city_deed_shared_bank_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/bank_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_bank_tatooine_deed, "object/tangible/deed/city_deed/bank_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_cantina_tatooine_deed = object_tangible_deed_city_deed_shared_cantina_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cantina_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cantina_tatooine_deed, "object/tangible/deed/city_deed/cantina_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_cityhall_tatooine_deed = object_tangible_deed_city_deed_shared_cityhall_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cityhall_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cityhall_tatooine_deed, "object/tangible/deed/city_deed/cityhall_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_cloning_tatooine_deed = object_tangible_deed_city_deed_shared_cloning_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/cloning_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_cloning_tatooine_deed, "object/tangible/deed/city_deed/cloning_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_garage_tatooine_deed = object_tangible_deed_city_deed_shared_garage_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garage_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garage_tatooine_deed, "object/tangible/deed/city_deed/garage_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_lrg_01_deed = object_tangible_deed_city_deed_shared_garden_tatooine_lrg_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_lrg_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_lrg_01_deed, "object/tangible/deed/city_deed/garden_tatooine_lrg_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_lrg_02_deed = object_tangible_deed_city_deed_shared_garden_tatooine_lrg_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_lrg_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_lrg_02_deed, "object/tangible/deed/city_deed/garden_tatooine_lrg_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_lrg_03_deed = object_tangible_deed_city_deed_shared_garden_tatooine_lrg_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_lrg_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_lrg_03_deed, "object/tangible/deed/city_deed/garden_tatooine_lrg_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_lrg_04_deed = object_tangible_deed_city_deed_shared_garden_tatooine_lrg_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_lrg_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_lrg_04_deed, "object/tangible/deed/city_deed/garden_tatooine_lrg_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_lrg_05_deed = object_tangible_deed_city_deed_shared_garden_tatooine_lrg_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_lrg_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_lrg_05_deed, "object/tangible/deed/city_deed/garden_tatooine_lrg_05_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_med_01_deed = object_tangible_deed_city_deed_shared_garden_tatooine_med_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_med_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_med_01_deed, "object/tangible/deed/city_deed/garden_tatooine_med_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_med_02_deed = object_tangible_deed_city_deed_shared_garden_tatooine_med_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_med_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_med_02_deed, "object/tangible/deed/city_deed/garden_tatooine_med_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_med_03_deed = object_tangible_deed_city_deed_shared_garden_tatooine_med_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_med_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_med_03_deed, "object/tangible/deed/city_deed/garden_tatooine_med_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_med_04_deed = object_tangible_deed_city_deed_shared_garden_tatooine_med_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_med_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_med_04_deed, "object/tangible/deed/city_deed/garden_tatooine_med_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_med_05_deed = object_tangible_deed_city_deed_shared_garden_tatooine_med_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_med_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_med_05_deed, "object/tangible/deed/city_deed/garden_tatooine_med_05_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_sml_01_deed = object_tangible_deed_city_deed_shared_garden_tatooine_sml_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_sml_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_sml_01_deed, "object/tangible/deed/city_deed/garden_tatooine_sml_01_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_sml_02_deed = object_tangible_deed_city_deed_shared_garden_tatooine_sml_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_sml_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_sml_02_deed, "object/tangible/deed/city_deed/garden_tatooine_sml_02_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_sml_03_deed = object_tangible_deed_city_deed_shared_garden_tatooine_sml_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_sml_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_sml_03_deed, "object/tangible/deed/city_deed/garden_tatooine_sml_03_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_sml_04_deed = object_tangible_deed_city_deed_shared_garden_tatooine_sml_04_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_sml_04.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_sml_04_deed, "object/tangible/deed/city_deed/garden_tatooine_sml_04_deed.iff")
+
+object_tangible_deed_city_deed_garden_tatooine_sml_05_deed = object_tangible_deed_city_deed_shared_garden_tatooine_sml_05_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/garden_tatooine_sml_05.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_garden_tatooine_sml_05_deed, "object/tangible/deed/city_deed/garden_tatooine_sml_05_deed.iff")
+
+object_tangible_deed_city_deed_hospital_tatooine_deed = object_tangible_deed_city_deed_shared_hospital_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/hospital_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_hospital_tatooine_deed, "object/tangible/deed/city_deed/hospital_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_shuttleport_tatooine_deed = object_tangible_deed_city_deed_shared_shuttleport_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/shuttleport_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_shuttleport_tatooine_deed, "object/tangible/deed/city_deed/shuttleport_tatooine_deed.iff")
+
+object_tangible_deed_city_deed_theater_tatooine_deed = object_tangible_deed_city_deed_shared_theater_tatooine_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/city/theater_tatooine.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_city_deed_theater_tatooine_deed, "object/tangible/deed/city_deed/theater_tatooine_deed.iff")
