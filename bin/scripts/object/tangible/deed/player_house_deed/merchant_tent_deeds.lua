7328bacab83495e246dfc0a143ed4c7a852b5691
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-04 17:15:58 +0000

    Player House Deed Server Templates
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2066 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ bin/scripts/object/tangible/deed/player_house_deed/merchant_tent_deeds.lua
@@ -0,0 +1,23 @@
+object_tangible_deed_player_house_deed_merchant_tent_style_01_deed = object_tangible_deed_player_house_deed_shared_merchant_tent_style_01_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/player_merchant_tent_style_01.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_player_house_deed_merchant_tent_style_01_deed, "object/tangible/deed/player_house_deed/merchant_tent_style_01_deed.iff")
+
+object_tangible_deed_player_house_deed_merchant_tent_style_02_deed = object_tangible_deed_player_house_deed_shared_merchant_tent_style_02_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/player_merchant_tent_style_02.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_player_house_deed_merchant_tent_style_02_deed, "object/tangible/deed/player_house_deed/merchant_tent_style_02_deed.iff")
+
+object_tangible_deed_player_house_deed_merchant_tent_style_03_deed = object_tangible_deed_player_house_deed_shared_merchant_tent_style_03_deed:new {
+	templateType = DEED,
+	generatedObjectTemplate = "object/building/player/player_merchant_tent_style_03.iff",
+	maxCondition = 0
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_player_house_deed_merchant_tent_style_03_deed, "object/tangible/deed/player_house_deed/merchant_tent_style_03_deed.iff")
