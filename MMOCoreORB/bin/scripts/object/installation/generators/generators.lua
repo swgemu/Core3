5637c567f12ee55accb76c65b405a314e5c1dbf7
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-04 19:42:03 +0000

    Factory, Generators, Harvester object server templates
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2071 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/bin/scripts/object/installation/generators/generators.lua
@@ -0,0 +1,31 @@
+object_installation_generators_power_generator_fusion_style_1 = object_installation_generators_shared_power_generator_fusion_style_1:new {
+	lotSize = 1,
+	baseMaintenanceRate = 60,
+	basePowerRate = 0
+}
+
+ObjectTemplates:addTemplate(object_installation_generators_power_generator_fusion_style_1, "object/installation/generators/power_generator_fusion_style_1.iff")
+
+object_installation_generators_power_generator_photo_bio_style_1 = object_installation_generators_shared_power_generator_photo_bio_style_1:new {
+	lotSize = 1,
+	baseMaintenanceRate = 60,
+	basePowerRate = 0
+}
+
+ObjectTemplates:addTemplate(object_installation_generators_power_generator_photo_bio_style_1, "object/installation/generators/power_generator_photo_bio_style_1.iff")
+
+object_installation_generators_power_generator_solar_style_1 = object_installation_generators_shared_power_generator_solar_style_1:new {
+	lotSize = 1,
+	baseMaintenanceRate = 60,
+	basePowerRate = 0
+}
+
+ObjectTemplates:addTemplate(object_installation_generators_power_generator_solar_style_1, "object/installation/generators/power_generator_solar_style_1.iff")
+
+object_installation_generators_power_generator_wind_style_1 = object_installation_generators_shared_power_generator_wind_style_1:new {
+	lotSize = 1,
+	baseMaintenanceRate = 60,
+	basePowerRate = 0
+}
+
+ObjectTemplates:addTemplate(object_installation_generators_power_generator_wind_style_1, "object/installation/generators/power_generator_wind_style_1.iff")
---
+++ MMOCoreORB/bin/scripts/object/installation/mining_organic/organic_harvesters.lua
@@ -0,0 +1,31 @@
+object_installation_mining_organic_mining_organic_creature_farm = object_installation_mining_organic_shared_mining_organic_creature_farm:new {
+	lotSize = 1,
+	baseMaintenanceRate = 30,
+	basePowerRate = 25
+}
+
+ObjectTemplates:addTemplate(object_installation_mining_organic_mining_organic_creature_farm, "object/installation/mining_organic/mining_organic_creature_farm.iff")
+
+object_installation_mining_organic_mining_organic_flora_farm = object_installation_mining_organic_shared_mining_organic_flora_farm:new {
+	lotSize = 1,
+	baseMaintenanceRate = 30,
+	basePowerRate = 25
+}
+
+ObjectTemplates:addTemplate(object_installation_mining_organic_mining_organic_flora_farm, "object/installation/mining_organic/mining_organic_flora_farm.iff")
+
+object_installation_mining_organic_mining_organic_flora_farm_medium = object_installation_mining_organic_shared_mining_organic_flora_farm_medium:new {
+	lotSize = 1,
+	baseMaintenanceRate = 60,
+	basePowerRate = 50
+}
+
+ObjectTemplates:addTemplate(object_installation_mining_organic_mining_organic_flora_farm_medium, "object/installation/mining_organic/mining_organic_flora_farm_medium.iff")
+
+object_installation_mining_organic_mining_organic_flora_farm_heavy = object_installation_mining_organic_shared_mining_organic_flora_farm_heavy:new {
+	lotSize = 1,
+	baseMaintenanceRate = 90,
+	basePowerRate = 75
+}
+
+ObjectTemplates:addTemplate(object_installation_mining_organic_mining_organic_flora_farm_heavy, "object/installation/mining_organic/mining_organic_flora_farm_heavy.iff")
