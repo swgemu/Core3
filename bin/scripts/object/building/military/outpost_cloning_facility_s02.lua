bf707d84d572e1518bf498edef356f0ea1d397b1
kellina <kellina@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-28 22:30:55 +0000

    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2174 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/building/military/outpost_cloning_facility_s02.lua
+++ bin/scripts/object/building/military/outpost_cloning_facility_s02.lua
@@ -7,6 +7,7 @@ object_building_military_outpost_cloning_facility_s02 = object_building_military
 			{ x = 4.44727, z = 0.125266, y = -3.80136, ow = 0.707745, ox = 0, oz = 0, oy = -0.706468, cellid = 5 },
 			{ x = 4.65395, z = 0.125266, y = 1.49466, ow = -0.699587, ox = 0, oz = 0, oy = 0.714548, cellid = 4 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING
 }
 
 ObjectTemplates:addTemplate(object_building_military_outpost_cloning_facility_s02, "object/building/military/outpost_cloning_facility_s02.iff")
--- bin/scripts/object/building/military/outpost_cloning_facility_s03.lua
+++ bin/scripts/object/building/military/outpost_cloning_facility_s03.lua
@@ -8,6 +8,7 @@ object_building_military_outpost_cloning_facility_s03 = object_building_military
 			{ x = 4.45469, z = 0.125266, y = 1.18026, ow = -0.68668, ox = 0, oz = 0, oy = 0.72696, cellid = 4 },
 			{ x = 4.73, z = 0.125266, y = 3.02662, ow = -0.696336, ox = 0, oz = 0, oy = 0.717716, cellid = 4 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING
 }
 
 ObjectTemplates:addTemplate(object_building_military_outpost_cloning_facility_s03, "object/building/military/outpost_cloning_facility_s03.iff")
