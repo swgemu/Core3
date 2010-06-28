45b1f7f5cb1aa0d7fdfa757d41ee984d7a6ecaf6
kellina <kellina@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-28 22:59:52 +0000

    Added Dathomir/Endor/Lok Cloning
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2175 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/building/military/outpost_cloning_facility_s02.lua
+++ bin/scripts/object/building/military/outpost_cloning_facility_s02.lua
@@ -5,7 +5,8 @@ object_building_military_outpost_cloning_facility_s02 = object_building_military
 	mapLocationsType3 = 0,
 	spawningPoints = { 			
 			{ x = 4.44727, z = 0.125266, y = -3.80136, ow = 0.707745, ox = 0, oz = 0, oy = -0.706468, cellid = 5 },
-			{ x = 4.65395, z = 0.125266, y = 1.49466, ow = -0.699587, ox = 0, oz = 0, oy = 0.714548, cellid = 4 }
+			{ x = 4.65395, z = 0.125266, y = 1.49466, ow = -0.699587, ox = 0, oz = 0, oy = 0.714548, cellid = 4 },
+			{ x = -3.99569, z = 0.125266, y = -3.51553, ow = 0.710131, ox = 0, oz = 0, oy = 0.70407, cellid = 6 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
 	templateType = CLONINGBUILDING
 }
--- bin/scripts/object/building/tatooine/cloning_facility_tatooine_small.lua
+++ bin/scripts/object/building/tatooine/cloning_facility_tatooine_small.lua
@@ -2,7 +2,12 @@ object_building_tatooine_cloning_facility_tatooine_small = object_building_tatoo
 	gameObjectType = 519,
 	mapLocationsType1 = 5,
 	mapLocationsType2 = 0,
-	mapLocationsType3 = 0
+	mapLocationsType3 = 0,
+	spawningPoints = { 
+			-- Lok Nyms Needs Revisited, Needs elevator terms.
+			{ x = 0.445015, z = 0.271775, y = 1.39199, ow = 0.707176, ox = 0, oz = 0, oy = 0.707038, cellid = 9 }
+			}, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING	
 }
 
 ObjectTemplates:addTemplate(object_building_tatooine_cloning_facility_tatooine_small, "object/building/tatooine/cloning_facility_tatooine_small.iff")
