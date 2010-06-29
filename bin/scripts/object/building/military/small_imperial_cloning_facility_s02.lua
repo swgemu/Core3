cc3d233140fbaaa8de1e088e653e000204fc0f6c
kellina <kellina@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-29 05:37:28 +0000

    Added Rori,Talus,Tatooine,Yavin Cloning
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2180 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/building/military/small_imperial_cloning_facility_s02.lua
+++ bin/scripts/object/building/military/small_imperial_cloning_facility_s02.lua
@@ -2,7 +2,12 @@ object_building_military_small_imperial_cloning_facility_s02 = object_building_m
 	gameObjectType = 519,
 	mapLocationsType1 = 5,
 	mapLocationsType2 = 0,
-	mapLocationsType3 = 0
+	mapLocationsType3 = 0,
+	spawningPoints = { 			
+			{ x = 3.61815, z = 0.125266, y = -3.75189, ow = -0.705876, ox = 0, oz = 0, oy = 0.708336, cellid = 5 }, 
+			{ x = 3.21398, z = 0.125266, y = 1.75308, ow = 0.717618, ox = 0, oz = 0, oy = -0.696437, cellid = 4 }
+			}, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING
 }
 
 ObjectTemplates:addTemplate(object_building_military_small_imperial_cloning_facility_s02, "object/building/military/small_imperial_cloning_facility_s02.iff")
