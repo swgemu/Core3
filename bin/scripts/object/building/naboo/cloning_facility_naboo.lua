f38f5895d78c72bf675c50ac8efc019fa80d1ef2
kellina <kellina@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-29 05:37:28 +0000

    Added Rori,Talus,Tatooine,Yavin Cloning
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2180 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/building/naboo/cloning_facility_naboo.lua
+++ bin/scripts/object/building/naboo/cloning_facility_naboo.lua
@@ -10,7 +10,7 @@ object_building_naboo_cloning_facility_naboo = object_building_naboo_shared_clon
 			{ x = 16.4255, z = -4.29167, y = -10.5014, ow = 0.707601, ox = -0, oz = 0, oy = -0.706612, cellid = 4 },
 			{ x = 6.38176, z = -4.79167, y = 0.603229, ow = -0.00984072, ox = 0, oz = 0, oy = 0.999952, cellid = 4 },
 			{ x = 1.74664, z = -4.79167, y = 0.582775, ow = 0.0143216, ox = 0, oz = 0, oy = 0.999897, cellid = 4 }, 
-			{ x = -2.76574, z = -4.79167, y = 0.657588, ow = -0.0511122, ox = 0, oz = 0, oy = 0.998693, cellid = 4 },			
+			{ x = -2.76574, z = -4.79167, y = 0.657588, ow = -0.0511122, ox = 0, oz = 0, oy = 0.998693, cellid = 4 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
 	templateType = CLONINGBUILDING
 }
