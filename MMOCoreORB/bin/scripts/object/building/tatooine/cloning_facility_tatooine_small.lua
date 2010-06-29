bbf9007a65f726f1b86e948d71f0dbc4aa3b3eb3
kellina <kellina@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-06-29 05:37:28 +0000

    Added Rori,Talus,Tatooine,Yavin Cloning
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2180 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/building/tatooine/cloning_facility_tatooine_small.lua
+++ MMOCoreORB/bin/scripts/object/building/tatooine/cloning_facility_tatooine_small.lua
@@ -4,7 +4,7 @@ object_building_tatooine_cloning_facility_tatooine_small = object_building_tatoo
 	mapLocationsType2 = 0,
 	mapLocationsType3 = 0,
 	spawningPoints = { 
-			-- Lok Nyms Needs Revisited, Needs elevator terms.
+			-- Needs Revisited, Needs elevator terms.
 			{ x = 0.445015, z = 0.271775, y = 1.39199, ow = 0.707176, ox = 0, oz = 0, oy = 0.707038, cellid = 9 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
 	templateType = CLONINGBUILDING	
