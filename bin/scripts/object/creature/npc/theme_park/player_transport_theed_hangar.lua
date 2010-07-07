5fe6fb55f9a80ad95a478dbd37a6b8ce107fb03b
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-07-07 13:43:36 +0000

    [fixed] shuttles, ticket collectors, travel terminals
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2193 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/creature/npc/theme_park/player_transport_theed_hangar.lua
+++ bin/scripts/object/creature/npc/theme_park/player_transport_theed_hangar.lua
@@ -42,7 +42,8 @@
 
 
 object_creature_npc_theme_park_player_transport_theed_hangar = object_creature_npc_theme_park_shared_player_transport_theed_hangar:new {
-
+	gameObjectType = 1035,
+	mapLocationsType1 = 15
 }
 
 ObjectTemplates:addTemplate(object_creature_npc_theme_park_player_transport_theed_hangar, "object/creature/npc/theme_park/player_transport_theed_hangar.iff")
--- bin/scripts/object/tangible/terminal/terminal_travel.lua
+++ bin/scripts/object/tangible/terminal/terminal_travel.lua
@@ -42,7 +42,7 @@
 
 
 object_tangible_terminal_terminal_travel = object_tangible_terminal_shared_terminal_travel:new {
-
+	gameObjectType = 16402
 }
 
 ObjectTemplates:addTemplate(object_tangible_terminal_terminal_travel, "object/tangible/terminal/terminal_travel.iff")
