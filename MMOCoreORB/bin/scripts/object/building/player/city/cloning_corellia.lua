32c0a73847311208ed5ff405a5f56149038cbd6f
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-07-11 15:23:56 +0000

    [fixed] armor in the bluefrog, merchants, and player cloners should work now
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2209 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/building/player/city/cloning_corellia.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/cloning_corellia.lua
@@ -48,6 +48,8 @@ object_building_player_city_cloning_corellia = object_building_player_city_share
 	mapLocationsType1 = 5,
 	allowedZones = {0,7},
 	cityRankRequired = 3,
+	spawningPoints = { {x = -9.7, z = -1.2, y = -6.2, ow = 1, ox = 0, oz = 0, oy = 0, cellid = 4} }, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING,
 	abilitiesRequired = {"placeCloningFacility"}
 }
 
