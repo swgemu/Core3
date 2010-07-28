f6f9cc27501b44e4bf6b8a4bbb53c9ab8dea2a77
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-07-28 03:28:16 +0000

    [Fixed] Items missing in last commit
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2238 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ src/server/zone/objects/factorycrate/FactoryCrateImplementation.cpp
@@ -0,0 +1,35 @@
+/*
+ * FactoryCrateImplementation.cpp
+ *
+ *  Created on: 07/25/2010
+ *      Author: Kyle
+ */
+
+#include "FactoryCrate.h"
+#include "server/zone/packets/factory/FactoryCrateObjectMessage3.h"
+#include "server/zone/packets/factory/FactoryCrateObjectMessage6.h"
+#include "server/zone/objects/player/PlayerCreature.h"
+
+void FactoryCrateImplementation::initializeTransientMembers() {
+	TangibleObjectImplementation::initializeTransientMembers();
+
+	setLoggingName("FactoryCrate");
+}
+
+void FactoryCrateImplementation::loadTemplateData(SharedObjectTemplate* templateData) {
+	TangibleObjectImplementation::loadTemplateData(templateData);
+
+}
+
+void FactoryCrateImplementation::sendBaselinesTo(SceneObject* player) {
+	/*StringBuffer msg;
+	msg << "sending cell number " << cellNumber << " baselines";
+	info(msg.toString(), true);*/
+
+	BaseMessage* fctyMsg3 = new FactoryCrateObjectMessage3(_this);
+	player->sendMessage(fctyMsg3);
+
+	BaseMessage* fctyMsg6 = new FactoryCrateObjectMessage6(_this);
+	player->sendMessage(fctyMsg6);
+
+}
