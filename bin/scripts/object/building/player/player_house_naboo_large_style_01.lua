48561e9c52f9f74f34781f09f4b51a52b86b7dac
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-08-18 01:27:07 +0000

    [fixed] Structure Terminals in Naboo houses
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2341 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/building/player/player_house_naboo_large_style_01.lua
+++ bin/scripts/object/building/player/player_house_naboo_large_style_01.lua
@@ -45,7 +45,7 @@ object_building_player_player_house_naboo_large_style_01 = object_building_playe
 	lotSize = 5,
 	baseMaintenanceRate = 50,
 	allowedZones = {1,5,6},
-	terminalLocation = {0, 14.85, 4.7, -1.76, 0, 0.707107, 0, -0.707107},
+	terminalLocation = {1, 14.85, 4.7, -1.76, 0, 0.707107, 0, -0.707107},
 	length = 7,
 	width = 6
 }
