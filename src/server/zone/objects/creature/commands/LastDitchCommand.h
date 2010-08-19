d5d5067a78bd70becb6334fe96d1bfbbf056bacc
Elvaron <Elvaron@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-08-19 02:05:19 +0000

    [Added] Smuggler combat specials
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2349 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/objects/creature/commands/LastDitchCommand.h
+++ src/server/zone/objects/creature/commands/LastDitchCommand.h
@@ -46,13 +46,27 @@ which carries forward this exception.
 #define LASTDITCHCOMMAND_H_
 
 #include "../../scene/SceneObject.h"
+#include "CombatQueueCommand.h"
 
-class LastDitchCommand : public QueueCommand {
+class LastDitchCommand : public CombatQueueCommand {
 public:
 
 	LastDitchCommand(const String& name, ZoneProcessServerImplementation* server)
-		: QueueCommand(name, server) {
+		: CombatQueueCommand(name, server) {
 
+		damageMultiplier = 6.0;
+		speedMultiplier = 4.0;
+		healthCostMultiplier = 1;
+		actionCostMultiplier = 1;
+		mindCostMultiplier = 1;
+
+		stunStateChance = 40;
+
+		animationCRC = String("fire_1_special_single_light").hashCode();
+
+		combatSpam = "lastditch";
+
+		range = -1;
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
@@ -63,7 +77,19 @@ public:
 		if (!checkInvalidPostures(creature))
 			return INVALIDPOSTURE;
 
-		return SUCCESS;
+		ManagedReference<WeaponObject*> weapon = creature->getWeapon();
+
+		if (!weapon->isPistolWeapon()) {
+			return INVALIDWEAPON;
+		}
+
+		creature->addSkillMod("pistol_accuracy",10,false);
+
+		int dmg = doCombatAction(creature, target);
+
+		creature->addSkillMod("pistol_accuracy",-10,false);
+
+		return dmg;
 	}
 
 };
