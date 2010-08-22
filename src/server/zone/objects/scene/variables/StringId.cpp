0ae6f6410c2ed277f237f1eba70f40c190b036f6
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-08-22 21:53:28 +0000

    [Updated] Factories, Email now sends appropriate names
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2389 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/objects/scene/variables/StringId.cpp
+++ src/server/zone/objects/scene/variables/StringId.cpp
@@ -101,3 +101,13 @@ void StringId::setStringId(const String& fullPath) {
 	}
 }
 
+String StringId::getDisplayedName() {
+
+	if(customName.isEmpty()) {
+		return "@" + file + ":" + stringID;;
+	} else {
+		return customName.toString();
+	}
+
+}
+
