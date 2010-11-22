858b1fa83df28411bda2c6517ef69d35d2d3bf40
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-11-22 14:43:15 +0000

    [added] Verify Player Name Callback for Harassment reports
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2685 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/packets/ui/VerifyPlayerNameResponseMessage.h
+++ MMOCoreORB/src/server/zone/packets/ui/VerifyPlayerNameResponseMessage.h
@@ -12,15 +12,9 @@ class VerifyPlayerNameResponseMessage : public BaseMessage {
 public:
    VerifyPlayerNameResponseMessage(bool success, int counter) : BaseMessage() {
 		insertShort(0x09);
-		insertInt(0xF4C498FD);  // CRC
-
-		if (success == true)
-			insertByte(1); //0 failed 1 succeeded.
-		else
-			insertByte(0);
-
+		insertInt(0xF4C498FD);  //VerifyPlayerNameResponseMessage
+		insertByte((byte) success); //Success or fail
 		insertInt(counter); //Pretty sure this is a counter from the Verify packet.
-
    }
 
 };
