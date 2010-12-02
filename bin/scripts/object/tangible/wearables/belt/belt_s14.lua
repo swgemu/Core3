91893e7f5d82290280bedd1799607d664b54bf77
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-12-02 16:26:40 +0000

    [Updated] Player masks on Belts and Bandoliers, thanks Lorrianna
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2735 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- bin/scripts/object/tangible/wearables/belt/belt_s14.lua
+++ bin/scripts/object/tangible/wearables/belt/belt_s14.lua
@@ -43,7 +43,7 @@
 
 object_tangible_wearables_belt_belt_s14 = object_tangible_wearables_belt_shared_belt_s14:new {
 	-- ALL, ALLSEXES, ALLFACTIONS, HUMANOIDS, HUMANOID_FOOTWEAR, HUMANOID_MALES, HUMANOID_FEMALES, HUMANOID_IMPERIALS, HUMANOID_REBELS, WOOKIEES, ITHORIANS, TWILEKS
-	playerUseMask = HUMANOIDS+WOOKIEES+ITHORIANS
+	playerUseMask = HUMANOIDS + WOOKIEES + ITHORIANS
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_belt_belt_s14, "object/tangible/wearables/belt/belt_s14.iff")
