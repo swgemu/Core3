d7597c51b4b2f349c21112a579365cbd5d2b033b
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-12-21 05:34:33 +0000

    [added] FilterFractal and FilterHeight algorithms
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2802 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ src/server/zone/objects/terrain/layer/filters/FilterFractal.cpp
@@ -0,0 +1,34 @@
+/*
+ * FilterFractal.cpp
+ *
+ *  Created on: 21/12/2010
+ *      Author: victor
+ */
+
+#include "FilterFractal.h"
+
+#include "../../TerrainGenerator.h"
+
+
+float FilterFractal::process(float x, float y, float& baseValue, TerrainGenerator* terrainGenerator) {
+	Mfrc* mfrc = terrainGenerator->getMfrc(fractalId - 1);
+
+	float noiseResult = mfrc->getNoise(x, y, 0, 0) * var6;
+	float result = 0;
+
+	if (noiseResult > min && noiseResult < max) {
+		float v9 = (max - min) * featheringAmount * 0.5;
+
+		if (min + v9 <= noiseResult) {
+			if (max - v9 >= noiseResult)
+				result = 1.0;
+			else
+				result = (max - noiseResult) / v9;
+		} else
+			result = (noiseResult - min) / v9;
+	} else
+		result = 0;
+
+	return result;
+
+}
