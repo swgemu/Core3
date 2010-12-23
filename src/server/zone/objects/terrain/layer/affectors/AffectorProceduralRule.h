7d0d94abfce7b6c75a11c0e5b1cf1a00b7d0aec2
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-12-23 05:45:59 +0000

    [added] missing files
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@2804 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ src/server/zone/objects/terrain/layer/affectors/AffectorProceduralRule.h
@@ -0,0 +1,34 @@
+/*
+ * AffectorProceduralRule.h
+ *
+ *  Created on: 21/12/2010
+ *      Author: victor
+ */
+
+#ifndef AFFECTORPROCEDURALRULE_H_
+#define AFFECTORPROCEDURALRULE_H_
+
+#include "../ProceduralRule.h"
+
+class TerrainGenerator;
+
+class AffectorProceduralRule {
+
+protected:
+
+public:
+	AffectorProceduralRule() {
+
+	}
+
+	virtual void process(float x, float y, float transformValue, float& baseValue, TerrainGenerator* terrainGenerator) {
+
+	}
+
+	virtual bool isEnabled() {
+		return false;
+	}
+
+};
+
+#endif /* AFFECTORPROCEDURALRULE_H_ */
