110944e8112375b720237130ebd3577e9ed593ae
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2010-12-23 05:45:59 +0000

    [added] missing files
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2804 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/src/server/zone/objects/terrain/layer/filters/FilterProceduralRule.h
@@ -0,0 +1,40 @@
+/*
+ * FilterProceduralRule.h
+ *
+ *  Created on: 21/12/2010
+ *      Author: victor
+ */
+
+#ifndef FILTERPROCEDURALRULE_H_
+#define FILTERPROCEDURALRULE_H_
+
+#include "../ProceduralRule.h"
+
+class TerrainGenerator;
+
+class FilterProceduralRule {
+protected:
+	int featheringType; // Feathering type? Leftover?
+	float featheringAmount;
+
+public:
+	FilterProceduralRule() {
+		featheringType = 0;
+	}
+
+	virtual float process(float x, float y, float transformValue, float& baseValue, TerrainGenerator* terrainGenerator) {
+		return 0;
+	}
+
+	virtual bool isEnabled() {
+		return false;
+	}
+
+	inline int getFeatheringType() {
+		return featheringType;
+	}
+
+};
+
+
+#endif /* FILTERPROCEDURALRULE_H_ */
