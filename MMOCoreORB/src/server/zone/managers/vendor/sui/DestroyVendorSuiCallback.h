77ff4118e479a783f300b17b5a9fd52b49738d08
polonel <polonel@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-03-31 22:06:18 +0000

    [fixed] Missing Files
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@2999 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/src/server/zone/managers/vendor/sui/DestroyVendorSuiCallback.h
@@ -0,0 +1,51 @@
+/*
+ * DestroyVendorSuiCallback.h
+ *
+ *  Created on: Mar 25, 2011
+ *      Author: polonel
+ */
+
+#ifndef DESTROYVENDORSUICALLBACK_H_
+#define DESTROYVENDORSUICALLBACK_H_
+
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/player/sui/messagebox/SuiMessageBox.h"
+#include "server/zone/objects/player/sessions/vendor/CreateVendorSession.h"
+#include "server/zone/managers/vendor/VendorManager.h"
+#include "server/zone/objects/auction/Vendor.h"
+#include "server/zone/objects/tangible/terminal/vendor/VendorTerminal.h"
+#include "server/zone/objects/creature/vendor/VendorCreature.h"
+
+class DestroyVendorSuiCallback : public SuiCallback {
+public:
+	DestroyVendorSuiCallback(ZoneServer* server)
+		: SuiCallback(server) {
+	}
+
+	void run(PlayerCreature* player, SuiBox* suiBox, bool cancelPressed, Vector<UnicodeString>* args) {
+		if (!suiBox->isMessageBox() || cancelPressed)
+			return;
+
+		if (args->size() < 1)
+			return;
+
+		ManagedReference<SceneObject*> sceno = suiBox->getUsingObject();
+
+		if (!sceno->isVendor())
+			return;
+
+		Vendor* vendor = NULL;
+
+		if (sceno->isTerminal())
+			vendor = ((VendorTerminal*) sceno.get())->getVendor();
+		else if (sceno->isCreatureObject())
+			vendor = ((VendorCreature*) sceno.get())->getVendor();
+
+		if (vendor == NULL)
+			return;
+
+		VendorManager::instance()->handleDestoryCallback(player, vendor);
+	}
+};
+
+#endif /* DESTROYVENDORSUICALLBACK_H_ */
