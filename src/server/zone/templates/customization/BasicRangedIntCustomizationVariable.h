5482afd2311bb313cc769cae784903f571d5e4b9
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-04-01 14:59:24 +0000

    [added] AssetCustomizationManagerTemplate parser and customization variable lookups based on appearance file
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@3002 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ src/server/zone/templates/customization/BasicRangedIntCustomizationVariable.h
@@ -0,0 +1,44 @@
+/*
+ * BasicRangedIntCustomizationVariable.h
+ *
+ *  Created on: 01/04/2011
+ *      Author: victor
+ */
+
+#ifndef BASICRANGEDINTCUSTOMIZATIONVARIABLE_H_
+#define BASICRANGEDINTCUSTOMIZATIONVARIABLE_H_
+
+#include "RangedIntCustomizationVariable.h"
+
+class BasicRangedIntCustomizationVariable : public RangedIntCustomizationVariable {
+	int minValueInclusive;
+	int maxValueExclusive;
+
+public:
+	BasicRangedIntCustomizationVariable(int min, int max, int def) {
+		minValueInclusive = min;
+		maxValueExclusive = max;
+		defaultValue = def;
+	}
+
+	BasicRangedIntCustomizationVariable(const BasicRangedIntCustomizationVariable& m) : RangedIntCustomizationVariable(m){
+		minValueInclusive = m.minValueInclusive;
+		maxValueExclusive = m.maxValueExclusive;
+		defaultValue = m.defaultValue;
+	}
+
+	inline int getMinValueInclusive() {
+		return minValueInclusive;
+	}
+
+	inline int getMaxValueExclusive() {
+		return maxValueExclusive;
+	}
+
+	inline int getDefaultValue() {
+		return defaultValue;
+	}
+};
+
+
+#endif /* BASICRANGEDINTCUSTOMIZATIONVARIABLE_H_ */
