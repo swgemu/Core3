14076d8a9dff4cf192fc102e1305ddbfff5c92d4
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-04-02 00:10:27 +0000

    [Updated] Templates for crafting and loot
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk/MMOCoreORB@3008 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- src/server/zone/templates/SharedTangibleObjectTemplate.cpp
+++ src/server/zone/templates/SharedTangibleObjectTemplate.cpp
@@ -7,6 +7,34 @@
 
 #include "SharedTangibleObjectTemplate.h"
 
+SharedTangibleObjectTemplate::SharedTangibleObjectTemplate() {
+
+	numberExperimentalProperties = new Vector<short>();
+	experimentalProperties = new Vector<String>();
+	experimentalWeights = new Vector<short>();
+	experimentalGroupTitles = new Vector<String>();
+	experimentalSubGroupTitles = new Vector<String>();
+	experimentalMin = new Vector<int>();
+	experimentalMax = new Vector<int>();
+	experimentalPrecision = new Vector<short>();
+
+	resourceWeights = new Vector<Reference<ResourceWeight* > >();
+}
+
+SharedTangibleObjectTemplate::~SharedTangibleObjectTemplate() {
+
+	delete numberExperimentalProperties;
+	delete experimentalProperties;
+	delete experimentalWeights;
+	delete experimentalGroupTitles;
+	delete experimentalSubGroupTitles;
+	delete experimentalMin;
+	delete experimentalMax;
+	delete experimentalPrecision;
+
+	delete resourceWeights;
+}
+
 void SharedTangibleObjectTemplate::readObject(LuaObject* templateData) {
 	SharedObjectTemplate::readObject(templateData);
 
@@ -33,4 +61,76 @@ void SharedTangibleObjectTemplate::readObject(LuaObject* templateData) {
 	pvpStatusBitmask = templateData->getIntField("pvpStatusBitmask");
 
 	sliceable = templateData->getIntField("sliceable");
+
+	LuaObject numberExperimentalPropertiesList = templateData->getObjectField("numberExperimentalProperties");
+	for (int i = 1; i <= numberExperimentalPropertiesList.getTableSize(); ++i) {
+		numberExperimentalProperties->add(
+				numberExperimentalPropertiesList.getIntAt(i));
+	}
+	numberExperimentalPropertiesList.pop();
+
+	LuaObject experimentalPropertiesList = templateData->getObjectField("experimentalProperties");
+	for (int i = 1; i <= experimentalPropertiesList.getTableSize(); ++i) {
+		experimentalProperties->add(experimentalPropertiesList.getStringAt(i));
+	}
+	experimentalPropertiesList.pop();
+
+	LuaObject experimentalWeightsList = templateData->getObjectField("experimentalWeights");
+	for (int i = 1; i <= experimentalWeightsList.getTableSize(); ++i) {
+		experimentalWeights->add(experimentalWeightsList.getIntAt(i));
+	}
+	experimentalWeightsList.pop();
+
+	LuaObject experimentalGroupTitlesList = templateData->getObjectField("experimentalGroupTitles");
+	for (int i = 1; i <= experimentalGroupTitlesList.getTableSize(); ++i) {
+		experimentalGroupTitles->add(experimentalGroupTitlesList.getStringAt(i));
+	}
+	experimentalGroupTitlesList.pop();
+
+	LuaObject experimentalSubGroupTitlesList = templateData->getObjectField("experimentalSubGroupTitles");
+	for (int i = 1; i <= experimentalSubGroupTitlesList.getTableSize(); ++i) {
+		experimentalSubGroupTitles->add(
+				experimentalSubGroupTitlesList.getStringAt(i));
+	}
+	experimentalSubGroupTitlesList.pop();
+
+	LuaObject experimentalMinList = templateData->getObjectField("experimentalMin");
+	for (int i = 1; i <= experimentalMinList.getTableSize(); ++i) {
+		experimentalMin->add(experimentalMinList.getIntAt(i));
+	}
+	experimentalMinList.pop();
+
+	LuaObject experimentalMaxList = templateData->getObjectField("experimentalMax");
+	for (int i = 1; i <= experimentalMaxList.getTableSize(); ++i) {
+		experimentalMax->add(experimentalMaxList.getIntAt(i));
+	}
+	experimentalMaxList.pop();
+
+	LuaObject experimentalPrecisionList = templateData->getObjectField("experimentalPrecision");
+	for (int i = 1; i <= experimentalPrecisionList.getTableSize(); ++i) {
+		experimentalPrecision->add(experimentalPrecisionList.getIntAt(i));
+	}
+	experimentalPrecisionList.pop();
+
+	// Add experimental properties groups to the draft schematic
+	uint32 weightIterator = 0;
+	String subtitle = "";
+	for (uint32 i = 0; i < numberExperimentalProperties->size(); i++) {
+
+		ResourceWeight* newWeight = new ResourceWeight();
+
+		newWeight->addProperties(experimentalGroupTitles->get(i),
+				experimentalSubGroupTitles->get(i), experimentalMin->get(i),
+				experimentalMax->get(i), experimentalPrecision->get(i));
+
+		for (uint32 j = 0; j < numberExperimentalProperties->get(i); j++) {
+
+			newWeight->addWeight(experimentalProperties->get(weightIterator),
+					experimentalWeights->get(weightIterator));
+
+			weightIterator++;
+		}
+
+		addResourceWeight(newWeight);
+	}
 }
