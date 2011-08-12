d469c795e03e57187c1adb5cfc4e8541c40218bd
crush <crush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-08-12 21:56:34 +0000

    [fixed] Dantooine, Dathomir, Endor, Lok, and Talus starports now have travel terminals, ticket collectors, and shuttles.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@3233 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/building/military/outpost_starport.lua
+++ MMOCoreORB/bin/scripts/object/building/military/outpost_starport.lua
@@ -43,8 +43,12 @@
 
 object_building_military_outpost_starport = object_building_military_shared_outpost_starport:new {
 	gameObjectType = 521,
-
-
+	planetMapCategory = "starport",
+	childObjects = {
+		{templateFile = "object/tangible/terminal/terminal_travel.iff", x = -3.12, z = 0.14659503, y = -17.57, ox = 0, oy = 0.707107, oz = 0, ow = -0.707107, cellid = 0, containmentType = -1},
+		{templateFile = "object/tangible/travel/ticket_collector/ticket_collector.iff", x = 1, z = 0, y = -10, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1},
+		{templateFile = "object/mobile/player_transport.iff", x = 0, z = 7, y = 0, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1}
+	}
 }
 
 ObjectTemplates:addTemplate(object_building_military_outpost_starport, "object/building/military/outpost_starport.iff")
