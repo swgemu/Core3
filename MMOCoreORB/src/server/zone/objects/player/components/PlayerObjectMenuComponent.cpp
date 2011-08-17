35ab97feb34d572226b3ee29a1c7ae37a2d7f6cd
Moncai <Moncai@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-08-17 20:13:46 +0000

    [fixed] Radial menu for players cleaned up at back end. [changed] Option "Teach" now using new code path (work in progress).
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@3262 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/player/components/PlayerObjectMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/player/components/PlayerObjectMenuComponent.cpp
@@ -1,8 +1,45 @@
 /*
- * PlayerObjectMenuComponent.cpp
- *
- *  Created on: 26/05/2011
- *      Author: victor
+ Copyright (C) 2007-2011 <SWGEmu>
+
+ This File is part of Core3.
+
+ This program is free software; you can redistribute
+ it and/or modify it under the terms of the GNU Lesser
+ General Public License as published by the Free Software
+ Foundation; either version 2 of the License,
+ or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU Lesser General Public License for
+ more details.
+
+ You should have received a copy of the GNU Lesser General
+ Public License along with this program; if not, write to
+ the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+ Linking Engine3 statically or dynamically with other modules
+ is making a combined work based on Engine3.
+ Thus, the terms and conditions of the GNU Lesser General Public License
+ cover the whole combination.
+
+ In addition, as a special exception, the copyright holders of Engine3
+ give you permission to combine Engine3 program with free software
+ programs or libraries that are released under the GNU LGPL and with
+ code included in the standard release of Core3 under the GNU LGPL
+ license (or modified versions of such code, with unchanged license).
+ You may copy and distribute such a system following the terms of the
+ GNU LGPL for Engine3 and the licenses of the other code concerned,
+ provided that you include the source code of that other code when
+ and as the GNU LGPL requires distribution of source code.
+
+ Note that people who make modified versions of Engine3 are not obligated
+ to grant this special exception for their modified versions;
+ it is their choice whether to do so. The GNU Lesser General Public License
+ gives permission to release a modified version without this exception;
+ this exception also makes it possible to release a modified version
+ which carries forward this exception.
  */
 
 #include "PlayerObjectMenuComponent.h"
@@ -12,9 +49,10 @@
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/objects/group/GroupObject.h"
-#include "server/zone/objects/player/sui/listbox/teachplayerlistbox/TeachPlayerListBox.h"
 
-void PlayerObjectMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* player) {
+void PlayerObjectMenuComponent::fillObjectMenuResponse(
+		SceneObject* sceneObject, ObjectMenuResponse* menuResponse,
+		CreatureObject* player) {
 	if (!sceneObject->isCreatureObject())
 		return;
 
@@ -28,58 +66,53 @@ void PlayerObjectMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject,
 
 	if (creature->isPlayingMusic()) {
 		if (!player->isListening())
-			menuResponse->addRadialMenuItem(113, 3, "@radial_performance:listen");
+			menuResponse->addRadialMenuItem(113, 3,
+					"@radial_performance:listen");
 		else
-			menuResponse->addRadialMenuItem(115, 3, "@radial_performance:listen_stop");
+			menuResponse->addRadialMenuItem(115, 3,
+					"@radial_performance:listen_stop");
 	}
 
 	if (creature->isDancing()) {
 		if (!player->isWatching())
 			menuResponse->addRadialMenuItem(114, 3, "@radial_performance:watch");
 		else
-			menuResponse->addRadialMenuItem(116, 3, "@radial_performance:watch_stop");
+			menuResponse->addRadialMenuItem(116, 3,
+					"@radial_performance:watch_stop");
 	}
 
 }
 
-int PlayerObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) {
-	CreatureObject* ownerPlayer = dynamic_cast<CreatureObject*>(sceneObject);
+int PlayerObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject,
+		CreatureObject* player, byte selectedID) {
+	CreatureObject* ownerPlayer = dynamic_cast<CreatureObject*> (sceneObject);
 	PlayerObject* ghost = ownerPlayer->getPlayerObject();
 
-	switch(selectedID) {
+	switch (selectedID) {
 	case 113:
-		player->executeObjectControllerAction(String("listen").hashCode(), sceneObject->getObjectID(), "");
+		player->executeObjectControllerAction(0x5855BB1B,
+				sceneObject->getObjectID(), ""); // listen
 		break;
 	case 115:
-		player->executeObjectControllerAction(String("stoplistening").hashCode(), sceneObject->getObjectID(), "");
+		player->executeObjectControllerAction(0xC2E4D4D0,
+				sceneObject->getObjectID(), ""); // stoplistening
 		break;
 
 	case 114:
-		player->executeObjectControllerAction(String("watch").hashCode(), sceneObject->getObjectID(), "");
+		player->executeObjectControllerAction(0xEC93CA43,
+				sceneObject->getObjectID(), ""); // watch
 		break;
 
 	case 116:
-		player->executeObjectControllerAction(String("stopwatching").hashCode(), sceneObject->getObjectID(), "");
+		player->executeObjectControllerAction(0x6651AD9A,
+				sceneObject->getObjectID(), ""); // stopwatching
 		break;
 
 	case 51:
-
-			if (ghost->isTeachingOrLearning() || ghost->isTeachingOrLearning()) {
-				player->sendSystemMessage("teaching", "teaching_failed");
-				return 1;
-			}
-
-			ghost->setTeachingOrLearning(true);
-
-			ManagedReference<TeachPlayerListBox*> teachPlayerListBox = new TeachPlayerListBox(player);
-			teachPlayerListBox->setCancelButton(true, "");
-
-			bool completed = teachPlayerListBox->generateSkillList(player, ownerPlayer);
-
-			if(!completed)
-				ghost->setTeachingOrLearning(false);
-
+		player->executeObjectControllerAction(0x5041F83A,
+				sceneObject->getObjectID(), ""); // teach
 		break;
+
 	}
 
 	return 0;
