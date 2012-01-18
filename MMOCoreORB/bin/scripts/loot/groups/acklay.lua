750c6a6b2ca9907e2285feb831d462d8a722b631
xyborn <xyborn@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-01-18 16:55:09 +0000

    [added] Armor segment and enhancement loot (not dropping yet) [added] Rare RIS components (not dropping yet) [added] Nightsister loot (dropping on nightsisters, but nightsister spawns not filled out yet) [fixed] DL44 and DL44 Metal loot stats
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@3679 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/loot/groups/acklay.lua
+++ MMOCoreORB/bin/scripts/loot/groups/acklay.lua
@@ -5,8 +5,9 @@ acklay = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "acklay_bone_armor_schematic", weight = 3500000},
+		{itemTemplate = "acklay_bone_armor_schematic", weight = 2000000},
 		{itemTemplate = "acklay_bones", weight = 1000000},
+		{itemTemplate = "acklay_hide", weight = 1500000},
 		{itemTemplate = "acklay_ris_armor_schematic", weight = 2000000},
 		{itemTemplate = "acklay_venom", weight = 3500000}
 	}
