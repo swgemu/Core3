6735bafcaa477b5f01a5270d5b2abe6544d1b527
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-01-29 14:44:24 +0000

    [fixed] reenabled tutorial
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@3840 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -388,8 +388,8 @@ bool PlayerCreationManager::createCharacter(MessageCallback* data) {
 	UnicodeString bio;
 	callback->getBiography(bio);
 
-	//bool doTutorial = callback->getTutorialFlag();
-	bool doTutorial = false;
+	bool doTutorial = callback->getTutorialFlag();
+	//bool doTutorial = false;
 
 	ManagedReference<CreatureObject*> playerCreature = dynamic_cast<CreatureObject*>(zoneServer->createObject(serverObjectCRC, 2));
 
