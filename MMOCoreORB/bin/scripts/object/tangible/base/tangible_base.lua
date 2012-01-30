e106e36d4de71f68dd1df1370e461a3ab8b2f40f
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-01-30 10:13:11 +0000

    [fixed] stability fixes
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@3870 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/tangible/base/tangible_base.lua
+++ MMOCoreORB/bin/scripts/object/tangible/base/tangible_base.lua
@@ -53,6 +53,7 @@ object_tangible_base_tangible_base = object_tangible_base_shared_tangible_base:n
 	optionsBitmask = 256, --Default all objects to not display ham bars.
 	pvpStatusBitmask = 0,
 	objectMenuComponent = "TangibleObjectMenuComponent",
+	attributeListComponent = "AttributeListComponent",
 
 	sliceable = 0
 }
