2cc419ec367e7858d6011bb104a18f3d9be2e852
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-03-12 22:24:15 +0000

    [Fixed] Fishing poles
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4228 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/weapon/base/base_weapon.lua
+++ MMOCoreORB/bin/scripts/object/weapon/base/base_weapon.lua
@@ -43,6 +43,27 @@
 
 object_weapon_base_base_weapon = object_weapon_base_shared_base_weapon:new {
 
+	playerRaces = { "object/creature/player/bothan_male.iff",
+			"object/creature/player/bothan_female.iff",
+			"object/creature/player/human_male.iff",
+			"object/creature/player/human_female.iff",
+			"object/creature/player/ithorian_male.iff",
+			"object/creature/player/ithorian_female.iff",
+			"object/creature/player/moncal_male.iff",
+			"object/creature/player/moncal_female.iff",
+			"object/creature/player/rodian_male.iff",
+			"object/creature/player/rodian_female.iff",
+			"object/creature/player/sullustan_male.iff",
+			"object/creature/player/sullustan_female.iff",
+			"object/creature/player/trandoshan_male.iff",
+			"object/creature/player/trandoshan_female.iff",
+			"object/creature/player/twilek_male.iff",
+			"object/creature/player/twilek_female.iff",
+			"object/creature/player/wookiee_male.iff",
+			"object/creature/player/wookiee_female.iff",
+			"object/creature/player/zabrak_male.iff",
+			"object/creature/player/zabrak_female.iff" },
+
 	objectMenuComponent = {"cpp", "WeaponObjectMenuComponent"},
 
 }
