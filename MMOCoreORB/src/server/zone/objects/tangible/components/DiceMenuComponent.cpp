ac811cb13701f1beb19185c2d6ea8f16d2b3e08a
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-03-22 19:24:48 +0000

    [Fixed] Cleaned up dice radials
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4304 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/tangible/components/DiceMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/DiceMenuComponent.cpp
@@ -48,10 +48,9 @@ void DiceMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject,
 	}
 
 	if (!diceTemplate->isConfigurable() && !diceTemplate->isChanceCube()) {
-		menuResponse->addRadialMenuItem(172, 3, "@dice/dice:dice_roll_one_single");
-		menuResponse->addRadialMenuItem(173, 3, "@dice/dice:dice_roll_two_single");
-		menuResponse->addRadialMenuItem(174, 3, "@dice/dice:dice_roll_three_single");
-		menuResponse->addRadialMenuItem(175, 3, "@dice/dice:dice_roll_four_single");
+		menuResponse->addRadialMenuItemToRadialID(172, 173, 3, "@dice/dice:dice_roll_two_single");
+		menuResponse->addRadialMenuItemToRadialID(172, 174, 3, "@dice/dice:dice_roll_three_single");
+		menuResponse->addRadialMenuItemToRadialID(172, 175, 3, "@dice/dice:dice_roll_four_single");
 	}
 
 	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject,
