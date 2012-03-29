684c98e09651dd0e1c2d34351e026353c72d256e
theanswer <theanswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-03-29 15:54:00 +0000

    patch by zing [fixed] Crafted Geo cores speed
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4371 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/tangible/component/weapon/geonosian_sword_core.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/weapon/geonosian_sword_core.lua
@@ -48,9 +48,9 @@ object_tangible_component_weapon_geonosian_sword_core = object_tangible_componen
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expDurability", "expRange", "expRange", "expRange", "expEffeciency", "expEffeciency", "expEffeciency"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrangemod", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 1, 1, -7, 0, 10, 0, 0, 0, -3, -3, -3},
-	experimentalMax = {0, 0, 50, 50, 0, 15, 100, 40, 40, 40, 10, 10, 10},
-	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	experimentalMin = {0, 0, 1, 1, 0, 0, 10, 0, 0, 0, -3, -3, -3},
+	experimentalMax = {0, 0, 50, 50, -0.7, 15, 100, 40, 40, 40, 10, 10, 10},
+	experimentalPrecision = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
 }
 
 ObjectTemplates:addTemplate(object_tangible_component_weapon_geonosian_sword_core, "object/tangible/component/weapon/geonosian_sword_core.iff")
