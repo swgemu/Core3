47f8ecb99cd82a07cd7b00d6873e665f60036c2b
theanswer <theanswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-04-03 15:33:54 +0000

    [fixed] terminals for non-buio objects
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4431 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/tangible/terminal/components/StructureTerminalMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/terminal/components/StructureTerminalMenuComponent.cpp
@@ -34,40 +34,42 @@ void StructureTerminalMenuComponent::fillObjectMenuResponse(SceneObject* sceneOb
 		menuResponse->addRadialMenuItemToRadialID(118, 124, 3, "@player_structure:management_status"); //Status
 		menuResponse->addRadialMenuItemToRadialID(118, 129, 3, "@player_structure:management_pay"); //Pay Maintenance
 		menuResponse->addRadialMenuItemToRadialID(118, 50, 3, "@player_structure:management_name_structure"); //Name Structure
-		menuResponse->addRadialMenuItemToRadialID(118, 127, 3, "@player_structure:management_residence"); //Declare Residence
-		menuResponse->addRadialMenuItemToRadialID(118, 125, 3, "@player_structure:management_privacy"); //Privacy
 
-		if (creature->hasSkill("crafting_artisan_business_03"))
-			menuResponse->addRadialMenuItemToRadialID(118, 130, 3, "@player_structure:create_vendor"); //Create Vendor
+		if (structureObject->isBuildingObject()) {
+			menuResponse->addRadialMenuItemToRadialID(118, 127, 3, "@player_structure:management_residence"); //Declare Residence
+			menuResponse->addRadialMenuItemToRadialID(118, 125, 3, "@player_structure:management_privacy"); //Privacy
 
-		//menuResponse->addRadialMenuItemToRadialID(118, 68, 3, "@player_structure:management_add_turnstile"); //Set Access Fee
-		//menuResponse->addRadialMenuItemToRadialID(118, 69, 3, "@player_structure:management_change_sign"); //Change Sign
-		//menuResponse->addRadialMenuItemToRadialID(118, 200, 3, "@player_structure:withdraw_maintenance"); //Withdraw from Treasury
-		menuResponse->addRadialMenuItemToRadialID(118, 201, 3, "@player_structure:delete_all_items"); //Delete all items
-		menuResponse->addRadialMenuItemToRadialID(118, 202, 3, "@player_structure:move_first_item"); //Find Lost Items
+
+			if (creature->hasSkill("crafting_artisan_business_03"))
+				menuResponse->addRadialMenuItemToRadialID(118, 130, 3, "@player_structure:create_vendor"); //Create Vendor
+
+			//menuResponse->addRadialMenuItemToRadialID(118, 68, 3, "@player_structure:management_add_turnstile"); //Set Access Fee
+			//menuResponse->addRadialMenuItemToRadialID(118, 69, 3, "@player_structure:management_change_sign"); //Change Sign
+			//menuResponse->addRadialMenuItemToRadialID(118, 200, 3, "@player_structure:withdraw_maintenance"); //Withdraw from Treasury
+			menuResponse->addRadialMenuItemToRadialID(118, 201, 3, "@player_structure:delete_all_items"); //Delete all items
+			menuResponse->addRadialMenuItemToRadialID(118, 202, 3, "@player_structure:move_first_item"); //Find Lost Items
+		}
 
 		menuResponse->addRadialMenuItem(117, 3, "@player_structure:permissions"); //Structure Permissions
 		menuResponse->addRadialMenuItemToRadialID(117, 121, 3, "@player_structure:permission_admin"); //Administrator List
-		menuResponse->addRadialMenuItemToRadialID(117, 119, 3, "@player_structure:permission_enter"); //Entry List
-		menuResponse->addRadialMenuItemToRadialID(117, 120, 3, "@player_structure:permission_banned"); //Ban List
-		menuResponse->addRadialMenuItemToRadialID(117, 122, 3, "@player_structure:permission_vendor"); //Vendor List
+
+		if (structureObject->isBuildingObject()) {
+			menuResponse->addRadialMenuItemToRadialID(117, 119, 3, "@player_structure:permission_enter"); //Entry List
+			menuResponse->addRadialMenuItemToRadialID(117, 120, 3, "@player_structure:permission_banned"); //Ban List
+			menuResponse->addRadialMenuItemToRadialID(117, 122, 3, "@player_structure:permission_vendor"); //Vendor List
+		}
 	}
 }
 
 int StructureTerminalMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) {
-	ManagedReference<StructureObject*> structureObject = NULL;
+	ManagedReference<Terminal*> terminal = cast<Terminal*>(sceneObject);
+	if(terminal == NULL)
+		return 1;
 
-	if (!sceneObject->isStructureObject()) {
-		ManagedReference<SceneObject*> parent = sceneObject->getRootParent();
+	ManagedReference<StructureObject*> structureObject = cast<StructureObject*>(terminal->getControlledObject());
 
-		if (parent != NULL && parent->isStructureObject()) {
-			structureObject = cast<StructureObject*>(parent.get());
-		} else {
-			return 1;
-		}
-	} else {
-		structureObject = cast<StructureObject*>(sceneObject);
-	}
+	if (structureObject == NULL)
+		return 1;
 
 	ManagedReference<Zone*> zone = structureObject->getZone();
 
