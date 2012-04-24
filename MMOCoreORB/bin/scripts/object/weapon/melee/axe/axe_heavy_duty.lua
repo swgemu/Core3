95cc7d0dea24c74838699aa133069071e0f316e4
zingzing <zingzing@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-04-24 05:05:38 +0000

    [Fixed] Armor piercing on 2h Axe. Mantis issue 2129
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4680 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/weapon/melee/axe/axe_heavy_duty.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/axe/axe_heavy_duty.lua
@@ -72,7 +72,7 @@ object_weapon_melee_axe_axe_heavy_duty = object_weapon_melee_axe_shared_axe_heav
 	damageType = KINETIC,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = NONE,
+	armorPiercing = MEDIUM,
 
 	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
 	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
