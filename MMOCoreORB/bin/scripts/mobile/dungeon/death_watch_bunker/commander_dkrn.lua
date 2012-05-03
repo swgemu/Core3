f5a20f96b84390a37d8b6cd51c43e56b792c8c27
cRush <cRush@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-03 00:26:24 +0000

    [fixed] 3 missing npcs from last commit
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4810 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/commander_dkrn.lua
@@ -0,0 +1,38 @@
+commander_dkrn = Creature:new {
+	objectName = "",
+	customName = "Commander D`krn",
+	socialGroup = "Townsperson",
+	pvpFaction = "",
+	faction = "",
+	level = 10,
+	chanceHit = 0.28,
+	damageMin = 90,
+	damageMax = 110,
+	baseXp = 356,
+	baseHAM = 810,
+	baseHAMmax = 990,
+	armor = 0,
+	resists = {0,0,0,0,10,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_imperial_general_m.iff"},
+	lootGroups = {},
+	weapons = {"pirate_weapons_medium"},
+	conversationTemplate = "",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(commander_dkrn, "commander_dkrn")
