3533bc4a8232598e2250c5b24f9959ac96de2576
theanswer <theanswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-04 11:57:39 +0000

    [fixed] lua loot typo
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4848 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/loot/items/high_powered_vibro_motor.lua
+++ MMOCoreORB/bin/scripts/loot/items/high_powered_vibro_motor.lua
@@ -4,7 +4,7 @@ high_powered_vibro_motor = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	customObjectName = "",
-	directObjectTemplate = "object/tangilbe/component/weapon/vibro_unit_enhancement_max_damage.iff",
+	directObjectTemplate = "object/tangible/component/weapon/vibro_unit_enhancement_max_damage.iff",
 	craftingValues = {
 		{"maxdamage",20,20,1},
 		{"useCount",1,5,0},
