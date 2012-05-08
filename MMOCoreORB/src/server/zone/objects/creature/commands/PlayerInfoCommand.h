55c3a0279d2514748774eb563738d2c0f68aae1e
theanswer <theanswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-08 17:04:00 +0000

    [added] ability list display to playerinfo
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4928 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/creature/commands/PlayerInfoCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/PlayerInfoCommand.h
@@ -52,108 +52,20 @@ public:
 			totalSkillPointsWasted += skill->getSkillPointsRequired();
 		}
 
-		promptText << "totalSkillPointsWasted = " << totalSkillPointsWasted << " skillPoints var:" << ghost->getSkillPoints();
+		promptText << "totalSkillPointsWasted = " << totalSkillPointsWasted << " skillPoints var:" << ghost->getSkillPoints() << endl;
+
+		promptText << "Ability list:" << endl;
+
+		AbilityList* abilityList = ghost->getAbilityList();
+
+		for (int i = 0; i < abilityList->size(); ++i) {
+			Ability* skill = abilityList->get(i);
+			promptText << skill->getAbilityName() << endl;
+		}
 
 		box->setPromptText(promptText.toString());
 
 		creature->sendMessage(box->generateMessage());
-
-		/*ManagedReference<PlayerManagementSession*> session =
-				cast<PlayerManagementSession*>(creature->getActiveSession(SessionFacadeType::PLAYERMANAGEMENT));
-
-		if(session == NULL) {
-			session = new PlayerManagementSession(creature);
-			creature->addActiveSession(SessionFacadeType::ACCOUNT_PLAYERMANAGEMENT, session);
-		}
-
-		ManagedReference<SuiListBox*> box = session->getPlayerInfoSui();
-		if(box == NULL) {
-			box = new SuiListBox(creature, SuiWindowType::PLAYER_INFO, SuiListBox::HANDLETHREEBUTTON);
-			session->addPlayerInfoSui(box);
-		}
-
-		box->clearOptions();
-		box->setPromptTitle("Player Info");
-
-		StringBuffer header;
-		header << "Account Name: " << account->getUsername() << endl;
-		header << "Account ID: " << String::valueOf(account->getAccountID()) << endl;
-		header << "Station ID: " << String::valueOf(account->getStationID()) << endl;
-		header << "Admin Level: " << String::valueOf(account->getAdminLevel()) << endl;
-		header << "Created: " << createdTime.getFormattedTime() << endl;
-		if(account->isBanned()) {
-			header << "\\#FF0000(BANNED)\\#FFFFFF " << endl;
-			header << getBanDuration(account);
-			header << "Reason: "  << account->getBanReason() << endl;
-
-			ManagedReference<Account*> adminAccount = playerManager->getAccount(account->getBanAdmin());
-
-			if(adminAccount != NULL)
-				header << "Banned by: " << adminAccount->getUsername() << endl;
-		}
-
-		box->setOtherButton(true, "@data Ban/Unban");
-
-		box->setPromptText(header.toString());
-
-		box->setOkButton(true, "@command_info");
-		box->setCancelButton(true, "@cancel");
-		box->setCallback(new PlayerManagementSessionSuiCallback(creature->getZoneServer()));
-		box->setUsingObject(creature);
-		admin->addSuiBox(box);
-
-		String username = account->getUsername();
-		if(account->isBanned())
-			username += "\\#FF0000(BANNED)\\#FFFFFF ";
-		box->addMenuItem(username, 0);
-
-		VectorMap<String, Vector<String> > characters;
-
-		while(characterList->next()) {
-
-			UnicodeString name;
-			characterList->getCharacterName(name);
-
-			targetCreature = playerManager->getPlayer(characterList->getCharacterFirstName());
-			if(targetCreature == NULL || !targetCreature->isPlayerCreature())
-				continue;
-
-			ghost = targetCreature->getPlayerObject();
-
-			if(!characters.contains(characterList->getGalaxyName()))
-				characters.put(characterList->getGalaxyName(), Vector<String>());
-
-			Vector<String>* galaxy = &characters.get(characterList->getGalaxyName());
-
-			StringBuffer line;
-			line << "\t\t" << name.toString();
-
-			if(ghost->isLinkDead())
-				line << " \\#EE7600(LD)\\#FFFFFF ";
-			else if(ghost->isOnline())
-				line << " \\#00FF00(ONLINE)\\#FFFFFF ";
-			else
-				line << " \\#AAAAAA(OFFLINE)\\#FFFFFF ";
-
-			if(ghost->isBanned()) {
-				line << " \\#FF0000(BANNED)\\#FFFFFF ";
-			}
-
-			galaxy->add(line.toString());
-		}
-
-		for(int i = 0; i < characters.size(); ++i) {
-			Vector<String>* galaxy = &characters.get(i);
-
-			box->addMenuItem("\t" + characters.elementAt(i).getKey(), 0);
-
-			for(int j = 0; j < galaxy->size(); ++j) {
-				box->addMenuItem(galaxy->get(j), 0);
-			}
-		}
-
-		creature->sendMessage(box->generateMessage());*/
-
 
 		return 0;
 	}
