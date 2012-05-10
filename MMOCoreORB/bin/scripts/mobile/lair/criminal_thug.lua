abeeff89a425b805649af0c41bda29c7c13a7539
loshult <loshult@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-10 21:06:49 +0000

    [added] spawn groups for factional destroy missions.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@4951 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/mobile/lair/criminal_thug.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/criminal_thug.lua
@@ -2,11 +2,11 @@
 criminal_thug = Lair:new {
 	mobiles = {{"criminal", 7}},
 	spawnLimit = 0,
-	buildingsVeryEasy = {"object/tangible/lair/base/shared_objective_power_generator.iff"},
-	buildingsEasy = {"object/tangible/lair/base/shared_objective_power_generator.iff"},
-	buildingsMedium = {"object/tangible/lair/base/shared_objective_power_generator.iff"},
-	buildingsHard = {"object/tangible/lair/base/shared_objective_power_generator.iff"},
-	buildingsVeryHard = {"object/tangible/lair/base/shared_objective_power_generator.iff"}
+	buildingsVeryEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsMedium = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsHard = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/objective_power_generator.iff"}
 }
 
 addLairTemplate("criminal_thug", criminal_thug)
