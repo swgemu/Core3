6b3f3ec689543bfde96053c66c1cf0b0bc805461
Itac <Itac@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-14 01:28:08 +0000

    [Added]admin_server_admin_02 to CSR permission set
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@5016 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/staff/levels/csr.lua
+++ MMOCoreORB/bin/scripts/staff/levels/csr.lua
@@ -12,6 +12,7 @@ csr = {
 		"admin_player_management_03",
 		"admin_quest_management_01",
 		"admin_server_admin_01",
+		"admin_server_admin_02",
 		"admin_skill_management_01",
 		"admin_spawn_management_02",
 		"admin_veteran_rewards_01"
