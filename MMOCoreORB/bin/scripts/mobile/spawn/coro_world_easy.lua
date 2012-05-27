0fd0af0c43539d43bca220c368a09e1dad295b05
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-27 16:01:01 +0000

    (unstable) [readded] old corellia spawns
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5230 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/mobile/spawn/coro_world_easy.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/coro_world_easy.lua
@@ -7,7 +7,7 @@ coro_world_easy = {
 
 	lairSpawns = {
 		{
-			lairTemplateName = "bageraset_lair",
+			lairTemplateName = "corellia_bageraset_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -16,7 +16,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "carrion_spat_lair",
+			lairTemplateName = "corellia_carrion_spat_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -25,7 +25,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "corellian_butterfly_lair",
+			lairTemplateName = "corellian_butterfly",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -34,7 +34,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "dalyrake_lair",
+			lairTemplateName = "corellia_dalyrake_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -43,7 +43,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "durni_lair",
+			lairTemplateName = "corellia_durni_warren_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -52,7 +52,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "gronda_lair",
+			lairTemplateName = "corellia_gronda_lair_neutral_large",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -61,7 +61,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "gubbur_lair",
+			lairTemplateName = "corellia_gubbur_lair_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -70,16 +70,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "gulginaw_lair",
-			spawnLimit = -1,
-			minDifficulty = 5,
-			maxDifficulty = 20,
-			numberToSpawn = 0,
-			weighting = 10,
-			size = 25
-		},
-		{
-			lairTemplateName = "krahbu_lair",
+			lairTemplateName = "corellia_gulginaw_nest_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -88,7 +79,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "murra_lair",
+			lairTemplateName = "corellia_krahbu_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -97,7 +88,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "plumed_rasp_lair",
+			lairTemplateName = "corellia_murra_lair_neutral_none",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -106,7 +97,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "sharnaff_lair",
+			lairTemplateName = "corellia_plumed_rasp_nest_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -115,7 +106,7 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "slice_hound_lair",
+			lairTemplateName = "corellia_sharnaff_lair_neutral_none",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
@@ -124,7 +115,16 @@ coro_world_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "vynock_lair",
+			lairTemplateName = "corellia_slice_hound_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 20,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "corellia_vynock_lair_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 5,
 			maxDifficulty = 20,
