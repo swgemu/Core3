c93557eb472623122cd84b53d831fe4b0ed5a5c0
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-05-27 05:00:27 +0000

    (unstable)[updated] PSG's
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5221 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObjectImplementation.cpp
@@ -355,6 +355,8 @@ void ArmorObjectImplementation::updateCraftingValues(CraftingValues* values, boo
 
 void ArmorObjectImplementation::calculateSpecialProtection(CraftingValues* craftingValues) {
 
+	specialResists = ((int)(craftingValues->getCurrentValue("armor_special_type")));
+
 	for (int i = 0; i <= 8; ++i) {
 
 		int type = pow((float)2,i);
