e28358a230e632fba27ab16336f36c734e228165
swgemu <swgemu@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-06-10 20:18:48 +0000

    (unstable) [fixed] Fixed issue with tutorial cloning.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5341 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/building/general/newbie_hall.lua
+++ MMOCoreORB/bin/scripts/object/building/general/newbie_hall.lua
@@ -42,7 +42,6 @@
 
 
 object_building_general_newbie_hall = object_building_general_shared_newbie_hall:new {
-	gameObjectType = 516,
 	spawningPoints = { 
 			{ x = 7.61262, z = -6.99999, y = -56.4838, ow = 0.822931, ox = 0, oz = 0, oy = 0.568142, cellid = 5 }
 			}, -- { x, z, y, ow, ox, oy, oz, cellid }
