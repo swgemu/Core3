b8dfa5c90754bf4b767ac56c9a73a2d3a105fb55
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-06-26 19:35:31 +0000

    (unstable)[Updated] Resource Spawner
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5492 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/managers/resource/resourcespawner/resourcepool/MinimumPool.cpp
+++ MMOCoreORB/src/server/zone/managers/resource/resourcespawner/resourcepool/MinimumPool.cpp
@@ -70,7 +70,18 @@ void MinimumPool::addResource(ManagedReference<ResourceSpawn*> resourceSpawn, co
 		return;
 	}
 
-	int index = includedResources.find(poolSlot);
+	int index = -1;
+
+	for(int i = 0; i < includedResources.size(); ++i) {
+		String resourceType = includedResources.elementAt(i).getKey();
+		ManagedReference<ResourceSpawn* > spawn = includedResources.elementAt(i).getValue();
+
+		if(resourceType == poolSlot && spawn == NULL) {
+			index = i;
+			break;
+		}
+	}
+
 	if(index >= 0) {
 		VectorMapEntry<String, ManagedReference<ResourceSpawn*> > newEntry(poolSlot, resourceSpawn);
 		includedResources.setElementAt(index, newEntry);
