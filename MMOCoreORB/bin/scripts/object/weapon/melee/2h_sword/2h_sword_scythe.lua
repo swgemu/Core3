f2ab51ae280d302333473e4de74fca5f87e6531f
kyle <kyle@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-07-12 03:21:16 +0000

    (unstable)[Fixed] Scythe to Medium AP
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5608 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_scythe.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_scythe.lua
@@ -72,7 +72,7 @@ object_weapon_melee_2h_sword_2h_sword_scythe = object_weapon_melee_2h_sword_shar
 	damageType = KINETIC,
 	
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = LIGHT,
+	armorPiercing = MEDIUM,
 	
 	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
 	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
