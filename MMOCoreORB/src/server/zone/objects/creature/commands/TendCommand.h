28b50ceb6bd7c5147fa9008deed974909c1335a5
swgemu <swgemu@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-07-20 18:40:09 +0000

    (stable) [fixed] merged dannuic tend command changes.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@5710 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
@@ -185,12 +185,10 @@ public:
 
 		CreatureObject* creatureTarget = cast<CreatureObject*>(object.get());
 
-		/*	if (!target->isPlayer() && !target->isNonPlayerCreature()) {
+		if (creatureTarget == NULL /*|| (!creatureTarget->isPlayer() && !creatureTarget->isNonPlayerCreature())*/) {
 			creature->sendSystemMessage("@healing_response:healing_response_a1"); //Target must be a player or a creature pet in order to tend damage.
-			return GENERALERROR;
-		}*/
-
-		//CreatureObject* creatureTarget = cast<CreatureObject*>( target);
+			return INVALIDTARGET;
+		}
 
 		Locker clocker(creatureTarget, creature);
 
@@ -202,70 +200,58 @@ public:
 
 		uint8 attribute = findAttribute(creatureTarget);
 
-		/*if (creatureTarget->isDead() || creatureTarget->isRidingCreature() || creatureTarget->isMounted())
-			creatureTarget = creature;*/
-
 		if (creature->isProne()) {
 			creature->sendSystemMessage("You cannot do that while prone.");
-			return GENERALERROR;
+			return INVALIDLOCOMOTION;
 		}
 
 		if (creature->isMeditating()) {
 			creature->sendSystemMessage("You cannot do that while Meditating.");
-			return GENERALERROR;
+			return INVALIDSTATE;
 		}
 
 		if (creature->isRidingCreature()) {
 			creature->sendSystemMessage("You cannot do that while Riding a Creature.");
-			return GENERALERROR;
+			return INVALIDLOCOMOTION;
 		}
 
 		if (creature->isMounted()) {
 			creature->sendSystemMessage("You cannot do that while Driving a Vehicle.");
-			return GENERALERROR;
+			return INVALIDLOCOMOTION;
 		}
 
 		if (!creatureTarget->isHealableBy(creature)) {
 			creature->sendSystemMessage("@healing:pvp_no_help");
-			return GENERALERROR;
+			return INVALIDTARGET;
 		}
 
 		if (creature->getHAM(CreatureAttribute::MIND) < mindCost) {
 			creature->sendSystemMessage("@healing_response:not_enough_mind"); //You do not have enough mind to do that.
-			return GENERALERROR;
+			return INSUFFICIENTHAM;
 		}
+
+		float bfScale = 1 - creatureTarget->calculateBFRatio();
 
 		if (tendDamage) {
 			if (!creatureTarget->hasDamage(CreatureAttribute::HEALTH) && !creatureTarget->hasDamage(CreatureAttribute::ACTION)) {
 				if (creatureTarget == creature)
 					creature->sendSystemMessage("@healing_response:healing_response_61"); //You have no damage to heal.
 				else {
-					StringIdChatParameter stringId("healing_response", "healing_response_63");
+					StringIdChatParameter stringId("healing_response", "healing_response_63"); //%NT has no damage to heal.
 					stringId.setTT(creatureTarget->getObjectID());
-					//creature->sendSystemMessage("healing_response", "healing_response_63", creatureTarget->getObjectID()); //%NT has no damage to heal.
+					creature->sendSystemMessage(stringId);
 				}
+
 				return GENERALERROR;
 			}
 
-			float modSkill = (float) creature->getSkillMod("healing_injury_treatment");
-
-			float bfScale = 1 - creatureTarget->calculateBFRatio();
-			int effectiveness = 150;
-
-			int healPower = (int) round((100.0f + modSkill) / 100.0f * effectiveness * bfScale);
+			int healPower = round(((float)creature->getSkillMod("healing_injury_treatment") / 3.f + 20.f) * bfScale);
 
 			int healedHealth = creatureTarget->healDamage(creature, CreatureAttribute::HEALTH, healPower);
 			int healedAction = creatureTarget->healDamage(creature, CreatureAttribute::ACTION, healPower, true, false);
 
-			if (creature->isPlayerCreature() && creatureTarget->isPlayerCreature()) {
-				PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
-				playerManager->sendBattleFatigueMessage(creature, creatureTarget);
-			}
-
 			sendHealMessage(creature, creatureTarget, healedHealth, healedAction);
-		}
-
-		if (tendWound) {
+		} else if (tendWound) {
 			if (attribute >= CreatureAttribute::MIND)
 				attribute = CreatureAttribute::UNKNOWN;
 
@@ -273,36 +259,23 @@ public:
 				if (creatureTarget == creature)
 					creature->sendSystemMessage("@healing_response:healing_response_67");
 				else if (creatureTarget->isPlayerCreature()){
-					creature->sendSystemMessage(creatureTarget->getFirstName()
-							+ " has no wounds of that type to heal.");
+					creature->sendSystemMessage(creatureTarget->getFirstName() + " has no wounds of that type to heal.");
 				}
 
 				return GENERALERROR;
 			}
 
-			//TODO: Add in medical city bonus
-			PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
-
-			//if (playerManager->getMedicalFacilityRating(enhancer) <= 0) {
-			float modEnvironment = 1 + (creature->getSkillMod("private_medical_rating") / 100.0f);
-			float modSkill = (float) creature->getSkillMod("healing_wound_treatment");
-
-			float bfScale = 1 - creatureTarget->calculateBFRatio();
-			int effectiveness = 150;
-
-			//Since this skill can be used anywhere, we need to check that modEnvironment is not 0.
-			modEnvironment = modEnvironment > 0.0f ? modEnvironment : 1.0f;
-
-			int healPower = (int) round(effectiveness * bfScale  * modEnvironment * (100.0f + modSkill) / 1000.0f);
+			int healPower = round(((float)creature->getSkillMod("healing_wound_treatment") / 3.f + 20.f) * bfScale);
 
 			int healedWounds = creatureTarget->healWound(creature, attribute, healPower);
 
-			if (creature->isPlayerCreature() && creatureTarget->isPlayerCreature()) {
-				PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
-				playerManager->sendBattleFatigueMessage(creature, creatureTarget);
-			}
-
 			sendWoundMessage(creature, creatureTarget, attribute, -healedWounds);
+		} else
+			return GENERALERROR;
+
+		if (creature->isPlayerCreature() && creatureTarget->isPlayerCreature()) {
+			PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
+			playerManager->sendBattleFatigueMessage(creature, creatureTarget);
 		}
 
 		creature->inflictDamage(creature, CreatureAttribute::MIND, mindCost, false);
