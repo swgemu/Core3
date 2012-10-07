bd9e7f2b5fe28b163a0df37f6b43223f9c275ada
theanswer <theanswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-10-07 14:13:50 +0000

    [added] Taverns. You may heal wounds, battle fatigue, and receive mind buffs at the following locations: Dantooine Mining Outpost, Dantooine Pirate Outpost, Rori Rebel Outpost, Anchorhead, Mos Taike. This addresses mantis #2846. patch by Valkyra
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5888 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/building/tatooine/housing_tatt_style03_med.lua
+++ MMOCoreORB/bin/scripts/object/building/tatooine/housing_tatt_style03_med.lua
@@ -44,7 +44,17 @@
 object_building_tatooine_housing_tatt_style03_med = object_building_tatooine_shared_housing_tatt_style03_med:new {
 
 
+-- Used as tavern for: Mos Taike?
 
+	planetMapCategory = "tavern",
+	
+	skillMods = {
+		{"private_med_wound_mind", 20},
+		{"private_buff_mind", 100},
+		{"private_med_battle_fatigue", 5},
+		{"private_med_wound_health", 100},
+		{"private_med_wound_action", 100}
+	},
 
 
 }
