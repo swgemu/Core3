cfc3bcf5cdf0c4d11d2e6096680bc93d7996558e
loshult <loshult@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-10-14 05:11:18 +0000

    (unstable) [added] gm commands to handle quests through screen play state manipulation, /completeQuest and /clearCompletedQuest
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5943 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/creature/commands/CompleteQuestCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/CompleteQuestCommand.h
@@ -63,6 +63,52 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		StringTokenizer args(arguments.toString());
+
+		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+
+		if (ghost == NULL || !ghost->isPrivileged()) {
+			creature->sendSystemMessage("@error_message:insufficient_permissions"); //You do not have sufficient permissions to perform the requested action.
+			return INSUFFICIENTPERMISSION;
+		}
+
+		try {
+			String commandType;
+			args.getStringToken(commandType);
+
+			if (commandType.beginsWith("screenplaystate")) {
+				ManagedReference<SceneObject*> obj = server->getZoneServer()->getObject(target);
+
+				if (obj == NULL || !obj->isCreatureObject()) {
+					return INVALIDTARGET;
+				}
+
+				CreatureObject* targetCreature = cast<CreatureObject*>(obj.get());
+
+				if (targetCreature == NULL) {
+					return INVALIDTARGET;
+				}
+
+				String screenPlayName;
+				args.getStringToken(screenPlayName);
+				uint64 state = args.getLongToken();
+
+				Locker targetLock(targetCreature);
+
+				targetCreature->setScreenPlayState(screenPlayName, state | targetCreature->getScreenPlayState(screenPlayName));
+
+				creature->sendSystemMessage(targetCreature->getFirstName() + " " + screenPlayName + " set to " + String::valueOf(targetCreature->getScreenPlayState(screenPlayName)));
+			} else {
+				creature->sendSystemMessage("SYNTAX: /completeQuest screenplaystate <screenPlayStateName> <state>");
+
+				return INVALIDPARAMETERS;
+			}
+		} catch (Exception& e) {
+			creature->sendSystemMessage("SYNTAX: /completeQuest screenplaystate <screenPlayStateName> <state>");
+
+			return INVALIDPARAMETERS;
+		}
+
 		return SUCCESS;
 	}
 
