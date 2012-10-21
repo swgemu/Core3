8422e1768ce5a0b839d1a7940a6ce2188b786638
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-10-21 21:22:00 +0000

    (unstable) [added] Imperial Faction Missions now spawn rebels when S.F - level 15 and 25 mission added
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5985 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/mobile/lair/rebel.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/rebel.lua
@@ -1,11 +1,11 @@
 rebel = Lair:new {
-	mobiles = {},
+	mobiles = {{"rebel_commando", 25}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsMedium = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsHard = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/objective_power_generator.iff"},
 }
 
 addLairTemplate("rebel", rebel)
--- MMOCoreORB/bin/scripts/mobile/lair/rebel_npc.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/rebel_npc.lua
@@ -1,11 +1,11 @@
 rebel_npc = Lair:new {
-	mobiles = {},
+	mobiles = {{"rebel_corporal", 15}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsEasy = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsMedium = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsHard = {"object/tangible/lair/base/objective_power_generator.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/objective_power_generator.iff"},
 }
 
 addLairTemplate("rebel_npc", rebel_npc)
