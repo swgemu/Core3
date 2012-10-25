e4540d0a4de0b7efac5c23986aafb88ac6fc3fd8
zingzing <zingzing@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-10-25 02:37:33 +0000

    (Unstable) [Updated] jabba theme park reward porcellus. please test, food crafter's apron only.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@5997 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_apron_field_01_quest_01.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_apron_field_01_quest_01.lua
@@ -48,11 +48,11 @@ object_draft_schematic_clothing_clothing_apron_field_01_quest_01 = object_draft_
    customObjectName = "Crafter\'s Apron",
 
    craftingToolTab = 8, -- (See DraftSchemticImplementation.h)
-   complexity = 20, 
-   size = 0, 
+   complexity = 30, 
+   size = 1, 
 
    xpType = "crafting_clothing_general", 
-   xp = 140, 
+   xp = 1000, 
 
    assemblySkill = "clothing_assembly", 
    experimentingSkill = "clothing_experimentation", 
@@ -62,12 +62,12 @@ object_draft_schematic_clothing_clothing_apron_field_01_quest_01 = object_draft_
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"apron", "binding_and_reinforcement", "binding_and_hardware", "chef_suit", "bow_tie"},
-   ingredientSlotType = {1, 0, 1, 1, 0},
-   resourceTypes = {"object/tangible/component/clothing/shared_fiberplast_panel.iff", "chemical", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
-   resourceQuantities = {4, 40, 2, 2, 20},
-   contribution = {100, 100, 100, 100, 100},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"body", "binding_and_reinforcement", "binding_and_hardware", "colorizable_cloth_panel", "lining", "herbs_spices", "cooking_utensils"},
+   ingredientSlotType = {1, 0, 0, 1, 0, 0, 3},
+   resourceTypes = {"object/tangible/component/clothing/shared_leather_heavy_duty.iff", "chemical", "chemical", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "hide", "flora_food", "object/tangible/crafting/station/shared_food_tool.iff"},
+   resourceQuantities = {2, 8, 3, 1, 50, 10, 1},
+   contribution = {100, 100, 100, 100, 100, 100, 100},
 
 
    targetTemplate = "object/tangible/wearables/apron/apron_porcellus.iff",
@@ -76,5 +76,4 @@ object_draft_schematic_clothing_clothing_apron_field_01_quest_01 = object_draft_
              }
 
 }
-
 ObjectTemplates:addTemplate(object_draft_schematic_clothing_clothing_apron_field_01_quest_01, "object/draft_schematic/clothing/clothing_apron_field_01_quest_01.iff")
--- MMOCoreORB/bin/scripts/object/tangible/wearables/apron/apron_porcellus.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/apron/apron_porcellus.lua
@@ -85,17 +85,14 @@ object_tangible_wearables_apron_apron_porcellus = object_tangible_wearables_apro
 				"object/mobile/vendor/wookiee_female.iff",
 				"object/mobile/vendor/wookiee_male.iff",
 				"object/mobile/vendor/zabrak_female.iff",
-				"object/mobile/vendor/zabrak_male.iff" },
+				"object/mobile/vendor/zabrak_male.iff",
 				
-	numberExperimentalProperties = {1, 1, 1, 1},
-	experimentalProperties = {"XX", "XX", "XX", "XX"},
-	experimentalWeights = {1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null", "null"},
-	experimentalSubGroupTitles = {"null", "null", "sockets", "hitpoints"},
-	experimentalMin = {0, 0, 0, 1000},
-	experimentalMax = {0, 0, 0, 1000},
-	experimentalPrecision = {0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 4, 4},
+	},
+				
+	skillMods = {
+	    {"food_assembly", 5},
+	    {"food_experimentation", 5},
+	},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_apron_apron_porcellus, "object/tangible/wearables/apron/apron_porcellus.iff")
