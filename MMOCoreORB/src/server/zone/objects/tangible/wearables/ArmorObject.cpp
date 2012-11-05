a3acc34a83ce00a73b0d3bd9b41fd69f270be71b
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-11-05 19:09:00 +0000

    (unstable) [fixed] Initialize all armor parameters in constructor - fixes zero stat armor bug
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@6027 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObject.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/wearables/ArmorObject.cpp
@@ -952,6 +952,10 @@ ArmorObjectImplementation::ArmorObjectImplementation() {
 	effectivenessSlice = 1;
 	// server/zone/objects/tangible/wearables/ArmorObject.idl():  		encumbranceSlice = 1;
 	encumbranceSlice = 1;
+	// server/zone/objects/tangible/wearables/ArmorObject.idl():  		baseProtection = 0;
+	baseProtection = 0;
+	// server/zone/objects/tangible/wearables/ArmorObject.idl():  		specialProtection = 0;
+	specialProtection = 0;
 	// server/zone/objects/tangible/wearables/ArmorObject.idl():  		specialResists = 0;
 	specialResists = 0;
 	// server/zone/objects/tangible/wearables/ArmorObject.idl():  		hitLocation = 0;
