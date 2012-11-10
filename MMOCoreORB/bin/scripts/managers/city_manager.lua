b5f7f3f902d02a41933824278f24ebb81b580bd9
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-11-10 23:28:47 +0000

    (stable) [fixed] Citizen requirements for city rank
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@6047 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/managers/city_manager.lua
+++ MMOCoreORB/bin/scripts/managers/city_manager.lua
@@ -73,8 +73,8 @@ OldCityGracePeriod = 30240
 EnableCityWarn = false
 
 --The number of citizens required to achieve each city rank. (Outpost, Village, Township, City, Metropolis)
-CitizensPerRank = {1, 1, 1, 1, 1}
---CitizensPerRank = {10, 20, 35, 55, 85}
+--CitizensPerRank = {1, 1, 1, 1, 1}
+CitizensPerRank = {10, 20, 35, 55, 85}
 
 --The radius in meters of the city at each city rank. (Outpost, Village, Township, City, Metropolis)
 RadiusPerRank = {150, 200, 300, 400, 450}
