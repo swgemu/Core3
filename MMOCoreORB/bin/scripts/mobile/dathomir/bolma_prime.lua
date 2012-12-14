e2f1e71a1b2c7eae41f24c7ab9257ecb998e4a7a
loshult <loshult@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2012-12-14 05:22:05 +0000

    (unstable) [added] patch by veckon: added bolma prime and fixed a few loot includes.
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@6171 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOCoreORB/bin/scripts/mobile/dathomir/bolma_prime.lua
@@ -0,0 +1,39 @@
+bolma_male = Creature:new {
+	objectName = "@mob/creature_names:bolma_prime",
+	socialGroup = "bolma",
+	pvpFaction = "",
+	faction = "",
+	level = 37,
+	chanceHit = 0.41,
+	damageMin = 325,
+	damageMax = 360,
+	baseXp = 3642,
+	baseHAM = 8900,
+	baseHAMmax = 10900,
+	armor = 0,
+	resists = {45,45,0,0,0,0,0,0,-1},
+	meatType = "meat_wild",
+	meatAmount = 500,
+	hideType = "hide_leathery",
+	hideAmount = 550,
+	boneType = "bone_mammal",
+	boneAmount = 500,
+	milk = 0,
+	tamingChance = 0.05,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + HERD,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/bolma_hue.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"intimidationattack","intimidationChance=50"},
+		{"posturedownattack","postureDownChance=50"}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(bolma_prime, "bolma_prime")
