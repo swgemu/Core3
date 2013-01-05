af14e86051fc76f8178dac5150317532384a2691
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2013-01-05 18:15:18 +0000

    (unstable) [changed] Admins can not add base defenses at any time
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@6262 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
@@ -1764,16 +1764,16 @@ void GCWManagerImplementation::sendSelectDeedToDonate(BuildingObject* building, 
 	if(creature ==NULL || baseData == NULL)
 			return;
 
-
-	if(baseData->isVulnerable()) {
-		creature->sendSystemMessage("@hq:under_attack"); // You cannot add defenses while the HQ is under attack.
-		return;
-	}
-
 	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
 
 	if(ghost == NULL)
 		return;
+
+	if(baseData->isVulnerable() && !ghost->isPrivileged() ) {
+		creature->sendSystemMessage("@hq:under_attack"); // You cannot add defenses while the HQ is under attack.
+		return;
+	}
+
 
 	if(ghost->hasSuiBoxWindowType(SuiWindowType::HQ_TERMINAL))
 		ghost->closeSuiWindowType(SuiWindowType::HQ_TERMINAL);
