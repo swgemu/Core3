b4e710c788e8bd274ce1362c193e31e78d8b4679
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2013-01-12 18:23:18 +0000

    (unstable) [fixed] Missing file from commit 6270 - prevented NPC from spawning
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@6281 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/src/server/zone/objects/structure/components/StructureZoneComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/structure/components/StructureZoneComponent.cpp
@@ -11,10 +11,15 @@
 #include "server/zone/managers/planet/PlanetManager.h"
 #include "server/zone/objects/area/ActiveArea.h"
 #include "server/zone/templates/tangible/SharedStructureObjectTemplate.h"
-
+#include "server/zone/objects/building/BuildingObject.h"
 void StructureZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* zone) {
 	ZoneComponent::notifyInsertToZone(sceneObject, zone);
 
+	if(sceneObject->isGCWBase()){
+		ManagedReference<BuildingObject*> building = cast<BuildingObject*>(sceneObject);
+		if(building->hasChildCreatures())
+			building->spawnChildCreatures();
+	}
 }
 
 void StructureZoneComponent::notifyRemoveFromZone(SceneObject* sceneObject) {
