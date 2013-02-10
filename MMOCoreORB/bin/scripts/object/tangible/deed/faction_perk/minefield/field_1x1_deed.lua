0566a7a6a3d171bfeb91c9bc232915448579763b
elpete <elpete@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2013-02-10 02:21:28 +0000

    (unstable) [removed] reverted changes
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/branches/unstable@6461 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/bin/scripts/object/tangible/deed/faction_perk/minefield/field_1x1_deed.lua
+++ MMOCoreORB/bin/scripts/object/tangible/deed/faction_perk/minefield/field_1x1_deed.lua
@@ -43,7 +43,7 @@
 
 object_tangible_deed_faction_perk_minefield_field_1x1_deed = object_tangible_deed_faction_perk_minefield_shared_field_1x1_deed:new {
 	templateType = STRUCTUREDEED,
-	placeStructureComponent = "PlaceStructureComponent",
+	--placeStructureComponent = "PlaceStructureComponent",
 	gameObjectType = 8388609,
 	generatedObjectTemplate="object/installation/faction_perk/minefield/field_1x1.iff",
 
--- MMOCoreORB/bin/scripts/object/tangible/lair/base/objective_banner_rebel.lua
+++ MMOCoreORB/bin/scripts/object/tangible/lair/base/objective_banner_rebel.lua
@@ -42,12 +42,7 @@
 
 
 object_tangible_lair_base_objective_banner_rebel = object_tangible_lair_base_shared_objective_banner_rebel:new {
-	--objectMenuComponent = {"cpp", "LairMenuComponent"},
-	optionsBitmask = 128,
-	pvpBitmask = ENEMY + OVERT + AGGRESSIVE,
-	faction = "rebel",
-	pvpFaction = "rebel",
-	buildingsVeryHard = "true",
+	objectMenuComponent = {"cpp", "LairMenuComponent"},
 }
 
 ObjectTemplates:addTemplate(object_tangible_lair_base_objective_banner_rebel, "object/tangible/lair/base/objective_banner_rebel.iff")
