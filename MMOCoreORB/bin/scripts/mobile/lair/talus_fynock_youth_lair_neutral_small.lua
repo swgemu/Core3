6f0ed41a7802243166171c38b7a6f1156361da50
Bert <tbarker22@gmail.com>
2013-03-18 23:56:29 -0700

    [FIXED] Fynock Youth Lair mob typo
    Change-Id: Ic510d7fd4b77650613e5adb57dd027117fed1e2d

--- MMOCoreORB/bin/scripts/mobile/lair/talus_fynock_youth_lair_neutral_small.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/talus_fynock_youth_lair_neutral_small.lua
@@ -1,5 +1,5 @@
 talus_fynock_youth_lair_neutral_small = Lair:new {
-	mobiles = {{"fynock_youth", 0}},
+	mobiles = {{"fearful_fynock_youth", 0}},
 	spawnLimit = 15,
 	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_thicket_small.iff","object/tangible/lair/base/poi_all_lair_nest_small_fog_green.iff"},
 	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_thicket_small.iff","object/tangible/lair/base/poi_all_lair_nest_small_fog_green.iff"},
