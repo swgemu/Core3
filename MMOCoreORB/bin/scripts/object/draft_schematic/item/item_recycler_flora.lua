787f038f193feb688106bc64652fd65ae88528b1
Ivojedi <bwardjivo@gmail.com>
2013-03-20 17:16:50 -0700

    [Added] Functionality to resource recycle tools
    [Added] Schematics for resource recyclers, agitator motors, and tumble
    blenders
    
    Change-Id: I87593ddd9f9a35556d7c50b751a02206a4d64f42

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/item_recycler_flora.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_recycler_flora.lua
@@ -65,7 +65,7 @@ object_draft_schematic_item_item_recycler_flora = object_draft_schematic_item_sh
    ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
    ingredientTitleNames = {"agitator_motor", "feed_tubes", "chemical_tubing", "processor_attachments", "spinning_blades", "end_result_bowl"},
    ingredientSlotType = {1, 1, 0, 1, 2, 0},
-   resourceTypes = {"object/tangible/loot/loot_schematic/shared_agitator_motor_schematic.iff", "object/tangible/loot/simple_kit/shared_feed_tubes.iff", "fiberplast", "object/tangible/loot/simple_kit/shared_processor_attachments.iff", "object/tangible/loot/simple_kit/shared_spinning_blade.iff", "metal"},
+   resourceTypes = {"object/tangible/loot/simple_kit/shared_motor_small_red.iff", "object/tangible/loot/simple_kit/shared_feed_tubes.iff", "fiberplast", "object/tangible/loot/simple_kit/shared_processor_attachments.iff", "object/tangible/loot/simple_kit/shared_spinning_blade.iff", "metal"},
    resourceQuantities = {1, 1, 50, 1, 4, 50},
    contribution = {100, 100, 100, 100, 100, 100},
 
--- MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/metal_recycler_schematic.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/loot_schematic/metal_recycler_schematic.lua
@@ -42,7 +42,12 @@
 
 
 object_tangible_loot_loot_schematic_metal_recycler_schematic = object_tangible_loot_loot_schematic_shared_metal_recycler_schematic:new {
-
+	templateType = LOOTSCHEMATIC,
+	objectMenuComponent = {"cpp", "LootSchematicMenuComponent"},
+	attributeListComponent = "LootSchematicAttributeListComponent",
+	requiredSkill = "crafting_artisan_engineering_01",
+	targetDraftSchematic = "object/draft_schematic/item/item_recycler_metal.iff",
+	targetUseCount = 1,
 }
 
 ObjectTemplates:addTemplate(object_tangible_loot_loot_schematic_metal_recycler_schematic, "object/tangible/loot/loot_schematic/metal_recycler_schematic.iff")
