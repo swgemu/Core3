6695b1de646f21a06a6e54f76dcd64b3c125d109
marsorbust <ajensenmtdew76@gmail.com>
2013-03-22 04:38:19 -0700

    [added]composit armor as loot and created a loot group for it.
    Change-Id: I04bc0bd0b7d5d276813752f7621995dd991d7ca6

---
+++ MMOCoreORB/bin/scripts/loot/items/composit_armor_bracer_right.lua
@@ -0,0 +1,22 @@
+composit_armor_bracer_r = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "a Composit Armor Right Bracer",
+	directObjectTemplate = "object/tangible/wearables/armor/composite/armor_composite_bracer_r.iff",
+	craftingValues = {
+		{"armor_rating",1,1,0},
+		{"electricityeffectiveness",10,55,10},
+		{"armor_effectiveness",8,39,10},
+		{"armor_integrity",27500, 75000,0},
+		{"armor_health_encumbrance",25,15,0},
+		{"armor_action_encumbrance",22,13,0},
+		{"armor_mind_encumbrance",22,13,0},
+	},
+	skillMods = {
+
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("composit_armor_bracer_r", composit_armor_bracer_r)
