dbabf8ed043af579c033f454557c6f782b1700e8
Valkyra <lyhtmyst@gmail.com>
2013-03-31 08:52:44 -0600

    [Added] Imperial Themepark - Mission NPC's have world waypoint added. [Fixed] Emperor's Mission (Escort) - Better model for handmaiden [Fixed] Veers and Thrawn's missiontypes that resulted previously in a conversation string error. [Fixed] Added permission needed in elevator to access Emperor from Veers.
    Change-Id: I3cf1ee63c212a45fc2bf3d78c42e7aa440b8396a

---
+++ MMOCoreORB/bin/scripts/mobile/naboo/theme_park_imperial_escort_droid.lua
@@ -0,0 +1,36 @@
+theme_park_imperial_escort_droid = Creature:new {
+	objectName = "@npc_spawner_n:j1_po",
+	socialGroup = "",
+	pvpFaction = "",
+	faction = "",
+	level = 30,
+	chanceHit = 0.390000,
+	damageMin = 290,
+	damageMax = 300,
+	baseXp = 2914,
+	baseHAM = 8400,
+	baseHAMmax = 10200,
+	armor = 0,
+	resists = {0,45,0,-1,40,-1,40,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0.000000,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/3po_protocol_droid_silver.iff"},
+	lootGroups = {},
+	weapons = {},
+	attacks = {},
+	conversationTemplate = "theme_park_imperial_mission_target_convotemplate",
+	optionsBitmask = 264
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_imperial_escort_droid, "theme_park_imperial_escort_droid")
---
+++ MMOCoreORB/bin/scripts/mobile/naboo/theme_park_imperial_seller.lua
@@ -0,0 +1,38 @@
+theme_park_imperial_seller = Creature:new {
+	objectName = "",
+	customName = "escort",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 4,
+	chanceHit = 0.24,
+	damageMin = 40,
+	damageMax = 45,
+	baseXp = 62,
+	baseHAM = 113,
+	baseHAMmax = 138,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_commoner_naboo_bothan_female_02.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_imperial_seller, "theme_park_imperial_seller")
