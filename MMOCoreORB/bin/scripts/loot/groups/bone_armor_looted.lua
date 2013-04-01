aa4c8fdb6cee8e3cf4c127ee3ca187ba0bcb4bab
marsorbust <ajensenmtdew76@gmail.com>
2013-04-01 10:43:21 -0700

    [added] bone armor as loot and created a loot group for it [added] chitin armor as loot and created a loot group for it [added] composit armor as loot and created a loot group for it [added] mabari armor as loot and created a loot group for it [added] padded armor as loot and created a loot group for it [added] tantel armor as loot and created a loot group for it [added] ubese armor as loot and created a loot group for it
    Change-Id: I140aecf1b7bb2844db6a58f6da1ab320503fdad4

---
+++ MMOCoreORB/bin/scripts/loot/groups/bone_armor_looted.lua
@@ -0,0 +1,18 @@
+bone_armor_looted = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "bone_armor_helmet", weight = 1000000},
+		{itemTemplate = "bone_armor_chest_plate", weight = 1000000},
+		{itemTemplate = "bone_armor_leggings", weight = 1000000},
+		{itemTemplate = "bone_armor_boots", weight = 1100000},
+		{itemTemplate = "bone_armor_gloves", weight = 1100000},
+		{itemTemplate = "bone_armor_bracer_l", weight = 1200000},
+		{itemTemplate = "bone_armor_bracer_r", weight = 1200000},
+		{itemTemplate = "bone_armor_bicep_l", weight = 1200000},
+		{itemTemplate = "bone_armor_bicep_r", weight = 1200000},
+	}
+}
+
+addLootGroupTemplate("bone_armor_looted", bone_armor_looted)
