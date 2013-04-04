455f7eb2a18b7bc38b1124448f38ed11c694c557
marsorbust <ajensenmtdew76@gmail.com>
2013-04-03 17:23:30 -0700

    [added] a few lairs to dantooine world spawner
    Change-Id: Ie6eb0f1c7d3554425acb1fc87aeabd4410498b36

--- MMOCoreORB/bin/scripts/mobile/lair/dantooine_thune_herd_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/dantooine_thune_herd_neutral_none.lua
@@ -1,11 +1,11 @@
 dantooine_thune_herd_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {{"hostile_thune_mother", 0},{"thune_herd_leader", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_fog_green.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_fog_green.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rocks_large_fog_green.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_fog_green.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_fog_green.iff"}
 }
 
 addLairTemplate("dantooine_thune_herd_neutral_none", dantooine_thune_herd_neutral_none)
