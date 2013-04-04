7ea5ad7ce55e8d394d2ee1daef68d27a1ce1eb9c
marsorbust <ajensenmtdew76@gmail.com>
2013-04-04 11:21:59 -0700

    [added] bone armor as loot and created a loot group for it [added] chitin armor as loot and created a loot group for it [added] composit armor as loot and created a loot group for it [added] mabari armor as loot and created a loot group for it [added] padded armor as loot and created a loot group for it [added] tantel armor as loot and created a loot group for it [added] ubese armor as loot and created a loot group for it
    Change-Id: I140aecf1b7bb2844db6a58f6da1ab320503fdad4

---
+++ MMOCoreORB/bin/scripts/loot/items/chitin_armor_bracer_right.lua
@@ -0,0 +1,24 @@
+chitin_armor_bracer_r = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Chitin Armor Right Bracer",
+	directObjectTemplate = "object/tangible/wearables/armor/chitin/armor_chitin_s01_bracer_r.iff",
+	craftingValues = {
+		{"armor_rating",1,1,0},
+		{"kineticeffectiveness",10,40,10},
+		{"armor_effectiveness",4,29,10},
+		{"armor_integrity",15000, 25000,0},
+		{"armor_health_encumbrance",13,8,0},
+		{"armor_action_encumbrance",13,8,0},
+		{"armor_mind_encumbrance",16,9,0},
+	},
+	skillMods = {
+
+	},
+	customizationStringNames = {"/private/index_color_0", "/private/index_color_1"},
+	customizationValues = {
+			{0, 1},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+}
+
+addLootItemTemplate("chitin_armor_bracer_r", chitin_armor_bracer_r)
---
+++ MMOCoreORB/bin/scripts/loot/items/ubese_armor_gloves.lua
@@ -0,0 +1,23 @@
+ubese_armor_gloves = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Ubese Armor Gloves",
+	directObjectTemplate = "object/tangible/wearables/armor/ubese/armor_ubese_gloves.iff",
+	craftingValues = {
+		{"armor_rating",1,1,0},
+		{"kineticeffectiveness",10,40,10},
+		{"armor_effectiveness",4,29,10},
+		{"armor_integrity",18750, 31250,0},
+		{"armor_health_encumbrance",11,7,0},
+		{"armor_action_encumbrance",28,17,0},
+		{"armor_mind_encumbrance",19,11,0},
+	},
+	skillMods = {
+
+	},
+	--no color pallets for customizing for ubese armor gloves
+	customizationStringNames = {},
+	customizationValues = {},
+}
+
+addLootItemTemplate("ubese_armor_gloves", ubese_armor_gloves)
