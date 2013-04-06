1f6d5bffe48bf184f5dd3f71e0cc007e715d763e
marsorbust <ajensenmtdew76@gmail.com>
2013-04-06 14:16:16 -0700

    [added] a few lairs to the lok world spawner [fixed] dantooine_huurton_matron_lair_neutral_none spawning above its level range
    Change-Id: I9294e5362267b0ba9c6a5d54c600347c1d23621f

--- MMOCoreORB/bin/scripts/mobile/lair/lok_perlek_lair_neutral_medium.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/lok_perlek_lair_neutral_medium.lua
@@ -1,11 +1,11 @@
 lok_perlek_lair_neutral_medium = Lair:new {
-	mobiles = {},
+	mobiles = {{"perlek_ravager", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
 }
 
 addLairTemplate("lok_perlek_lair_neutral_medium", lok_perlek_lair_neutral_medium)
--- MMOCoreORB/bin/scripts/mobile/lair/lok_perlek_pack_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/lok_perlek_pack_neutral_none.lua
@@ -1,11 +1,11 @@
 lok_perlek_pack_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {{"perlek", 0},{"perlek_scavenger", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_nest_small_fog_mustard.iff"},
 }
 
 addLairTemplate("lok_perlek_pack_neutral_none", lok_perlek_pack_neutral_none)
