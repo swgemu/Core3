69540ac4d9e817f54b1aa697ff4e0a9e2c88416d
becker7 <randybeckemeyer@gmail.com>
2013-04-06 21:56:11 -0700

    [Changed] Bounty Hunter Armor drops with random palette
    Change-Id: Ia552a054d507401871f55cd68f3975e5cdd7beca

--- MMOCoreORB/bin/scripts/loot/items/bh_armor_bicep_r.lua
+++ MMOCoreORB/bin/scripts/loot/items/bh_armor_bicep_r.lua
@@ -6,20 +6,23 @@ bh_armor_bicep_r = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_bicep_r.iff",
 	craftingValues = {
-	         {"armor_rating",1,1,0},
-	         {"armor_effectiveness",10,15,10},
-             {"kineticeffectiveness",10,15,10},
-	         {"energyeffectiveness",10,15,10},
-	         {"heateffectiveness",10,15,10},
-	         {"coldeffectiveness",10,15,10},
-	         {"acideffectiveness",10,15,10},
-	         {"armor_integrity",20000,70000,0},
-	         {"armor_health_encumbrance",25,15,0},
-	         {"armor_action_encumbrance",22,13,0},
-	         {"armor_mind_encumbrance",25,15,0},
+		{"armor_rating",1,1,0},
+		{"armor_effectiveness",10,15,10},
+		{"kineticeffectiveness",10,15,10},
+		{"energyeffectiveness",10,15,10},
+		{"heateffectiveness",10,15,10},
+		{"coldeffectiveness",10,15,10},
+		{"acideffectiveness",10,15,10},
+		{"armor_integrity",20000,70000,0},
+		{"armor_health_encumbrance",25,15,0},
+		{"armor_action_encumbrance",22,13,0},
+		{"armor_mind_encumbrance",25,15,0},
 	},
-	customizationStringNames = {},
-	customizationValues = {}
+	customizationStringNames = {"/private/index_color_1","/private/index_color_2"},
+	customizationValues = {{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143},{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95}},
+	skillMods = {
+
+	}
 }
 
 addLootItemTemplate("bh_armor_bicep_r", bh_armor_bicep_r)
