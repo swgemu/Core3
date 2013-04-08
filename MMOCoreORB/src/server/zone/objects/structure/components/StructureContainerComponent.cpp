a60be15eb0421613cd963e2670b26f75da24708c
Pete <elpete@swgemu.com>
2013-04-07 22:17:16 -0500

    [removed] removed invalid check from structurecontainercomponent
    Change-Id: Ie4d2357cedec2792baea50e8b6fc090fbd1a5e6b

--- MMOCoreORB/src/server/zone/objects/structure/components/StructureContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/structure/components/StructureContainerComponent.cpp
@@ -25,8 +25,6 @@ bool StructureContainerComponent::checkContainerPermission(SceneObject* sceneObj
 		}
 	}
 
-	if(permission != ContainerPermissions::OPEN && building->containsChildObject(sceneObject))
-		return false;
 
 	if (building->isOwnerOf(creature) || building->isOnAdminList(creature))
 		return true;
