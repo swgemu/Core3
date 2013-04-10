835ba6ca31d4fcb955b4cb7c98a8541041433ebf
marsorbust <ajensenmtdew76@gmail.com>
2013-04-10 12:16:02 -0700

    [added] a few lairs to the lok world spawner [fixed] dantooine_huurton_matron_lair_neutral_none spawning above its level range
    Change-Id: I9294e5362267b0ba9c6a5d54c600347c1d23621f

--- MMOCoreORB/bin/scripts/mobile/lair/lok_kimogilla_pack2_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/lok_kimogilla_pack2_neutral_none.lua
@@ -1,11 +1,11 @@
 lok_kimogilla_pack2_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {{"kimogila_hatchling", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/kimogila/lair_kimogila_grassland.iff"},
+	buildingsEasy = {"object/tangible/lair/kimogila/lair_kimogila_grassland.iff"},
+	buildingsMedium = {"object/tangible/lair/kimogila/lair_kimogila_grassland.iff"},
+	buildingsHard = {"object/tangible/lair/kimogila/lair_kimogila_grassland.iff"},
+	buildingsVeryHard = {"object/tangible/lair/kimogila/lair_kimogila_grassland.iff"},
 }
 
 addLairTemplate("lok_kimogilla_pack2_neutral_none", lok_kimogilla_pack2_neutral_none)
--- MMOCoreORB/bin/scripts/mobile/lair/lok_langlatch_pack_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/lok_langlatch_pack_neutral_none.lua
@@ -1,11 +1,11 @@
 lok_langlatch_pack_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {{"langlatch_destroyer", 0},{"sharptooth_langlatch", 0},{"langlatch_marauder", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/langlatch/lair_langlatch.iff"},
+	buildingsEasy = {"object/tangible/lair/langlatch/lair_langlatch.iff"},
+	buildingsMedium = {"object/tangible/lair/langlatch/lair_langlatch.iff"},
+	buildingsHard = {"object/tangible/lair/langlatch/lair_langlatch.iff"},
+	buildingsVeryHard = {"object/tangible/lair/langlatch/lair_langlatch.iff"}
 }
 
 addLairTemplate("lok_langlatch_pack_neutral_none", lok_langlatch_pack_neutral_none)
