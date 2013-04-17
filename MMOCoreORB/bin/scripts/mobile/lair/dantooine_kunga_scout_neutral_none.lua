81e8cff590cf1ce875699880ea45c4ff75764f45
marsorbust <ajensenmtdew76@gmail.com>
2013-04-17 03:40:38 -0700

    [Fixed] level ranges on Mokk, Kunga and Janta lairs [Added] Mokk scout, Kunga scout and Janta scout lairs to the mission terminals
    Change-Id: Ia34710945a67364e0ec81e7c1b9d3c6babd6ec43

--- MMOCoreORB/bin/scripts/mobile/lair/dantooine_kunga_scout_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/dantooine_kunga_scout_neutral_none.lua
@@ -1,11 +1,12 @@
 dantooine_kunga_scout_neutral_none = Lair:new {
-	mobiles = {},
-	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	mobiles = {{"kunga_scout", 0}},
+	spawnLimit = 8,
+	buildingsVeryEasy = {"object/tangible/lair/base/objective_dantari_fire_pit.iff"},
+	buildingsEasy = {"object/tangible/lair/base/objective_dantari_fire_pit.iff"},
+	buildingsMedium = {"object/tangible/lair/base/objective_dantari_fire_pit.iff"},
+	buildingsHard = {"object/tangible/lair/base/objective_dantari_fire_pit.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/objective_dantari_fire_pit.iff"},
+	lairType = "npc"
 }
 
 addLairTemplate("dantooine_kunga_scout_neutral_none", dantooine_kunga_scout_neutral_none)
