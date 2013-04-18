35f70e5da3ea62f9aa78d2a030ca90fa1c9d090b
marsorbust <ajensenmtdew76@gmail.com>
2013-04-17 20:58:04 -0700

    [Added] newbie lairspawnarea around Narmle and Restuss
    Change-Id: Ia5788c2a1b8abfafbf0b887e4439bd284b23cc2a

--- MMOCoreORB/bin/scripts/mobile/lair/rori_capper_spineflap_hive_neutral_large.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/rori_capper_spineflap_hive_neutral_large.lua
@@ -1,11 +1,11 @@
 rori_capper_spineflap_hive_neutral_large = Lair:new {
-	mobiles = {},
+	mobiles = {{"capper_spineflap_drone", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/capper_spineflap/lair_capper_spineflap_forest.iff"},
+	buildingsEasy = {"object/tangible/lair/capper_spineflap/lair_capper_spineflap_forest.iff"},
+	buildingsMedium = {"object/tangible/lair/capper_spineflap/lair_capper_spineflap_forest.iff"},
+	buildingsHard = {"object/tangible/lair/capper_spineflap/lair_capper_spineflap_forest.iff"},
+	buildingsVeryHard = {"object/tangible/lair/capper_spineflap/lair_capper_spineflap_forest.iff"},
 }
 
 addLairTemplate("rori_capper_spineflap_hive_neutral_large", rori_capper_spineflap_hive_neutral_large)
