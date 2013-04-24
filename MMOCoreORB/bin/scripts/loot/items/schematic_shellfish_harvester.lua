e1749d6079971b2f15f21858c66f80c27dc839e3
Ivojedi <ivojedi@swgemu.com>
2013-04-24 12:57:50 -0700

    [Removed] duplicate, unused loot file [Renamed] shellfish harvester schematic
    Change-Id: I4b95ac8532940070ca19c1a8d8e368fc7e06d6cc

--- MMOCoreORB/bin/scripts/loot/items/schematic_shellfish_harvester.lua
+++ MMOCoreORB/bin/scripts/loot/items/schematic_shellfish_harvester.lua
@@ -1,7 +1,7 @@
 schematic_shellfish_harvester = {
 	minimumLevel = 0,
 	maximumLevel = -1,
-	customObjectName = "",
+	customObjectName = "Schematic: Shellfish Harvesting Tool",
 	directObjectTemplate = "object/tangible/loot/quest/shellfish_harvester_schematic.iff",
 	craftingValues = {
 	},
