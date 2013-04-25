91764f98a3b0ea17d85b2897f6dfdfa50957423f
marsorbust <ajensenmtdew76@gmail.com>
2013-04-24 23:47:05 -0700

    [Added] Loot to dantari npc's on dantooine
    Change-Id: I5c2ac3971a6385f1a8385d8fb31eedbd2def3d68

--- MMOCoreORB/bin/scripts/mobile/dantooine/dantari_battlelord.lua
+++ MMOCoreORB/bin/scripts/mobile/dantooine/dantari_battlelord.lua
@@ -29,7 +29,17 @@ dantari_battlelord = Creature:new {
 	templates = {
 		"object/mobile/dantari_male.iff",
 		"object/mobile/dantari_female.iff"},
-	lootGroups = {},
+	lootGroups = {
+	    {
+			groups = {
+				{group = "junk", chance = 6000000},
+				{group = "loot_kit_parts", chance = 3000000},
+				{group = "armor_attachments", chance = 500000},
+				{group = "clothing_attachments", chance = 500000}
+			},
+			lootChance = 3000000
+		}	
+	},
 	weapons = {"primitive_weapons"},
 	conversationTemplate = "",
 	attacks = merge(brawlermaster,marksmanmaster)
