68fde66cb434bda810076ac128d46ab6f53c979d
marsorbust <ajensenmtdew76@gmail.com>
2013-04-28 00:30:56 -0700

    [Fixed] mantis 3970 giant peko peko lair
    Change-Id: Idedba8a4eb00bcd97c4bb5851fabef528479a9fd

--- MMOCoreORB/bin/scripts/mobile/lair/naboo_peko_peko_giant_pair_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/naboo_peko_peko_giant_pair_neutral_none.lua
@@ -1,11 +1,11 @@
 naboo_peko_peko_giant_pair_neutral_none = Lair:new {
 	mobiles = {{"giant_peko_peko", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {"object/tangible/lair/peko_peko/lair_peko_peko_grassland.iff"},
-	buildingsEasy = {"object/tangible/lair/peko_peko/lair_peko_peko_grassland.iff"},
-	buildingsMedium = {"object/tangible/lair/peko_peko/lair_peko_peko_grassland.iff"},
-	buildingsHard = {"object/tangible/lair/peko_peko/lair_peko_peko_grassland.iff"},
-	buildingsVeryHard = {"object/tangible/lair/peko_peko/lair_peko_peko_grassland.iff"},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_red.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_red.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_red.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_red.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_bones_large_evil_fire_red.iff"}
 }
 
 addLairTemplate("naboo_peko_peko_giant_pair_neutral_none", naboo_peko_peko_giant_pair_neutral_none)
