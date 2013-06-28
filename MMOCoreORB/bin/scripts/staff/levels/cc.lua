9c521fedace9b6e02595d29d09bdf38adf83e2af
Naikoi <Naikoi@swgemu.com>
2013-06-28 00:43:18 +0000

    Added 2 new admin levels for CT's and CCC's for TC:Nova
    Change-Id: I22cc91af8bb78f479b02fdc9edcadf7cb60f5a17

---
+++ MMOCoreORB/bin/scripts/staff/levels/cc.lua
@@ -0,0 +1,19 @@
+cc = {
+	level = 8,
+	name = "cc",
+	tag = "SWGEmu-CC",
+	skills = {
+		"admin_base",
+		"admin_debug_01",
+		"admin_general_admin_01",
+		"admin_general_admin_02",
+		"admin_general_admin_03",
+	        "admin_quest_management_01",
+		"admin_server_admin_01",
+		"admin_skill_management_01",
+		"admin_spawn_management_01",
+		"admin_veteran_rewards_01"
+	}
+}
+
+addLevel(cc)
