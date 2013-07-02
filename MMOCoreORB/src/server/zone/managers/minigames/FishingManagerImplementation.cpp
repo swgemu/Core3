12e691cb5819d1f186e8084d28c8a68224f66f29
Victor Popovici <victorpopovici@gmail.com>
2013-07-03 00:55:06 +0200

    [fixed] out of bounds exception

--- MMOCoreORB/src/server/zone/managers/minigames/FishingManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/minigames/FishingManagerImplementation.cpp
@@ -546,13 +546,18 @@ uint32 FishingManagerImplementation::createWindow(CreatureObject* player, uint32
 		freshness = 0;
 	}
 
+	int nextAct = getNextAction(player);
+
+	if (nextAct >= action.size())
+		nextAct = 0;
+
 	String prompt = "STATUS : " + state.get(getFishingState(player)) + "\nBAIT STATUS : "
 			+ baitStatus.get(freshness) + "\nLINE RANGE : "
 			+ String::valueOf( ceil( player->getDistanceTo(marker) * 100 ) / 100)
 			+ "\nFISH DENSITY : "+ property.get(den)
 			+ "\nVEGETATION : " + property.get(veg)
 			+ "\nNEXT ACTION : "
-			+ action.get( getNextAction(player) );
+			+ action.get( nextAct );
 
 	// create new window
 	ManagedReference<SuiListBox*> box = new SuiListBox(player, SuiWindowType::FISHING, 0);
