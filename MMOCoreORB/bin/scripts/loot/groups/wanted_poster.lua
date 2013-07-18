eb084bda77b6701022bd572c3772d1c5e0c52930
marsorbust <ajensenmtdew76@gmail.com>
2013-07-18 03:46:25 -0700

    [Added] wanted posters to stormtroopers loot
    Change-Id: I8e75ba912bc0ffa5f19299c02ab8380550a42e54

---
+++ MMOCoreORB/bin/scripts/loot/groups/wanted_poster.lua
@@ -0,0 +1,14 @@
+wanted_poster = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "painting_han_wanted", weight = 2500000},
+		{itemTemplate = "painting_leia_wanted", weight = 2500000},
+		{itemTemplate = "painting_luke_wanted", weight = 2500000},
+		{itemTemplate = "painting_trandoshan_wanted", weight = 2500000},
+
+	}
+}
+
+addLootGroupTemplate("wanted_poster", wanted_poster)
