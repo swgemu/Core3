164bfcb66aa351648a6134f7faadbafed4a4195b
marsorbust <ajensenmtdew76@gmail.com>
2013-07-19 01:56:50 -0700

    [Added] wanted posters to stormtroopers
    Change-Id: I8e75ba912bc0ffa5f19299c02ab8380550a42e54

---
+++ MMOCoreORB/bin/scripts/loot/items/painting_trandoshan_wanted.lua
@@ -0,0 +1,12 @@
+painting_trandoshan_wanted = {
+	minimumLevel = 0,
+	maximumLevel = 0,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/painting/painting_trandoshan_wanted.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("painting_trandoshan_wanted", painting_trandoshan_wanted)
--- MMOCoreORB/bin/scripts/mobile/faction/imperial/stormtrooper_captain.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/imperial/stormtrooper_captain.lua
@@ -31,13 +31,14 @@ stormtrooper_captain = Creature:new {
 		{
 			groups = {
 				{group = "color_crystals", chance = 100000},
-				{group = "junk", chance = 7650000},
+				{group = "junk", chance = 6600000},
 				{group = "rifles", chance = 550000},
 				{group = "pistols", chance = 550000},
-                {group = "melee_weapons", chance = 550000},
-                {group = "carbines", chance = 550000},
-				{group = "clothing_attachments", chance = 25000},
-				{group = "armor_attachments", chance = 25000}
+				{group = "melee_weapons", chance = 550000},
+				{group = "carbines", chance = 550000},
+				{group = "wanted_poster", chance = 600000},
+				{group = "clothing_attachments", chance = 250000},
+				{group = "armor_attachments", chance = 250000}
 			},
 			lootChance = 2800000
 		}						
