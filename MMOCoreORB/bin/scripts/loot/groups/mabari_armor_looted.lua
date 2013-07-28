7f815f223a6a4af54e45ea553ae25e3417a4310f
marsorbust <ajensenmtdew76@gmail.com>
2013-07-28 13:44:00 -0700

    [added] armor to rancors loot tables [added] armor as loot and created loot groups for it
    Change-Id: I3cff33979061a8537a90f4467872969a41d18887

---
+++ MMOCoreORB/bin/scripts/loot/groups/mabari_armor_looted.lua
@@ -0,0 +1,15 @@
+mabari_armor_looted = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "mabari_armor_helmet", weight = 1670000},
+		{itemTemplate = "mabari_armor_chest_plate", weight = 1660000},
+		{itemTemplate = "mabari_armor_pants", weight = 1660000},
+		{itemTemplate = "mabari_armor_boots", weight = 1670000},
+		{itemTemplate = "mabari_armor_gloves", weight = 1670000},
+		{itemTemplate = "mabari_armor_belt", weight = 1670000},
+	}
+}
+
+addLootGroupTemplate("mabari_armor_looted", mabari_armor_looted)
---
+++ MMOCoreORB/bin/scripts/loot/items/armor/ubese_armor_bandolier.lua
@@ -0,0 +1,19 @@
+ubese_armor_bandolier = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Ubese Armor Bandolier",
+	directObjectTemplate = "object/tangible/wearables/armor/ubese/armor_ubese_bandolier.iff",
+	craftingValues = {
+
+		{"armor_integrity",18750, 31250,0},
+
+	},
+	skillMods = {
+
+	},
+	--no color pallets for customizing for ubese armor bandolier
+	customizationStringNames = {},
+	customizationValues = {},
+}
+
+addLootItemTemplate("ubese_armor_bandolier", ubese_armor_bandolier)
