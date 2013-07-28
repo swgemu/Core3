987560c5709ad0f8354f5afe8d9dafab31987405
marsorbust <ajensenmtdew76@gmail.com>
2013-07-28 13:13:53 -0700

    [added] Armor to rancors loot tables [added] armor as loot and created loot groups for it
    Change-Id: I140aecf1b7bb2844db6a58f6da1ab320503fdad4

---
+++ MMOCoreORB/bin/scripts/loot/groups/chitin_armor_looted.lua
@@ -0,0 +1,18 @@
+chitin_armor_looted = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "chitin_armor_helmet", weight = 1000000},
+		{itemTemplate = "chitin_armor_chest_plate", weight = 1000000},
+		{itemTemplate = "chitin_armor_leggings", weight = 1000000},
+		{itemTemplate = "chitin_armor_boots", weight = 1100000},
+		{itemTemplate = "chitin_armor_gloves", weight = 1100000},
+		{itemTemplate = "chitin_armor_bracer_l", weight = 1200000},
+		{itemTemplate = "chitin_armor_bracer_r", weight = 1200000},
+		{itemTemplate = "chitin_armor_bicep_l", weight = 1200000},
+		{itemTemplate = "chitin_armor_bicep_r", weight = 1200000},
+	}
+}
+
+addLootGroupTemplate("chitin_armor_looted", chitin_armor_looted)
---
+++ MMOCoreORB/bin/scripts/loot/items/armor/ubese_armor_boots.lua
@@ -0,0 +1,24 @@
+ubese_armor_boots = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Ubese Armor Boots",
+	directObjectTemplate = "object/tangible/wearables/armor/ubese/armor_ubese_boots.iff",
+	craftingValues = {
+		{"armor_rating",1,1,0},
+		{"kineticeffectiveness",10,40,10},
+		{"armor_effectiveness",4,29,10},
+		{"armor_integrity",18750, 31250,0},
+		{"armor_health_encumbrance",11,7,0},
+		{"armor_action_encumbrance",28,17,0},
+		{"armor_mind_encumbrance",19,11,0},
+	},
+	skillMods = {
+
+	},
+	customizationStringNames = {"/private/index_color_0", "/private/index_color_1"},
+	customizationValues = {
+			{0, 1},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47}},
+}
+
+addLootItemTemplate("ubese_armor_boots", ubese_armor_boots)
