38fa314d55fdc0281f6b64c871733fcf6454905b
marsorbust <ajensenmtdew76@gmail.com>
2013-07-28 13:44:00 -0700

    [added] armor to rancors loot tables [added] armor as loot and created loot groups for it
    Change-Id: I3cff33979061a8537a90f4467872969a41d18887

---
+++ MMOCoreORB/bin/scripts/loot/groups/padded_armor_looted.lua
@@ -0,0 +1,19 @@
+padded_armor_looted = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "padded_armor_helmet", weight = 1000000},
+		{itemTemplate = "padded_armor_chest_plate", weight = 1000000},
+		{itemTemplate = "padded_armor_leggings", weight = 1000000},
+		{itemTemplate = "padded_armor_boots", weight = 1000000},
+		{itemTemplate = "padded_armor_gloves", weight = 1000000},
+		{itemTemplate = "padded_armor_bracer_l", weight = 1000000},
+		{itemTemplate = "padded_armor_bracer_r", weight = 1000000},
+		{itemTemplate = "padded_armor_bicep_l", weight = 1000000},
+		{itemTemplate = "padded_armor_bicep_r", weight = 1000000},
+		{itemTemplate = "padded_armor_belt", weight = 1000000},
+	}
+}
+
+addLootGroupTemplate("padded_armor_looted", padded_armor_looted)
