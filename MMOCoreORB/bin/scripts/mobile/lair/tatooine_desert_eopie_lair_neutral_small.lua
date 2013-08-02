b07cba45976ccfca938d54605a9915f5a06c1821
Naikoi <Naikoi@swgemu.com>
2013-08-02 05:03:08 +0000

    [fixed] Desert Eopie lair's now display the correct lair.
    Change-Id: I3c9bb4161a0fd09f36857ecd7d2abcc9c8da47f5

--- MMOCoreORB/bin/scripts/mobile/lair/tatooine_desert_eopie_lair_neutral_small.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/tatooine_desert_eopie_lair_neutral_small.lua
@@ -2,12 +2,13 @@
 tatooine_desert_eopie_lair_neutral_small = Lair:new {
 	mobiles = {{"desert_eopie", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_insecthill_small_fog_mustard.iff"},
-	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_insecthill_small_fog_mustard.iff"},
-	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_insecthill_small_fog_mustard.iff"},
-	buildingsHard = {"object/tangible/lair/base/poi_all_lair_insecthill_small_fog_mustard.iff"},
-	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_insecthill_small_fog_mustard.iff"}
+	buildingsVeryEasy = {"object/tangible/lair/eopie/lair_eopie.iff"},
+	buildingsEasy = {"object/tangible/lair/eopie/lair_eopie.iff"},
+	buildingsMedium = {"object/tangible/lair/eopie/lair_eopie.iff"},
+	buildingsHard = {"object/tangible/lair/eopie/lair_eopie.iff"},
+	buildingsVeryHard = {"object/tangible/lair/eopie/lair_eopie.iff"}
 }
 
 addLairTemplate("tatooine_desert_eopie_lair_neutral_small", tatooine_desert_eopie_lair_neutral_small)
+
 
