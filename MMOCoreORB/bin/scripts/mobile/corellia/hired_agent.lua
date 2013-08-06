8636cbcc9c130b3bd19b4131d1775f8899cf7643
papasmurf520 <s.michelson89@gmail.com>
2013-08-06 03:21:53 +0000

    [added] Diktat Quest
    Change-Id: I9c55a572515ba882828c54f60747052ead3182e2

---
+++ MMOCoreORB/bin/scripts/mobile/corellia/hired_agent.lua
@@ -0,0 +1,38 @@
+hired_agent = Creature:new {
+	objectName = "",
+	customName = "Qeko Lobikoba",
+	socialGroup = "corsec",
+	pvpFaction = "corsec",
+	faction = "corsec",
+	level = 17,
+	chanceHit = 0.32,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 1102,
+	baseHAM = 3500,
+	baseHAMmax = 4300,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 264,
+	diet = HERBIVORE,
+	templates = {
+		"object/mobile/dressed_corsec_pilot_human_male_01.iff"
+	},
+	lootGroups = { },
+	weapons = {},
+	conversationTemplate = "diktatTargetConvo",
+	attacks = { }
+}
+
+CreatureTemplates:addCreatureTemplate(hired_agent, "hired_agent")
