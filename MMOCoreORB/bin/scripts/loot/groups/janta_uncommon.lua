e761ff58cabd5d00b9152648f6b7d19d87945e6a
marsorbust <ajensenmtdew76@gmail.com>
2013-08-08 15:09:05 -0700

    [added]Janta knife to janta loot tables
    Change-Id: I2e03ccbffcac98db191550925fcbd734e367b5ff

---
+++ MMOCoreORB/bin/scripts/loot/groups/janta_uncommon.lua
@@ -0,0 +1,12 @@
+janta_uncommon = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "janta_blood", weight = 2500000},
+		{itemTemplate = "knife_janta", weight = 2500000},
+		{itemTemplate = "janta_hides", weight = 5000000}
+	}
+}
+
+addLootGroupTemplate("janta_uncommon", janta_uncommon)
