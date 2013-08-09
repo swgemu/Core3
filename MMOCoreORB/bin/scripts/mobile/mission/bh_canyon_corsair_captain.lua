ce128b8f9b045edeb201935d21434167d1ea23c7
marsorbust <ajensenmtdew76@gmail.com>
2013-08-08 19:38:56 -0700

    [added] mobile files for tier 2 nad 3 bounty targets in bin/scripts/mobile/mission folder to enable customization of bounty targets without changing the random or static spawns of the same type [added] 3 more bounty hunter targets to tier 3 missions [modified] loot for tier 2 and 3 bounty targets
    Change-Id: Ied77382fe2ac1f39b3adededf277c259fa614e1e

---
+++ MMOCoreORB/bin/scripts/mobile/mission/bh_canyon_corsair_captain.lua
@@ -0,0 +1,71 @@
+bh_canyon_corsair_captain = Creature:new {
+	objectName = "@mob/creature_names:canyon_corsair_captain",
+	socialGroup = "canyon_corsair",
+	pvpFaction = "canyon_corsair",
+	faction = "canyon_corsair",
+	level = 50,
+	chanceHit = 0.5,
+	damageMin = 395,
+	damageMax = 500,
+	baseXp = 4916,
+	baseHAM = 10000,
+	baseHAMmax = 12000,
+	armor = 1,
+	resists = {20,40,5,5,-1,5,5,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_corsair_captain_hum_m.iff",
+		"object/mobile/dressed_corsair_captain_nikto_m.iff",
+		"object/mobile/dressed_corsair_captain_wee_m.iff",
+		"object/mobile/dressed_corsair_captain_zab_m.iff"
+		},
+
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4150000},
+				{group = "tailor_components", chance = 1000000},
+				{group = "color_crystals", chance = 400000},
+				{group = "crystals_good", chance = 400000},
+				{group = "melee_two_handed", chance = 600000},
+				{group = "carbines", chance = 600000},
+				{group = "pistols", chance = 600000},
+				{group = "clothing_attachments", chance = 450000},
+				{group = "armor_attachments", chance = 450000},
+				{group = "canyon_corsair_common", chance = 1350000}
+			},
+			lootChance = 4500000
+		},
+		{
+			groups = {
+				{group = "color_crystals", chance = 1500000},
+				{group = "crystals_poor", chance = 1000000},
+				{group = "crystals_good", chance = 1000000},
+				{group = "melee_two_handed", chance = 1500000},
+				{group = "carbines", chance = 1500000},
+				{group = "pistols", chance = 1500000},
+				{group = "clothing_attachments", chance = 1000000},
+				{group = "armor_attachments", chance = 1000000}
+			},
+			lootChance = 6000000
+		}
+	},
+	weapons = {"canyon_corsair_weapons"},
+	conversationTemplate = "",
+	attacks = merge(swordsmanmaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(bh_canyon_corsair_captain, "bh_canyon_corsair_captain")
---
+++ MMOCoreORB/bin/scripts/mobile/mission/bh_dune_stalker_brawler.lua
@@ -0,0 +1,58 @@
+bh_dune_stalker_brawler = Creature:new {
+	objectName = "@mob/creature_names:dune_stalker_brawler",
+	socialGroup = "dune_stalker",
+	pvpFaction = "",
+	faction = "",
+	level = 17,
+	chanceHit = 0.32,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 1102,
+	baseHAM = 3500,
+	baseHAMmax = 4300,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE,
+	creatureBitmask = PACK + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_tatooine_dune_stalker_brawler.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 7000000},
+				{group = "tailor_components", chance = 1500000},
+				{group = "loot_kits", chance = 1500000}
+			},
+			lootChance = 4500000
+		},
+		{
+			groups = {
+				{group = "color_crystals", chance = 1500000},
+				{group = "crystals_poor", chance = 1000000},
+				{group = "crystals_okay", chance = 1000000},
+				{group = "melee_two_handed", chance = 1500000},
+				{group = "carbines", chance = 1500000},
+				{group = "pistols", chance = 1500000},
+				{group = "clothing_attachments", chance = 1000000},
+				{group = "armor_attachments", chance = 1000000}
+			},
+			lootChance = 6000000
+		}				
+	},
+	weapons = {"tusken_weapons"},
+	conversationTemplate = "",
+	attacks = merge(brawlermaster,marksmanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(bh_dune_stalker_brawler, "bh_dune_stalker_brawler")
