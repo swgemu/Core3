be1cc02ad3996660dfe540d84cdc2a039ef205af
Victor Popovici <victorpopovici@gmail.com>
2013-08-10 17:02:58 +0200

    [added] basic screnplay test
    Change-Id: If84463c4bb65a4cc0fe436a0d5ab8383c5ed0b4c

---
+++ MMOCoreORB/src/server/zone/managers/director/tests/BasicScreenPlayTest.cpp
@@ -0,0 +1,39 @@
+/*
+ * BasicScreenPlayTest.cpp
+ *
+ *  Created on: Aug 10, 2013
+ *      Author: TheAnswer
+ */
+
+#ifndef BASICSCREENPLAYTEST_CPP_
+#define BASICSCREENPLAYTEST_CPP_
+
+#include "gtest/gtest.h"
+#include "server/zone/managers/director/DirectorManager.h"
+
+class BasicScreenPlayTest : public ::testing::Test {
+public:
+
+	BasicScreenPlayTest() {
+		// Perform creation setup here.
+	}
+
+	~BasicScreenPlayTest() {
+		// Clean up.
+	}
+
+	void SetUp() {
+		// Perform setup of common constructs here.
+	}
+
+	void TearDown() {
+		// Perform clean up of common constructs here.
+	}
+};
+
+TEST_F(BasicScreenPlayTest, ScreenPlayLuaInitialize) {
+	DirectorManager::DEBUG_MODE = 1;
+	EXPECT_EQ(DirectorManager::instance()->runLuaInstance(), 0);
+}
+
+#endif /* BASICSCREENPLAYTEST_CPP_ */
