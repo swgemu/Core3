8c816340d87ea3b7ee097458d7b84bca836dc40e
Naikoi <Naikoi@swgemu.com>
2013-08-13 11:42:56 +0000

    [Fixed] Mantis 4143 Clothing for naked NPC in Mos Espa, Kaeline Ungasan.
    Change-Id: I950c25d01c9e7fd9845a994aa61b6f427784cdab

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/kaeline_ungasan_outfit.lua
@@ -0,0 +1,11 @@
+kaeline_ungasan_outfit = {
+	{
+		{objectTemplate = "object/tangible/wearables/shirt/shirt_s08.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/pants/pants_s10.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/shoes/shoes_s01.iff", customizationVariables = {} },
+		
+	}
+
+}
+
+addOutfitGroup("kaeline_ungasan_outfit", kaeline_ungasan_outfit)
