5a64d31fd527e9301e6a2464723b3107dd443a16
Klivian <klivian@hotmail.com>
2013-08-13 04:46:44 +0000

    [added] rebel radar data terminal schematic
    Change-Id: If9b3fcfae6a5027cdc0c5312e092c452414085a1

---
+++ MMOCoreORB/bin/scripts/loot/items/radar_screen_schematic.lua
@@ -0,0 +1,12 @@
+radar_screen_schematic = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/loot_schematic/radar_screen_schematic.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("radar_screen_schematic", radar_screen_schematic)
