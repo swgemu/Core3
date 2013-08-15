e8e6d87a7a47c9fc98764eae4b6886d4bedc9a6f
Klivian <klivian@hotmail.com>
2013-08-15 02:40:58 +0000

    [added] Unit test to catch when loot groups don't total 100%
    Change-Id: I0fc8400c9f933876c50edee995a6faccfffa6357

--- MMOCoreORB/src/server/zone/managers/creature/tests/LuaMobileTest.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/tests/LuaMobileTest.cpp
@@ -7,6 +7,7 @@
 
 #include "gtest/gtest.h"
 #include "server/zone/managers/creature/CreatureTemplateManager.h"
+#include "server/zone/managers/loot/lootgroup/LootGroupCollectionEntry.h"
 
 class LuaMobileTest : public ::testing::Test {
 public:
@@ -30,7 +31,39 @@ public:
 TEST_F(LuaMobileTest, LuaMobileTemplatesTest) {
 	CreatureTemplateManager::DEBUG_MODE = 1;
 
+	// Verify that all mobiles load
 	EXPECT_EQ(CreatureTemplateManager::instance()->loadTemplates(), 0);
+
+	// Verify loot group percentages
+	HashTableIterator<uint32, Reference<CreatureTemplate*> > iter = CreatureTemplateManager::instance()->iterator();
+	while (iter.hasNext()) {
+		CreatureTemplate* creature = iter.next();
+		std::string templateName( creature->getTemplateName().toCharArray() );
+
+		LootGroupCollection* groupCollection = creature->getLootGroups();
+		if( groupCollection->count() > 0 ){
+
+
+			for( int i = 0; i < groupCollection->count(); i++ ){
+
+				LootGroupCollectionEntry* collectionEntry = groupCollection->get(i);
+				LootGroups* groups = collectionEntry->getLootGroups();
+				if( groups->count() > 0){
+
+					int totalChance = 0;
+					for( int j = 0; j < groups->count(); j++ ){
+
+						LootGroupEntry* lootGroup = groups->get(j);
+						totalChance += lootGroup->getLootChance();
+
+					}
+
+					EXPECT_EQ( 10000000, totalChance ) << "Loot groups total chance is incorrect " << templateName;
+				}
+			}
+		}
+	}
+
 }
 
 
