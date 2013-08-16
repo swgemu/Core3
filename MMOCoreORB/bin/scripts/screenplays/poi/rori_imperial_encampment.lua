9018e73a9909d6a7bfed623276b853067bde805f
marsorbust <ajensenmtdew76@gmail.com>
2013-08-15 22:36:31 -0700

    [added] rebel spawns to rori imperial encampment screenplay
    Change-Id: Ib17bf97dfd8dec5a31916734b79a31998f42b5e5

--- MMOCoreORB/bin/scripts/screenplays/poi/rori_imperial_encampment.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/rori_imperial_encampment.lua
@@ -6,12 +6,18 @@ RoriImperialEncampmentScreenPlay = ScreenPlay:new {
 registerScreenPlay("RoriImperialEncampmentScreenPlay", true)
 
 function RoriImperialEncampmentScreenPlay:start()
-	if (isZoneEnabled("rori")) then
+	if isZoneEnabled("rori") then
 		self:spawnMobiles()
+
+		if math.random(100) >= 50 then
+			self:spawnRebels()
+		else
+			self:spawnImperials()
+		end
 	end
 end
 
-function RoriImperialEncampmentScreenPlay:spawnMobiles()
+function RoriImperialEncampmentScreenPlay:spawnImperials()
 
 	--inside the walls (not in buildings)
 
@@ -90,6 +96,87 @@ function RoriImperialEncampmentScreenPlay:spawnMobiles()
 	spawnMobile("rori", "imperial_first_lieutenant", 300, 0.0, 7.0, 12.6, 0, 2935415)
 	spawnMobile("rori", "imperial_medic", 300, 14.1, 7.0, 11.8, 60, 2935416)
 
+end
+
+function RoriImperialEncampmentScreenPlay:spawnRebels()
+
+	--inside the walls (not in buildings)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5506.5, 75.9, -5661.1, 90, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5506.5, 75.9, -5666.9, 90, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5502.5, 75.9, -5664.0, 90, 0)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5623.5, 75.9, -5665.5, -90, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5623.5, 75.9, -5658.7, -90, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5627.5, 75.9, -5661.6, -90, 0)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5599.3, 75.9, -5633.7, 180, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5595.8, 75.9, -5633.7, 180, 0)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5532.3, 75.9, -5633.7, 180, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5527.8, 75.9, -5633.7, 180, 0)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5599.3, 75.9, -5690.0, 0, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5595.8, 75.9, -5690.0, 0, 0)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, -5532.3, 75.9, -5690.0, 0, 0)
+	spawnMobile("rori", "senior_specforce_marine", 300, -5527.8, 75.9, -5690.0, 0, 0)
+
+	spawnMobile("rori", "rebel_trooper", 300, -5569.1, 75.9, -5630.4, 0, 0)
+	spawnMobile("rori", "rebel_trooper", 300, -5562.1, 75.9, -5630.4, 0, 0)
+
+	--towers
+
+	spawnMobile("rori", "senior_specforce_pathfinder", 300, -5510.3, 82.9, -5702.1, 135, 0)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, -5516.1, 82.9, -5702.4, 180, 0)
+
+	spawnMobile("rori", "senior_specforce_pathfinder", 300, -5508.7, 82.9, -5630.7, 90, 0)
+	spawnMobile("rori", "senior_specforce_heavy_weapons_specialist", 300, -5509.3, 82.9, -5623.6, 45, 0)
+	spawnMobile("rori", "rebel_colonel", 300, -5514.7, 82.9, -5622.7, 0, 0)
+
+	spawnMobile("rori", "senior_specforce_pathfinder", 300, -5612.8, 83.0, -5623.3, 0, 0)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, -5618.4, 83.0, -5623.7, -45, 0)
+
+	spawnMobile("rori", "senior_specforce_pathfinder", 300, -5619.4, 82.6, -5696.9, -90, 0)
+	spawnMobile("rori", "senior_specforce_heavy_weapons_specialist", 300, -5618.5, 82.6, -5702.6, -135, 0)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, -5613.1, 82.6, -5703.4, 180, 0)
+
+	--buildings
+
+	spawnMobile("rori", "specforce_major", 300, 4.9, 0.0, -2.0, -135, 3475375)
+	spawnMobile("rori", "senior_specforce_heavy_weapons_specialist", 300, 0.0, 0.0, -5.3, 0, 3475373)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, -3.8, 0.0, -3.6, 90, 3475376)
+
+	spawnMobile("rori", "senior_specforce_marine", 300, 0.0, 0.0, -3.6, 0, 3475366)
+	spawnMobile("rori", "senior_specforce_heavy_weapons_specialist", 300, 0.0, 0.0, -5.3, 0, 3475366)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, 0.0, 0.0, -1.0, 0, 3475366)
+
+	spawnMobile("rori", "senior_specforce_heavy_weapons_specialist", 300, 3.9, 0.0, 2.1, 90, 2935496)
+	spawnMobile("rori", "senior_specforce_lieutenant", 300, 0.0, 0.0, -5.3, 0, 2935495)
+
+	--main building
+
+	spawnMobile("rori", "rebel_surface_marshall", 300, 1.4, 1.0, 9.9, -90, 2935406)
+	spawnMobile("rori", "rebel_surface_marshall", 300, 6.4, 1.0, -7.2, -90, 2935406)
+	spawnMobile("rori", "rebel_surface_marshall", 300, 0.0, 1.0, 0.9, 0, 2935406)
+
+	spawnMobile("rori", "rebel_first_lieutenant", 300, 15.7, 1.0, 15.2, 90, 2935407)
+	spawnMobile("rori", "rebel_surface_marshall", 300, -17.7, 1.0, 16.5, 180, 2935408)
+	spawnMobile("rori", "rebel_major", 300, 18.3, 1.0, -19.7, -45, 2935410)
+	spawnMobile("rori", "rebel_medic", 300, 11.7, 1.0, -19.5, 0, 2935410)
+	spawnMobile("rori", "rebel_trooper", 300, -17.5, 1.0, -19.6, 45, 2935412)
+
+	spawnMobile("rori", "rebel_trooper", 300, -9.2, 7.0, -13.5, 0, 2935414)
+	spawnMobile("rori", "rebel_trooper", 300, -11.7, 7.0, -13.5, 0, 2935414)
+
+	spawnMobile("rori", "rebel_major", 300, -11.5, 7.0, 9.3, 90, 2935415)
+	spawnMobile("rori", "rebel_lieutenant_colonel", 300, -9.1, 7.0, 12.5, 0, 2935415)
+	spawnMobile("rori", "rebel_first_lieutenant", 300, 0.0, 7.0, 12.6, 0, 2935415)
+	spawnMobile("rori", "rebel_medic", 300, 14.1, 7.0, 11.8, 60, 2935416)
+
+end
+
+function RoriImperialEncampmentScreenPlay:spawnMobiles()
 	--outside the walls
 
 	spawnMobile("rori", "thug", 21, -5574.1, 75.9, -5759.4, 120, 0)
