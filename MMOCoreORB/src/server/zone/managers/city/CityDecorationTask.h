ffcba45136a3252154da3155176a35c9c46f0432
Pete <elpete@swgemu.com>
2013-08-26 13:32:21 -0500

    [added] Streetlamps, statues and fountains can now be placed as decoration
    Change-Id: Ib97765c72bf266f80e04c19a2ad64243881eb9a3

---
+++ MMOCoreORB/src/server/zone/managers/city/CityDecorationTask.h
@@ -0,0 +1,143 @@
+/*
+ * CityDecorationTask.h
+ *
+ *  Created on: Aug 26, 2013
+ *      Author: root
+ */
+
+#ifndef CITYDECORATIONTASK_H_
+#define CITYDECORATIONTASK_H_
+
+#include "engine/engine.h"
+#include "server/chat/ChatManager.h"
+#include "server/zone/objects/region/CityRegion.h"
+
+
+class CityDecorationTask : public Task {
+	ManagedReference<CreatureObject*> mayor;
+	ManagedReference<SceneObject*> obj;
+	byte option;
+
+public:
+	static const byte PLACE = 0;
+	static const byte REMOVE = 1;
+
+	CityDecorationTask( CreatureObject* creature, SceneObject* object, byte function) {
+		mayor = creature;
+		obj = object;
+		option = function;
+	}
+
+	void run() {
+
+		switch (option) {
+			case 0:
+				placeDecoration();
+				break;
+
+			case 1:
+				removeDecoration();
+				break;
+		}
+	}
+
+	void placeDecoration(){
+		Locker _lock(mayor);
+
+		ManagedReference<CityRegion*> city = mayor->getCityRegion();
+
+		if(city == NULL){
+			mayor->sendSystemMessage("@player_structure:cant_place_civic"); //This structure must be placed within the borders of the city in which you are mayor.
+			return;
+		}
+
+		CityManager* cityManager = mayor->getZoneServer()->getCityManager();
+
+		if(!city->isMayor(mayor->getObjectID())){
+			mayor->sendSystemMessage("@player_structure:cant_place_civic"); //This structure must be placed within the borders of the city in which you are mayor.
+			return;
+		}
+
+		if(!cityManager->canSupportMoreDecorations(city)){
+			StringIdChatParameter param("city/city", "no_more_decos"); //"Your city can't support any more decorations at its current rank!");
+			mayor->sendSystemMessage(param);
+			return;
+		}
+
+		Zone* zone = mayor->getZone();
+
+		if (zone == NULL)
+			return;
+
+		if(city->getCityTreasury() < 1000){
+			StringIdChatParameter msg;
+			msg.setStringId("@city/city:action_no_money");
+			msg.setDI(1000);
+			mayor->sendSystemMessage(msg); //"The city treasury must have %DI credits in order to perform that action.");
+			return;
+
+		}
+		Locker tlock(obj, mayor);
+
+		if(!obj->isASubChildOf(mayor)){
+			mayor->sendSystemMessage("@space/quest:not_in_inv"); // The object must be in your inventory
+			return;
+		}
+
+		obj->initializePosition(mayor->getWorldPositionX(), mayor->getWorldPositionZ(),mayor->getWorldPositionY());
+		obj->rotate(mayor->getDirectionAngle() - obj->getDirectionAngle());
+
+		if(zone->transferObject(obj, -1, true)){
+			tlock.release();
+			Locker clock(city, mayor);
+			city->addDecoration(obj);
+			city->subtractFromCityTreasury(1000);
+		}
+
+	}
+
+	void removeDecoration(){
+		Locker _lock(mayor);
+
+		ManagedReference<CityRegion*> city = mayor->getCityRegion();
+
+		if(city == NULL)
+			return;
+
+		if(!city->isMayor(mayor->getObjectID())){
+			return;
+		}
+
+		Zone* zone = mayor->getZone();
+
+		if (zone == NULL)
+			return;
+
+		ManagedReference<SceneObject*> inv = mayor->getSlottedObject("inventory");
+
+		if(inv == NULL)
+			return;
+
+		if(inv->isContainerFull()){
+			//mayor->sendSystemMessage("@error_message:inv_full"); // You inventory is full
+			mayor->sendSystemMessage("@event_perk:promoter_full_inv"); //"Your inventory is full. Please make some room and try again.");
+			return;
+		} else {
+			Locker tlock(obj, mayor);
+
+			if(	inv->transferObject(obj, -1, true)){
+				inv->broadcastObject(obj, true);
+				tlock.release();
+				Locker clock(city, mayor);
+				city->removeDecoration(obj);
+			}
+		}
+	}
+
+};
+
+
+
+
+
+#endif /* CITYDECORATIONTASK_H_ */
