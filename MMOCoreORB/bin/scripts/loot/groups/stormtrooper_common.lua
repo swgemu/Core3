39fa6e1ce432ff64a36833b3d6942128213eb1b3
Klivian <klivian@hotmail.com>
2013-08-27 04:07:27 +0000

    [fixed] typo preventing painting_bw_stormtrooper from dropping
    Change-Id: I93daf6f117e58b2d240457c5b72de2bbbfa4716f

--- MMOCoreORB/bin/scripts/loot/groups/stormtrooper_common.lua
+++ MMOCoreORB/bin/scripts/loot/groups/stormtrooper_common.lua
@@ -3,7 +3,7 @@ stormtrooper_common = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "painting_bw_stormtooper", weight = 2500000},
+		{itemTemplate = "painting_bw_stormtrooper", weight = 2500000},
 		{itemTemplate = "painting_han_wanted", weight = 2500000},
 		{itemTemplate = "painting_leia_wanted", weight = 2500000},
 		{itemTemplate = "painting_luke_wanted", weight = 2500000}	
