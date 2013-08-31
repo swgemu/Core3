a056c13f0210b5852ecac7eee671dbc4b6bd9e66
marsorbust <ajensenmtdew76@gmail.com>
2013-08-31 14:11:26 -0700

    [fixed] lair levels for rori_world_creatures
    Change-Id: I1c879bc62edb8def61f92b44861d4c1cbc381d54

--- MMOCoreORB/bin/scripts/mobile/lair/rori_kai_tok_pack_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/rori_kai_tok_pack_neutral_none.lua
@@ -1,11 +1,11 @@
 rori_kai_tok_pack_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {{"kai_tok_prowler", -5},{"kai_tok_scavenger", 0}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_bones_large.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_bones_large.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_bones_large.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_bones_large.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_bones_large.iff"}
 }
 
 addLairTemplate("rori_kai_tok_pack_neutral_none", rori_kai_tok_pack_neutral_none)
