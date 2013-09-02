3769501eb73fe0dd9e38c35177eca081d6d6d134
Klivian <klivian@hotmail.com>
2013-09-02 14:47:25 +0000

    [added] broken electrobinoculars to junk loot group
    Change-Id: I4857d948ae49f3b439c62367a0f40a12bbeecdcf

---
+++ MMOCoreORB/bin/scripts/loot/items/broken_binoculars_s1.lua
@@ -0,0 +1,12 @@
+broken_binoculars_s1 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/tool/binoculars_broken_s1.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("broken_binoculars_s1", broken_binoculars_s1)
