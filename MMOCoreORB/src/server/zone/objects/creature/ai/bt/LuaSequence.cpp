dbf3228804e9fa0b5ca8dbce3140705096427f75
Victor Popovici <victorpopovici@gmail.com>
2013-09-05 21:31:09 +0200

    [ai] stub luasqeuence and stubbed in placeholders for ai scripts. QA: nothing todo here. Here for base review for dev team
    Change-Id: Ie9d784d8278f4ed451434622a087bcafde3ae160

---
+++ MMOCoreORB/src/server/zone/objects/creature/ai/bt/LuaSequence.cpp
@@ -0,0 +1,16 @@
+/*
+ * LuaSequence.cpp
+ *
+ *  Created on: Sep 4, 2013
+ *      Author: swgemu
+ */
+
+#include "LuaSequence.h"
+
+
+LuaSequence::LuaSequence(): SequenceBehavior(NULL) {
+}
+
+LuaSequence::~LuaSequence() {
+}
+
