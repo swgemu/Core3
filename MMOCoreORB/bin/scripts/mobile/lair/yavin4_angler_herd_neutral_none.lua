05fafa8094204d4a845cee43008d6a50265ac4a3
marsorbust <ajensenmtdew76@gmail.com>
2013-09-06 18:52:17 -0700

    [fixed] lair levels for yavin_world_creatures
    Change-Id: I66b53cbcf9bf300b72378255f4ada69b52dd8cf7

--- MMOCoreORB/bin/scripts/mobile/lair/yavin4_angler_herd_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/yavin4_angler_herd_neutral_none.lua
@@ -1,5 +1,5 @@
 yavin4_angler_herd_neutral_none = Lair:new {
-	mobiles = {{"angler_recluse", 0}, {"bone_angler", 0}},
+	mobiles = {{"angler_recluse", -1}, {"bone_angler", 0}},
 	spawnLimit = 15,
 	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_nest_large_evil_fire_red.iff"},
 	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_nest_large_evil_fire_red.iff"},
