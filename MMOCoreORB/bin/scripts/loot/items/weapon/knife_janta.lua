fb35f82309cb09f6f4c320a8bb150b31962e2fdf
marsorbust <ajensenmtdew76@gmail.com>
2013-09-14 14:38:37 -0700

    [added] semi-static mind poison dot to janta knife
    Change-Id: Ib9628f8f5e748ea371b8334851d58155b26afb99

--- MMOCoreORB/bin/scripts/loot/items/weapon/knife_janta.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/knife_janta.lua
@@ -25,6 +25,22 @@ knife_janta = {
 
 	-- randomDotChance: The chance of this weapon object dropping with a random dot on it. Higher number means less chance. Set to 0 to always have a random dot.
 	randomDotChance = 10,
+
+	-- staticDotChance: The chance of this weapon object dropping with a static dot on it. Higher number means less chance. Set to 0 to always have a static dot.
+	staticDotChance = 8, -- not a true static dot on this weapon, just a higher chance of having a mind poison in addition to any other dots
+
+	-- staticDotType: 1 = Poison, 2 = Disease, 3 = Fire, 4 = Bleed
+	staticDotType = 1,
+
+	-- staticDotValues: Object map that can randomly or statically generate a dot (used for weapon objects.)
+	staticDotValues = {
+		{"attribute", 6, 6}, -- See CreatureAttributes.h in src for numbers.
+		{"strength", 110, 250}, --because of the system random in the strength calculation, setting at 110, 250 will always produce a strength between 110 and 140 + % increase based on level of mob looted from.
+		{"duration", 30, 240},
+		{"potency", 10, 99},
+		{"uses", 2000, 9999}
+	},
+
 	junkDealerTypeNeeded = JUNKWEAPONS,
 	junkMinValue = 2,
 	junkMaxValue = 22
