512434c2641d803608d06ab224277bccb8cc3b71
Valkyra <lyhtmyst@gmail.com>
2013-09-29 18:54:28 +0000

    [added] Imperial Destroy Corvette mission files, Lt Velso and
    Sabol conversing is disabled until more can be added.
    [added] Main corvette screenplay, all spawns disabled but Lt Velso and
    Lt Sabol.
    [moved] Sabol spawn from Tatooine spawn manager.
    [moved] Elevator terminals from Imperial Corvette to screenplay.
    
    Change-Id: I41feb4a97385c28876e14ac4e8604d562f678460

--- MMOCoreORB/bin/scripts/mobile/conversations.lua
+++ MMOCoreORB/bin/scripts/mobile/conversations.lua
@@ -38,6 +38,11 @@ includeFile("conversations/dungeon/warren/dirk_maggin_conv.lua")
 includeFile("conversations/dungeon/warren/captain_heff_conv.lua")
 includeFile("conversations/dungeon/warren/manx_try_conv.lua")
 
+-- Corvette
+includeFile("conversations/dungeon/corvette/imperial_destroy_conv.lua")
+--includeFile("conversations/dungeon/corvette/imperial_pilot_conv.lua")
+--includeFile("conversations/dungeon/corvette/imperial_assassin_conv.lua")
+
 -- ** OTHER **
 
 includeFile("conversations/trainer/trainers_conv.lua")
