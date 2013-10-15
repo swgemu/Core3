6b93bd4c1f02cefb34d8d50cb1c18e3fb48e18b4
Loshult <loshult@swgemu.com>
2013-10-15 20:50:24 +0000

    [Changed] moved withSceneObject, withCreatureObject, withPlayerObject, withCreaturePlayerObject and withCreatureAndPlayerObject methods to ScreenPlay.
    Change-Id: Ib0ffc7d587b8a82635c2f3204424dad564a2c191

--- MMOCoreORB/bin/scripts/screenplays/screenplay.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplay.lua
@@ -78,7 +78,63 @@ function ScreenPlay:spawnContainerLoot(pContainer)
 	writeData(container:getObjectID(), time + self.lootContainerRespawn)
 end
 
+-- Perform the supplied function with a scene object created from the pointer.
+-- @param pSceneObject a pointer to a scene object.
+-- @param performThisFunction a function that takes a scene object as its argument.
+-- @return whatever performThisFunction returns or nil if the pSceneObject pointer is nil.
+function ScreenPlay.withSceneObject(pSceneObject, performThisFunction)
+	if pSceneObject == nil then
+		return nil
+	end
+	local sceneObject = LuaSceneObject(pSceneObject)
+	return performThisFunction(sceneObject)
+end
 
+-- Perform the supplied function with a creature object created from the pointer.
+-- @param pCreatureObject a pointer to a creature object.
+-- @param performThisFunction a function that takes a creature object as its argument.
+-- @return whatever performThisFunction returns or nil if the pCreatureObject pointer is nil.
+function ScreenPlay.withCreatureObject(pCreatureObject, performThisFunction)
+	if pCreatureObject == nil then
+		return nil
+	end
+	local creatureObject = LuaCreatureObject(pCreatureObject)
+	return performThisFunction(creatureObject)
+end
+
+-- Perform the supplied function with a player object created from the pointer.
+-- @param pPlayerObject a pointer to a player object.
+-- @param performThisFunction a function that takes a player object as its argument.
+-- @return whatever performThisFunction returns or nil if the pPlayerObject pointer is nil.
+function ScreenPlay.withPlayerObject(pPlayerObject, performThisFunction)
+	if pPlayerObject == nil then
+		return nil
+	end
+	local playerObject = LuaPlayerObject(pPlayerObject)
+	return performThisFunction(playerObject)
+end
+
+-- Perform the supplied function with a player object fetched from the creature object pointer.
+-- @param pCreatureObject a pointer to a creature object.
+-- @param performThisFunction a function that takes a player object as its argument.
+-- @return whatever performThisFunction returns or nil if the pCreatureObject pointer is nil or does not have a player object.
+function ScreenPlay.withCreaturePlayerObject(pCreatureObject, performThisFunction)
+	return ScreenPlay.withCreatureObject(pCreatureObject, function(creatureObject)
+		return ScreenPlay.withPlayerObject(creatureObject:getPlayerObject(), performThisFunction)
+	end)
+end
+
+-- Perform the supplied function with a creature object and a player object fetched from the creature object pointer.
+-- @param pCreatureObject a pointer to a creature object.
+-- @param performThisFunction a function that takes a creature object and a player object as its argument.
+-- @return whatever performThisFunction returns or nil if the pCreatureObject pointer is nil or does not have a player object.
+function ScreenPlay.withCreatureAndPlayerObject(pCreatureObject, performThisFunction)
+	return ScreenPlay.withCreatureObject(pCreatureObject, function(creatureObject)
+		return ScreenPlay.withPlayerObject(creatureObject:getPlayerObject(), function(playerObject)
+			return performThisFunction(creatureObject, playerObject)
+		end)
+	end)
+end
 
 Act = Object:new {
 
