82083d776ff474d072de3a3333188e935aa4866c
Loshult <loshult@swgemu.com>
2013-10-15 21:12:25 +0000

    [Changed] split up village jedi manager into several files.
    Change-Id: Ic0f33fe3790f97937debbbe8b5925667508b7d93

---
+++ MMOCoreORB/bin/scripts/managers/jedi/village/tests/village_jedi_manager_holocron_Test.lua
@@ -0,0 +1,244 @@
+package.path = package.path .. ";scripts/managers/jedi/village/?.lua"
+VillageJediManagerHolocron = require("village_jedi_manager_holocron")
+
+describe("Village Jedi Manager Holocron", function()
+	describe("Interface functions", function() 
+		describe("useHolocron", function()
+			it("Should use the holocron if the player is a jedi and does not have max force at the moment", function() 
+				local pCreatureObject = { "creatureObjectPointer" }
+				local pSceneObject = { "sceneObjectPointer" }
+				local realCanUseHolocron = VillageJediManagerHolocron.canUseHolocron
+				local realCanReplenishForce = VillageJediManagerHolocron.canReplenishForce
+				local realUseTheHolocron = VillageJediManagerHolocron.useTheHolocron
+				
+				VillageJediManagerHolocron.canUseHolocron = spy.new(function() return true end)
+				VillageJediManagerHolocron.canReplenishForce = spy.new(function() return true end)
+				VillageJediManagerHolocron.useTheHolocron = spy.new(function() end)
+
+				VillageJediManagerHolocron.useHolocron(pSceneObject, pCreatureObject)
+
+				assert.spy(VillageJediManagerHolocron.canUseHolocron).was.called_with(pCreatureObject)
+				assert.spy(VillageJediManagerHolocron.canReplenishForce).was.called_with(pCreatureObject)
+				assert.spy(VillageJediManagerHolocron.useTheHolocron).was.called_with(pSceneObject, pCreatureObject)
+
+				VillageJediManagerHolocron.canUseHolocron = realCanUseHolocron
+				VillageJediManagerHolocron.canReplenishForce = realCanReplenishForce
+				VillageJediManagerHolocron.useTheHolocron = realUseTheHolocron
+			end)
+
+			it("Should send message about not able to replenish force if player is jedi but has full force", function() 
+				local pCreatureObject = { "creatureObjectPointer" }
+				local pSceneObject = { "sceneObjectPointer" }
+				local realCanUseHolocron = VillageJediManagerHolocron.canUseHolocron
+				local realCanReplenishForce = VillageJediManagerHolocron.canReplenishForce
+				local realCannotReplenishForce = VillageJediManagerHolocron.cannotReplenishForce
+				
+				VillageJediManagerHolocron.canUseHolocron = spy.new(function() return true end)
+				VillageJediManagerHolocron.canReplenishForce = spy.new(function() return false end)
+				VillageJediManagerHolocron.cannotReplenishForce = spy.new(function() end)
+
+				VillageJediManagerHolocron.useHolocron(pSceneObject, pCreatureObject)
+
+				assert.spy(VillageJediManagerHolocron.canUseHolocron).was.called_with(pCreatureObject)
+				assert.spy(VillageJediManagerHolocron.canReplenishForce).was.called_with(pCreatureObject)
+				assert.spy(VillageJediManagerHolocron.cannotReplenishForce).was.called_with(pCreatureObject)
+
+				VillageJediManagerHolocron.canUseHolocron = realCanUseHolocron
+				VillageJediManagerHolocron.canReplenishForce = realCanReplenishForce
+				VillageJediManagerHolocron.cannotReplenishForce = realCannotReplenishForce
+			end)
+
+			it("Should send message about not being able to use the holocron if player is not jedi", function() 
+				local pCreatureObject = { "creatureObjectPointer" }
+				local pSceneObject = { "sceneObjectPointer" }
+				local realCanUseHolocron = VillageJediManagerHolocron.canUseHolocron
+				local realCannotUseHolocron = VillageJediManagerHolocron.cannotUseHolocron
+				
+				VillageJediManagerHolocron.canUseHolocron = spy.new(function() return false end)
+				VillageJediManagerHolocron.cannotUseHolocron = spy.new(function() end)
+
+				VillageJediManagerHolocron.useHolocron(pSceneObject, pCreatureObject)
+
+				assert.spy(VillageJediManagerHolocron.canUseHolocron).was.called_with(pCreatureObject)
+				assert.spy(VillageJediManagerHolocron.cannotUseHolocron).was.called_with(pCreatureObject)
+
+				VillageJediManagerHolocron.canUseHolocron = realCanUseHolocron
+				VillageJediManagerHolocron.cannotUseHolocron = realCannotUseHolocron
+			end)
+		end)
+	end)
+
+	describe("Private functions", function() 
+		describe("canUseHolocron", function()
+			it("Should return false if player is not jedi", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local isJediSpy = spy.new(function() return false end)
+				local checkCooldownRecoverySpy = spy.new(function() end)
+				local creatureObject = { checkCooldownRecovery = checkCooldownRecoverySpy }
+				local playerObject = { isJedi = isJediSpy }
+				local realWithCreatureAndPlayerObject = VillageJediManagerHolocron.withCreatureAndPlayerObject
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject, playerObject)
+				end)
+
+				assert.is_false(VillageJediManagerHolocron.canUseHolocron(pCreatureObject))
+
+				assert.spy(isJediSpy).was.called(1)
+				assert.spy(checkCooldownRecoverySpy).was.called(0)
+				assert.spy(VillageJediManagerHolocron.withCreatureAndPlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = realWithCreatureAndPlayerObject
+			end)
+
+			it("Should return false if player has a cooldown for using a holocron", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local isJediSpy = spy.new(function() return true end)
+				local checkCooldownRecoverySpy = spy.new(function(name) return true end)
+				local creatureObject = { checkCooldownRecovery = checkCooldownRecoverySpy }
+				local playerObject = { isJedi = isJediSpy }
+				local realWithCreatureAndPlayerObject = VillageJediManagerHolocron.withCreatureAndPlayerObject
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject, playerObject)
+				end)
+
+				assert.is_false(VillageJediManagerHolocron.canUseHolocron(pCreatureObject))
+
+				assert.spy(isJediSpy).was.called(1)
+				assert.spy(checkCooldownRecoverySpy).was.called_with(creatureObject, USEDHOLOCRON)
+				assert.spy(VillageJediManagerHolocron.withCreatureAndPlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = realWithCreatureAndPlayerObject
+			end)
+
+			it("Should return true if player does not have a cooldown for using a holocron and is jedi", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local isJediSpy = spy.new(function() return true end)
+				local checkCooldownRecoverySpy = spy.new(function(name) return false end)
+				local creatureObject = { checkCooldownRecovery = checkCooldownRecoverySpy }
+				local playerObject = { isJedi = isJediSpy }
+				local realWithCreatureAndPlayerObject = VillageJediManagerHolocron.withCreatureAndPlayerObject
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject, playerObject)
+				end)
+
+				assert.is_true(VillageJediManagerHolocron.canUseHolocron(pCreatureObject))
+
+				assert.spy(isJediSpy).was.called(1)
+				assert.spy(checkCooldownRecoverySpy).was.called_with(creatureObject, USEDHOLOCRON)
+				assert.spy(VillageJediManagerHolocron.withCreatureAndPlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = realWithCreatureAndPlayerObject
+			end)
+		end)
+
+		describe("canReplenishForce", function()
+			it("Should return false if the player force is at max", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local getForcePowerSpy = spy.new(function() return 23 end)
+				local getForcePowerMaxSpy = spy.new(function() return 23 end)
+				local playerObject = { getForcePower = getForcePowerSpy, getForcePowerMax = getForcePowerMaxSpy }
+				local realGetForcePower = VillageJediManagerHolocron.getForcePower
+				local realGetForcePowerMax = VillageJediManagerHolocron.getForcePowerMax
+				local realWithCreaturePlayerObject = VillageJediManagerHolocron.withCreaturePlayerObject
+				VillageJediManagerHolocron.withCreaturePlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(playerObject)
+				end)
+
+				assert.is_false(VillageJediManagerHolocron.canReplenishForce(pCreatureObject))
+
+				assert.spy(getForcePowerSpy).was.called(1)
+				assert.spy(getForcePowerMaxSpy).was.called(1)
+				assert.spy(VillageJediManagerHolocron.withCreaturePlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreaturePlayerObject = realWithCreaturePlayerObject
+			end)
+
+			it("Should return false if the player force is not at max", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local getForcePowerSpy = spy.new(function() return 22 end)
+				local getForcePowerMaxSpy = spy.new(function() return 23 end)
+				local playerObject = { getForcePower = getForcePowerSpy, getForcePowerMax = getForcePowerMaxSpy }
+				local realGetForcePower = VillageJediManagerHolocron.getForcePower
+				local realGetForcePowerMax = VillageJediManagerHolocron.getForcePowerMax
+				local realWithCreaturePlayerObject = VillageJediManagerHolocron.withCreaturePlayerObject
+				VillageJediManagerHolocron.withCreaturePlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(playerObject)
+				end)
+
+				assert.is_true(VillageJediManagerHolocron.canReplenishForce(pCreatureObject))
+
+				assert.spy(getForcePowerSpy).was.called(1)
+				assert.spy(getForcePowerMaxSpy).was.called(1)
+				assert.spy(VillageJediManagerHolocron.withCreaturePlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreaturePlayerObject = realWithCreaturePlayerObject
+			end)
+		end)
+
+		describe("useTheHolocron", function()
+			it("Should send system message, set force to max, add cooldown and destroy the holocron", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local pSceneObject = { "sceneObjectPointer" }
+				local destroyObjectFromWorldSpy = spy.new(function() end)
+				local setForcePowerSpy = spy.new(function() end)
+				local getForcePowerMaxSpy = spy.new(function() return 23 end)
+				local addCooldownSpy = spy.new(function() end)
+				local sendSystemMessageSpy = spy.new(function() end)
+				local creatureObject = { addCooldown = addCooldownSpy, sendSystemMessage = sendSystemMessageSpy }
+				local playerObject = { setForcePower = setForcePowerSpy, getForcePowerMax = getForcePowerMaxSpy }
+				local sceneObject = { destroyObjectFromWorld = destroyObjectFromWorldSpy }
+				local realWithCreatureAndPlayerObject = VillageJediManagerHolocron.withCreatureAndPlayerObject
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject, playerObject)
+				end)
+				LuaSceneObject = spy.new(function() return sceneObject end)
+
+				VillageJediManagerHolocron.useTheHolocron(pSceneObject, pCreatureObject)
+
+				assert.spy(destroyObjectFromWorldSpy).was.called(1)
+				assert.spy(setForcePowerSpy).was.called(1)
+				assert.spy(sendSystemMessageSpy).was.called(1)
+				assert.spy(addCooldownSpy).was.called(1)
+				assert.spy(VillageJediManagerHolocron.withCreatureAndPlayerObject).was.called(1)
+
+				VillageJediManagerHolocron.withCreatureAndPlayerObject = realWithCreatureAndPlayerObject
+			end)
+		end)
+
+		describe("cannotReplenishForce", function()
+			it("Should send a system message to the player about not being able to replenish the force", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local sendSystemMessageSpy = spy.new(function() end)
+				local creatureObject = { sendSystemMessage = sendSystemMessageSpy }
+				local realWithCreatureObject = VillageJediManagerHolocron.withCreatureObject
+				VillageJediManagerHolocron.withCreatureObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject)
+				end)
+
+				VillageJediManagerHolocron.cannotReplenishForce(pCreatureObject)
+
+				assert.spy(sendSystemMessageSpy).was.called_with(creatureObject, "@jedi_spam:holocron_force_max")
+
+				VillageJediManagerHolocron.withCreatureObject = realWithCreatureObject
+			end)
+		end)
+
+		describe("cannotUseHolocron", function()
+			it("Should send a system message to the player about not being able to use the holocron", function()
+				local pCreatureObject = { "creatureObjectPointer" }
+				local sendSystemMessageSpy = spy.new(function() end)
+				local creatureObject = { sendSystemMessage = sendSystemMessageSpy }
+				local realWithCreatureObject = VillageJediManagerHolocron.withCreatureObject
+				VillageJediManagerHolocron.withCreatureObject = spy.new(function(pCreatureObject, performThisFunction)
+					return performThisFunction(creatureObject)
+				end)
+
+				VillageJediManagerHolocron.cannotUseHolocron(pCreatureObject)
+
+				assert.spy(sendSystemMessageSpy).was.called_with(creatureObject, "@jedi_spam:holocron_no_effect")
+
+				VillageJediManagerHolocron.withCreatureObject = realWithCreatureObject
+			end)
+		end)
+	end)
+end)
--- MMOCoreORB/bin/scripts/managers/jedi/village/village_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/village/village_jedi_manager.lua
@@ -1,5 +1,6 @@
-package.path = package.path .. ";scripts/managers/jedi/?.lua"
+package.path = package.path .. ";scripts/managers/jedi/?.lua;scripts/managers/jedi/village/?.lua"
 JediManager = require("jedi_manager")
+VillageJediManagerHolocron = require("village_jedi_manager_holocron")
 
 jediManagerName = "VillageJediManager"
 
@@ -127,9 +128,6 @@ PROFESSIONBADGES = {
 	PILOT_TATOOINE 
 }
 
-USEDHOLOCRON = "used_holocron"
-HOLOCRONCOOLDOWNTIME = 24 * 60 * 60 * 1000 -- 24 hours
-
 VillageJediManager = JediManager:new {
 	screenplayName = jediManagerName,
 	jediManagerName = jediManagerName,
@@ -137,69 +135,11 @@ VillageJediManager = JediManager:new {
 	startingEvent = nil,
 }
 
--- Check if the player can use the holocron.
--- @param pCreatureObject pointer to the creature object of the player who tries to use the holocron.
--- @return true if the player can use the holocron.
-function VillageJediManager.canUseHolocron(pCreatureObject)
-	return VillageJediManager.withCreatureAndPlayerObject(pCreatureObject, function(creatureObject, playerObject) 
-		return playerObject:isJedi() and not creatureObject:checkCooldownRecovery(USEDHOLOCRON)
-	end)
-end
-
--- Checks if the player can replenish the force or not.
--- @param pCreatureObject pointer to the creature object of the player who should be checked.
--- @return true if the player can replenish the force.
-function VillageJediManager.canReplenishForce(pCreatureObject)
-	return VillageJediManager.withCreaturePlayerObject(pCreatureObject, function(playerObject)
-		return playerObject:getForcePower() < playerObject:getForcePowerMax()
-	end)
-end
-
--- Use the holocron on the player.
--- @param pSceneObject pointer to the scene object of the holocron.
--- @param pCreatureObject pointer to the creature object of the player who is using the holocron.
-function VillageJediManager.useTheHolocron(pSceneObject, pCreatureObject)
-	VillageJediManager.withCreatureAndPlayerObject(pCreatureObject, function(creatureObject, playerObject) 
-		-- The holocrom hums softly as you feel your Force power replenish.
-		creatureObject:sendSystemMessage("@jedi_spam:holocron_force_replenish")
-		playerObject:setForcePower(playerObject:getForcePowerMax());
-		creatureObject:addCooldown(USEDHOLOCRON, HOLOCRONCOOLDOWNTIME)
-	end)
-	local sceneObject = LuaSceneObject(pSceneObject)
-	sceneObject:destroyObjectFromWorld()
-end
-
--- Send message to the player that he cannot replenish the force.
--- @param pCreatureObject pointer to the creature object of the player that tries to use the holocron.
-function VillageJediManager.cannotReplenishForce(pCreatureObject)
-	VillageJediManager.withCreatureObject(pCreatureObject, function(creatureObject)
-		-- You are already at your maximum Force power.
-		creatureObject:sendSystemMessage("@jedi_spam:holocron_force_max")
-	end)
-end
-
--- Send message to the player that he cannot use the holocron.
--- @param pCreatureObject pointer to the creature object of the player that tries to use the holocron.
-function VillageJediManager.cannotUseHolocron(pCreatureObject)
-	VillageJediManager.withCreatureObject(pCreatureObject, function(creatureObject)
-		-- The holocron hums briefly, but otherwise does nothing.
-		creatureObject:sendSystemMessage("@jedi_spam:holocron_no_effect")
-	end)
-end
-
 -- Handling of the useHolocron event.
 -- @param pSceneObject pointer to the holocron object.
 -- @param pCreatureObject pointer to the creature object that used the holocron.
 function VillageJediManager:useHolocron(pSceneObject, pCreatureObject)
-	if VillageJediManager.canUseHolocron(pCreatureObject) then
-		if VillageJediManager.canReplenishForce(pCreatureObject) then
-			VillageJediManager.useTheHolocron(pSceneObject, pCreatureObject)
-		else
-			VillageJediManager.cannotReplenishForce(pCreatureObject)
-		end
-	else
-		VillageJediManager.cannotUseHolocron(pCreatureObject)
-	end
+	VillageJediManagerHolocron.useHolocron(pSceneObject, pCreatureObject)
 end
 
 -- Count the number of badges that the player got in the list.
