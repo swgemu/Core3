e44475de6d503a988bd9840889b612b8dca7b445
Valkyra <lyhtmyst@gmail.com>
2013-10-26 15:51:03 +0000

    [added] Functionality to lair observers to get spawned creature vector [fixed] Not getting the correct amount of experience when destroying a lair (mantis not reported)
    Change-Id: I5bcb4dc6ff253fb26a11c9a11ae7a64b070e7adc

--- MMOCoreORB/src/server/zone/managers/creature/LairObserverImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/LairObserverImplementation.cpp
@@ -91,6 +91,9 @@ void LairObserverImplementation::notifyDestruction(TangibleObject* lair, Tangibl
 	threatMap->removeObservers();
 	threatMap->removeAll(); // we can clear the original one
 
+	PlayerManager* playerManager = lair->getZoneServer()->getPlayerManager();
+	playerManager->disseminateExperience(lair, &copyThreatMap);
+
 	if (lair->getZone() == NULL) {
 		spawnedCreatures.removeAll();
 		return;
@@ -112,9 +115,6 @@ void LairObserverImplementation::notifyDestruction(TangibleObject* lair, Tangibl
 	}
 
 	spawnedCreatures.removeAll();
-
-	PlayerManager* playerManager = lair->getZoneServer()->getPlayerManager();
-	playerManager->disseminateExperience(lair, &copyThreatMap);
 }
 
 void LairObserverImplementation::doAggro(TangibleObject* lair, TangibleObject* attacker){
