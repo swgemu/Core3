e7957878cc1a2a04fe374b40c305658e96aa1f55
Valkyra <lyhtmyst@gmail.com>
2013-10-27 23:41:04 +0000

    [added] Functionality to lair observers to get spawned creature vector [fixed] Not getting the correct amount of experience when destroying a lair (mantis not reported), with lock checks.
    Change-Id: I5bcb4dc6ff253fb26a11c9a11ae7a64b070e7adc

---
+++ MMOCoreORB/src/server/zone/managers/creature/DisseminateExperienceTask.h
@@ -0,0 +1,39 @@
+/*
+ * DisseminateExperienceTask.h
+ *
+ *  Created on: Oct 27, 2013
+ *      Author: swgemu
+ */
+
+#ifndef DISSEMINATEEXPERIENCETASK_H_
+#define DISSEMINATEEXPERIENCETASK_H_
+
+#include "engine/engine.h"
+#include "server/zone/managers/creature/LairObserver.h"
+#include "server/zone/objects/tangible/TangibleObject.h"
+class DisseminateExperienceTask : public Task {
+	ManagedWeakReference<TangibleObject*> lair;
+
+	ThreatMap copyThreatMap;
+
+public:
+
+	DisseminateExperienceTask(TangibleObject* obj, ThreatMap* threatMap) : lair(obj),
+		copyThreatMap(*threatMap) {
+	}
+
+	void run() {
+		ManagedReference<TangibleObject*> strongRef = lair.get();
+
+		if (strongRef == NULL)
+			return;
+
+		Locker locker(strongRef);
+
+		PlayerManager* playerManager = strongRef->getZoneServer()->getPlayerManager();
+		playerManager->disseminateExperience(strongRef, &copyThreatMap);
+	}
+};
+
+
+#endif /* DISSEMINATEEXPERIENCETASK_H_ */
