7dd26865a3ff4091e713d115a2e1759f47580390
Valkyra <lyhtmyst@gmail.com>
2013-11-15 21:10:10 +0000

    [added] Dungeon Tickets object and attribute list component. [added] Method in Director Manager to set attributes for ticket.
    Change-Id: I60f93a6422b0c96813925116e06132b55ee6ebba

---
+++ MMOCoreORB/src/server/zone/objects/tangible/components/DungeonTicketObjectAttributeListComponent.h
@@ -0,0 +1,35 @@
+
+#ifndef DUNGEONTICKETOBJECTATTIBUTELISTCOMPONENT_H_
+#define DUNGEONTICKETOBJECTATTIBUTELISTCOMPONENT_H_
+
+#include "server/zone/objects/scene/components/AttributeListComponent.h"
+#include "server/zone/objects/tangible/TangibleObject.h"
+#include "server/zone/objects/tangible/ticket/TicketObject.h"
+
+class DungeonTicketObjectAttributeListComponent: public AttributeListComponent {
+public:
+
+	/**
+	 * Fills the Attributes
+	 * @pre { this object is locked }
+	 * @post { this object is locked, menuResponse is complete}
+	 * @param menuResponse ObjectMenuResponse that will be sent to the client
+	 */
+	void fillAttributeList(AttributeListMessage* alm, CreatureObject* creature, SceneObject* object) {
+
+		ManagedReference<TangibleObject*> tano = cast<TangibleObject*>(object);
+
+		TicketObject* ticketObject = tano.castTo<TicketObject*>();
+
+		String dep = ticketObject->getDeparturePlanet();
+		String point = ticketObject->getDeparturePoint();
+		String arrive = ticketObject->getArrivalPoint();
+
+		alm->insertAttribute("travel_departure_planet", "@planet_n:" + dep);
+		alm->insertAttribute("travel_departure_point", "@dungeon/space_dungeon:corvette_" + point + "_pilot");
+		alm->insertAttribute("travel_arrival_point", "@dungeon/space_dungeon:corvette_" + arrive);
+	}
+
+};
+
+#endif /* ATTIBUTELISTCOMPONENT_H_ */
