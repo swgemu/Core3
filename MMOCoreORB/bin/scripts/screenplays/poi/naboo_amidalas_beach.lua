2fb7f3e7f904e88b3bd17b3c631cbef273be813e
Cem Canatar <cemcanatar88@gmail.com>
2013-11-16 19:43:58 +0000

    [Added] 2 naboo poi lua files
    Change-Id: I3c083f91a000240204c71c1e5baeb494bb85427e

---
+++ MMOCoreORB/bin/scripts/screenplays/poi/naboo_amidalas_beach.lua
@@ -0,0 +1,25 @@
+NabooAmidalasBeachScreenPlay = ScreenPlay:new {
+	numberOfActs = 1,
+	
+	screenplayName = "NabooAmidalasBeachScreenPlay",
+
+}
+
+registerScreenPlay("NabooAmidalasBeachScreenPlay", true)
+
+function NabooAmidalasBeachScreenPlay:start()
+	if (isZoneEnabled("naboo")) then
+		self:spawnMobiles()
+
+	end
+end
+
+function NabooAmidalasBeachScreenPlay:spawnMobiles()
+
+	--Screenplay needs more added.
+
+	--Outside
+	spawnMobile("naboo", "informant_npc_lvl_2", 0,-5548,-150,-13,225,0)
+	spawnMobile("naboo", "informant_npc_lvl_2", 0,-5476,-150,-76,270,0)
+
+end
