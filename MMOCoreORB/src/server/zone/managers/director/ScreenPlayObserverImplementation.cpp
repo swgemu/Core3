1540ef7aa0141ee5e43591b96778b2e926867356
Victor Popovici <victorpopovici@gmail.com>
2013-11-23 10:02:45 +0100

    [fixed] stability issue
    Change-Id: Id7f2f8b9617823f8a35d5e560fb717f875430e99

--- MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
@@ -43,27 +43,35 @@ which carries forward this exception.
 
 #include "server/zone/managers/director/ScreenPlayObserver.h"
 #include "DirectorManager.h"
+#include "engine/lua/LuaPanicException.h"
 
 int ScreenPlayObserverImplementation::notifyObserverEvent(uint32 eventType, Observable* observable, ManagedObject* arg1, int64 arg2) {
-	Lua* lua = DirectorManager::instance()->getLuaInstance();
+	int ret = 1;
 
-	LuaFunction startScreenPlay(lua->getLuaState(), play, key, 1);
-	startScreenPlay << observable;
-	startScreenPlay << arg1;
-	startScreenPlay << arg2;
+	try {
+		Lua* lua = DirectorManager::instance()->getLuaInstance();
 
-	startScreenPlay.callFunction();
+		LuaFunction startScreenPlay(lua->getLuaState(), play, key, 1);
+		startScreenPlay << observable;
+		startScreenPlay << arg1;
+		startScreenPlay << arg2;
 
-	if (lua_gettop(lua->getLuaState()) == 0) {
-		Logger::console.error("ScreenPlayObserverImplementation::notifyObserverEvent didnt return a value from " + play + ":" + key);
+		startScreenPlay.callFunction();
 
-		assert(0 && "no return value in  ScreenPlayObserverImplementation::notifyObserverEvent");
+		if (lua_gettop(lua->getLuaState()) == 0) {
+			Logger::console.error("ScreenPlayObserverImplementation::notifyObserverEvent didnt return a value from " + play + ":" + key);
 
-		return 1;
+			assert(0 && "no return value in  ScreenPlayObserverImplementation::notifyObserverEvent");
+
+			return 1;
+		}
+
+
+		ret = lua->getIntParameter(lua->getLuaState());
+
+	} catch (LuaPanicException& panic) {
+		Logger::console.error("Panic exception: " + panic.getMessage() + " while trying to run SceenPlayObserver: " + play + ":" + key);
 	}
-
-
-	int ret = lua->getIntParameter(lua->getLuaState());
 
 	//1 remove observer, 0 keep observer
 
