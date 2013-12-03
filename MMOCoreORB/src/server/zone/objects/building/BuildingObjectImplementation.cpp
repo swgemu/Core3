1c9b094a1022de33ac900592a6a469cf9f918b72
Victor Popovici <victorpopovici@gmail.com>
2013-12-03 09:56:10 +0100

    [fixed] stability issues
    Change-Id: I74994b195a202aea21d391a51981c3db873b6200

--- MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
@@ -103,7 +103,11 @@ void BuildingObjectImplementation::notifyLoadFromDatabase() {
 			CellObject* cell = cells.get(i);
 
 			for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
+				ReadLocker rlocker(cell->getContainerLock());
+
 				SceneObject* child = cell->getContainerObject(j);
+
+				rlocker.release();
 
 				zone->updateActiveAreas(child);
 			}
@@ -120,7 +124,11 @@ void BuildingObjectImplementation::notifyInsertToZone(Zone* zone) {
 		CellObject* cell = cells.get(i);
 
 		for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
+			ReadLocker rlocker(cell->getContainerLock());
+
 			SceneObject* child = cell->getContainerObject(j);
+
+			rlocker.release();
 
 			notifyObjectInsertedToZone(child);
 		}
@@ -193,7 +201,11 @@ void BuildingObjectImplementation::sendTo(SceneObject* player, bool doClose) {
 		}
 
 		for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
+			ReadLocker rlocker(cell->getContainerLock());
+
 			ManagedReference<SceneObject*> containerObject = cell->getContainerObject(j);
+
+			rlocker.release();
 
 			if (containerObject != NULL && ((containerObject->isCreatureObject() && publicStructure) || player == containerObject
 							|| (closeObjects != NULL && closeObjects->contains(containerObject.get()))))
@@ -285,7 +297,9 @@ void BuildingObjectImplementation::notifyRemoveFromZone() {
 		//cell->resetCurrentNumerOfPlayerItems();
 
 		while (cell->getContainerObjectsSize() > 0) {
+			ReadLocker rlocker(cell->getContainerLock());
 			ManagedReference<SceneObject*> obj = cell->getContainerObject(0);
+			rlocker.release();
 
 			/*obj->removeFromZone();
 
@@ -443,7 +457,9 @@ void BuildingObjectImplementation::notifyInsert(QuadTreeEntry* obj) {
 
 		try {
 			for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
-				SceneObject* child = cell->getContainerObject(j);
+				ReadLocker rlocker(cell->getContainerLock());
+				ManagedReference<SceneObject*> child = cell->getContainerObject(j);
+				rlocker.release();
 
 				if (child != obj && child != NULL) {
 					if ((objectInThisBuilding || (child->isCreatureObject() && isPublicStructure())) || isStaticBuilding()) {
@@ -487,7 +503,10 @@ void BuildingObjectImplementation::notifyDissapear(QuadTreeEntry* obj) {
 		CellObject* cell = cells.get(i);
 
 		for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
+
+			ReadLocker rlocker(cell->getContainerLock());
 			ManagedReference<SceneObject*> child = cell->getContainerObject(j);
+			rlocker.release();
 
 			if (child == NULL)
 				continue;
@@ -544,7 +563,9 @@ void BuildingObjectImplementation::destroyObjectFromDatabase(
 		CellObject* cell = cells.get(i);
 
 		for (int j = cell->getContainerObjectsSize() - 1; j >= 0 ; --j) {
-			SceneObject* child = cell->getContainerObject(j);
+			ReadLocker rlocker(cell->getContainerLock());
+			ManagedReference<SceneObject*> child = cell->getContainerObject(j);
+			rlocker.release();
 
 			if (child->isPlayerCreature()) {
 				child->teleport(x, z, y);
@@ -832,7 +853,10 @@ int BuildingObjectImplementation::notifyObjectInsertedToChild(SceneObject* objec
 
 				if (cell != NULL) {
 					for (int j = 0; j < cell->getContainerObjectsSize(); ++j) {
-						SceneObject* child = cell->getContainerObject(j);
+
+						ReadLocker rlocker(cell->getContainerLock());
+						ManagedReference<SceneObject*> child = cell->getContainerObject(j);
+						rlocker.release();
 
 						if (child != object) {
 							//if (is)
