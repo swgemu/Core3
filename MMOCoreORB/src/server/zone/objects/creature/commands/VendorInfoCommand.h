875f8d76f4f156f4b2ba3d8e8058045b5175c738
Ivojedi <ivojedi@swgemu.com>
2014-01-11 11:55:23 -0800

    [Added] VendorInfo Command
    Change-Id: I6bcce0215f8332938b48e109f762edea0256c969

--- MMOCoreORB/src/server/zone/objects/creature/commands/VendorInfoCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/VendorInfoCommand.h
@@ -16,9 +16,94 @@
 class VendorInfoCommand {
 public:
 	static int executeCommand(CreatureObject* creature, uint64 target, const UnicodeString& arguments) {
-		if (!creature->isPlayerCreature())
+		if (!creature->isPlayerCreature() && !creature->getPlayerObject()->isPrivileged())
 			return 1;
 
+		ManagedReference<CreatureObject*> targetObject;
+
+		if (!arguments.isEmpty()) {
+			ManagedReference<PlayerManager*> playerManager = creature->getZoneServer()->getPlayerManager();
+
+			targetObject = playerManager->getPlayer(arguments.toString());
+		} else {
+			targetObject = creature->getZoneServer()->getObject(creature->getTargetID()).castTo<CreatureObject*>();
+		}
+
+		if (targetObject == NULL || !targetObject->isPlayerCreature()) {
+			targetObject = creature;
+		}
+
+		Locker locker(targetObject, creature);
+
+		PlayerObject* ghost = targetObject->getPlayerObject();
+
+		ManagedReference<SuiMessageBox*> box = new SuiMessageBox(creature, 0);
+
+		box->setPromptTitle("Vendor Info");
+
+		StringBuffer promptText;
+
+		promptText << "Player Name:\t" << targetObject->getFirstName() << endl;
+		promptText << "ObjectID:\t" << targetObject->getObjectID() << endl;
+		promptText << "Max number of vendors:\t" << targetObject->getSkillMod("manage_vendor") << endl << endl;
+		promptText << "Vendors:" << endl;
+
+		SortedVector<unsigned long long>* ownedVendors = ghost->getOwnedVendors();
+		for (int i = 0; i < ownedVendors->size(); i++) {
+			ManagedReference<SceneObject*> vendor = creature->getZoneServer()->getObject(ownedVendors->elementAt(i));
+
+			int num = i + 1;
+			promptText << endl << String::valueOf(num) << ". ";
+
+			if (vendor == NULL) {
+				promptText << "NULL Vendor" << endl << endl;
+				continue;
+			}
+
+			promptText << "VendorID:\t" << vendor->getObjectID() << endl;
+
+			DataObjectComponentReference* data = vendor->getDataObjectComponent();
+			if(data == NULL || data->get() == NULL || !data->get()->isVendorData()) {
+				promptText << "    NULL Data Component" << endl << endl;
+				continue;
+			}
+
+			VendorDataComponent* vendorData = cast<VendorDataComponent*>(data->get());
+			if(vendorData == NULL) {
+				promptText << "    NULL Vendor Data Component" << endl << endl;
+				continue;
+			}
+
+			bool init = false;
+			if (vendorData->isInitialized())
+				init = true;
+
+			promptText << "    Initialized?\t" << (init ? "Yes" : "No");
+			promptText << endl;
+
+			promptText << "    Parent:\t";
+
+			ManagedReference<SceneObject*> parent = vendor->getParent().get();
+			if (parent == NULL)
+				promptText << "NULL" << endl;
+			else
+				promptText << parent->getObjectID() << endl;
+
+			promptText << "    Zone:\t";
+
+			Zone* zone = vendor->getZone();
+			if (zone == NULL) {
+				promptText << "NULL" << endl;
+			} else {
+				promptText << zone->getZoneName() << endl;
+				promptText << "    World Position:\t" << vendor->getWorldPositionX() << ", " << vendor->getWorldPositionY() << endl;
+			}
+
+		}
+
+		box->setPromptText(promptText.toString());
+
+		creature->sendMessage(box->generateMessage());
 
 		return 0;
 	}
