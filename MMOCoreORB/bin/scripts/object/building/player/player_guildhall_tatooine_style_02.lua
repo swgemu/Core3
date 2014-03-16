2895a5ddf418c9189785998b3260ecb544cc28aa
Klivian <klivian@hotmail.com>
2014-03-16 18:48:42 +0000

    [added] merchant signs to guild halls [added] merchant signs to merchant tents
    Change-Id: I7ff485bbc054137eb995a4cbf50d28caa25014e5

--- MMOCoreORB/bin/scripts/object/building/player/player_guildhall_tatooine_style_02.lua
+++ MMOCoreORB/bin/scripts/object/building/player/player_guildhall_tatooine_style_02.lua
@@ -58,7 +58,14 @@ object_building_player_player_guildhall_tatooine_style_02 = object_building_play
 			{templateFile = "object/tangible/sign/player/house_address_tatooine.iff", x = 0, z = 1.4, y = 16.24, ox = 0, oy = 1, oz = 0, ow = 0, cellid = -1, containmentType = -1},
 			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 9.13004, z = 1.49609, y = 6.3587, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = 2, containmentType = -1}, 
 			{templateFile = "object/tangible/terminal/terminal_guild.iff", x = 10.7, z = 1.50, y = 9.3, ow = 0, ox = 0, oy = 1, oz = 0, cellid = 2, containmentType = -1}
-	}
+	},
+	shopSigns = {
+			{templateFile = "object/tangible/sign/player/house_address_tatooine.iff", x = 0, z = 1.4, y = 16.24, ox = 0, oy = 1, oz = 0, ow = 0, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},	
+			{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = 0, z = 0.5, y = 17, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_01", suiItem = "@player_structure:shop_sign1"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = 0, z = 0.5, y = 17, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_02", suiItem = "@player_structure:shop_sign2"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = 0, z = 0.5, y = 17, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_03", suiItem = "@player_structure:shop_sign3"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = 0, z = 0.5, y = 17, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_04", suiItem = "@player_structure:shop_sign4"},			
+	},	
 }
 
 ObjectTemplates:addTemplate(object_building_player_player_guildhall_tatooine_style_02, "object/building/player/player_guildhall_tatooine_style_02.iff")
