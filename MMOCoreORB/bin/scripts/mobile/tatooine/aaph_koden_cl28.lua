9fe3ea4dc74273f1eb5df870e9c0526de97f3a0e
Ruledo <ruledo707@gmail.com>
2014-03-16 20:53:14 +0000

    [added] Aaph Koden Quest
    Change-Id: I6be8a2afa5d2533da814059bb4e9b27f83b8b878

---
+++ MMOCoreORB/bin/scripts/mobile/tatooine/aaph_koden_cl28.lua
@@ -0,0 +1,52 @@
+aaph_koden_cl28 = Creature:new {
+	objectName = "",
+	customName = "(Imperial Stormtrooper Commander)",	
+	socialGroup = "imperial",
+	pvpFaction = "imperial",
+	faction = "imperial",
+	level = 29,
+	chanceHit = 3.08,
+	damageMin = 280,
+	damageMax = 290,
+	baseXp = 3005,
+	baseHAM = 8300,
+	baseHAMmax = 10100,
+	armor = 0,
+	resists = {20,20,20,-1,30,-1,30,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_stormtrooper_groupleader_m.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 100000},
+				{group = "junk", chance = 6500000},
+				{group = "rifles", chance = 550000},
+				{group = "pistols", chance = 550000},
+				{group = "melee_weapons", chance = 550000},
+				{group = "carbines", chance = 550000},
+				{group = "clothing_attachments", chance = 250000},
+				{group = "armor_attachments", chance = 250000},
+				{group = "stormtrooper_common", chance = 700000}
+			},
+			lootChance = 2800000
+		}						
+	},
+	weapons = {"stormtrooper_weapons"},
+	conversationTemplate = "",
+	attacks = merge(riflemanmaster,carbineermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(aaph_koden_cl28, "aaph_koden_cl28")
