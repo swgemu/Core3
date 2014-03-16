965707f52d1d220cf9a2f05e0fd66aa08568576a
Cosmic Cleric <cosmiccleric@gmail.com>
2014-03-16 20:13:16 +0000

    [added]Implemented 'Register at Location' swg.wikia.com/wiki/Register_(Ability) functionality via the /register command.  Radial menu registering is not implemented yet.
    Change-Id: I29584a857f687ec65001400483fc8236e4d8792f

--- MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
@@ -153,6 +153,8 @@ void SceneObjectImplementation::initializePrivateData() {
 	planetMapCategory = 0;
 	planetMapSubCategory = 0;
 
+	planetMapLocationActiveCode = 0;
+
 	gameObjectType = 0;
 
 	containmentType = 4;
@@ -1672,4 +1674,13 @@ bool SceneObjectImplementation::hasObjectInSlottedContainer(SceneObject* object)
 
 int SceneObjectImplementation::getArrangementDescriptorSize() {
 	return templateObject->getArrangementDescriptors().size();
+}
+
+int SceneObjectImplementation::getPlanetMapLocationActiveCode() {
+	return planetMapLocationActiveCode;
+}
+
+/** 0 = None, 1 = Moon, 2 = Star */
+void SceneObjectImplementation::setPlanetMapLocationActiveCode(int activeCode) {
+	planetMapLocationActiveCode = activeCode;
 }
