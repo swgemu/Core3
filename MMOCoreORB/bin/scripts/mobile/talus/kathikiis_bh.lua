2bfffef722106343f60af403481081ee8ca5595f
bluree <bluree@swgemu.com>
2014-03-21 18:50:33 -0600

    [Added] Kathikiis Ruwahurr quest in Dearic cantina.
    Change-Id: I22d9f0a5b3d9c11e2e2ae6db84fad7202a65ffc3

---
+++ MMOCoreORB/bin/scripts/mobile/talus/kathikiis_bh.lua
@@ -0,0 +1,70 @@
+kathikiis_bh = Creature:new {
+	objectName = "",
+	socialGroup = "lok_mercenaries",
+	pvpFaction = "lok_mercenaries",
+	faction = "lok_mercenaries",
+	level = 44,
+	chanceHit = 0.47,
+	damageMin = 370,
+	damageMax = 450,
+	baseXp = 4370,
+	baseHAM = 9700,
+	baseHAMmax = 11900,
+	armor = 1,
+	resists = {0,0,0,0,0,-1,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_mercenary_warlord_hum_m.iff",
+		"object/mobile/dressed_mercenary_warlord_nikto_m.iff",
+		"object/mobile/dressed_mercenary_warlord_wee_m.iff"
+		},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4600000},
+				{group = "tailor_components", chance = 1000000},
+				{group = "color_crystals", chance = 400000},
+				{group = "crystals_okay", chance = 400000},
+				{group = "melee_unarmed", chance = 600000},
+				{group = "melee_polearm", chance = 600000},
+				{group = "pistols", chance = 600000},
+				{group = "carbines", chance = 600000},
+				{group = "rifles", chance = 600000},
+				{group = "clothing_attachments", chance = 300000},
+				{group = "armor_attachments", chance = 300000}
+			},
+			lootChance = 3500000
+		},
+		{
+			groups = {
+				{group = "color_crystals", chance = 1500000},
+				{group = "crystals_poor", chance = 1000000},
+				{group = "crystals_good", chance = 1000000},
+				{group = "grenades_looted", chance = 1500000},
+				{group = "carbines", chance = 1500000},
+				{group = "pistols", chance = 1500000},
+				{group = "clothing_attachments", chance = 1000000},
+				{group = "armor_attachments", chance = 1000000}
+			},
+			lootChance = 6000000
+		}
+	},
+	weapons = {"rebel_weapons_heavy"},
+	conversationTemplate = "kathikiis_ruwahurr_mission_target_convotemplate",
+	attacks = merge(riflemanmaster,commandomaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(kathikiis_bh, "kathikiis_bh")
