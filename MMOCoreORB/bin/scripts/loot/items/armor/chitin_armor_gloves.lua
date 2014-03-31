07fcfa0cb4ffe2e80ad85752663e201247a6329c
Ivojedi <ivojedi@swgemu.com>
2014-03-31 08:05:19 -0700

    [Adjusted] color customization palettes on looted composite, chitin, and bone armors [Added] junk dealer config for looted composite, chitin, and bone armors
    Change-Id: I3d03399a7609496a438d5b1a945347b9ac5881f4

--- MMOCoreORB/bin/scripts/loot/items/armor/chitin_armor_gloves.lua
+++ MMOCoreORB/bin/scripts/loot/items/armor/chitin_armor_gloves.lua
@@ -18,7 +18,12 @@ chitin_armor_gloves = {
 	customizationStringNames = {"/private/index_color_0", "/private/index_color_1"},
 	customizationValues = {
 			{0, 1},
-			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+			{0, 1, 2, 3, 4, 5}
+	},
+
+	junkDealerTypeNeeded = JUNKARMOUR,
+	junkMinValue = 35,
+	junkMaxValue = 70
 }
 
 addLootItemTemplate("chitin_armor_gloves", chitin_armor_gloves)
