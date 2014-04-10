b327e3b5608b784c91a9ef1cc386deb3eec3669c
Klivian <klivian@hotmail.com>
2014-04-10 03:01:24 +0000

    [added] wearable loot to beldonnas mobiles
    Change-Id: I3c10eecbcef15366022008b51ff56befc6880bf0

--- MMOCoreORB/bin/scripts/mobile/corellia/beldonnas_league_marshal.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/beldonnas_league_marshal.lua
@@ -45,7 +45,8 @@ beldonnas_league_marshal = Creature:new {
 	lootGroups = {
        	{
 			groups = {
-				{group = "junk", chance = 5000000},
+				{group = "junk", chance = 3000000},
+				{group = "wearables_all", chance = 2000000},				
 				{group = "loot_kit_parts", chance = 2000000},
 				{group = "tailor_components", chance = 1000000},
 				{group = "beldonnas_common", chance = 2000000}
--- MMOCoreORB/bin/scripts/mobile/corellia/beldonnas_league_soldier.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/beldonnas_league_soldier.lua
@@ -44,7 +44,8 @@ beldonnas_league_soldier = Creature:new {
 	lootGroups = {
       	{
 			groups = {
-				{group = "junk", chance = 5000000},
+				{group = "junk", chance = 3000000},
+				{group = "wearables_common", chance = 2000000},				
 				{group = "loot_kit_parts", chance = 2000000},
 				{group = "tailor_components", chance = 1000000},
 				{group = "beldonnas_common", chance = 2000000}
