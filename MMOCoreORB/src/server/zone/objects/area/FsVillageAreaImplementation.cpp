ebd7aa5eb5025e231c2769919e9896a160483d42
Valkyra <lyhtmyst@gmail.com>
2014-04-13 05:58:55 +0000

    [Updated] Admin characters can enter the village active area.
    Change-Id: I9126123f6641102fa1f791b46846697d7bb8f19b

--- MMOCoreORB/src/server/zone/objects/area/FsVillageAreaImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/area/FsVillageAreaImplementation.cpp
@@ -8,6 +8,7 @@
 #include "server/zone/objects/area/FsVillageArea.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/Zone.h"
+#include "server/zone/objects/player/PlayerObject.h"
 
 void FsVillageAreaImplementation::notifyEnter(SceneObject* player) {
 	if (!player->isPlayerCreature()) {
@@ -23,11 +24,17 @@ void FsVillageAreaImplementation::notifyEnter(SceneObject* player) {
 	float angle = atan2(diffY == 0.f ? 1 : diffY, diffX == 0.f ? 1 : diffX);
 
 	CreatureObject* playerCreature = cast<CreatureObject*>(player);
+	PlayerObject* ghost = playerCreature->getPlayerObject();
 
-	playerCreature->sendSystemMessage("@fs_quest_village:expel_shield");
+	if (ghost == NULL)
+		return;
 
 	float newPosX = getPositionX() + (cos(angle) * 530);
 	float newPosY = getPositionY() + (sin(angle) * 530);
 
-	playerCreature->teleport(newPosX, getZone()->getHeight(newPosX, newPosY), newPosY, 0);
+	if (!ghost->isPrivileged()) {
+		playerCreature->teleport(newPosX, getZone()->getHeight(newPosX, newPosY), newPosY, 0);
+		playerCreature->sendSystemMessage("@fs_quest_village:expel_shield");
+	}
+
 }
