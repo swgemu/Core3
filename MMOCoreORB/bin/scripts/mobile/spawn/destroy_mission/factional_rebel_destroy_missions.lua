ce6eafd38d67714d866d14a070897d23b5f01710
Ivojedi <ivojedi@swgemu.com>
2014-05-19 21:16:18 -0700

    [Fixed] rebel faction destroy missions
    Change-Id: If6f87830599e90d605431e287075f380b7d58da2

--- MMOCoreORB/bin/scripts/mobile/spawn/destroy_mission/factional_rebel_destroy_missions.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/destroy_mission/factional_rebel_destroy_missions.lua
@@ -6,37 +6,37 @@ factional_rebel_destroy_missions = {
 
 	destroyMissionSpawns = {
 		{
-			lairtemplateName = "imperial_banner",
+			lairTemplateName = "imperial_banner",
 			minDifficulty = 14,
 			maxDifficulty = 20,
 			size = 25,
 		},
 		{
-			lairtemplateName = "imperial",
+			lairTemplateName = "imperial",
 			minDifficulty = 22,
 			maxDifficulty = 28,
 			size = 25,
 		},
 		{
-			lairtemplateName = "global_stormtrooper",
+			lairTemplateName = "global_stormtrooper",
 			minDifficulty = 22,
 			maxDifficulty = 28,
 			size = 25,
 		},
 		{
-			lairtemplateName = "global_imperial_cadet_camp_imperial_small_theater",
+			lairTemplateName = "global_imperial_cadet_camp_imperial_small_theater",
 			minDifficulty = 6,
 			maxDifficulty = 8,
 			size = 25,
 		},
         	{
-			lairtemplateName = "global_imperial_cadet_senior_camp_imperial_small_theater",
+			lairTemplateName = "global_imperial_cadet_senior_camp_imperial_small_theater",
 			minDifficulty = 8,
 			maxDifficulty = 12,
 			size = 25,
 		},
 		{
-			lairtemplateName = "global_imperial_cadet_squadleader_camp_imperial_small_theater",
+			lairTemplateName = "global_imperial_cadet_squadleader_camp_imperial_small_theater",
 			minDifficulty = 9,
 			maxDifficulty = 13,
 			size = 25,
