77755a664f98e6f40d9a56a959645b11f8b8d8a9
Miztah <myztah@gmail.com>
2014-05-21 21:04:45 +0000

    [added] Stoos Olko questline
    Change-Id: I5e94b5dbc52b038c857f12d455b6215a10c3d081

--- MMOCoreORB/bin/scripts/mobile/dantooine/stoos_olko.lua
+++ MMOCoreORB/bin/scripts/mobile/dantooine/stoos_olko.lua
@@ -24,13 +24,13 @@ stoos_olko = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = 128,
+	optionsBitmask = 136,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_stoos_olko.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "stoos_olko_mission_giver_convotemplate",
 	attacks = {
 	}
 }
