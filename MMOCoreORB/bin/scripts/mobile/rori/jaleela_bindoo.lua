5bf9ce83f96b7a5d5c4c9caa2e9d89585a9b3bec
Miztah <myztah@gmail.com>
2014-05-22 19:20:54 +0000

    [added] Jaleela Bindoo questline
    Change-Id: I173cd0e934b4ecafd11d8e41264aff8daf81cc83

---
+++ MMOCoreORB/bin/scripts/mobile/rori/jaleela_bindoo.lua
@@ -0,0 +1,38 @@
+jaleela_bindoo = Creature:new {
+	objectName = "",
+	customName = "Jaleela Bindoo",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 100,
+	chanceHit = 1,
+	damageMin = 645,
+	damageMax = 1000,
+	baseXp = 9429,
+	baseHAM = 24000,
+	baseHAMmax = 30000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_jaleela_bindoo.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "jaleela_bindoo_mission_giver_convotemplate",
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(jaleela_bindoo, "jaleela_bindoo")
---
+++ MMOCoreORB/bin/scripts/mobile/rori/jaleela_protestor_leader.lua
@@ -0,0 +1,45 @@
+jaleela_protestor_leader = Creature:new {
+  objectName = "",
+  socialGroup = "thug",
+  pvpFaction = "thug",
+  faction = "thug",
+  level = 17,
+  chanceHit = 0.320000,
+  damageMin = 180,
+  damageMax = 190,
+  baseXp = 1102,
+  baseHAM = 2400,
+  baseHAMmax = 3000,
+  armor = 0,
+  resists = {0,0,0,0,0,0,0,-1,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0.000000,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE + ENEMY + AGGRESSIVE,
+  creatureBitmask = PACK,
+  optionsBitmask = 128,
+  diet = HERBIVORE,
+
+  templates = { "object/mobile/dressed_criminal_thug_human_male_01.iff" },
+  lootGroups = {
+        {
+      groups = {
+        {group = "junk", chance = 4000000},
+        {group = "wearables_common", chance = 3000000},
+        {group = "loot_kit_parts", chance = 2000000},
+        {group = "tailor_components", chance = 1000000},
+      },
+      lootChance = 3000000
+    }
+  },
+  weapons = {"pirate_weapons_heavy"},
+  attacks = merge(riflemanmaster,pistoleermaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(jaleela_protestor_leader, "jaleela_protestor_leader")
