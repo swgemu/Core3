61be5c4a51e0f5676b537224db5933804d4e3c2c
Miztah <myztah@gmail.com>
2014-05-22 19:20:54 +0000

    [added] Jaleela Bindoo questline
    Change-Id: I173cd0e934b4ecafd11d8e41264aff8daf81cc83

---
+++ MMOCoreORB/bin/scripts/mobile/rori/odoof_grunda.lua
@@ -0,0 +1,38 @@
+odoof_grunda = Creature:new {
+  objectName = "",
+  customName = "Odoof Grunda",
+  socialGroup = "townsperson",
+  pvpFaction = "townsperson",
+  faction = "townsperson",
+  level = 100,
+  chanceHit = 1,
+  damageMin = 645,
+  damageMax = 1000,
+  baseXp = 9429,
+  baseHAM = 24000,
+  baseHAMmax = 30000,
+  armor = 0,
+  resists = {0,0,0,0,0,0,0,0,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = NONE,
+  creatureBitmask = NONE,
+  optionsBitmask = 136,
+  diet = HERBIVORE,
+
+  templates = {"object/mobile/gungan_male.iff"},
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "jaleela_bindoo_mission_target_convotemplate",
+  attacks = {
+  }
+}
+
+CreatureTemplates:addCreatureTemplate(odoof_grunda, "odoof_grunda")
