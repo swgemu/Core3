da16ba1000b7324523dac45921149a2649e061d7
Miztah <myztah@gmail.com>
2014-05-24 17:33:35 +0000

    [added] Raxa Binn quests (Rori)
    Change-Id: Ic34fb09fa9fa27b9c9d519fc3ff5e9a54e6a3db9

---
+++ MMOCoreORB/bin/scripts/mobile/rori/raxa_stormtrooper_squad_leader.lua
@@ -0,0 +1,52 @@
+raxa_stormtrooper_squad_leader = Creature:new {
+	objectName = "@mob/creature_names:stormtrooper_squad_leader",
+	socialGroup = "imperial",
+	pvpFaction = "imperial",
+	faction = "imperial",
+  level = 22,
+  chanceHit = 0.33,
+  damageMin = 190,
+  damageMax = 200,
+  baseXp = 2219,
+  baseHAM = 5000,
+  baseHAMmax = 6100,
+	armor = 0,
+	resists = {20,20,20,-1,30,-1,30,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_stormtrooper_squad_leader_white_white.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 100000},
+				{group = "junk", chance = 5500000},
+				{group = "rifles", chance = 550000},
+				{group = "pistols", chance = 550000},
+                {group = "melee_weapons", chance = 550000},
+                {group = "carbines", chance = 550000},
+				{group = "clothing_attachments", chance = 250000},
+				{group = "armor_attachments", chance = 250000},
+				{group = "stormtrooper_common", chance = 700000},
+				{group = "wearables_common", chance = 1000000}
+			},
+			lootChance = 2800000
+		}						
+	},
+	weapons = {"stormtrooper_weapons"},
+  conversationTemplate = "",
+	attacks = merge(riflemanmaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(raxa_stormtrooper_squad_leader, "raxa_stormtrooper_squad_leader")
