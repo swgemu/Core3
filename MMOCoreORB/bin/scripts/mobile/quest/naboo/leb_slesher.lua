5bd57adad518e38ee7fc9e3d2162cbf0200c8afd
Miztah <myztah@gmail.com>
2014-05-26 17:25:18 +0000

    [added] Leb Slesher quests (Naboo)
    Change-Id: Ia7ebd1704941a6990ee21065be157682184dc6ff

--- MMOCoreORB/bin/scripts/mobile/quest/naboo/leb_slesher.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/leb_slesher.lua
@@ -24,13 +24,13 @@ leb_slesher = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = 128,
+	optionsBitmask = 136,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_leb_slesher.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "leb_slesher_mission_giver_convotemplate",
 	attacks = {
 	}
 }
