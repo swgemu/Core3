9e67626054621fb54a71eaaba1c36a32ea06a331
Valkyra <lyhtmyst@gmail.com>
2014-05-28 20:20:48 +0000

    [fixed] Paemos's location in the Village. [updated] Paemos bitmask to be able to converse. [added] Paemos conversation script by Nee. [added] Start of Paemos conversation logic, including branch unlocking and experience conversion.
    Change-Id: I6bcc61b0b950fc3b3c2e923f9d9ed38a0212a698

---
+++ MMOCoreORB/bin/scripts/screenplays/village/fs_experience_converter_conv_handler.lua
@@ -0,0 +1,87 @@
+local ExperienceConverter = require("screenplays.village.experience_converter")
+
+fs_experience_converter_conv_handler = Object:new {
+}
+
+function fs_experience_converter_conv_handler:getNextConversationScreen(pConversationTemplate, pConversingPlayer, selectedOption)
+	local creature = LuaCreatureObject(pConversingPlayer)
+	local convosession = creature:getConversationSession()
+
+	local lastConversationScreen = nil
+
+	if (convosession ~= nil) then
+		local session = LuaConversationSession(convosession)
+		lastConversationScreen = session:getLastConversationScreen()
+	end
+
+	local conversation = LuaConversationTemplate(pConversationTemplate)
+
+	local nextConversationScreen
+
+	if (lastConversationScreen ~= nil) then
+		local luaLastConversationScreen = LuaConversationScreen(lastConversationScreen)
+
+		--Get the linked screen for the selected option.
+		local optionLink = luaLastConversationScreen:getOptionLink(selectedOption)
+
+		nextConversationScreen = conversation:getScreen(optionLink)
+
+		if nextConversationScreen ~= nil then
+			local nextLuaConversationScreen = LuaConversationScreen(nextConversationScreen)
+		else
+			nextConversationScreen = conversation:getScreen("init")
+		end
+	else
+		nextConversationScreen = conversation:getScreen("init")
+	end
+	return nextConversationScreen
+end
+
+function fs_experience_converter_conv_handler:runScreenHandlers(pConversationTemplate, pConversingPlayer, pConversingNpc, selectedOption, pConversationScreen)
+	local screen = LuaConversationScreen(pConversationScreen)
+
+	local screenID = screen:getScreenID()
+
+	if screenID == "init" then
+		pConversationScreen = fs_experience_converter_conv_handler.handleInit(pConversationTemplate, pConversingPlayer, pConversingNpc, selectedOption, pConversationScreen)
+	elseif screenID == "cs_jsPlumb_1_7" then
+		local nextUnlockableBranches = ExperienceConverter:getNextUnlockableBranches(pConversingPlayer)
+
+		pConversationScreen = screen:cloneScreen()
+		local clonedConversation = LuaConversationScreen(pConversationScreen)
+
+		if (nextUnlockableBranches ~= nil) then
+			local theTOText = nextUnlockableBranches[1]
+			if (table.getn(nextUnlockableBranches) > 1) then
+				for i = 2, table.getn(nextUnlockableBranches), 1 do
+					theTOText = theTOText .. " , " .. nextUnlockableBranches[i]
+				end
+			end
+			clonedConversation:setDialogTextTO(theTOText)
+		elseif (nextUnlockableBranches == nil) then
+			clonedConversation:setCustomDialogText("I sense you are unable to learn any new skills at this time.")
+		end
+	elseif screenID == "cs_jsPlumb_1_17" then -- Unlocking new branches.
+		--ExperienceConverter:unlockNewBranchDialog(pConversingPlayer)
+	elseif screenID == "cs_jsPlumb_1_11" then -- Unlocking new branches.
+		ExperienceConverter:chooseExperienceTypeDialog(pConversingPlayer)
+	end
+
+	return pConversationScreen
+end
+
+function fs_experience_converter_conv_handler.handleInit(pConversationTemplate, pConversingPlayer, pConversingNpc, selectedOption, pConversationScreen)
+	local conversationTemplate = LuaConversationTemplate(pConversationTemplate)
+	local nextScreen = nil
+
+	local playerCreature = LuaCreatureObject(pConversingPlayer)
+
+	-- See if they have a quest to unlock, or having XP to convert.
+	if (ExperienceConverter.qualifiesForConversation(pConversingPlayer) == true) then
+		nextScreen = "cs_jsPlumb_1_5"
+	elseif ((ExperienceConverter.qualifiesForConversation(pConversingPlayer) == false) or nextScreen == nil) then
+		nextScreen = "cs_jsPlumb_1_19"
+	end
+
+	return conversationTemplate:getScreen(nextScreen)
+end
