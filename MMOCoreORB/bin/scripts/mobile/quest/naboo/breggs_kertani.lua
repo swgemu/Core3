939d36c34af43568f1bd3139c5f358a419fe96d8
Miztah <myztah@gmail.com>
2014-05-30 18:49:43 +0000

    [added] Lergo Brazee quests (Naboo)
    Change-Id: Iea3f8aaf6bf87536244189eb976a61e1b42d3d86

---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/breggs_kertani.lua
@@ -0,0 +1,36 @@
+breggs_kertani = Creature:new {
+	objectName = "",
+	socialGroup = "",
+	pvpFaction = "",
+	faction = "",
+	level = 20,
+	chanceHit = 0.33,
+	damageMin = 190,
+	damageMax = 200,
+	baseXp = 1803,
+	baseHAM = 5000,
+	baseHAMmax = 6100,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_rebel_pilot_rodian_male_01.iff"},
+	lootGroups = {	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "lergo_brazee_mission_target_convotemplate",
+	attacks = merge(riflemanmaster,pistoleermaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(breggs_kertani, "breggs_kertani")
---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/lergo_brazee.lua
@@ -0,0 +1,98 @@
+lergo_brazee_missions =
+  {
+    {
+      missionType = "deliver",
+      primarySpawns =
+      {
+        { npcTemplate = "breggs_kertani", planetName = "naboo", npcName = "Breggs Ker'tani" }
+      },
+      secondarySpawns = {},
+      itemSpawns =
+      {
+        { itemTemplate = "object/tangible/mission/quest_item/lergo_brazee_q1_needed.iff", itemName = "Pilot's Passcodes" }
+      },
+      rewards =
+      {
+        { rewardType = "credits", amount = 50 },
+      }
+    },
+    {
+      missionType = "escort",
+      primarySpawns =
+      {
+        { npcTemplate = "slym_shudee", planetName = "naboo", npcName = "Slym Shu'dee" }
+      },
+      secondarySpawns = {},
+      itemSpawns = {},
+      rewards =
+      {
+        { rewardType = "credits", amount = 50 },
+      }
+    },
+    {
+      missionType = "escort",
+      primarySpawns =
+      {
+        { npcTemplate = "linay_sindir", planetName = "naboo", npcName = "Linay Sind'ir" }
+      },
+      secondarySpawns =
+      {
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" },
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" },
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" },
+      },
+      itemSpawns = {},
+      rewards =
+      {
+        { rewardType = "credits", amount = 50 },
+      }
+    },
+    {
+      missionType = "assassinate",
+      primarySpawns =
+      {
+        { npcTemplate = "big_blismo", planetName = "naboo", npcName = "Big Blismo" }
+      },
+      secondarySpawns =
+      {
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" },
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" },
+        { npcTemplate = "gungan_thug", planetName = "naboo", npcName = "Gungan Thug" }
+      },
+      itemSpawns = {},
+      rewards =
+      {
+        { rewardType = "credits", amount = 100 },
+      }
+    },
+  }
+
+npcMapLergoBrazee =
+  {
+    {
+      spawnData = { planetName = "naboo", npcTemplate = "lergo_brazee", x = 4.0, z = -0.9, y = -4.8, direction = -7, cellID = 5, position = STAND },
+      worldPosition = { x = 2103, y = 2531 },
+      npcNumber = 1,
+      stfFile = "@static_npc/naboo/lergo_brazee",
+      hasWaypointNames = "no",
+      missions = lergo_brazee_missions
+    }
+  }
+
+LergoBrazee = ThemeParkLogic:new {
+  numberOfActs = 1,
+  npcMap = npcMapLergoBrazee,
+  className = "LergoBrazee",
+  screenPlayState = "lergo_brazee_quest",
+  missionCompletionMessageStf = "@theme_park/messages:static_completion_message",
+  distance = 600,
+}
+
+registerScreenPlay("LergoBrazee", true)
+
+lergo_brazee_mission_giver_conv_handler = mission_giver_conv_handler:new {
+  themePark = LergoBrazee
+}
+lergo_brazee_mission_target_conv_handler = mission_target_conv_handler:new {
+  themePark = LergoBrazee
+}
