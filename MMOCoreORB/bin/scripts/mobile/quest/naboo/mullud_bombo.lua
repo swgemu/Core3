b8d51d753b80296261c0ca56a3d969bc66b763bd
Miztah <myztah@gmail.com>
2014-05-30 16:13:15 +0000

    [added] Mullud Bombo quest (Naboo)
    Change-Id: I3c2e65e4c1840db84d1c93d2e1a281e55b0b3809

--- MMOCoreORB/bin/scripts/mobile/quest/naboo/mullud_bombo.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/mullud_bombo.lua
@@ -24,13 +24,13 @@ mullud_bombo = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = 128,
+	optionsBitmask = 136,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_mullud_bombo.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "mullud_bombo_mission_giver_convotemplate",
 	attacks = {
 	}
 }
---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/mullud_bombo.lua
@@ -0,0 +1,49 @@
+mullud_bombo_missions =
+	{
+		{
+			missionType = "deliver",
+			primarySpawns =
+			{
+				{ npcTemplate = "sentellus_gavrel", planetName = "naboo", npcName = "Sentellus Gavrel" }
+			},
+			secondarySpawns = {},
+			itemSpawns = 
+			{
+				{ itemTemplate = "object/tangible/mission/quest_item/mullud_bombo_q1_needed.iff", itemName = "Some survey equipment." }
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 25 },
+			}
+		},
+	}
+
+npcMapMulludBombo =
+	{
+		{
+			spawnData = { planetName = "naboo", npcTemplate = "mullud_bombo", x = 5045.9, z = -192, y = 6787.1, direction = -87, cellID = 0, position = STAND },
+			worldPosition = { x = 5046, y = 6787 },
+			npcNumber = 1,
+			stfFile = "@static_npc/naboo/mullud_bombo",
+			hasWaypointNames = "no",
+			missions = mullud_bombo_missions
+		}
+	}
+
+MulludBombo = ThemeParkLogic:new {
+	numberOfActs = 1,
+	npcMap = npcMapMulludBombo,
+	className = "MulludBombo",
+	screenPlayState = "mullud_bombo_quest",
+	missionCompletionMessageStf = "@theme_park/messages:static_completion_message",
+	distance = 600
+}
+
+registerScreenPlay("MulludBombo", true)
+
+mullud_bombo_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = MulludBombo
+}
+mullud_bombo_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = MulludBombo
+}
