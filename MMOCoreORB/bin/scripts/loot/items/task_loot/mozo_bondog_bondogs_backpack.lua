d7806366422ac761d904c5e9606a62e8dc2e6f5d
Miztah <myztah@gmail.com>
2014-05-31 00:38:07 +0000

    [added] Mozo Bondog quests (Dathomir) [added] Dolac Legasi quests (Dathomir)
    Change-Id: Ifbe129b4171bb9e7453b701b8ef007ce35d4e8c6

---
+++ MMOCoreORB/bin/scripts/loot/items/task_loot/mozo_bondog_bondogs_backpack.lua
@@ -0,0 +1,15 @@
+mozo_bondog_bondogs_backpack = {
+  minimumLevel = 0,
+  maximumLevel = -1,
+  customObjectName = "",
+  directObjectTemplate = "object/tangible/mission/quest_item/mozo_bondog_q2_needed.iff",
+  craftingValues = {
+  },
+  customizationStringNames = {},
+  customizationValues = {},
+  skillMods = {
+
+  }
+}
+
+addLootItemTemplate("mozo_bondog_bondogs_backpack", mozo_bondog_bondogs_backpack)
---
+++ MMOCoreORB/bin/scripts/mobile/quest/dathomir/mozo_mercenary_warlord.lua
@@ -0,0 +1,47 @@
+mozo_mercenary_warlord = Creature:new {
+	objectName = "@mob/creature_names:mercenary_warlord",
+	socialGroup = "lok_mercenaries",
+	pvpFaction = "lok_mercenaries",
+	faction = "lok_mercenaries",
+	level = 44,
+	chanceHit = 0.47,
+	damageMin = 370,
+	damageMax = 450,
+	baseXp = 4370,
+	baseHAM = 9700,
+	baseHAMmax = 11900,
+	armor = 1,
+	resists = {0,0,0,0,0,-1,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_mercenary_warlord_hum_m.iff",
+		"object/mobile/dressed_mercenary_warlord_nikto_m.iff",
+		"object/mobile/dressed_mercenary_warlord_wee_m.iff"
+		},
+	lootGroups = {
+		{
+			groups = {
+				{group = "task_loot_mozo_bondog_bondogs_backpack", chance = 10000000}
+			},
+			lootChance = 10000000
+		}
+	},
+	weapons = {"rebel_weapons_heavy"},
+	conversationTemplate = "",
+	attacks = merge(riflemanmaster,commandomaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(mozo_mercenary_warlord, "mozo_mercenary_warlord")
