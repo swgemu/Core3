60a701b409d7bbc5247d8ed16775449f990b43a3
Klivian <klivian@hotmail.com>
2014-06-10 01:06:08 +0000

    [added] /clearVeteranReward admin command
    Change-Id: Iab4576b0f19e75dc91bbe613e17926d07f31bf88

--- MMOCoreORB/src/server/zone/objects/creature/commands/ClearVeteranRewardCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ClearVeteranRewardCommand.h
@@ -56,18 +56,46 @@ public:
 
 	}
 
-	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
+	int doQueueCommand(CreatureObject* player, const uint64& target, const UnicodeString& arguments) {
 
-		if (!checkStateMask(creature))
+		if (!checkStateMask(player))
 			return INVALIDSTATE;
 
-		if (!checkInvalidLocomotions(creature))
+		if (!checkInvalidLocomotions(player))
 			return INVALIDLOCOMOTION;
 
-		ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+		ManagedReference<PlayerObject*> ghost = player->getPlayerObject();
 
 		if (ghost == NULL || !ghost->isPrivileged())
 			return INSUFFICIENTPERMISSION;
+
+		StringTokenizer tokenizer(arguments.toString());
+		tokenizer.setDelimeter(" ");
+		int milestone = tokenizer.getIntToken();
+		if( milestone < 0 ){
+			player->sendSystemMessage("SYNTAX: /clearVeteranReward player milestone");
+			return INVALIDPARAMETERS;
+		}
+
+		ManagedReference<SceneObject*> obj = server->getZoneServer()->getObject(target);
+		if (obj == NULL || !obj->isPlayerCreature()) {
+			player->sendSystemMessage("SYNTAX: /clearVeteranReward player milestone");
+			return INVALIDPARAMETERS;
+		}
+
+		CreatureObject* targetCreature = cast<CreatureObject*>( obj.get());
+		PlayerObject* targetGhost = targetCreature->getPlayerObject();
+
+		Locker clocker(targetCreature, player);
+
+		String reward = targetGhost->getChosenVeteranReward( milestone );
+		if( reward.isEmpty() ){
+			player->sendSystemMessage("Player has not chosen a reward for that milestone");
+			return GENERALERROR;
+		}
+
+		targetGhost->clearVeteranReward( milestone );
+		player->sendSystemMessage( targetCreature->getFirstName() + "'s " + String::valueOf(milestone) + "-day reward has been cleared" );
 
 		return SUCCESS;
 	}
