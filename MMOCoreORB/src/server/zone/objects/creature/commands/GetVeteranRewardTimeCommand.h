267fa9810db416fa83b3ca14b640a1a94cff586d
Klivian <klivian@hotmail.com>
2014-06-12 00:53:43 +0000

    [added] /getVeteranRewardTime command
    Change-Id: I5c5c7cf32337869ff97b36e61fa78be8f404d5f3

--- MMOCoreORB/src/server/zone/objects/creature/commands/GetVeteranRewardTimeCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/GetVeteranRewardTimeCommand.h
@@ -55,13 +55,57 @@ public:
 
 	}
 
-	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
+	int doQueueCommand(CreatureObject* player, const uint64& target, const UnicodeString& arguments) {
 
-		if (!checkStateMask(creature))
+		if (!checkStateMask(player))
 			return INVALIDSTATE;
 
-		if (!checkInvalidLocomotions(creature))
+		if (!checkInvalidLocomotions(player))
 			return INVALIDLOCOMOTION;
+
+		if( player == NULL || !player->isPlayerCreature() )
+			return GENERALERROR;
+
+		PlayerObject* playerGhost = player->getPlayerObject();
+		if( playerGhost == NULL )
+			return GENERALERROR;
+
+		PlayerManager* playerManager = player->getZoneServer()->getPlayerManager();
+		if( playerManager == NULL )
+			return GENERALERROR;
+
+		// Get account
+		ManagedReference<Account*> account = playerManager->getAccount( playerGhost->getAccountID() );
+		if( account == NULL )
+			return GENERALERROR;
+
+		// Send message with current account age
+		StringIdChatParameter timeActiveMsg;
+		timeActiveMsg.setStringId("veteran", "self_time_active"); // You have %DI days logged for veteran rewards.
+		timeActiveMsg.setDI( account->getAgeInDays() );
+		player->sendSystemMessage( timeActiveMsg );
+
+		StringBuffer buff;
+
+		// Check if player is eligible for a reward
+		int eligibleMilestone = playerManager->getEligibleMilestone( playerGhost, account );
+		if( eligibleMilestone > -1 ){
+			buff << "You are eligible for the " << String::valueOf(eligibleMilestone) << "-day reward!";
+			player->sendSystemMessage( buff.toString() );
+			player->sendSystemMessage( "Use /claimVeteranReward to choose your reward!" );
+			return SUCCESS;
+		}
+		else{
+			player->sendSystemMessage( "@veteran:not_eligible"); // You are not currently eligible for a veteran reward.
+		}
+
+		// Check next milestone
+		int nextMilestone = playerManager->getFirstIneligibleMilestone( playerGhost, account );
+		if( nextMilestone > -1 ){
+			buff << "You will be eligible for the " << String::valueOf(nextMilestone) << "-day reward in ";
+			buff << String::valueOf(nextMilestone - account->getAgeInDays() ) << " days";
+			player->sendSystemMessage( buff.toString() );
+		}
 
 		return SUCCESS;
 	}
