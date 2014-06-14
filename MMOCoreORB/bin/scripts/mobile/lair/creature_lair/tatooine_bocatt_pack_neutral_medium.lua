edef17b4360cd050fddd38964011a9fe2b5e0e38
Klivian <klivian@hotmail.com>
2014-06-14 16:10:47 +0000

    [added] population to tatooine bocatt lairs
    Change-Id: I9460d3123c1d51e7fde16958242635eabe37d24d

--- MMOCoreORB/bin/scripts/mobile/lair/creature_lair/tatooine_bocatt_pack_neutral_medium.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_lair/tatooine_bocatt_pack_neutral_medium.lua
@@ -1,11 +1,11 @@
 tatooine_bocatt_pack_neutral_medium = Lair:new {
-	mobiles = {},
+	mobiles = {{"bocatt",1}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rock_shelter_small.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rock_shelter_small.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rock_shelter_small.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rock_shelter_small.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rock_shelter_small.iff"}
 }
 
 addLairTemplate("tatooine_bocatt_pack_neutral_medium", tatooine_bocatt_pack_neutral_medium)
