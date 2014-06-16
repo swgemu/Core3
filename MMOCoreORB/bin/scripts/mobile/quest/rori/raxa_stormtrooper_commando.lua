c0aa1351b9a1b81436949305ca3892501e6c383f
Miztah <myztah@gmail.com>
2014-06-16 22:47:34 +0000

    [fixed] Various quest mob issues
    Change-Id: Ie041c493996528e2e7cbcf519eab4b487a4d770e

---
+++ MMOCoreORB/bin/scripts/mobile/quest/rori/raxa_stormtrooper_commando.lua
@@ -0,0 +1,52 @@
+raxa_stormtrooper_commando = Creature:new {
+	objectName = "@mob/creature_names:storm_commando",
+	socialGroup = "imperial",
+	pvpFaction = "imperial",
+	faction = "imperial",
+	level = 29,
+	chanceHit = 0.38,
+	damageMin = 280,
+	damageMax = 290,
+	baseXp = 3005,
+	baseHAM = 8300,
+	baseHAMmax = 10100,
+	armor = 0,
+	resists = {0,0,0,30,-1,30,-1,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_stormtrooper_commando_m.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 100000},
+				{group = "junk", chance = 5500000},
+				{group = "rifles", chance = 550000},
+				{group = "pistols", chance = 550000},
+				{group = "melee_weapons", chance = 550000},
+				{group = "carbines", chance = 550000},
+				{group = "clothing_attachments", chance = 250000},
+				{group = "armor_attachments", chance = 250000},
+				{group = "stormtrooper_common", chance = 700000},
+				{group = "wearables_common", chance = 1000000}
+			},
+			lootChance = 2800000
+		}						
+	},
+	weapons = {"stormtrooper_weapons"},
+	conversationTemplate = "",
+	attacks = merge(riflemanmaster,carbineermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(raxa_stormtrooper_commando, "raxa_stormtrooper_commando")
