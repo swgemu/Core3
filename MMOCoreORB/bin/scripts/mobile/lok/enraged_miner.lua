cf60c00b3d011a82fd317c9d6f73280991808b35
Miztah <myztah@gmail.com>
2014-06-21 01:41:04 +0000

    [added] Populated Lok Gas Mine
    Change-Id: Ia054ca0ca8e54284a47a67823f4ffc31f6f9a354

---
+++ MMOCoreORB/bin/scripts/mobile/lok/enraged_miner.lua
@@ -0,0 +1,37 @@
+enraged_miner = Creature:new {
+	objectName = "",
+	customName = "an Enraged Miner",
+	socialGroup = "",
+	pvpFaction = "",
+	faction = "",
+	level = 9,
+	chanceHit = 0.27,
+	damageMin = 80,
+	damageMax = 90,
+	baseXp = 356,
+	baseHAM = 675,
+	baseHAMmax = 825,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + HERD + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = { "object/mobile/dressed_kobola_miner_human_male_01.iff" },
+	lootGroups = {},
+	weapons = {"pirate_weapons_medium"},
+	conversationTemplate = "",
+	attacks = merge(brawlernovice,marksmannovice)
+}
+
+CreatureTemplates:addCreatureTemplate(enraged_miner, "enraged_miner")
