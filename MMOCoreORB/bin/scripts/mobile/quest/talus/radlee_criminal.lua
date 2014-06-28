028cd7a47670a1f2365c0582688f1848cb369393
Miztah <myztah@gmail.com>
2014-06-28 06:37:36 +0000

    [added] Radlee Mathiss quests (Talus)
    Change-Id: I8270d29c70f6f94bf8300b3de59e5c44fc67e62b

---
+++ MMOCoreORB/bin/scripts/mobile/quest/talus/radlee_criminal.lua
@@ -0,0 +1,56 @@
+radlee_criminal = Creature:new {
+	objectName = "@mob/creature_names:criminal",
+	socialGroup = "thug",
+	pvpFaction = "thug",
+	faction = "thug",
+	level = 14,
+	chanceHit = 0.300000,
+	damageMin = 150,
+	damageMax = 160,
+	baseXp = 714,
+	baseHAM = 2000,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_criminal_thug_human_male_01.iff",
+					"object/mobile/dressed_criminal_thug_bothan_female_01.iff",
+					"object/mobile/dressed_goon_twk_male_01.iff",
+					"object/mobile/dressed_robber_twk_female_01.iff",
+					"object/mobile/dressed_goon_twk_female_01.iff",
+					"object/mobile/dressed_robber_human_female_01.iff",
+					"object/mobile/dressed_villain_trandoshan_male_01.iff",
+					"object/mobile/dressed_criminal_thug_bothan_male_01.iff",
+					"object/mobile/dressed_villain_trandoshan_female_01.iff"
+					},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4000000},
+				{group = "wearables_common", chance = 2000000},				
+				{group = "pistols", chance = 1000000},
+				{group = "tailor_components", chance = 1500000},
+				{group = "loot_kit_parts", chance = 1500000}
+			},
+			lootChance = 2200000
+		}
+	},
+	weapons = {"pirate_weapons_light"},
+	conversationTemplate = "",
+	attacks = merge(marksmanmaster,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(radlee_criminal, "radlee_criminal")
---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/talus/radlee_mathiss.lua
@@ -0,0 +1,85 @@
+radlee_mathiss_missions =
+	{
+		{
+			missionType = "retrieve",
+			primarySpawns =
+			{
+				{ npcTemplate = "radlee_businessman", planetName = "talus", npcName = "a Businessman" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "radlee_thug", planetName = "talus", npcName = "a Thug" },
+				{ npcTemplate = "radlee_thug", planetName = "talus", npcName = "a Thug" },
+			},
+			itemSpawns =
+			{
+				{ itemTemplate = "object/tangible/mission/quest_item/radlee_mathiss_q1_needed.iff", itemName = "Credit Disc" }
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 30 },
+			}
+		},
+		{
+			missionType = "escort",
+			primarySpawns =
+			{
+				{ npcTemplate = "radlee_businessman", planetName = "talus", npcName = "a CSA Representive" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "radlee_thug", planetName = "talus", npcName = "a Thug" },
+				{ npcTemplate = "radlee_thug", planetName = "talus", npcName = "a Thug" },
+				{ npcTemplate = "radlee_thug", planetName = "talus", npcName = "a Thug" },
+			},
+			itemSpawns = { },
+			rewards =
+			{
+				{ rewardType = "credits", amount = 50 },
+			}
+		},
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "radlee_criminal", planetName = "talus", npcName = "a Criminal" },
+				{ npcTemplate = "radlee_criminal", planetName = "talus", npcName = "a Criminal" },
+				{ npcTemplate = "radlee_criminal", planetName = "talus", npcName = "a Criminal" },
+			},
+			secondarySpawns = { },
+			itemSpawns = { },
+			rewards =
+			{
+				{ rewardType = "credits", amount = 75 },
+			}
+		},
+	}
+
+npcMapRadleeMathiss =
+	{
+		{
+			spawnData = { planetName = "talus", npcTemplate = "radlee_mathiss", x = -2.6, z = 0, y = 6.3, direction = 90, cellID = 3175391, position = STAND },
+			worldPosition = { x = 435, y = -2922 },
+			npcNumber = 1,
+			stfFile = "@static_npc/talus/talus_dearic_radlee_mathiss",
+			missions = radlee_mathiss_missions
+		},
+	}
+
+RadleeMathiss = ThemeParkLogic:new {
+	numberOfActs = 1,
+	npcMap = npcMapRadleeMathiss,
+	className = "RadleeMathiss",
+	screenPlayState = "radlee_mathiss_quest",
+	distance = 800,
+	missionCompletionMessageStf = "@theme_park/messages:static_completion_message",
+}
+
+registerScreenPlay("RadleeMathiss", true)
+
+radlee_mathiss_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = RadleeMathiss
+}
+radlee_mathiss_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = RadleeMathiss
+}
