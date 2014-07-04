b3dc054751443cca89ba11f4b557bb15395fbbb0
Klivian <klivian@hotmail.com>
2014-07-04 23:31:39 +0000

    [added] veteran rewards may only be claimed once per account [added] clearVeteranReward clears rewards for all account's characters
    Change-Id: Id4461bab9ffd62da2161cde1359addb313aacfc3

--- MMOCoreORB/src/server/zone/objects/creature/commands/ClearVeteranRewardCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ClearVeteranRewardCommand.h
@@ -45,8 +45,10 @@ which carries forward this exception.
 #ifndef CLEARVETERANREWARDCOMMAND_H_
 #define CLEARVETERANREWARDCOMMAND_H_
 
+#include "server/zone/ZoneProcessServer.h"
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
+#include "server/login/account/Account.h"
 
 class ClearVeteranRewardCommand : public QueueCommand {
 public:
@@ -83,6 +85,10 @@ public:
 			return INVALIDPARAMETERS;
 		}
 
+		PlayerManager* playerManager = player->getZoneServer()->getPlayerManager();
+		if( playerManager == NULL )
+			return GENERALERROR;
+
 		CreatureObject* targetCreature = cast<CreatureObject*>( obj.get());
 		PlayerObject* targetGhost = targetCreature->getPlayerObject();
 
@@ -94,8 +100,28 @@ public:
 			return GENERALERROR;
 		}
 
-		targetGhost->clearVeteranReward( milestone );
-		player->sendSystemMessage( targetCreature->getFirstName() + "'s " + String::valueOf(milestone) + "-day reward has been cleared" );
+		// Get account
+		ManagedReference<Account*> account = playerManager->getAccount( targetGhost->getAccountID() );
+		if( account == NULL ){
+			player->sendSystemMessage("Error finding account");
+			return GENERALERROR;
+		}
+
+		// Clear reward in all characters registered to the account
+		CharacterList* characters = account->getCharacterList();
+		for(int i = 0; i < characters->size(); ++i) {
+			CharacterListEntry* entry = &characters->get(i);
+			if(entry->getGalaxyID() == server->getZoneServer()->getGalaxyID()) {
+
+				ManagedReference<CreatureObject*> altPlayer = playerManager->getPlayer(entry->getFirstName());
+				if(altPlayer != NULL && altPlayer->getPlayerObject() != NULL) {
+					Locker alocker(altPlayer, player);
+					altPlayer->getPlayerObject()->clearVeteranReward(milestone);
+					player->sendSystemMessage( altPlayer->getFirstName() + "'s " + String::valueOf(milestone) + "-day reward has been cleared" );
+					alocker.release();
+				}
+			}
+		}
 
 		return SUCCESS;
 	}
