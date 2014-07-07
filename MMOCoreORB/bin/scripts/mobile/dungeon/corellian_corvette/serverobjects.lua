67746363601c80f132f4c8db444d9c48bd0e2434
Ivojedi <ivojedi@swgemu.com>
2014-07-06 19:44:21 -0700

    [Removed] several redundant mobile templates [Moved] some mobile templates [Added] enemy flag to a few mobs
    Change-Id: Ic3c29f4c27c171cfc537e36ad95e7830d14592a8

--- MMOCoreORB/bin/scripts/mobile/dungeon/corellian_corvette/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/corellian_corvette/serverobjects.lua
@@ -1,8 +1,12 @@
 
 --Imperial
-
+includeFile("dungeon/corellian_corvette/imperial/darkstone.lua")
+includeFile("dungeon/corellian_corvette/imperial/lt_velso.lua")
+includeFile("dungeon/corellian_corvette/imperial/sabol.lua")
 
 --Neutral
+includeFile("dungeon/corellian_corvette/neutral/bronell.lua")
+includeFile("dungeon/corellian_corvette/neutral/bruce_mcbrain.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_security_specialist.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_soldier.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_special_agent.lua")
@@ -15,5 +19,9 @@ includeFile("dungeon/corellian_corvette/neutral/corsec_special_ops_major.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_special_ops_master_sergeant.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_special_ops_sergeant.lua")
 includeFile("dungeon/corellian_corvette/neutral/corsec_special_ops_trainee.lua")
+includeFile("dungeon/corellian_corvette/neutral/yondalla.lua")
 
 --Rebel
+includeFile("dungeon/corellian_corvette/rebel/adar.lua")
+includeFile("dungeon/corellian_corvette/rebel/crowley.lua")
+includeFile("dungeon/corellian_corvette/rebel/pashna.lua")
