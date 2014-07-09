d263722759c325d00d623cd665787a42725ca311
dannuic <dannuic@swgemu.com>
2014-07-08 22:08:04 -0600

    [aibt] changed default ai behavior for screenplay spawns
    Change-Id: I435ee69762a75daa4c5e59aae354b231e4bb338a

--- MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
+++ MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
@@ -1544,6 +1544,9 @@ int DirectorManager::spawnMobile(lua_State* L) {
 		if (creature->isAiAgent()) {
 			AiAgent* ai = cast<AiAgent*>(creature);
 			ai->setRespawnTimer(respawnTimer);
+			// TODO (dannuic): this is a temporary measure until we add an AI setting method to DirectorManager -- make stationary the default
+			ai->clearBehaviorList();
+			ai->setupBehaviorTree(AiMap::instance()->getTemplate("stationary"));
 		}
 
 		creature->_setUpdated(true); //mark updated so the GC doesnt delete it while in LUA
@@ -1598,6 +1601,9 @@ int DirectorManager::spawnMobileRandom(lua_State* L) {
 			AiAgent* ai = cast<AiAgent*>(creature);
 			ai->setRespawnTimer(respawnTimer);
 			ai->setRandomRespawn(true);
+			// TODO (dannuic): this is a temporary measure until we add an AI setting method to DirectorManager -- make stationary the default
+			ai->clearBehaviorList();
+			ai->setupBehaviorTree(AiMap::instance()->getTemplate("stationary"));
 		}
 
 		creature->_setUpdated(true); //mark updated so the GC doesnt delete it while in LUA
