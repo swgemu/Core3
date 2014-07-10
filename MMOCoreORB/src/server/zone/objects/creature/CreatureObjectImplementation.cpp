b878b00981640d9c85be9622dbcf3a4e64c14eae
Ivojedi <ivojedi@swgemu.com>
2014-07-10 15:24:29 -0700

    [Fixed] stability issue
    Change-Id: Ibf17f05b7476c7597d27bb830424c87668d4437e

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -2838,15 +2838,6 @@ void CreatureObjectImplementation::setFaction(unsigned int crc) {
 		if (player == NULL)
 			return;
 
-		// Notify nearby active areas of faction change
-		SortedVector<ManagedReference<ActiveArea* > > activeAreas;
-		zone->getInRangeActiveAreas(player->getPositionX(), player->getPositionY(), &activeAreas, true);
-
-		for (int i = 0; i < activeAreas.size(); i++) {
-			ActiveArea* area = activeAreas.get(i);
-			area->notifyEnter(_this.get());
-		}
-
 		Zone* currentZone = getZone();
 
 		if (currentZone != NULL) {
