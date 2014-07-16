af15cb396a0297d1e94ced086aaef24d610e24c4
Klivian <klivian@hotmail.com>
2014-07-16 18:45:56 +0000

    [added] population to rori npc lairs (part 3)
    Change-Id: Ie3ef6b44468bbaaceffa3f4731af04facc8f7771

--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/rori_narmle_base_neutral_large_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/rori_narmle_base_neutral_large_theater.lua
@@ -1,11 +1,12 @@
 rori_narmle_base_neutral_large_theater = Lair:new {
-	mobiles = {},
+	mobiles = {{"narmle_militia_captain",1},{"narmle_militia_lieutenant",1},{"narmle_militia_major",1},{"narmle_militia_colonel",1}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/building/poi/naboo_gunrunners_large1.iff","object/building/poi/naboo_gunrunners_large2.iff"},
+	buildingsEasy = {"object/building/poi/naboo_gunrunners_large1.iff","object/building/poi/naboo_gunrunners_large2.iff"},
+	buildingsMedium = {"object/building/poi/naboo_gunrunners_large1.iff","object/building/poi/naboo_gunrunners_large2.iff"},
+	buildingsHard = {"object/building/poi/naboo_gunrunners_large1.iff","object/building/poi/naboo_gunrunners_large2.iff"},
+	buildingsVeryHard = {"object/building/poi/naboo_gunrunners_large1.iff","object/building/poi/naboo_gunrunners_large2.iff"},
+	buildingType = "theater"
 }
 
 addLairTemplate("rori_narmle_base_neutral_large_theater", rori_narmle_base_neutral_large_theater)
