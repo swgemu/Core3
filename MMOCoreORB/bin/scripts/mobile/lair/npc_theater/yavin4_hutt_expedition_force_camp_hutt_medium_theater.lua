b66f2635f58ddd2f8d494346d787e0f72b59c868
Klivian <klivian@hotmail.com>
2014-07-18 16:23:37 +0000

    [added] population to yavin npc lairs
    Change-Id: I7f1b061f6aa27b3e5a48e9f55fc484111b8a7037

--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/yavin4_hutt_expedition_force_camp_hutt_medium_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/yavin4_hutt_expedition_force_camp_hutt_medium_theater.lua
@@ -1,11 +1,11 @@
 yavin4_hutt_expedition_force_camp_hutt_medium_theater = Lair:new {
-	mobiles = {{"hutt_expeditonary_force_member", 2},{"hutt_expeditonary_force_leader", 1}},
-	spawnLimit = 15,
-	buildingsVeryEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
-	buildingsEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
-	buildingsMedium = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
-	buildingsHard = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
-	buildingsVeryHard = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	mobiles = {{"hutt_expeditionary_force_surveyor", 2},{"hutt_expeditonary_force_member", 2},{"hutt_expeditonary_force_leader", 1}},
+	spawnLimit = 12,
+	buildingsVeryEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_large1.iff","object/building/poi/yavin4_hutt_scavenger_camp_large2.iff"},
+	buildingsEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_large1.iff","object/building/poi/yavin4_hutt_scavenger_camp_large2.iff"},
+	buildingsMedium = {"object/building/poi/yavin4_hutt_scavenger_camp_large1.iff","object/building/poi/yavin4_hutt_scavenger_camp_large2.iff"},
+	buildingsHard = {"object/building/poi/yavin4_hutt_scavenger_camp_large1.iff","object/building/poi/yavin4_hutt_scavenger_camp_large2.iff"},
+	buildingsVeryHard = {"object/building/poi/yavin4_hutt_scavenger_camp_large1.iff","object/building/poi/yavin4_hutt_scavenger_camp_large2.iff"},
 	mobType = "npc",
 	buildingType = "theater",
 }
--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/yavin4_hutt_expedition_force_patrl_hutt_medium_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/yavin4_hutt_expedition_force_patrl_hutt_medium_theater.lua
@@ -1,11 +1,12 @@
 yavin4_hutt_expedition_force_patrl_hutt_medium_theater = Lair:new {
-	mobiles = {},
-	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	mobiles = {{"hutt_expeditonary_force_member", 1},{"hutt_expeditionary_force_surveyor", 1}},
+	spawnLimit = 12,
+	buildingsVeryEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	buildingsEasy = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	buildingsMedium = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	buildingsHard = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	buildingsVeryHard = {"object/building/poi/yavin4_hutt_scavenger_camp_medium1.iff","object/building/poi/yavin4_hutt_scavenger_camp_medium2.iff"},
+	buildingType = "theater"
 }
 
 addLairTemplate("yavin4_hutt_expedition_force_patrl_hutt_medium_theater", yavin4_hutt_expedition_force_patrl_hutt_medium_theater)
