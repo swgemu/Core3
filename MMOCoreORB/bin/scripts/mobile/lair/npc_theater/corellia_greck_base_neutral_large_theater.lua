eaacfb09301237286883ffe7dbdf43b9abb14c88
Ivojedi <ivojedi@swgemu.com>
2014-07-17 17:17:23 -0700

    [Removed] bad theater from a spawn
    Change-Id: Ic876deeadeab67e38460b3efe526c07e4e55232f

--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/corellia_greck_base_neutral_large_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/corellia_greck_base_neutral_large_theater.lua
@@ -1,11 +1,11 @@
 corellia_greck_base_neutral_large_theater = Lair:new {
 	mobiles = {{"grecks_assassin",1},{"grecks_henchman",1},{"grecks_mugger",1},{"grecks_smuggler",1}},
 	spawnLimit = 15,
-	buildingsVeryEasy = {"object/building/poi/corellia_greck_large1.iff","object/building/poi/corellia_greck_large2.iff"},
-	buildingsEasy = {"object/building/poi/corellia_greck_large1.iff","object/building/poi/corellia_greck_large2.iff"},
-	buildingsMedium = {"object/building/poi/corellia_greck_large1.iff","object/building/poi/corellia_greck_large2.iff"},
-	buildingsHard = {"object/building/poi/corellia_greck_large1.iff","object/building/poi/corellia_greck_large2.iff"},
-	buildingsVeryHard = {"object/building/poi/corellia_greck_large1.iff","object/building/poi/corellia_greck_large2.iff"},
+	buildingsVeryEasy = {"object/building/poi/corellia_greck_large2.iff"},
+	buildingsEasy = {"object/building/poi/corellia_greck_large2.iff"},
+	buildingsMedium = {"object/building/poi/corellia_greck_large2.iff"},
+	buildingsHard = {"object/building/poi/corellia_greck_large2.iff"},
+	buildingsVeryHard = {"object/building/poi/corellia_greck_large2.iff"},
 	buildingType = "theater"
 }
 
