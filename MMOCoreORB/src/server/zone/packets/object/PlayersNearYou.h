e93906db15a92d1a551969b8fca10c423c94aa72
Valkyra <lyhtmyst@gmail.com>
2014-07-18 01:41:49 -0400

    [updated] Documentation on PlayersNearYou packet. [added] Who slashcommand, arguement parsing coming later, but can retrieve information about players within a 128m range.
    Change-Id: Id387e043866420ca62c175c692169e62c5963b2c

--- MMOCoreORB/src/server/zone/packets/object/PlayersNearYou.h
+++ MMOCoreORB/src/server/zone/packets/object/PlayersNearYou.h
@@ -1,29 +1,29 @@
 /*
 Copyright (C) 2007 <SWGEmu>
- 
+
 This File is part of Core3.
- 
+
 This program is free software; you can redistribute 
 it and/or modify it under the terms of the GNU Lesser 
 General Public License as published by the Free Software
 Foundation; either version 2 of the License, 
 or (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 See the GNU Lesser General Public License for
 more details.
- 
+
 You should have received a copy of the GNU Lesser General 
 Public License along with this program; if not, write to
 the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- 
+
 Linking Engine3 statically or dynamically with other modules 
 is making a combined work based on Engine3. 
 Thus, the terms and conditions of the GNU Lesser General Public License 
 cover the whole combination.
- 
+
 In addition, as a special exception, the copyright holders of Engine3 
 give you permission to combine Engine3 program with free software 
 programs or libraries that are released under the GNU LGPL and with 
@@ -33,57 +33,90 @@ You may copy and distribute such a system following the terms of the
 GNU LGPL for Engine3 and the licenses of the other code concerned, 
 provided that you include the source code of that other code when 
 and as the GNU LGPL requires distribution of source code.
- 
+
 Note that people who make modified versions of Engine3 are not obligated 
 to grant this special exception for their modified versions; 
 it is their choice whether to do so. The GNU Lesser General Public License 
 gives permission to release a modified version without this exception; 
 this exception also makes it possible to release a modified version 
 which carries forward this exception.
-*/
+ */
 
 #ifndef PLAYERSNEARYOU_H_
 #define PLAYERSNEARYOU_H_
- 
+
 #include "ObjectControllerMessage.h"
- 
-#include "../../objects/player/PlayerObject.h"
+
+#include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 
-#include "../../objects/player/Races.h"
- 
-#include "../../objects/terrain/RegionNames.h"
- 
+#include "server/zone/managers/planet/PlanetManager.h"
+
+#include "server/zone/objects/player/Races.h"
+
+#include "server/zone/objects/terrain/RegionNames.h"
+
 class PlayersNearYouMessage : public ObjectControllerMessage {
 public:
 	PlayersNearYouMessage(CreatureObject* creo) 
-			: ObjectControllerMessage(creo->getObjectID(), 0x0B, 0x1E7) {
- 		insertInt(0);
+: ObjectControllerMessage(creo->getObjectID(), 0x0B, 0x1E7) {
+
 	}
- 
+
 	void addFoundPlayer(CreatureObject* player) {
-		insertInt(0); //List counter for something.
-		insertUnicode(player->getDisplayedName());
- 
-		//TODO: Change this...
-		insertInt(Races::getRaceID(player->getRaceName())); // Race ID
- 
-		insertAscii(Region::getRegionName(player->getRegionID())); //Region Name
+		// Bitmask.
+		PlayerObject* ghost = player->getPlayerObject();
+
+		uint32 playerBitmask = 0;
+
+		if (ghost != NULL) {
+			playerBitmask = ghost->getCharacterBitmask();
+		}
+
+		insertInt(0); // Bitmask size / 4, still valid no matter what set?
+		insertInt(playerBitmask); // Flags bitmask.
+
+		insertUnicode(player->getDisplayedName()); // Player name.
+
+		int race = 0;
+		if (ghost != NULL) {
+			race = ghost->getRaceID();
+		}
+
+		insertInt(race); // Race ID
+
+		PlanetManager* planetManager = player->getZone()->getPlanetManager();
+		CityRegion* cityRegion = planetManager->getRegionAt(player->getWorldPositionX(), player->getWorldPositionY());
+
+		String regionName = "";
+
+		if (cityRegion != NULL) {
+			regionName = cityRegion->getRegionName();
+		}
+
+		insertAscii(regionName); //Region Name
 		insertAscii(player->getZone()->getZoneName()); //Planet
- 
-  		if (player->isGuilded())
-			insertAscii(player->getGuildName()); //Guild
-		else
+
+		if (player->isInGuild()) {
+			insertAscii(player->getGuildObject()->getGuildName()); //Guild
+		} else {
 			insertAscii("");
- 
-		insertAscii(player->getPlayerObject()->getCurrentTitle()); //Title	
+		}
+
+		String title = "";
+
+		if (ghost != NULL) {
+			title = ghost->getTitle();
+		}
+
+		insertAscii(title); // Profession Title
 	}
- 
+
 	void insertPlayerCounter(uint32 foundCount) {
 		insertInt(30, foundCount);
-		insertInt(34 ,0);
+		insertInt(34, 0);
 	}
- 
+
 };
- 
+
 #endif /*PLAYERSNEARYOU_H_*/
