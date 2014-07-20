21868bc60177aafcfa334e2596a2ff334d1bf98e
Miztah <myztah@gmail.com>
2014-07-20 20:21:21 +0000

    [added] Hunt mission type to themepark logic to support quests that involve killing a world spawned mobile [added] Tolan Nokkar quest to kill a Tusken King (Tatooine)
    Change-Id: Ia85b56a24ca232b10365801a660c4a8b2571895d

---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/tusken_king_quest.lua
@@ -0,0 +1,51 @@
+tusken_king_quest = Creature:new {
+	objectName = "@mob/creature_names:tusken_king_quest",
+	socialGroup = "tusken_raider",
+	pvpFaction = "tusken_raider",
+	faction = "tusken_raider",
+	level = 95,
+	chanceHit = 0.9,
+	damageMin = 630,
+	damageMax = 970,
+	baseXp = 9057,
+	baseHAM = 22000,
+	baseHAMmax = 27000,
+	armor = 2,
+	resists = {45,35,5,80,-1,50,5,5,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/tusken_raider.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 1500000},
+				{group = "tusken_common", chance = 3000000},
+				{group = "wearables_all", chance = 2000000},				
+				{group = "bone_armor", chance = 750000},
+				{group = "chitin_armor", chance = 750000},
+				{group = "armor_attachments", chance = 500000},
+				{group = "clothing_attachments", chance = 500000},
+				{group = "color_crystals", chance = 500000},
+				{group = "crystals_quality", chance = 500000}
+			},
+			lootChance = 4000000
+		}			
+	},
+	weapons = {"tusken_weapons"},
+	conversationTemplate = "",
+	attacks = merge(marksmanmaster,brawlermaster,fencermaster,riflemanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(tusken_king_quest, "tusken_king_quest")
