1c394ba2fceb907f29863ee8e03fa6c9be3ca506
Klivian <klivian@hotmail.com>
2014-07-20 14:35:17 +0000

    [added] population to tatooine npc lairs (part 3)
    Change-Id: I77a6010e68ecfd2b1b255b0e0e75512939cdb505

--- MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/tatooine_tusken_raider_warband_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/tatooine_tusken_raider_warband_neutral_none.lua
@@ -1,11 +1,21 @@
 tatooine_tusken_raider_warband_neutral_none = Lair:new {
-	mobiles = {},
+	mobiles = {
+		{"tusken_war_master",1},
+		{"tusken_flesh_hunter",1},
+		{"tusken_savage",2},
+		{"tusken_avenger",2},
+		{"tusken_berserker",2},
+		{"tusken_raid_hunter",2},
+		{"tusken_death_hunter",1},
+		{"tusken_raid_leader",1},
+	},
 	spawnLimit = 15,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
 	buildingsVeryHard = {},
+	buildingType = "none"
 }
 
 addLairTemplate("tatooine_tusken_raider_warband_neutral_none", tatooine_tusken_raider_warband_neutral_none)
--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/tatooine_weequay_outpost_neutral_large_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/tatooine_weequay_outpost_neutral_large_theater.lua
@@ -1,11 +1,18 @@
 tatooine_weequay_outpost_neutral_large_theater = Lair:new {
-	mobiles = {},
+	mobiles = {
+		{"weequay_captain",1},
+		{"weequay_champion",1},
+		{"weequay_soldier",2},
+		{"weequay_thug",2},
+		{"weequay_zealot",2}
+	},
 	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	buildingsVeryEasy = {"object/building/poi/tatooine_weequayzealots_large1.iff","object/building/poi/tatooine_weequayzealots_large2.iff"},
+	buildingsEasy = {"object/building/poi/tatooine_weequayzealots_large1.iff","object/building/poi/tatooine_weequayzealots_large2.iff"},
+	buildingsMedium = {"object/building/poi/tatooine_weequayzealots_large1.iff","object/building/poi/tatooine_weequayzealots_large2.iff"},
+	buildingsHard = {"object/building/poi/tatooine_weequayzealots_large1.iff","object/building/poi/tatooine_weequayzealots_large2.iff"},
+	buildingsVeryHard = {"object/building/poi/tatooine_weequayzealots_large1.iff","object/building/poi/tatooine_weequayzealots_large2.iff"},
+	buildingType = "theater"
 }
 
 addLairTemplate("tatooine_weequay_outpost_neutral_large_theater", tatooine_weequay_outpost_neutral_large_theater)
