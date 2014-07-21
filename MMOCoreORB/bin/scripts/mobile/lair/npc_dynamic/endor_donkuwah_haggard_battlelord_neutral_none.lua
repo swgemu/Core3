0e435666069e82a0e22d2bb3889333304ed4d350
Klivian <klivian@hotmail.com>
2014-07-21 20:08:17 +0000

    [added] population to endor npc lairs (part 1)
    Change-Id: I60057884f32a169da5ab5917d0c44f0cafaa772c

--- MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/endor_donkuwah_haggard_battlelord_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/endor_donkuwah_haggard_battlelord_neutral_none.lua
@@ -1,11 +1,16 @@
 endor_donkuwah_haggard_battlelord_neutral_none = Lair:new {
-	mobiles = {},
-	spawnLimit = 15,
+	mobiles = {
+		{"haggard_donkuwah_battlelord",1},
+		{"donkuwah_shaman",1},
+		{"donkuwah_tribesman",1}
+	},
+	spawnLimit = 12,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
 	buildingsVeryHard = {},
+	buildingType = "none"
 }
 
 addLairTemplate("endor_donkuwah_haggard_battlelord_neutral_none", endor_donkuwah_haggard_battlelord_neutral_none)
--- MMOCoreORB/bin/scripts/mobile/lair/npc_theater/endor_donkuwah_tribesman_neutral_small_theater.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_theater/endor_donkuwah_tribesman_neutral_small_theater.lua
@@ -1,11 +1,15 @@
 endor_donkuwah_tribesman_neutral_small_theater = Lair:new {
-	mobiles = {},
-	spawnLimit = 15,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	mobiles = {
+		{"donkuwah_tribesman",1},
+		{"donkuwah_laborer",1}
+	},
+	spawnLimit = 9,
+	buildingsVeryEasy = {"object/building/poi/endor_ewok_small1.iff","object/building/poi/endor_ewok_small2.iff","object/building/poi/endor_ewok_small3.iff"},
+	buildingsEasy = {"object/building/poi/endor_ewok_small1.iff","object/building/poi/endor_ewok_small2.iff","object/building/poi/endor_ewok_small3.iff"},
+	buildingsMedium = {"object/building/poi/endor_ewok_small1.iff","object/building/poi/endor_ewok_small2.iff","object/building/poi/endor_ewok_small3.iff"},
+	buildingsHard = {"object/building/poi/endor_ewok_small1.iff","object/building/poi/endor_ewok_small2.iff","object/building/poi/endor_ewok_small3.iff"},
+	buildingsVeryHard = {"object/building/poi/endor_ewok_small1.iff","object/building/poi/endor_ewok_small2.iff","object/building/poi/endor_ewok_small3.iff"},
+	buildingType = "theater"
 }
 
 addLairTemplate("endor_donkuwah_tribesman_neutral_small_theater", endor_donkuwah_tribesman_neutral_small_theater)
