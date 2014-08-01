35b0369921c7c96332348416c6a9f3fdf7f601fc
Miztah <myztah@gmail.com>
2014-08-01 23:45:30 +0000

    [fixed] Number of difficult POI badges required for glowing - mantis 4673
    Change-Id: I7530be3a216b8646f85631621613d95aba663a68

--- MMOCoreORB/bin/scripts/managers/jedi/village/tests/glowing_Test.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/village/tests/glowing_Test.lua
@@ -157,7 +157,7 @@ describe("Village Jedi Manager - Glowing", function()
 					{ PROFESSIONBADGES, 1, false },
 					{ JEDIBADGES, 3, false },
 					{ CONTENTBADGES, 5, false },
-					{ DIFFICULTBADGES, 3, false },
+					{ DIFFICULTBADGES, 2, false },
 					{ EASYBADGES, 5, false }
 				}
 
