beddbdb72cfce23624ed6d0f9828f54bb5938c03
Klivian <klivian@hotmail.com>
2014-08-03 18:12:18 +0000

    [added] population to endor jinda lairs
    Change-Id: I69f5476a55545ee92f61677bb3aed6b24c6097cd

--- MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/endor_jinda_warrior_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/npc_dynamic/endor_jinda_warrior_neutral_none.lua
@@ -1,11 +1,17 @@
 endor_jinda_warrior_neutral_none = Lair:new {
-	mobiles = {},
-	spawnLimit = 15,
+	mobiles = {
+		{"masterful_jinda_warrior",2},
+		{"hardened_jinda_warrior",1},
+		{"seasoned_jinda_warrior",3},
+		{"jinda_warrior",4}
+	},
+	spawnLimit = 12,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
 	buildingsVeryHard = {},
+	buildingType = "none"
 }
 
 addLairTemplate("endor_jinda_warrior_neutral_none", endor_jinda_warrior_neutral_none)
