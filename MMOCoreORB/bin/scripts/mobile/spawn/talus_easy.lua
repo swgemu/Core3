75b26ca649a4a43a82c15d2005b57af5300f7ffb
Ivojedi <ivojedi@swgemu.com>
2014-08-11 15:52:18 -0700

    [Added] spawns to the Talus easy spawn areas [Added] medium spawn areas to Talus
    Change-Id: I2964eadcc848045c15e2505d490103f2221641df

--- MMOCoreORB/bin/scripts/mobile/spawn/talus_easy.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/talus_easy.lua
@@ -2,44 +2,53 @@ talus_easy = {
 
 	lairSpawns = {
 		{
-			lairTemplateName = "talus_paralope_lair_neutral_medium",
-			spawnLimit = -1,			
-			minDifficulty = 2,
-			maxDifficulty = 5,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "talus_fynock_youth_lair_neutral_small",
-			spawnLimit = -1,			
-			minDifficulty = 2,
-			maxDifficulty = 5,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "talus_vynock_lair_neutral_medium",
-			spawnLimit = -1,			
-			minDifficulty = 4,
-			maxDifficulty = 7,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "talus_decay_mite_lair_neutral_small",
-			spawnLimit = -1,			
-			minDifficulty = 6,
+			lairTemplateName = "talus_selonian_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 5,
 			maxDifficulty = 9,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "talus_rot_mite_lair_neutral_small",
-			spawnLimit = -1,			
+			lairTemplateName = "talus_meatlump_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 9,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_fynock_youth_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 9,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 9,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_chunker_bruiser_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_sleemo_vandal_camp_neutral_medium_theater",
+			spawnLimit = -1,
 			minDifficulty = 6,
 			maxDifficulty = 10,
 			numberToSpawn = 0,
@@ -47,13 +56,148 @@ talus_easy = {
 			size = 25
 		},
 		{
-			lairTemplateName = "talus_wrix_lair_neutral_medium",
-			spawnLimit = -1,			
+			lairTemplateName = "talus_sleemo_delinquent_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_chunker_goon_camp_neutral_small_theater",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 15
+		},
+		{
+			lairTemplateName = "talus_paralope_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_paralope_lair_neutral_medium_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_paralope_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_putrid_decay_mite_hatchling_neutral_none",
+			spawnLimit = -1,
 			minDifficulty = 7,
 			maxDifficulty = 11,
 			numberToSpawn = 0,
 			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_feared_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_selonian_assasin_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "talus_meatlump_outpost_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_liberation_partisan_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_liberation_loyalist_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_fed_dub_constable_camp_neutral_large_theater",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 30
+		},
+		{
+			lairTemplateName = "talus_rasp_flite_large_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
 		},
 	}
 }
---
+++ MMOCoreORB/bin/scripts/mobile/spawn/talus_medium.lua
@@ -0,0 +1,583 @@
+talus_medium = {
+
+	lairSpawns = {
+		{
+			lairTemplateName = "talus_liberation_patriot_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_liberation_fanatic_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_fed_dub_patrolman_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_binayre_ruffian_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_vynock_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_vynock_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_guf_drolg_minor_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_giant_gubbur_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_giant_gubbur_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_small_03",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dalyrake_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_dalyrake_nest_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_lost_aqualish_infiltrator_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_song_rasp_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 10,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_song_rasp_flock_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 10,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_selonian_squad_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_drall_patriot_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_rot_mite_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 5,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_boss_04",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_lost_aqualish_outrider_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_wrix_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_wrix_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_percussive_rasp_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_percussive_rasp_flock_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 5,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_boss_04",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_fynock_lair_neutral_boss_03",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_crazed_roba_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_crazed_roba_lair_neutral_large",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_small_03",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_rot_mite_lair_neutral_boss_03",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_jungle_fynock_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_jungle_fynock_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_gulginaw_nest_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_gulginaw_flight_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_dung_mite_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 5,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_dung_mite_lair_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_small_03",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_boss_04",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_decay_mite_lair_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "talus_lost_aqualish_commando_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_fed_dub_investigator_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_tortur_lair_neutral_large",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 30
+		},
+		{
+			lairTemplateName = "talus_guf_drolg_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_guf_drolg_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_foaming_vynock_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "talus_foaming_vynock_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+	}
+}
+
+addSpawnGroup("talus_medium", talus_medium);
