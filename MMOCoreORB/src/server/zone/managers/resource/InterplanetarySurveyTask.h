29c618f230a055d5a16db89a63d99117e9d26139
Sal Scotto <sal.scotto@gmail.com>
2014-08-16 07:57:07 -0400

    [fixed] Resource report will now include family-> type -> name in the output. Also fixed the resource class name in the title and added some nice outputs for certain tool reports
    Change-Id: Ic32f0b821e8993bd4af20c6ebf3f12a47861a7a4

--- MMOCoreORB/src/server/zone/managers/resource/InterplanetarySurveyTask.h
+++ MMOCoreORB/src/server/zone/managers/resource/InterplanetarySurveyTask.h
@@ -31,17 +31,32 @@ public:
 		ManagedReference<ResourceSpawn*> resourceSpawn;
 		// We need to sort this by family name
 		HashTable<String, Vector<String>* > mapped;
+		HashTable<String, Vector<String>* > typeMap;
 		for(int i=0;i<resources.size();i++) {
 			resourceSpawn = resources.get(i);
 			String family = resourceSpawn->getFamilyName();
+			String type = resourceSpawn->getFinalClass();
 			String name = resourceSpawn->getName();
-			Vector<String>* list = mapped.get(family);
+			// map type to family
+			Vector<String>* list = typeMap.get(family);
 			if (list != NULL) {
-				list->add(name);
+				if (!list->contains(type))
+					list->add(type);
 			} else {
 				list = new Vector<String>();
-				list->add(name);
-				mapped.put(family,list);
+				list->add(type);
+				typeMap.put(family,list);
+			}
+
+			// map type to spawn name
+			Vector<String>* mlist = mapped.get(type);
+			if (mlist != NULL) {
+				if (!mlist->contains(name))
+					mlist->add(name);
+			} else {
+				mlist = new Vector<String>();
+				mlist->add(name);
+				mapped.put(type,mlist);
 			}
 		}
 		// Create Email:
@@ -49,19 +64,39 @@ public:
 		UnicodeString sender(String("Interplanetary Survey Droid"));
 		Reference<StringIdManager* > stringIdManager = StringIdManager::instance();
 		String planetName = stringIdManager->getStringId(String("@planet_n:" + surveyData->getPlanet()).hashCode()).toString();
-		UnicodeString subject(String("Interplanetary Survey: " + planetName  + " - " + surveyData->getSurveyType()));
+		String sType = surveyData->getSurveyType();
+		// Some override for untranslated names
+		if (sType == "floral_resources") {
+			sType = "Floral";
+		}
+		if (sType == "energy_renewable_unlimited_solar") {
+			sType = "Solar";
+		}
+		if (sType == "energy_renewable_unlimited_wind") {
+			sType = "Wind";
+		}
+		StringBuffer tBuff;
+		char tmp = sType.charAt(0);
+		tmp = toupper(tmp);
+		tBuff.append(tmp);
+		tBuff.append(sType.subString(1));
+		String surveyType = tBuff.toString();
+		UnicodeString subject(String("Interplanetary Survey: " + planetName  + " - " + surveyType));
 		body << "Incoming planetary survey report...\n\n";
 		body << "\\#pcontrast3 Planet: \\#pcontrast1 " << planetName <<"\n";
-		body << "\\#pcontrast3 Resource Class: \\#pcontrast1 " << surveyData->getSurveyType() << "\n\n";
+		body << "\\#pcontrast3 Resource Class: \\#pcontrast1 " << surveyType << "\n\n";
 		body << "\\#pcontrast3 Resources located...\\#.\n\n";
-		HashTableIterator<String, Vector<String>* > iterator = mapped.iterator();
-		while(iterator.hasNext()) {
-			String key = iterator.getNextKey();
-			Vector<String>* values = mapped.get(key);
-			if (values->size() > 0) {
-				body << key << "\n";
-				for(int i=0;i<values->size();i++) {
-					body << "\\#pcontrast1 " << values->get(i) << "\\#.\n";
+		HashTableIterator<String, Vector<String>* > familyit = typeMap.iterator();
+		while(familyit.hasNext()) {
+			String family = familyit.getNextKey();
+			Vector<String>* tValues = typeMap.get(family);
+			body << family << "\n";
+			for(int i=0;i<tValues->size();i++) {
+				String sType = tValues->get(i);
+				body << "\t" << sType << "\n";
+				Vector<String>* values = mapped.get(sType);
+				for(int j=0;j<values->size();j++) {
+					body << "\t\t\\#pcontrast1 " << values->get(j) << "\\#.\n";
 				}
 			}
 		}
