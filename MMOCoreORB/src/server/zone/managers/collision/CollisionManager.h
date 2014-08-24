7de81eb9ea25f4f13ea701484e2e5492c6efc893
theanswer <victorpopovici@gmail.com>
2014-08-24 05:58:26 -0700

    [fixed] optimized ai getWorldCollision calls
    Change-Id: Ibe09bf70f7e58780fab30baba112c4d37ce3ff0b

--- MMOCoreORB/src/server/zone/managers/collision/CollisionManager.h
+++ MMOCoreORB/src/server/zone/managers/collision/CollisionManager.h
@@ -65,6 +65,8 @@ public:
 
 	static float getWorldFloorCollision(float x, float y, Zone* zone, bool testWater);
 	static void getWorldFloorCollisions(float x, float y, Zone* zone, bool testWater, SortedVector<IntersectionResult>* result, CloseObjectsVector* closeObjectsVector = NULL);
+
+	static void getWorldFloorCollisions(float x, float y, Zone* zone, bool testWater, SortedVector<IntersectionResult>* result, const SortedVector<ManagedReference<QuadTreeEntry*> >& inRangeObjects);
 	static Vector<float>* getCellFloorCollision(float x, float y, CellObject* cellObject);
 
 	//static bool
