ab12f671f7a6746dd00a9618163afffc5cfaec21
Ivojedi <ivojedi@swgemu.com>
2014-08-24 14:34:59 -0700

    [Added] many spawns to the Rori easy and medium spawn areas
    Change-Id: I173bec63f2772897858503c80953de9d41290f8f

--- MMOCoreORB/bin/scripts/mobile/spawn/rori_easy.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/rori_easy.lua
@@ -2,86 +2,177 @@ rori_easy = {
 
 	lairSpawns = {
 		{
-			lairTemplateName = "rori_squall_warren_neutral_medium",
+			lairTemplateName = "rori_gundark_raider_camp_neutral_medium_theater",
 			spawnLimit = -1,
-			minDifficulty = 6,
-			maxDifficulty = 6,
+			minDifficulty = 5,
+			maxDifficulty = 9,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "rori_narmle_patrol_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 5,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_gundark_hooligan_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_squall_meek_avenger_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_capper_spineflap_workerpod_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_capper_spineflap_pod_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 6,
-			maxDifficulty = 6,
+			maxDifficulty = 10,
 			numberToSpawn = 0,
 			weighting = 15,
-			size = 25
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_capper_spineflap_hive_neutral_large",
 			spawnLimit = -1,
 			minDifficulty = 6,
-			maxDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_capper_spineflap_flight_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_capper_spineflap_escort_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_borgle_gruntling_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "rori_borgal_young_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 6,
+			maxDifficulty = 10,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_nightspider_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_nightspider_lair_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 7,
-			maxDifficulty = 7,
+			maxDifficulty = 11,
 			numberToSpawn = 0,
 			weighting = 15,
-			size = 25
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_ikopi_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 7,
-			maxDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_ikopi_lair_neutral_medium_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_ikopi_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 7,
+			maxDifficulty = 11,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "krevol",
+			lairTemplateName = "rori_hermit_spider_neutral_none",
 			spawnLimit = -1,
-			minDifficulty = 8,
-			maxDifficulty = 8,
+			minDifficulty = 7,
+			maxDifficulty = 11,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "rori_sickle_rasp_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 8,
+			maxDifficulty = 12,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_sickle_rasp_nest_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 8,
-			maxDifficulty = 8,
+			maxDifficulty = 12,
 			numberToSpawn = 0,
 			weighting = 15,
-			size = 25
+			size = 20
 		},
-		{
-			lairTemplateName = "rori_nudfuh_lair_neutral_medium",
-			spawnLimit = -1,
-			minDifficulty = 9,
-			maxDifficulty = 9,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "barkmite_hills",
-			spawnLimit = -1,
-			minDifficulty = 9,
-			maxDifficulty = 9,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
+
 	}
 }
 
--- MMOCoreORB/bin/scripts/mobile/spawn/rori_medium.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/rori_medium.lua
@@ -2,28 +2,136 @@ rori_medium = {
 
 	lairSpawns = {
 		{
-			lairTemplateName = "rori_nudfuh_lair_neutral_medium",
+			lairTemplateName = "rori_brigade_squad_neutral_none",
 			spawnLimit = -1,
-			minDifficulty = 9,
-			maxDifficulty = 9,
+			minDifficulty = 6,
+			maxDifficulty = 15,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "barkmite_hills",
+			lairTemplateName = "rori_brigade_scout_camp_neutral_medium_theater",
 			spawnLimit = -1,
-			minDifficulty = 9,
-			maxDifficulty = 9,
+			minDifficulty = 7,
+			maxDifficulty = 16,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "bearded_jax",
+			lairTemplateName = "rori_spice_collective_mineral_scouts_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_kobola_smuggler_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_cobral_mugger_camp_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_nudfuh_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 30
+		},
+		{
+			lairTemplateName = "rori_capper_spineflap_nest_neutral_large",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 13,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_spice_collective_miner_base_neutral_large_theater",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 30
+		},
+		{
+			lairTemplateName = "rori_kobola_assasin_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_wood_mite_swarm_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_small",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 9,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_squall_warren_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 10,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_dashing_vir_vur_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 10,
+			maxDifficulty = 14,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wormed_vrobal_pack_neutral_none",
 			spawnLimit = -1,
 			minDifficulty = 11,
-			maxDifficulty = 11,
+			maxDifficulty = 15,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
@@ -32,16 +140,97 @@ rori_medium = {
 			lairTemplateName = "rori_wormed_vrobal_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 11,
-			maxDifficulty = 11,
+			maxDifficulty = 15,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
+			lairTemplateName = "rori_fuzzy_jax_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 11,
+			maxDifficulty = 15,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_rorgungan_scouts_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_mauler_sect_neutral_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_mauler_roamers_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_mauler_pilgrimage_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_vrobal_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_squall_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
 			lairTemplateName = "rori_dashing_vir_vur_lair_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 12,
-			maxDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_dashing_vir_vur_lair_neutral_small_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
+			numberToSpawn = 0,
+			weighting = 8,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_borgle_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 12,
+			maxDifficulty = 16,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
@@ -50,16 +239,61 @@ rori_medium = {
 			lairTemplateName = "rori_borgle_lair_neutral_large",
 			spawnLimit = -1,
 			minDifficulty = 12,
-			maxDifficulty = 12,
+			maxDifficulty = 16,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "rori_fuzzy_jax_lair_neutral_medium",
+			lairTemplateName = "rori_cobral_runner_neutral_none",
 			spawnLimit = -1,
 			minDifficulty = 13,
-			maxDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_brigade_infiltrator_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_nudfuh_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 30
+		},
+		{
+			lairTemplateName = "rori_midnight_vir_vur_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_kai_tok_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_gualama_troop_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
@@ -68,74 +302,209 @@ rori_medium = {
 			lairTemplateName = "rori_gualama_small_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 13,
-			maxDifficulty = 13,
+			maxDifficulty = 17,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "rori_gualama_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 25
+		},
+		{
+			lairTemplateName = "rori_fuzzy_jax_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 13,
+			maxDifficulty = 17,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_small_03",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_boss_04",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_boss_03",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_wood_mite_lair_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_vrobal_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 14,
-			maxDifficulty = 14,
+			maxDifficulty = 18,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
 		{
-			lairTemplateName = "rori_kai_tok_lair_neutral_medium",
+			lairTemplateName = "rori_gualama_large_lair_neutral_medium",
 			spawnLimit = -1,
 			minDifficulty = 14,
-			maxDifficulty = 14,
+			maxDifficulty = 18,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
+		},
+		{
+			lairTemplateName = "rori_forest_mite_swarm_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 15,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_small_04",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_small_03",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_small_02",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_small_01",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_boss_04",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_boss_03",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_boss_02",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
+		},
+		{
+			lairTemplateName = "rori_forest_mite_neutral_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 14,
+			maxDifficulty = 18,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 20
 		},
 		{
 			lairTemplateName = "rori_forest_mite_lair_neutral_small",
 			spawnLimit = -1,
 			minDifficulty = 14,
-			maxDifficulty = 14,
+			maxDifficulty = 18,
 			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
+			weighting = 3,
+			size = 20
 		},
 		{
-			lairTemplateName = "pugoriss",
+			lairTemplateName = "rori_elder_pugoriss_pack_neutral_none",
 			spawnLimit = -1,
 			minDifficulty = 14,
-			maxDifficulty = 14,
+			maxDifficulty = 18,
 			numberToSpawn = 0,
 			weighting = 15,
 			size = 25
 		},
-		{
-			lairTemplateName = "rori_chared_krevol_lair_neutral_medium",
-			spawnLimit = -1,
-			minDifficulty = 15,
-			maxDifficulty = 15,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "rori_savage_pugoriss_lair_neutral_medium",
-			spawnLimit = -1,
-			minDifficulty = 15,
-			maxDifficulty = 15,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		},
-		{
-			lairTemplateName = "rori_deranged_squall_warren_neutral_medium",
-			spawnLimit = -1,
-			minDifficulty = 15,
-			maxDifficulty = 15,
-			numberToSpawn = 0,
-			weighting = 15,
-			size = 25
-		}
 	}
 }
 
