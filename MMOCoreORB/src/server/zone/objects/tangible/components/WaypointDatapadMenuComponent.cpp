78284b435568332334acba2397bf90081abfa9ec
Valkyra <lyhtmyst@gmail.com>
2014-09-18 18:01:45 -0400

    [added] Using the looted datapad from the Sith intro theater. [added] Go to location generic quest module. [added] Go to Dathomir quest extension to force sensitive intro, with Lua tests for extension and Sith intro camp using disc. [added] Quests enum in player variables. [added] Players with the quest active or completed needed to get into the Village active area now can. [added] Rohak to village spawner.
    Change-Id: I67b7a00205ec2a008059af10346e79f22056dc84

--- MMOCoreORB/src/server/zone/objects/tangible/components/WaypointDatapadMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/WaypointDatapadMenuComponent.cpp
@@ -60,7 +60,12 @@ void WaypointDatapadMenuComponent::fillObjectMenuResponse(SceneObject* sceneObje
 
 	TangibleObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player);
 
-	menuResponse->addRadialMenuItem(6, 3, "@ui_radial:item_activate"); //Activate
+	if (sceneObject->getObjectTemplate()->getFullTemplateString() == "object/tangible/loot/quest/force_sensitive/waypoint_datapad.iff")
+		menuResponse->addRadialMenuItem(6, 3, "@ui_radial:item_activate"); //Activate
+
+	if (sceneObject->getObjectTemplate()->getFullTemplateString() == "object/tangible/loot/quest/force_sensitive/theater_datapad.iff")
+		menuResponse->addRadialMenuItem(6, 3, "@ui_radial:item_activate"); //Activate
+
 }
 
 int WaypointDatapadMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) {
@@ -70,7 +75,11 @@ int WaypointDatapadMenuComponent::handleObjectMenuSelect(SceneObject* sceneObjec
 	if (!sceneObject->isASubChildOf(player))
 		return 0;
 
+	if (selectedID == 6)
 	JediManager::instance()->useItem(sceneObject, JediManager::ITEMWAYPOINTDATAPAD, player);
+
+	if (selectedID == 7)
+	JediManager::instance()->useItem(sceneObject, JediManager::ITEMTHEATERDATAPAD, player);
 
 	return 0;
 }
