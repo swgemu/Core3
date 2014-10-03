1f5e0e14fb441d76f6c72d4fb8dd1ab34f2efc50
Miztah <myztah@gmail.com>
2014-10-03 19:18:25 +0000

    [fixed] Replaced armorsmith trainer with proper artisan trainer in Kor Vella - mantis 5182 [fixed] Various incorrect mob stats - mantis 5567, 5568, 5569, 5570, 5571, 5573, 5574 [fixed] Incorrect ordering of various mob attacks - mantis 5441, 5442
    Change-Id: I4bec5e863a14240e8a517c62a3c47082fbd383f2

--- MMOCoreORB/bin/scripts/mobile/naboo/nightspider.lua
+++ MMOCoreORB/bin/scripts/mobile/naboo/nightspider.lua
@@ -32,8 +32,8 @@ nightspider = Creature:new {
 	weapons = {},
 	conversationTemplate = "",
 	attacks = {
-		{"mildpoison",""},
-		{"posturedownattack","postureDownChance=50"}
+		{"posturedownattack","postureDownChance=50"},
+		{"mildpoison",""}
 	}
 }
 
