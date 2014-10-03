59afe5146e2a4b5f0a5a1a525aada8b1824b0228
Miztah <myztah@gmail.com>
2014-10-03 19:18:25 +0000

    [fixed] Replaced armorsmith trainer with proper artisan trainer in Kor Vella - mantis 5182 [fixed] Various incorrect mob stats - mantis 5567, 5568, 5569, 5570, 5571, 5573, 5574 [fixed] Incorrect ordering of various mob attacks - mantis 5441, 5442
    Change-Id: I4bec5e863a14240e8a517c62a3c47082fbd383f2

--- MMOCoreORB/bin/scripts/mobile/talus/tortur.lua
+++ MMOCoreORB/bin/scripts/mobile/talus/tortur.lua
@@ -31,8 +31,8 @@ tortur = Creature:new {
 	weapons = {},
 	conversationTemplate = "",
 	attacks = {
-		{"dizzyattack","dizzyChance=50"},
-		{"knockdownattack","knockdownChance=50"}
+		{"knockdownattack","knockdownChance=50"},
+		{"dizzyattack","dizzyChance=50"}
 	}
 }
 
