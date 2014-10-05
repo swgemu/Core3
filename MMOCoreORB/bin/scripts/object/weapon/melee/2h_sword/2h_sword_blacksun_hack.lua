7ed89b86af5929d4be757adf9c04a239436b144c
Miztah <myztah@gmail.com>
2014-10-05 06:12:59 +0000

    [fixed] Certification requirement on Executioner's Hack - mantis 5578 [changed] Removed firework limit on show for priviledged users
    Change-Id: If17d15bdc9bea1e627f3b4482dd16b00706dbf92

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_blacksun_hack.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_blacksun_hack.lua
@@ -80,7 +80,7 @@ object_weapon_melee_2h_sword_2h_sword_blacksun_hack = object_weapon_melee_2h_swo
 	xpType = "combat_meleespecialize_twohand",
 	       
 	-- See http://www.ocdsoft.com/files/certifications.xls
-	certificationsRequired = { "cert_sword_2h_scythe" },
+	certificationsRequired = { "combat_2hsword_speed_02" },
 	-- See http://www.ocdsoft.com/files/accuracy.xls
 	creatureAccuracyModifiers = { "twohandmelee_accuracy" },
 	
