015305b85e241e7212b009ef912b521b0659e2cf
Ivojedi <ivojedi@swgemu.com>
2014-10-07 20:16:57 -0700

    [Fixed] stability issue
    Change-Id: I392faa5bf2e83e596c769a91ddcca8a7c3885ca0

--- MMOCoreORB/bin/scripts/screenplays/recruiters/recruiterConvoHandler.lua
+++ MMOCoreORB/bin/scripts/screenplays/recruiters/recruiterConvoHandler.lua
@@ -111,15 +111,15 @@ function RecruiterConvoHandler:runScreenHandlers(conversationTemplate, conversin
 
 		elseif screenID == "confirm_bribe" and player:hasSkill("combat_smuggler_underworld_04") and (player:getCashCredits() >= 100000)
 			and (getFactionPointsCap(player:getFactionRank()) >= playerObject:getFactionStanding(recruiterScreenplay:getRecruiterFaction(conversingNPC)) + 1250) then
-			self:add100kBribeOption(clonedConversation)
+			self:add100kBribeOption(conversingNPC, clonedConversation)
 
 		elseif (screenID == "accepted_bribe_20k") and player:hasSkill("combat_smuggler_underworld_04") and (player:getCashCredits() >= 20000)
 			and (getFactionPointsCap(player:getFactionRank()) >= playerObject:getFactionStanding(recruiterScreenplay:getRecruiterFaction(conversingNPC)) + 250) then
-			recruiterScreenplay:grantBribe(conversingPlayer, 20000, 250)
+			recruiterScreenplay:grantBribe(conversingNPC, conversingPlayer, 20000, 250)
 
 		elseif (screenID == "accepted_bribe_100k") and player:hasSkill("combat_smuggler_underworld_04") and (player:getCashCredits() >= 100000)
 			and (getFactionPointsCap(player:getFactionRank()) >= playerObject:getFactionStanding(recruiterScreenplay:getRecruiterFaction(conversingNPC)) + 1250) then
-			recruiterScreenplay:grantBribe(conversingPlayer, 100000, 1250)
+			recruiterScreenplay:grantBribe(conversingNPC, conversingPlayer, 100000, 1250)
 			
 		elseif (screenID == "fp_furniture" or screenID == "fp_weapons_armor" or screenID == "fp_installations" or screenID == "fp_uniforms" or screenID == "fp_hirelings") then
 			recruiterScreenplay:sendPurchaseSui(conversingNPC, conversingPlayer, screenID)
