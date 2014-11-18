3eedff1021d2de2206e75ae90d265aa4173b9869
Valkyra <lyhtmyst@gmail.com>
2014-11-18 11:16:01 -0500

    [added] Using the looted datapad from the Sith intro theater. [added] Go to location generic quest module. [added] Go to Dathomir quest extension to force sensitive intro, with Lua tests for extension and Sith intro camp using disc. [added] Quests enum in player variables. [added] Players with the quest active or completed needed to get into the Village active area now can. [added] Rohak to village spawner.
    Change-Id: I67b7a00205ec2a008059af10346e79f22056dc84

---
+++ MMOCoreORB/bin/scripts/managers/jedi/village/dathomir_go_to.lua
@@ -0,0 +1,24 @@
+local GoToLocation = require("quest.tasks.go_to_location")
+local ObjectManager = require("managers.object.object_manager")
+local QuestManager = require("managers.quest.quest_manager")
+require("utils.helpers")
+
+GoToDathomir = GoToLocation:new {
+	-- Task properties
+	taskName = "GoToDathomir",
+	-- GoToLocation properties
+	waypointDescription = "@quest/force_sensitive/intro:goto_dath_sum",
+	spawnPoint = {x=5306,y=-4145},
+	spawnPlanet = "dathomir",
+	spawnRadius = 128
+}
+
+-- Event handler for the enter active area event.
+-- The event will complete the task.
+-- @param pCreatureObject pointer to the creature object of the player.
+function GoToDathomir:onEnteredActiveArea(pCreatureObject)
+	QuestManager.completeQuest(pCreatureObject, QuestManager.quests.FS_VILLAGE_ELDER)
+	self:taskFinish(pCreatureObject)
+end
+
+return GoToDathomir
