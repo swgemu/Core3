dc145378018fe1a140ca1eb18423d81a339825bf
Ivojedi <ivojedi@swgemu.com>
2014-11-22 16:28:36 -0800

    [Fixed] aggressive mobs will now aggro more reliably - mantis 5784 [Changed] weak mobs will no longer run away when not outdoors - mantis 5706
    Change-Id: I435c636c41b67fe3f495b3b2dce4d20751fcef7d

--- MMOCoreORB/bin/scripts/ai/interrupts.lua
+++ MMOCoreORB/bin/scripts/ai/interrupts.lua
@@ -161,27 +161,27 @@ function DefaultInterrupt:startAwarenessInterrupt(pAgent, pObject)
 			--agent:info("1c")
 			agent:setOblivious()
 		end
-	elseif pFollow == nil and inRange and (agent:isAggressiveTo(pObject) or agent:getAvgSpeed() > (target:getWalkSpeed() * target:getWalkSpeed())) then
+	elseif agent:isAggressiveTo(pObject) and inRange then
+		--if not scno:isAiAgent() then agent:info("3") end
+		--if scno:isAiAgent() then ObjectManager.withCreatureAiAgent(pObject, function(ai) ai:info("attacking me!") end) end
+		agent:addDefender(pObject)
+	elseif pFollow == nil and inRange and (agent:getAvgSpeed() > (target:getWalkSpeed() * target:getWalkSpeed())) then
 		--if not scno:isAiAgent() then agent:info("2") end
 		agent:setWatchObject(pObject)
 		agent:setAlertDuration(10000); -- TODO (dannuic): make this wait time more dynamic
 		ObjectManager.withSceneObject(pAgent, function(scno) scno:showFlyText("npc_reaction/flytext", "alert", 255, 0, 0) end)
 		agent:activateAwareness(pObject)
-	elseif agent:isAggressiveTo(pObject) and pObject == pFollow and inRange then
-		--if not scno:isAiAgent() then agent:info("3") end
-		--if scno:isAiAgent() then ObjectManager.withCreatureAiAgent(pObject, function(ai) ai:info("attacking me!") end) end
-		agent:addDefender(pObject)
 	elseif pObject == pFollow and agent:alertedTimeIsPast() and agent:getAvgSpeed() <= (target:getWalkSpeed() * target:getWalkSpeed()) then
 		--if not scno:isAiAgent() then agent:info("4") end
 		agent:setOblivious() -- if we're "standing still" (and they aren't aggressive) forget about us
 	elseif pObject == pFollow and not inRange then
 		agent:activateAwareness(pObject)
-	elseif pObject == pFollow and inRange then
+	elseif pObject == pFollow and inRange and scno:getParent() == nil then -- TODO: Do we want weaker mobs to run away when indoors? Revisit when indoor pathing is better
 		--if not scno:isAiAgent() then agent:info("5") end
 		-- TODO (dannuic): Not sure about this stuff, needs testing
 		if ObjectManager.withSceneObject(pFollow, function(followObj) return followObj:isCreatureObject() end) then		
-		  local creoLevel = ObjectManager.withCreatureObject(pFollow, function(creo) return creo:getLevel() end)
-		 
+			local creoLevel = ObjectManager.withCreatureObject(pFollow, function(creo) return creo:getLevel() end)
+ 
 			if effectiveLevel < creoLevel then 
 				agent:runAway(pFollow, 64 - radius)
 			else
