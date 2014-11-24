992f465e16aa07cfda3e17bcf6c6ef7475c039a9
Sal Scotto <sal.scotto@gmail.com>
2014-11-24 15:33:49 -0500

    [fix] wound and enhance packs will check for droid vs building and just use the highest
    Change-Id: I44b4cffa7d70c83d4494af21f0a5b4eecf2cfdbe

---
+++ MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/EnhancePackImplementation.cpp
@@ -0,0 +1,19 @@
+#include "server/zone/objects/tangible/pharmaceutical/EnhancePack.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/managers/skill/SkillModManager.h"
+
+uint32 EnhancePackImplementation::calculatePower(CreatureObject* healer, CreatureObject* patient, bool applyBattleFatigue) {
+		float power = getEffectiveness();
+
+		if (applyBattleFatigue)
+			power = power - (power * patient->calculateBFRatio() * healer->calculateBFRatio());
+		int droidBuff = healer->getSkillModOfType("private_medical_rating",SkillModManager::TEMPORARYMOD);
+		int bldBuff = healer->getSkillModOfType("private_medical_rating", SkillModManager::STRUCTURE);
+		int mod = healer->getSkillModOfType("private_medical_rating", SkillModManager::CITY);
+		mod +=  droidBuff > bldBuff ? droidBuff : bldBuff;
+
+		float modEnvironment = ((float) mod / 100);
+		float modSkill = (float) healer->getSkillMod("healing_wound_treatment");
+
+		return power * modEnvironment * (100 + modSkill) / 100;
+}
