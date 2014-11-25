e8b55773e986cf03865ece4bf0a1dd65d0b57666
Sal Scotto <sal.scotto@gmail.com>
2014-11-25 10:24:40 -0500

    [wip] stimpack dispenser module. Till QA says otherwise it will find the first stimpack in your inv and use it. [added] cehck load stimpack player has pharacology 4 [fix] converted request stimpack to a task [fix] warning [added] method on stimpack to determine if it is class A [added] requeststimpack command functions. [fixes] as per commetns posted. [fix] changed power cost to on use of request a stimpack, fix typo in lua file Change-Id: Iad43c6dd65a4ee1852c928da83bf9b76056cc42e

--- MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/StimPackImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/StimPackImplementation.cpp
@@ -1,5 +1,6 @@
 
 #include "server/zone/objects/tangible/pharmaceutical/StimPack.h"
+#include "server/zone/templates/tangible/StimPackTemplate.h"
 
 bool StimPackImplementation::isPetStimPack() {
 	return getObjectNameStringIdName().contains("pet_stimpack");
@@ -7,4 +8,19 @@ bool StimPackImplementation::isPetStimPack() {
 
 bool StimPackImplementation::isDroidRepairKit() {
 	return getObjectNameStringIdName().contains("droid_damage_kit");
+}
+bool StimPackImplementation::isClassA() {
+	return getMedicineClass() == StimPackTemplate::STIM_A;
+}
+bool StimPackImplementation::isClassB() {
+	return getMedicineClass() == StimPackTemplate::STIM_B;
+}
+bool StimPackImplementation::isClassC() {
+	return getMedicineClass() == StimPackTemplate::STIM_C;
+}
+bool StimPackImplementation::isClassD() {
+	return getMedicineClass() == StimPackTemplate::STIM_D;
+}
+bool StimPackImplementation::isClassE() {
+	return getMedicineClass() == StimPackTemplate::STIM_E;
 }
