5680632872648816a8e4c21d21b4ec9295bd02b1
Klivian <klivian@hotmail.com>
2014-12-07 21:01:36 +0000

    [wip] droid structure maintenance module [added] structures can be assigned/removed [added] beginnings of maintenance session
    Change-Id: Idfc5aefb269eb2c775f3943139611378700b5572

---
+++ MMOCoreORB/src/server/zone/objects/player/sessions/DroidMaintenanceSessionImplementation.cpp
@@ -0,0 +1,24 @@
+/*
+ * DroidMaintenanceSessionImplementation.cpp
+ *
+ *  Created on: 1 13, 2015
+ *      Author: Klivian
+ */
+
+#include "server/zone/objects/player/sessions/DroidMaintenanceSession.h"
+#include "server/zone/Zone.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/objects/player/sui/SuiWindowType.h"
+
+void DroidMaintenanceSessionImplementation::sendMaintanceRunBox(){
+
+}
+
+void DroidMaintenanceSessionImplementation::sendMaintenanceTransferBox( StructureObject* structure ){
+
+}
+
+void DroidMaintenanceSessionImplementation::performMaintenanceRun(){
+
+}
