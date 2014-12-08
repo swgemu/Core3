f9431a3834b5aa4566a961c65ab1e9183337211e
Sal Scotto <sal.scotto@gmail.com>
2014-12-08 16:43:43 -0500

    [fix] fixed droid deeds losing components, you can now drag single stimpacks on stimpack dispenser droids to load them as i read it in a review i dont remember where, removed custom serialization override of idlc that was a bad idea [fix] removed some debug [fix] removed some un needed imports
    Change-Id: I4cab6bd197bd09d29e812df279c2b678a5796d98

--- MMOCoreORB/src/server/zone/objects/tangible/components/droid/DroidStimpackModuleDataComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/droid/DroidStimpackModuleDataComponent.cpp
@@ -261,13 +261,15 @@ void DroidStimpackModuleDataComponent::sendLoadUI(CreatureObject* player) {
 			PharmaceuticalObject* pharma = cast<PharmaceuticalObject*>( tano);
 			if (pharma->isStimPack()) {
 				StimPack* stim = cast<StimPack*>(pharma);
-				String name;
-				if(stim->getCustomObjectName().isEmpty()) {
-					name = "@"+stim->getObjectNameStringIdFile()+":"+stim->getObjectNameStringIdName();
-				} else {
-					name = stim->getCustomObjectName().toString();
+				if (stim->isClassA()) {
+					String name;
+					if(stim->getCustomObjectName().isEmpty()) {
+						name = "@"+stim->getObjectNameStringIdFile()+":"+stim->getObjectNameStringIdName();
+					} else {
+						name = stim->getCustomObjectName().toString();
+					}
+					loader->addMenuItem(name,stim->getObjectID());
 				}
-				loader->addMenuItem(name,stim->getObjectID());
 			}
 		}
 	}
@@ -279,8 +281,25 @@ void DroidStimpackModuleDataComponent::sendLoadUI(CreatureObject* player) {
 }
 void DroidStimpackModuleDataComponent::handleInsertStimpack(CreatureObject* player, StimPack* pack) {
 	// we need to send the invlid stimpack message just wher eis a good question
+	if (player != NULL && !player->hasSkill("science_medic_ability_04")) {
+		return;
+	}
+	ManagedReference<DroidObject*> droid = getDroidObject();
+	if (droid == NULL) {
+		return;
+	}
+	if (pack == NULL) {
+		player->sendSystemMessage("@pet/droid_modules:invalid_stimpack");
+		return;
+	}
+	if(!pack->isClassA()) {
+		player->sendSystemMessage("@pet/droid_modules:invalid_stimpack");
+		return;
+	}
+	if(droid->getLinkedCreature().get() != player) {
+		return;
+	}
 	// we have the player and the stim to add to ourselves.
-	DroidObject* droid = getDroidObject();
 	DroidComponent* droidComponent = cast<DroidComponent*>(getParent());
 	ManagedReference<SceneObject*> craftingComponents = droidComponent->getSlottedObject("crafted_components");
 	if (craftingComponents != NULL) {
