eaee03f4a03001772b1446db2eb55c53138027d8
Miztah <myztah@gmail.com>
2014-12-11 04:17:49 +0000

    [added] Framework for Heralds [added] Heralds to Naboo
    Change-Id: I47ee3163b8597024c18c17e2edc2e32ed4478023

---
+++ MMOCoreORB/bin/scripts/mobile/misc/herald_rsf_security_guard.lua
@@ -0,0 +1,36 @@
+herald_rsf_security_guard = Creature:new {
+	objectName = "@mob/creature_names:rsf_security_guard",
+	socialGroup = "naboo_security_force",
+	pvpFaction = "naboo_security_force",
+	faction = "naboo_security_force",
+	level = 8,
+	chanceHit = 0.27,
+	damageMin = 70,
+	damageMax = 75,
+	baseXp = 235,
+	baseHAM = 405,
+	baseHAMmax = 495,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_rsf_security_guard.iff"},
+	lootGroups = {},
+	weapons = {"rebel_weapons_light"},
+	conversationTemplate = "heraldConvoTemplate",
+	attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(herald_rsf_security_guard, "herald_rsf_security_guard")
