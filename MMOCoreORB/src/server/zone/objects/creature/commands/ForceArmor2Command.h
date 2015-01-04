96191209318ffcf5cb402356d6192d869b91d9ca
Valkyra <lyhtmyst@gmail.com>
2015-01-04 01:03:35 -0500

    [added] New Slash command logic for Jedi-related queue commands. QA, please contact Valkyra in regards to what commands have been changed thus far. [fixed] Not being able to use Saber attacks while kneeling.
    Change-Id: I4b9ec76d706f95ffcdef3364bb36db4aa9358e54

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceArmor2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceArmor2Command.h
@@ -40,78 +40,33 @@ it is their choice whether to do so. The GNU Lesser General Public License
 gives permission to release a modified version without this exception;
 this exception also makes it possible to release a modified version
 which carries forward this exception.
-*/
+ */
 
 #ifndef FORCEARMOR2COMMAND_H_
 #define FORCEARMOR2COMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
-#include "server/zone/objects/creature/buffs/SingleUseBuff.h"
+#include "JediQueueCommand.h"
+#include "server/zone/objects/creature/buffs/BuffCRC.h"
 
-class ForceArmor2Command : public QueueCommand {
+class ForceArmor2Command : public JediQueueCommand {
 public:
 
 	ForceArmor2Command(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+: JediQueueCommand(name, server) {
+		forceCost = 150;
+		uint32 buffCRC1 = BuffCRC::JEDI_FORCE_ARMOR_2;
+		uint32 buffCRC2 = BuffCRC::JEDI_FORCE_ARMOR_1;
+		buffCRCs.add(buffCRC1);
+		buffCRCs.add(buffCRC2);
+		duration = 1800;
+		clientEffect = "clienteffect/pl_force_armor_self.cef";
+		skillMods.put("force_armor", 45);
 
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
-
-		if (!checkStateMask(creature))
-			return INVALIDSTATE;
-
-		if (!checkInvalidLocomotions(creature))
-			return INVALIDLOCOMOTION;
-
-		if (isWearingArmor(creature)) {
-			return NOJEDIARMOR;
-		}
-
-		uint32 buffcrc1 = BuffCRC::JEDI_FORCE_ARMOR_1;
-		uint32 buffcrc2 = BuffCRC::JEDI_FORCE_ARMOR_2;
-
-		if(creature->hasBuff(buffcrc1) || creature->hasBuff(buffcrc2)) {
-			creature->sendSystemMessage("@jedi_spam:force_buff_present");
-			return GENERALERROR;
-		}
-
-
-		// Force cost of skill.
-		int forceCost = 150;
-
-
-		//Check for and deduct Force cost.
-
-		ManagedReference<PlayerObject*> playerObject = creature->getPlayerObject();
-
-
-		if (playerObject->getForcePower() <= forceCost) {
-			creature->sendSystemMessage("@jedi_spam:no_force_power"); //"You do not have enough Force Power to peform that action.
-
-			return GENERALERROR;
-		}
-
-		playerObject->setForcePower(playerObject->getForcePower() - forceCost);
-
-		StringIdChatParameter startStringId("jedi_spam", "apply_forcearmor2");
-		StringIdChatParameter endStringId("jedi_spam", "remove_forcearmor2");
-
-		int duration = 1800;
-
-		Vector<unsigned int> eventTypes;
-		eventTypes.add(ObserverEventType::FORCEBUFFHIT);
-
-		ManagedReference<SingleUseBuff*> buff = new SingleUseBuff(creature, buffcrc2, duration, BuffType::JEDI, getNameCRC());
-		buff->setStartMessage(startStringId);
-		buff->setEndMessage(endStringId);
-		buff->setSkillModifier("force_armor", 45);
-		buff->init(&eventTypes);
-
-		creature->addBuff(buff);
-		creature->playEffect("clienteffect/pl_force_armor_self.cef", "");
-
-		return SUCCESS;
+		return doJediBuffCommand(creature, target, arguments);
 	}
 
 	void handleBuff(SceneObject* creature, ManagedObject* object, int64 param) {
