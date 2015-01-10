0e36cb753527e06ff2325ea2a9d316fc07e070a0
Miztah <myztah@gmail.com>
2015-01-10 02:22:03 +0000

    [removed] Jetpack parts other than jetpack base from Black Sun world spawn loot table - mantis 5900 [adjusted] BH armor droprate on Black Sun world spawns
    Change-Id: I63d53e6ebf0a1a94beb37ca132610c7793d36eba

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/black_sun_assassin.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/black_sun_assassin.lua
@@ -28,20 +28,19 @@ black_sun_assassin = Creature:new {
 
 	templates = {"object/mobile/dressed_black_sun_assassin.iff"},
 	lootGroups = {
-         {
+		{
 			groups = {
-				{group = "junk", chance = 5800000},
+				{group = "junk", chance = 6500000},
 				{group = "pistols", chance = 750000},
 				{group = "rifles", chance = 750000},
 				{group = "carbines", chance = 750000},
-				{group = "jetpack_parts", chance = 500000},				
-				{group = "bounty_hunter_armor", chance = 400000},
+				{group = "bounty_hunter_armor", chance = 200000},
 				{group = "jetpack_base", chance = 50000},
 				{group = "wearables_common", chance = 500000},
 				{group = "wearables_uncommon", chance = 500000}
 			},
 			lootChance = 3500000
-		}	
+		}
 	},
 	weapons = {"pirate_weapons_heavy"},
 	conversationTemplate = "",
