57f41a9ffb4a46b18d58baa943b685dfc6ab255a
Victor Popovici <victorpopovici@gmail.com>
2015-01-21 01:04:10 +0100

    [fixed] stability issue
    Change-Id: I686581d3f6bf20bfa93c34727d1f0d5c0eb86a64

---
+++ MMOCoreORB/src/server/zone/managers/guild/GuildMailTask.h
@@ -0,0 +1,48 @@
+/*
+ * GuildMailTask.h
+ *
+ *  Created on: Jan 21, 2015
+ *      Author: TheAnswer
+ */
+
+#ifndef GUILDMAILTASK_H_
+#define GUILDMAILTASK_H_
+
+class GuildMailTask : public Task {
+	ManagedReference<GuildObject*> guild;
+	StringIdChatParameter body;
+	String subject;
+
+public:
+	GuildMailTask(const String& subject, StringIdChatParameter& body,
+			GuildObject* guild) : body(body), guild(guild), subject(subject) {
+	}
+
+	void run() {
+		Locker locker(guild);
+
+		GuildMemberList* memberList = guild->getGuildMemberList();
+
+		if (memberList == NULL)
+			return;
+
+		for (int i = 0; i < memberList->size(); ++i) {
+			GuildMemberInfo* gmi = &memberList->get(i);
+
+			if (gmi == NULL)
+				continue;
+
+			ManagedReference<SceneObject*> obj = guild->getZoneServer()->getObject(gmi->getPlayerID());
+
+			if (obj == NULL || !obj->isPlayerCreature())
+				continue;
+
+			CreatureObject* recipient = cast<CreatureObject*>( obj.get());
+
+			guild->getZoneServer()->getChatManager()->sendMail(guild->getGuildName(), subject, body, recipient->getFirstName());
+		}
+	}
+};
+
+
+#endif /* GUILDMAILTASK_H_ */
