8a31d89678a5855ecbf23854186f59555dc1ca7f
Ivojedi <ivojedi@swgemu.com>
2015-02-06 17:51:42 -0800

    [added] Victory Paintings to Imperial Officer loot - mantis 4561
    Change-Id: Ife543ea12e18026f9ae39ea48ec47045a19208ed

--- MMOCoreORB/bin/scripts/loot/groups/npc/imperial_officer_common.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/imperial_officer_common.lua
@@ -3,9 +3,10 @@ imperial_officer_common = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "technical_console_schematic_1", weight = 3333333},
-		{itemTemplate = "technical_console_schematic_2", weight = 3333333},
-		{itemTemplate = "couch_blue_schematic", weight = 3333334}
+		{itemTemplate = "technical_console_schematic_1", weight = 2500000},
+		{itemTemplate = "technical_console_schematic_2", weight = 2500000},
+		{itemTemplate = "couch_blue_schematic", weight = 2500000},
+		{itemTemplate = "painting_vader_victory", weight = 2500000},
 	}
 }
 
