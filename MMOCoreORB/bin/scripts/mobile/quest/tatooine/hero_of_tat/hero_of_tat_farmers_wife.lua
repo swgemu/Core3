97bbddca16d56e85baf14a6097a7fc763e28cf6d
Miztah <myztah@gmail.com>
2015-02-15 04:59:01 +0000

    [added] Mark of Altruism part of Hero of Tatooine questline [fixed] Looting Mark of Courage not granting player the badge - mantis 5985 [adjusted] Sennex loot cave respawn timers
    Change-Id: I6628bb881dfc6a3434cee87e2a4e89290a5b3058

---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/hero_of_tat/hero_of_tat_farmers_wife.lua
@@ -0,0 +1,36 @@
+hero_of_tat_farmers_wife = Creature:new {
+	objectName = "@mob/creature_names:quest_hero_of_tatooine_farmer_wife",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 4,
+	chanceHit = 0.24,
+	damageMin = 40,
+	damageMax = 45,
+	baseXp = 62,
+	baseHAM = 113,
+	baseHAMmax = 138,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = HERD,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_quest_farmer_wife.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "heroOfTatMotherConvoTemplate",
+	attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(hero_of_tat_farmers_wife, "hero_of_tat_farmers_wife")
