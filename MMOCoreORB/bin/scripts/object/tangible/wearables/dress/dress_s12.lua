8906dcf1f5b9ebd2885ccd6176bf5519739c895c
Ivojedi <ivojedi@swgemu.com>
2015-02-18 16:58:24 -0800

    [Fixed] plain short robe not being able to generate sockets - mantis 6041
    Change-Id: I102d3f51ff49c758d2e07681ad63ee5cf11e2740

--- MMOCoreORB/bin/scripts/object/tangible/wearables/dress/dress_s12.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/dress/dress_s12.lua
@@ -85,15 +85,15 @@ object_tangible_wearables_dress_dress_s12 = object_tangible_wearables_dress_shar
 				"object/mobile/vendor/zabrak_female.iff",
 				"object/mobile/vendor/zabrak_male.iff" },
 
-	numberExperimentalProperties = {1, 1, 1},
-	experimentalProperties = {"XX", "XX", "XX"},
-	experimentalWeights = {1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null"},
-	experimentalSubGroupTitles = {"null", "null", "hitpoints"},
-	experimentalMin = {0, 0, 1000},
-	experimentalMax = {0, 0, 1000},
-	experimentalPrecision = {0, 0, 0},
-	experimentalCombineType = {0, 0, 4},
+	numberExperimentalProperties = {1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX", "XX"},
+	experimentalWeights = {1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null", "null"},
+	experimentalSubGroupTitles = {"null", "null", "sockets", "hitpoints"},
+	experimentalMin = {0, 0, 0, 1000},
+	experimentalMax = {0, 0, 0, 1000},
+	experimentalPrecision = {0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 4, 4},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_dress_dress_s12, "object/tangible/wearables/dress/dress_s12.iff")
