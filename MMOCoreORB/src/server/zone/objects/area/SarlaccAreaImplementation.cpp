7e5d6a4d4bfa7273bdcb2307184013540a882430
Miztah <myztah@gmail.com>
2015-03-02 17:41:56 +0000

    [changed] The sarlacc will now poison all players nearby at once when it erupts, but will not erupt more than once every 10 seconds [added] Erupt effect to sarlacc
    Change-Id: If9dab22e6a294cd8f6ef04ee450b822f2258e114

--- MMOCoreORB/src/server/zone/objects/area/SarlaccAreaImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/area/SarlaccAreaImplementation.cpp
@@ -2,27 +2,54 @@
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/objects/creature/CreatureAttribute.h"
+#include "server/zone/packets/scene/PlayClientEffectLocMessage.h"
 #include "server/zone/Zone.h"
 
-void SarlaccAreaImplementation::notifyEnter(SceneObject* player) {
-	if (!player->isPlayerCreature()) {
+void SarlaccAreaImplementation::notifyEnter(SceneObject* object) {
+	if (object == NULL || !object->isPlayerCreature())
 		return;
-	}
 
-	Locker plocker(player);
+	Locker plocker(object);
 
-	CreatureObject* playerCreature = cast<CreatureObject*>(player);
-	playerCreature->sendSystemMessage("@mob/sarlacc:sarlacc_poison"); // The air is thick with the smell of rot and disease.
+	CreatureObject* player = cast<CreatureObject*>(object);
 
-	SceneObject* sco = NULL;
-	for (int i=0; i< player->getSlottedObjectsSize(); i++) {
-		sco = player->getSlottedObject(i);
-		if (sco == NULL)
-			continue;
+	if (player == NULL)
+		return;
 
-		if (sco->getServerObjectCRC() == String("object/tangible/wearables/bodysuit/bodysuit_sarlacc_coverall.iff").hashCode())
+	if (getRadius() == 60) {
+		player->sendSystemMessage("@mob/sarlacc:sarlacc_poison"); // The air is thick with the smell of rot and disease.
+	} else {
+		Time currentTime;
+		int timeDiff = currentTime.getMiliTime() - lastErupt.getMiliTime();
+
+		if (timeDiff < 10000) // 10 second cooldown on erupt
 			return;
+
+		PlayClientEffectLoc* effectLoc = new PlayClientEffectLoc("clienteffect/cr_sarlacc_erupt.cef", "tatooine", getPositionX(), getPositionZ(), getPositionY(), 0, 3);
+		player->broadcastMessage(effectLoc, true);
+
+		Reference<SortedVector<ManagedReference<QuadTreeEntry*> >*> closeObjects = new SortedVector<ManagedReference<QuadTreeEntry*> >();
+		zone->getInRangeObjects(getWorldPositionX(), getWorldPositionY(), 60, closeObjects, true);
+
+		for (int i = 0; i < closeObjects->size(); ++i) {
+			SceneObject* scno = cast<SceneObject*>(closeObjects->get(i).get());
+
+			if (scno != NULL && scno->isPlayerCreature()) {
+				ManagedReference<CreatureObject*> playerCreature = cast<CreatureObject*>(scno);
+
+				if (playerCreature == NULL)
+					continue;
+
+				playerCreature->sendSystemMessage("@mob/sarlacc:sarlacc_erupt"); // The Sarlacc suddenly erupts, spewing a diseased and corrosive substance into the air!
+
+				if (playerCreature->getSkillMod("resistance_disease") < 24) {
+					playerCreature->addDotState(player, CreatureState::DISEASED, 0, 30 + System::random(20), CreatureAttribute::HEALTH, 30 * 60, 2000, 0);
+					playerCreature->sendSystemMessage("@mob/sarlacc:sarlacc_dot"); // You suddenly feel weak and sick.
+				}
+			}
+		}
+
+		lastErupt.updateToCurrentTime();
+
 	}
-	playerCreature->addDotState(playerCreature, CreatureState::DISEASED, 0, 30 + System::random(20), CreatureAttribute::HEALTH, 30 * 60, 2000, 0);
-	playerCreature->sendSystemMessage("@mob/sarlacc:sarlacc_dot"); // You suddenly feel weak and sick.
 }
