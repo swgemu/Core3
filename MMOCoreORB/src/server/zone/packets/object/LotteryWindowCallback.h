5beb13678d22727bd1107db669102400c0931b3a
dannuic <dannuic@swgemu.com>
2015-03-16 18:40:31 -0600

    [added] the rest of the Loot Window packet structure
    Change-Id: Iae3a6a3f0f4a5a820ed02c1507609be025095dfc

---
+++ MMOCoreORB/src/server/zone/packets/object/LotteryWindowCallback.h
@@ -0,0 +1,43 @@
+/*
+ * LotteryWindowCallback.h
+ *
+ *  Created on: Mar 16, 2015
+ *      Author: swgemu
+ */
+
+#ifndef LOTTERYWINDOWCALLBACK_H_
+#define LOTTERYWINDOWCALLBACK_H_
+
+
+#include "server/zone/packets/MessageCallback.h"
+
+class LotteryWindowCallback : public MessageCallback {
+	uint64 containerID;
+
+	uint32 listSize;
+	Vector<uint64> lootIDs;
+
+	ObjectControllerMessageCallback* objectControllerMain;
+
+public:
+	LotteryWindowCallback(ObjectControllerMessageCallback* objectControllerCallback) :
+		MessageCallback(objectControllerCallback->getClient(), objectControllerCallback->getServer()) {
+
+		objectControllerMain = objectControllerCallback;
+	}
+
+	void parse(Message* msg) {
+		msg->parseInt();
+		containerID = msg->parseLong();
+		listSize = msg->parseInt();
+
+		for (int i = 0; i < listSize; i++)
+			lootIDs.add(msg->parseLong());
+	}
+
+	void run() {
+	}
+};
+
+
+#endif /* LOTTERYWINDOWCALLBACK_H_ */
