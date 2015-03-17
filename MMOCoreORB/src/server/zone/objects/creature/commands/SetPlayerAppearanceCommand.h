fc1e8883a9804d85b192b10104058cca30fc15c5
Miztah <myztah@gmail.com>
2015-03-17 03:40:32 +0000

    [added] setPlayerAppearance admin command for setting a player's appearance to that of a mobile template
    Change-Id: I15c548ae4d8a1e15b6c0a48a70768ad372c0c209

--- MMOCoreORB/src/server/zone/objects/creature/commands/SetPlayerAppearanceCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SetPlayerAppearanceCommand.h
@@ -63,7 +63,67 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		PlayerObject* ghost = creature->getPlayerObject();
+		if (!ghost->isPrivileged())
+			return INSUFFICIENTPERMISSION;
+
+		ManagedReference<CreatureObject*> targetCreature = NULL;
+		PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
+
+		UnicodeTokenizer tokenizer(arguments);
+		String templateName = "";
+		String targetName = "";
+		String argument;
+
+		if (creature->getTargetID() != 0) {
+			targetCreature = server->getZoneServer()->getObject(creature->getTargetID()).castTo<CreatureObject*>();
+		} else {
+			if (!tokenizer.hasMoreTokens()) {
+				sendSyntax(creature);
+				return GENERALERROR;
+			}
+
+			tokenizer.getStringToken(targetName);
+			targetCreature = playerManager->getPlayer(targetName);
+		}
+
+		if (targetCreature == NULL || !targetCreature->isPlayerCreature()) {
+			sendSyntax(creature);
+			return INVALIDTARGET;
+		}
+
+		if (tokenizer.hasMoreTokens())
+			tokenizer.getStringToken(argument);
+
+		if (argument != "" && (argument.indexOf(".iff") == -1 || argument.indexOf("object/mobile/shared_") == -1)) {
+			creature->sendSystemMessage("Invalid template. Template must be in object/mobile and have shared_ in its filename or left blank to reset to the default template. Example: object/mobile/shared_darth_vader.iff");
+			return GENERALERROR;
+		} else if (argument != "") {
+			TemplateManager* templateManager = TemplateManager::instance();
+			String templateTest = argument.replaceFirst("shared_", "");
+			SharedObjectTemplate* templateData = templateManager->getTemplate(templateTest.hashCode());
+			if (templateData == NULL) {
+				creature->sendSystemMessage("Unable to find template. Template must be in object/mobile and have shared_ in its filename or left blank to reset to the default template. Example: object/mobile/shared_darth_vader.iff");
+				return GENERALERROR;
+			}
+			templateName = argument;
+		}
+
+		targetCreature->setAlternateAppearance(templateName, true);
+
+		// Required to reset a target to its normal template
+		if (templateName == "") {
+			creature->sendSystemMessage("The target's player appearance temple has been reset to its default.");
+			targetCreature->switchZone(targetCreature->getZone()->getZoneName(), targetCreature->getPositionX(), targetCreature->getPositionZ(), targetCreature->getPositionY(), targetCreature->getParentID());
+		}
+
 		return SUCCESS;
+	}
+
+	void sendSyntax(CreatureObject* creature) {
+		creature->sendSystemMessage("SYNTAX: /setPlayerAppearance [player] [mobile object template with shared in filename or blank to reset]");
+		creature->sendSystemMessage("EXAMPLE: /setPlayerAppearance playerName object/mobile/shared_darth_vader.iff");
+		creature->sendSystemMessage("EXAMPLE RESET: /setPlayerAppearance playerName");
 	}
 
 };
