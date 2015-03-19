5ac45644eefdcb52740b2c8acbf37b8aa643dca6
Ivojedi <ivojedi@swgemu.com>
2015-03-19 16:19:01 -0700

    [fixed] Imperial vs Gungan POI issues - mantis 4274, 5410
    Change-Id: Ib0ff9bac89b84f331d1c908d21641fdb35cde605

---
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/rebel_gungan_general.lua
@@ -0,0 +1,46 @@
+rebel_gungan_general = Creature:new {
+	objectName = "@mob/creature_names:gungan_general",
+	socialGroup = "rebel",
+	faction = "rebel",
+	level = 26,
+	chanceHit = 0.36,
+	damageMin = 250,
+	damageMax = 260,
+	baseXp = 2730,
+	baseHAM = 7700,
+	baseHAMmax = 9400,
+	armor = 0,
+	resists = {15,25,15,-1,30,-1,15,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/gungan_male.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 2000000},
+				{group = "gungan_common", chance = 2000000},
+				{group = "wearables_all", chance = 2000000},
+				{group = "tailor_components", chance = 2000000},
+				{group = "loot_kit_parts", chance = 2000000}
+			},
+			lootChance = 3200000
+		}
+	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "",
+	attacks = merge(brawlermaster,marksmanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(rebel_gungan_general, "rebel_gungan_general")
