f8aa9c78f7bbfe92e7462125da9e8302c1516f4b
dannuic <dannuic@swgemu.com>
2015-03-20 12:05:47 -0600

    [fixed] state combat rolls
    Change-Id: Ia6e593b555300fedd7e2fc217da899633df8d841

---
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/PrivateSkillMultiplierBuffImplementation.cpp
@@ -0,0 +1,61 @@
+/*
+ * PrivateSkillMultiplierBuffImplementation.cpp
+ *
+ *  Created on: Mar 20, 2015
+ *      Author: swgemu
+ */
+
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/creature/buffs/PrivateSkillMultiplierBuff.h"
+
+void PrivateSkillMultiplierBuffImplementation::applySkillModifiers() {
+	ManagedReference<CreatureObject*> strongCreo = creature.get();
+	if (strongCreo == NULL)
+		return;
+
+	int size = skillModifiers.size();
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<String, int>* entry = &skillModifiers.elementAt(i);
+
+		String key = entry->getKey();
+		int value = entry->getValue();
+
+		if (value == 0)
+			continue;
+
+		int prevMod = strongCreo->getSkillMod(key);
+
+		strongCreo->addSkillMod(SkillModManager::BUFF, key, prevMod == 0 ? value : prevMod*(value-1), true);
+	}
+
+	creature.get()->updateSpeedAndAccelerationMods();
+}
+
+void PrivateSkillMultiplierBuffImplementation::removeSkillModifiers() {
+	ManagedReference<CreatureObject*> strongCreo = creature.get();
+	if (strongCreo == NULL)
+		return;
+
+	int size = skillModifiers.size();
+
+	for (int i = 0; i < size; ++i) {
+		VectorMapEntry<String, int>* entry = &skillModifiers.elementAt(i);
+
+		String key = entry->getKey();
+		int value = entry->getValue();
+
+		if (value == 0)
+			continue;
+
+		int prevMod = strongCreo->getSkillMod(key);
+
+		strongCreo->addSkillMod(SkillModManager::BUFF, key, prevMod <= value ? -prevMod : (int)(prevMod*(1/value-1)), true);
+
+	}
+
+	creature.get()->updateSpeedAndAccelerationMods();
+}
+
+
+
