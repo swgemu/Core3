0f25afc906f4fff1a46fa43c2d34177f4a7b54a3
Miztah <myztah@gmail.com>
2015-03-20 00:50:09 +0000

    [fixed] Imperial vs Gungan POI issues - mantis 4274, 5410
    Change-Id: Ib0ff9bac89b84f331d1c908d21641fdb35cde605

---
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/rebel_gungan_boss.lua
@@ -0,0 +1,45 @@
+rebel_gungan_boss = Creature:new {
+	objectName = "@mob/creature_names:gungan_boss",
+	socialGroup = "rebel",
+	faction = "rebel",
+	level = 19,
+	chanceHit = 0.32,
+	damageMin = 170,
+	damageMax = 180,
+	baseXp = 1426,
+	baseHAM = 4100,
+	baseHAMmax = 5000,
+	armor = 0,
+	resists = {0,20,0,35,35,-1,-1,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/gungan_male.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4000000},
+				{group = "gungan_common", chance = 2000000},
+				{group = "tailor_components", chance = 2000000},
+				{group = "loot_kit_parts", chance = 2000000}
+			},
+			lootChance = 3200000
+		}
+	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "",
+	attacks = merge(brawlermaster,marksmanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(rebel_gungan_boss, "rebel_gungan_boss")
