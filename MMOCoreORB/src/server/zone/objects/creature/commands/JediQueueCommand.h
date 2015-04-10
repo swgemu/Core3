7354e6ae6cab5bb261aa38c6745412912e2eb6e2
Valkyra <lyhtmyst@gmail.com>
2015-04-10 03:08:51 -0400

    [added] New system for Jedi queue commands to have Lua parameters able to be specified/read in the script. [added] A more simplified check system for Jedi self commands. [modified] ForceRun1 (only) into this new system.
    Change-Id: I205c29d8fdb97dad12288dacbb4ccd581fdee7dd

---
+++ MMOCoreORB/src/server/zone/objects/creature/commands/JediQueueCommand.h
@@ -0,0 +1,93 @@
+/*
+ * JediQueueCommand.h
+ *
+ *  Created on: Apr 9, 2015
+ *      Author: valkyra
+ */
+
+#ifndef JEDIQUEUECOMMAND_H_
+#define JEDIQUEUECOMMAND_H_
+
+
+#include "server/zone/objects/creature/buffs/Buff.h"
+#include "QueueCommand.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+
+
+class JediQueueCommand : public QueueCommand {
+protected:
+	int forceCost;
+	int duration;
+	uint32 animationCRC;
+	String clientEffect;
+	float speedMod;
+
+public:
+	JediQueueCommand(const String& name, ZoneProcessServer* server) : QueueCommand(name, server) {
+		forceCost = 0;
+		duration = 0;
+		animationCRC = 0;
+		clientEffect = "";
+		speedMod = 0;
+	}
+
+	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
+		return SUCCESS;
+	}
+
+	bool isJediQueueCommand() {
+		return true;
+	}
+
+	ManagedReference<Buff*> doJediSelfBuff(CreatureObject* creature, Vector<uint32> buffCRCs, VectorMap<String, int> skillMods) const {
+
+		for (int i=0; i < buffCRCs.size(); ++i) {
+			if (creature->hasBuff(buffCRCs.get(i))) {
+				return NULL;
+			}
+		}
+
+		ManagedReference<Buff*> buff = new Buff(creature, buffCRCs.get(0), duration, BuffType::JEDI);
+
+		if (speedMod > 0) {
+			buff->setSpeedMultiplierMod(speedMod);
+			buff->setAccelerationMultiplierMod(speedMod);
+		}
+
+		StringIdChatParameter start("jedi_spam", "apply_" + name);
+		StringIdChatParameter end("jedi_spam", "remove_" + name);
+
+		buff->setStartMessage(start);
+		buff->setEndMessage(end);
+
+		for (int i=0; i < skillMods.size(); ++i) {
+			buff->setSkillModifier(skillMods.elementAt(i).getKey(), skillMods.elementAt(i).getValue());
+		}
+
+		return buff;
+	}
+
+	void setForceCost(int fc) {
+		forceCost = fc;
+	}
+
+	void setDuration(int dur) {
+		duration = dur;
+	}
+
+	void setAnimationCRC(uint32 crc) {
+		animationCRC = crc;
+	}
+
+	void setClientEffect(String ce) {
+		clientEffect = ce;
+	}
+
+	void setSpeedMod(float sm) {
+		speedMod = sm;
+	}
+
+};
+
+
+#endif /* SRC_SERVER_ZONE_OBJECTS_CREATURE_COMMANDS_JEDIQUEUECOMMAND_H_ */
