895395e98fae39a0505655663de1d31aff4c82dc
Ivojedi <ivojedi@swgemu.com>
2015-04-16 11:23:11 -0700

    [Added] ai will turn to face the target of their hi/bye reaction chat
    Change-Id: Ic86252b4b5847dbd529fe37543791862e809633a

--- MMOCoreORB/bin/scripts/ai/interrupts.lua
+++ MMOCoreORB/bin/scripts/ai/interrupts.lua
@@ -148,6 +148,8 @@ function DefaultInterrupt:checkForReactionChat(pAgent, pObject)
 		state = REACTION_MID
 	end
 
+	SceneObject(pAgent):faceObject(pObject)
+
 	if SceneObject(pObject):isFacingObject(pAgent) then
 		AiAgent(pAgent):sendReactionChat(REACTION_HI, state)
 	else
