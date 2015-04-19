b8dd8be981715054b0bd051bf496f044c175b000
Victor Popovici <victorpopovici@gmail.com>
2015-04-19 11:24:07 +0200

    [added] (Lua)BuildingObject::getCell(name)
    Change-Id: I8cf7d64a6e98b118892062b6c2d3990f0b650ceb

---
+++ MMOCoreORB/src/server/zone/templates/appearance/CellProperty.h
@@ -0,0 +1,63 @@
+/*
+ * CellProperty.h
+ *
+ *  Created on: Apr 19, 2015
+ *      Author: TheAnswer
+ */
+
+#ifndef SRC_SERVER_ZONE_TEMPLATES_APPEARANCE_CELLPROPERTY_H_
+#define SRC_SERVER_ZONE_TEMPLATES_APPEARANCE_CELLPROPERTY_H_
+
+#include "engine/engine.h"
+
+#include "../IffTemplate.h"
+
+class MeshAppearanceTemplate;
+class FloorMesh;
+
+class CellProperty : public IffTemplate, public Logger {
+protected:
+	String name;
+	bool canSeeParentCell;
+	int numberOfPortals;
+	FloorMesh* floorMesh;
+	MeshAppearanceTemplate* appearanceTemplate;
+	int cellID;
+
+public:
+	CellProperty();
+	CellProperty(int cellID);
+	CellProperty(const CellProperty& c);
+
+	CellProperty& operator=(const CellProperty& c);
+
+	void readObject(IffStream* iffStream);
+
+	MeshAppearanceTemplate* getAppearanceTemplate() {
+		return appearanceTemplate;
+	}
+
+	bool isCanSeeParentCell() const {
+		return canSeeParentCell;
+	}
+
+	FloorMesh* getFloorMesh() {
+		return floorMesh;
+	}
+
+	const String& getName() const {
+		return name;
+	}
+
+	int getCellID() const {
+		return cellID;
+	}
+
+	int getNumberOfPortals() const {
+		return numberOfPortals;
+	}
+
+};
+
+
+#endif /* SRC_SERVER_ZONE_TEMPLATES_APPEARANCE_CELLPROPERTY_H_ */
