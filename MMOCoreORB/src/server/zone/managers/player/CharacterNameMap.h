4ee64a6c1a75da0e7153b4e3d2b56ee1934f5aa3
Victor Popovici <theanswer@swgemu.com>
2015-05-13 11:20:53 +0200

    [fixed] stability issue
    Change-Id: I7f02a358061e259abd8486b0a10bb72b0195d950

--- MMOCoreORB/src/server/zone/managers/player/CharacterNameMap.h
+++ MMOCoreORB/src/server/zone/managers/player/CharacterNameMap.h
@@ -13,16 +13,21 @@
 
 class CharacterNameMap : public Object {
 	HashTable<String, uint64> names;
+	ReadWriteLock guard;
 
 public:
 	CharacterNameMap() : names(3000) {
 	}
 
 	void put(CreatureObject* player) {
+		Locker locker(&guard);
+
 		names.put(player->getFirstName().toLowerCase(), player->getObjectID());
 	}
 
 	bool put(const String& name, uint64 oid) {
+		Locker locker(&guard);
+
 		if (names.put(name, oid) != names.getNullValue())
 			return false;
 
@@ -30,14 +35,20 @@ public:
 	}
 
 	void remove(const String& name) {
+		Locker locker(&guard);
+
 		names.remove(name.toLowerCase());
 	}
 
 	uint64& get(const String& name) {
+		ReadLocker locker(&guard);
+
 		return names.get(name);
 	}
 
 	bool containsKey(const String& name) {
+		ReadLocker locker(&guard);
+
 		return names.containsKey(name);
 	}
 
