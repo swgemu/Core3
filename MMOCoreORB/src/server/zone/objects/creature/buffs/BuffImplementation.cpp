6d9267b20a1148cda42265604f20568ee95ab004
Ivojedi <ivojedi@swgemu.com>
2015-05-16 19:46:45 -0700

    [Fixed] buffs that apply terrain negotiation not updating the client with the change
    Change-Id: I09bf983e10b7bf1b25e42a2386ff783015e283d2

--- MMOCoreORB/src/server/zone/objects/creature/buffs/BuffImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/BuffImplementation.cpp
@@ -296,6 +296,7 @@ void BuffImplementation::applySkillModifiers() {
 	// if there was a speed or acceleration mod change, this will take care of immediately setting them.
 	// the checks for if they haven't changed are in these methods
 	creature.get()->updateSpeedAndAccelerationMods();
+	creature.get()->updateTerrainNegotiation();
 }
 
 void BuffImplementation::applyStates() {
@@ -376,6 +377,7 @@ void BuffImplementation::removeSkillModifiers() {
 	// if there was a speed or acceleration mod change, this will take care of immediately setting them.
 	// the checks for if they haven't changed are in these methods
 	creature.get()->updateSpeedAndAccelerationMods();
+	creature.get()->updateTerrainNegotiation();
 }
 
 void BuffImplementation::removeStates() {
