d2bb1ff9811f29fa3ef35c34e701af45546caaec
Ivojedi <ivojedi@swgemu.com>
2015-05-27 10:31:08 -0700

    [Fixed] old cure/dot packs not working - mantis 6278
    Change-Id: I9a0f1a994faff72294d6b35f5a7aa9ebfef845d8

---
+++ MMOCoreORB/src/server/zone/objects/tangible/pharmaceutical/DotPackImplementation.cpp
@@ -0,0 +1,13 @@
+/*
+				Copyright <SWGEmu>
+		See file COPYING for copying conditions. */
+
+#include "server/zone/objects/tangible/pharmaceutical/DotPack.h"
+
+void DotPackImplementation::notifyLoadFromDatabase() {
+	PharmaceuticalObjectImplementation::notifyLoadFromDatabase();
+
+	if (commandToExecute.beginsWith("/")) {
+		commandToExecute.replaceFirst("/", "");
+	}
+}
