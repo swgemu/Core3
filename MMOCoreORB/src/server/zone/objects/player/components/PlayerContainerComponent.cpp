6f2d6d985a560cba46d93d95d42e219e7430a14c
ValkyraII <valkyra@swgemu.com>
2015-06-01 20:34:39 -0400

    [fixed] Mantis #4894 - Jedi Robes' Force Power Max and Regen not applying when equipped (with lightsaber).
    Change-Id: Ib47eb5935bc3a3cb1c5040f25f9625cc4308c1b1

--- MMOCoreORB/src/server/zone/objects/player/components/PlayerContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/player/components/PlayerContainerComponent.cpp
@@ -121,11 +121,22 @@ int PlayerContainerComponent::notifyObjectInserted(SceneObject* sceneObject, Sce
 		tano->applySkillModsTo(creo);
 	}
 
+	// Jedi stuff below.
 	PlayerObject* ghost = creo->getPlayerObject();
 
 	if (ghost && ghost->isJedi()) {
-		if ((object->isRobeObject() && cast<RobeObject*>( object)->getSkillRequired() != "") || (object->isWeaponObject() && cast<WeaponObject*>(object)->isJediWeapon())) {
+		if ((object->isRobeObject())) {
 			VisibilityManager::instance()->increaseVisibility(creo);
+			RobeObject* robeObject = cast<RobeObject*>(object);
+			if (creo->getWeapon()->isJediWeapon()) {
+				ghost->setForcePowerMax(ghost->getForcePowerMax() + robeObject->getTemplateSkillMods()->get(robeObject->getTemplateSkillMods()->find("jedi_force_power_max")));
+				ghost->setForcePowerRegen(ghost->getForcePowerRegen() + robeObject->getTemplateSkillMods()->get(robeObject->getTemplateSkillMods()->find("jedi_force_power_regen")));
+			}
+		} else if (object->isWeaponObject()) {
+			WeaponObject* weaponObject = cast<WeaponObject*>(object);
+			if (weaponObject->isJediWeapon()) {
+				VisibilityManager::instance()->increaseVisibility(creo);
+			}
 		}
 	}
 
@@ -175,9 +186,22 @@ int PlayerContainerComponent::notifyObjectRemoved(SceneObject* sceneObject, Scen
 			creo->stopEntertaining();
 	}
 
-	if (creo->getPlayerObject().get() != NULL && creo->getPlayerObject()->isJedi()) {
-		if ((object->isRobeObject() && cast<RobeObject*>( object)->getSkillRequired() != "") || (object->isWeaponObject() && cast<WeaponObject*>(object)->isJediWeapon())) {
+	// Jedi stuff below.
+	PlayerObject* ghost = creo->getPlayerObject();
+
+	if (ghost && ghost->isJedi()) {
+		if ((object->isRobeObject())) {
 			VisibilityManager::instance()->increaseVisibility(creo);
+			RobeObject* robeObject = cast<RobeObject*>(object);
+			if (creo->getWeapon()->isJediWeapon()) {
+				ghost->setForcePowerMax(ghost->getForcePowerMax() - robeObject->getTemplateSkillMods()->get(robeObject->getTemplateSkillMods()->find("jedi_force_power_max")));
+				ghost->setForcePowerRegen(ghost->getForcePowerRegen() - robeObject->getTemplateSkillMods()->get(robeObject->getTemplateSkillMods()->find("jedi_force_power_regen")));
+			}
+		} else if (object->isWeaponObject()) {
+			WeaponObject* weaponObject = cast<WeaponObject*>(object);
+			if (weaponObject->isJediWeapon()) {
+				VisibilityManager::instance()->increaseVisibility(creo);
+			}
 		}
 	}
 
