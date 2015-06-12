b722982b9ea77d12f4efb6e523248afe24234553
Miztahh <miztah@swgemu.com>
2015-06-12 00:20:51 +0000

    [fixed] Secondary Barada mission npc killing the primary target npc - mantis 6365
    Change-Id: I3428a856de11a0983dcd02acff98651b836d3544

---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/theme_park_jabba_weak_mercenary.lua
@@ -0,0 +1,52 @@
+theme_park_jabba_weak_mercenary = Creature:new {
+	objectName = "@mob/creature_names:mercenary_weak",
+	randomNameType = NAME_GENERIC_TAG,
+	socialGroup = "valarian",
+	faction = "valarian",
+	level = 34,
+	chanceHit = 0.41,
+	damageMin = 315,
+	damageMax = 340,
+	baseXp = 3460,
+	baseHAM = 8700,
+	baseHAMmax = 10700,
+	armor = 0,
+	resists = {30,20,20,20,20,20,-1,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_mercenary_weak_hum_m.iff",
+		"object/mobile/dressed_mercenary_weak_hum_f.iff",
+		"object/mobile/dressed_mercenary_weak_rod_m.iff"
+	},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "rifles", chance = 1000000},
+				{group = "pistols", chance = 1000000},
+				{group = "melee_weapons", chance = 1000000},
+				{group = "carbines", chance = 1000000},
+				{group = "wearables_common", chance = 1000000}
+			}
+		}
+	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(riflemanmaster,carbineermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_jabba_weak_mercenary, "theme_park_jabba_weak_mercenary")
