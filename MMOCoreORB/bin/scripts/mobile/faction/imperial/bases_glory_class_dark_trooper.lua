736ef1b64bb80e4583223adfd35e46fbb8c6d770
Nee <nee@swgemu.com>
2015-07-30 19:10:35 +0000

    [added/changed] More accurate to 'Publish 14.1 era' spawns to all player-dropped Faction Bases, Mantis 6157 (phase 1)
    Change-Id: I735ec72533a21ef30ad7c3e0475a54be2606e74a

---
+++ MMOCoreORB/bin/scripts/mobile/faction/imperial/bases_glory_class_dark_trooper.lua
@@ -0,0 +1,54 @@
+bases_glory_class_dark_trooper = Creature:new {
+	objectName = "@mob/creature_names:a_glory_class_dark_trooper",
+	randomNameType = NAME_DARKTROOPER,
+	socialGroup = "imperial",
+	faction = "imperial",
+	level = 75,
+	chanceHit = 0.4,
+	damageMin = 405,
+	damageMax = 520,
+	baseXp = 4465,
+	baseHAM = 27000,
+	baseHAMmax = 30000,
+	armor = 0,
+	resists = {40,20,20,50,50,50,50,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE + OVERT,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+	scale = 1.5,
+
+	templates = {"object/mobile/dark_trooper.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 300000},
+				{group = "junk", chance = 6300000},
+				{group = "rifles", chance = 600000},
+				{group = "holocron_dark", chance = 150000},
+				{group = "holocron_light", chance = 150000},
+				{group = "carbines", chance = 600000},
+				{group = "pistols", chance = 600000},
+				{group = "clothing_attachments", chance = 150000},
+				{group = "armor_attachments", chance = 150000},
+				{group = "wearables_common", chance = 1000000}
+			}
+		}
+	},
+	weapons = {"dark_trooper_weapons"},
+	conversationTemplate = "",
+	reactionStf = "@npc_reaction/stormtrooper",
+	personalityStf = "@hireling/hireling_stormtrooper",	
+	attacks = merge(riflemanmaster,marksmanmaster,fencermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(bases_glory_class_dark_trooper, "bases_glory_class_dark_trooper")
