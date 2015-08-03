8bfdbcef197609cd1af1043132f59105600cf8e5
Nee <nee@swgemu.com>
2015-08-03 00:55:05 +0000

    [added] Bubble Tank schematic to loot-tables, Mantis 5966 (part 1) -- (part 2 will be adding the corresponding quest NPC, Risha Sinan, to Restuss 'screenplay'...as well as functionality of the quest & journal)
    Change-Id: I5868f232d06b607c85cb23092e3df6db5d823890

---
+++ MMOCoreORB/bin/scripts/loot/items/bubble_tank_schematic.lua
@@ -0,0 +1,12 @@
+bubble_tank_schematic = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/loot_schematic/bubble_tank_schematic.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("bubble_tank_schematic", bubble_tank_schematic)
