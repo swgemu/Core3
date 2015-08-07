4ee652732f9582758ae05ede11de1bfafae716f0
Nee <nee@swgemu.com>
2015-08-07 21:03:48 +0000

    [fixed] TK meditate no longer increases 'force regen' rate, Mantis 5251 (Miztah code)
    Change-Id: If97761fb450c138e9749914fa3f0dd293f5c648a

--- MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -78,6 +78,7 @@
 #include "server/login/account/Account.h"
 #include "server/zone/objects/tangible/deed/eventperk/EventPerkDeed.h"
 #include "server/zone/managers/player/QuestInfo.h"
+#include "server/zone/objects/player/events/ForceMeditateTask.h"
 
 void PlayerObjectImplementation::initializeTransientMembers() {
 	IntangibleObjectImplementation::initializeTransientMembers();
@@ -1847,16 +1848,14 @@ void PlayerObjectImplementation::doForceRegen() {
 
 	uint32 modifier = 1;
 
-	// TODO: Re-factor Force Meditate so TKA meditate doesn't effect.
-	if (creature->isMeditating())
-		modifier = 3;
+	if (creature->isMeditating()) {
+		Reference<ForceMeditateTask*> medTask = creature->getPendingTask("forcemeditate").castTo<ForceMeditateTask*>();
+
+		if (medTask != NULL)
+			modifier = 3;
+	}
 
 	uint32 forceTick = tick * modifier;
-
-	//forceTick cant be <1 as per above code, tick is always positive and modifier as well
-	/*if (forceTick < 1)
-		forceTick = 1;
-		*/
 
 	if (forceTick > getForcePowerMax() - getForcePower()){   // If the player's Force Power is going to regen again and it's close to max,
 		setForcePower(getForcePowerMax());             // Set it to max, so it doesn't go over max.
