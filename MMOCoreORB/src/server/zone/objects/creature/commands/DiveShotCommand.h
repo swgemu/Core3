7d5e6e1c41398881880fe533d3489a962c666341
Nee <nee@swgemu.com>
2015-08-12 02:22:03 +0000

    [WIP] possible fix for Marksman/Pistols 'Kip-up shot', 'Dive shot', & 'Roll shot', Mantis 6064 (part 1)
    Change-Id: I9e9f6bcd5a1ff9dcececdbc621c87147050ddadc

--- MMOCoreORB/src/server/zone/objects/creature/commands/DiveShotCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/DiveShotCommand.h
@@ -8,6 +8,8 @@
 #include "server/zone/objects/scene/SceneObject.h"
 #include "CombatQueueCommand.h"
 #include "server/zone/packets/creature/CreatureObjectDeltaMessage3.h"
+#include "server/zone/objects/creature/events/AnimationTask.h"
+#include "server/zone/objects/creature/buffs/StateBuff.h"
 
 class DiveShotCommand : public CombatQueueCommand {
 public:
@@ -24,20 +26,42 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-		int ret = doCombatAction(creature, target);
-
-		if (ret != SUCCESS)
-			return ret;
-
 		if (creature->isDizzied() && System::random(100) < 85) {
 			creature->queueDizzyFallEvent();
 		} else {
+			//Check for and deduct HAM cost.
+			int actionCost = creature->calculateCostAdjustment(CreatureAttribute::QUICKNESS, 100);
+			if (creature->getHAM(CreatureAttribute::ACTION) <= actionCost)
+				return INSUFFICIENTHAM;
+
+			creature->inflictDamage(creature, CreatureAttribute::ACTION, actionCost, true);
+
 			creature->setPosture(CreaturePosture::PRONE, false);
+
+			Reference<CreatureObject*> defender = server->getZoneServer()->getObject(target).castTo<CreatureObject*>();
+			if (defender == NULL)
+				creature->doCombatAnimation(creature,STRING_HASHCODE("tumble"),0,0xFF);
+			else
+				creature->doCombatAnimation(defender,STRING_HASHCODE("tumble_facing"),0,0xFF);
+
+			Reference<StateBuff*> buff = new StateBuff(creature, CreatureState::TUMBLING, 1);
+
+			Locker locker(buff);
+
+			buff->setSkillModifier("melee_defense", 50);
+			buff->setSkillModifier("ranged_defense", 50);
+
+			creature->addBuff(buff);
+
+			locker.release();
+
+			return doCombatAction(creature, target);
 
 			CreatureObjectDeltaMessage3* pmsg = new CreatureObjectDeltaMessage3(creature);
 			pmsg->updatePosture();
 			pmsg->close();
 			creature->broadcastMessage(pmsg, true);
+			creature->sendStateCombatSpam("cbt_spam", "tum_prone", 0);
 		}
 
 		return SUCCESS;
--- MMOCoreORB/src/server/zone/objects/creature/commands/TumbleToProneCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TumbleToProneCommand.h
@@ -7,6 +7,8 @@
 
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/creature/events/AnimationTask.h"
+#include "server/zone/objects/creature/buffs/StateBuff.h"
+#include "server/zone/packets/creature/CreatureObjectDeltaMessage3.h"
 
 class TumbleToProneCommand : public QueueCommand {
 public:
