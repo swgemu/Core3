a6c4397e7ca30941437db7a1fac010c1da2354cf
Victor Popovici <theanswer@swgemu.com>
2015-08-16 01:08:21 +0200

    [added] more experimental terrain cache
    Change-Id: Iee5b928fa416a2066febcc325b6c52e7eda809bb

--- MMOCoreORB/src/server/zone/objects/scene/variables/DeltaShortVariable.h
+++ MMOCoreORB/src/server/zone/objects/scene/variables/DeltaShortVariable.h
@@ -8,12 +8,12 @@
 #ifndef DELTASHORTVARIABLE_H_
 #define DELTASHORTVARIABLE_H_
 
-#include "DeltaVariable.h"
+#include "DeltaBasicVariable.h"
 
 template<int BaselineName, uint8 Type, int DeltaID>
-class DeltaShortVariable : public DeltaVariable<uint64> {
+class DeltaShortVariable : public DeltaBasicVariable<uint64> {
 public:
-	void update(int newValue, bool broadcastStandalone = false, SceneObject* obj = NULL) {
+	void update(int newValue, bool broadcastStandalone = false, server::zone::objects::scene::SceneObject* obj = NULL) {
 		set(newValue);
 
 		if (broadcastStandalone) {
@@ -21,7 +21,7 @@ public:
 		}
 	}
 
-	void broadcastStandaloneDeltaMessage(SceneObject* obj) {
+	void broadcastStandaloneDeltaMessage(server::zone::objects::scene::SceneObject* obj) {
 		DeltaMessage* msg = new DeltaMessage(obj->getObjectID(), BaselineName, Type);
 		msg->addShortUpdate(DeltaID, object);
 		msg->close();
