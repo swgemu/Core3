4e934be3d7b9a0d86b7239015c604332b102d5c4
Nee <nee@swgemu.com>
2015-08-25 10:14:58 +0000

    [modified] a few additional screenplay 'populations' of various buildings in spaceports -- ENDOR & YAVIN4 phases
    Change-Id: Iecde9c0facd761fea3c4637ff9dbe1985845bc93

--- MMOCoreORB/bin/scripts/screenplays/cities/yavin4_labor_outpost.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/yavin4_labor_outpost.lua
@@ -14,5 +14,17 @@ end
 
 function Yavin4LaborOutpostScreenPlay:spawnMobiles()
 
-	--Atm adding the file to cites folder to get it out of the way.
+	local pNpc = spawnMobile("yavin4", "devaronian_male",60,7.3,0.6,-6.7,-149,3035379)
+	self:setMoodString(pNpc, "npc_standing_drinking")
+	pNpc = spawnMobile("yavin4", "commoner_technician",60,5.8,0.6,-6.2,-141,3035379)
+	self:setMoodString(pNpc, "npc_use_terminal_low")
+	pNpc = spawnMobile("yavin4", "chiss_female",60,-6.8,0.6,6.3,179,3035377)
+	self:setMoodString(pNpc, "npc_sitting_chair")
+	pNpc = spawnMobile("yavin4", "chiss_male",60,-6.8,0.6,5.0,1,3035377)
+	self:setMoodString(pNpc, "happy")
+	pNpc = spawnMobile("yavin4", "medic",60,-6.1,0.6,-3.8,-102,3035374)
+	self:setMoodString(pNpc, "sad")
+	pNpc = spawnMobile("yavin4", "commoner_technician",60,-6.1,0.6,-4.8,-10,3035374)
+	self:setMoodString(pNpc, "npc_consoling")
+
 end
--- MMOCoreORB/bin/scripts/screenplays/cities/yavin4_mining_outpost.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/yavin4_mining_outpost.lua
@@ -14,5 +14,22 @@ end
 
 function Yavin4MiningOutpostScreenPlay:spawnMobiles()
 
-	--Atm adding the file to cites folder to get it out of the way.
+	--tavern a
+	local pNpc = spawnMobile("yavin4", "sullustan_male",60,3.1,0.7,2.4,0,7925478)
+	self:setMoodString(pNpc, "npc_sitting_chair")
+	pNpc = spawnMobile("yavin4", "medic",60,2.3,0.7,6.5,-5,7925478)
+	self:setMoodString(pNpc, "npc_use_terminal_low")
+	pNpc = spawnMobile("yavin4", "surgical_droid_21b",60,-1.9,0.7,2.5,175,7925478)
+	self:setMoodString(pNpc, "neutral")
+	--tavern b
+	pNpc = spawnMobile("yavin4", "shadowy_figure",60,0.4,0.6,-0.7,-179,7925451)
+	self:setMoodString(pNpc, "npc_sitting_chair")
+	pNpc = spawnMobile("yavin4", "bartender",60,3.4,0.6,5.6,173,7925449)
+	self:setMoodString(pNpc, "conversation")
+	pNpc = spawnMobile("yavin4", "commoner_fat",60,3.5,0.6,4.2,-5,7925449)
+	self:setMoodString(pNpc, "npc_standing_drinking")
+	--building empty
+	pNpc = spawnMobile("yavin4", "ithorian_male",60,3.6,0.1,-0.5,0,1713372)
+	self:setMoodString(pNpc, "npc_sitting_chair")
+
 end
