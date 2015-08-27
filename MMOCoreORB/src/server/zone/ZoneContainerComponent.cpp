08a7e18199fd9063ce6fb5bdc9b02a3bec08a950
Victor Popovici <theanswer@swgemu.com>
2015-08-27 19:15:39 +0200

    [removed] more unnecessary ref counting
    Change-Id: I1201b2b81cc1f1d8dc2d1f3eaded9370100ae365

--- MMOCoreORB/src/server/zone/ZoneContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/ZoneContainerComponent.cpp
@@ -45,13 +45,13 @@ bool ZoneContainerComponent::insertActiveArea(Zone* newZone, ActiveArea* activeA
 	//regionTree->inRange(activeArea, 512);
 
 	// lets update area to the in range players
-	SortedVector<ManagedReference<QuadTreeEntry*> > objects;
+	SortedVector<QuadTreeEntry*> objects;
 	float range = activeArea->getRadius() + 64;
 
 	newZone->getInRangeObjects(activeArea->getPositionX(), activeArea->getPositionY(), range, &objects, false);
 
 	for (int i = 0; i < objects.size(); ++i) {
-		SceneObject* object = cast<SceneObject*>(objects.get(i).get());
+		SceneObject* object = cast<SceneObject*>(objects.get(i));
 
 		if (!object->isTangibleObject()) {
 			continue;
@@ -84,7 +84,7 @@ bool ZoneContainerComponent::removeActiveArea(Zone* zone, ActiveArea* activeArea
 	regionTree->remove(activeArea);
 
 	// lets remove the in range active areas of players
-	SortedVector<ManagedReference<QuadTreeEntry*> > objects;
+	SortedVector<QuadTreeEntry*> objects;
 	float range = activeArea->getRadius() + 64;
 
 	zone->getInRangeObjects(activeArea->getPositionX(), activeArea->getPositionY(), range, &objects, false);
@@ -94,7 +94,7 @@ bool ZoneContainerComponent::removeActiveArea(Zone* zone, ActiveArea* activeArea
 	zoneLocker.release();
 
 	for (int i = 0; i < objects.size(); ++i) {
-		SceneObject* object = cast<SceneObject*>(objects.get(i).get());
+		SceneObject* object = cast<SceneObject*>(objects.get(i));
 
 	//	Locker olocker(object);
 
