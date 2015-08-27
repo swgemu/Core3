367469c2ae6ee8298eb6d579f338b36ca449eeb1
Victor Popovici <theanswer@swgemu.com>
2015-08-27 19:15:39 +0200

    [removed] more unnecessary ref counting
    Change-Id: I1201b2b81cc1f1d8dc2d1f3eaded9370100ae365

---
+++ MMOCoreORB/src/server/zone/ActiveAreasVector.h
@@ -0,0 +1,18 @@
+/*
+ * ActiveAreasVector.h
+ *
+ *  Created on: Aug 27, 2015
+ *      Author: TheAnswer
+ */
+
+#ifndef SRC_SERVER_ZONE_ACTIVEAREASVECTOR_H_
+#define SRC_SERVER_ZONE_ACTIVEAREASVECTOR_H_
+
+#include "engine/engine.h"
+
+#include "server/zone/objects/area/ActiveArea.h"
+
+typedef SortedVector<ActiveArea*> ActiveAreasVector;
+
+
+#endif /* SRC_SERVER_ZONE_ACTIVEAREASVECTOR_H_ */
