36580bb23c58cadaa8982afee97bc521c9e3321c
Nee <nee@swgemu.com>
2015-09-01 05:45:38 +0000

    [modified] Creature 'taming task' phrasing & order to be more accurate emulation, Mantis 6592 (*Work-in-progress* ... Still figuring out how/where to make phrases RANDOMLY spoken per round per sequence, as well as limit tame-sequence to 3 'rounds' )
    Change-Id: I8fb0691c4570f56bbad510e78cb28bf6e9313101

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -853,7 +853,7 @@ void CreatureManagerImplementation::harvest(Creature* creature, CreatureObject* 
 
 	if (!creature->canHarvestMe(player))
 		return;
-		
+
 	if (!player->isInRange(creature, 7))
 		return;
 
@@ -1099,7 +1099,7 @@ void CreatureManagerImplementation::tame(Creature* creature, CreatureObject* pla
 
 	ChatManager* chatManager = player->getZoneServer()->getChatManager();
 
-	chatManager->broadcastMessage(player, "@hireling/hireling:taming_1"); // Easy.
+	chatManager->broadcastMessage(player, "@hireling/hireling:taming_" + String::valueOf(System::random(4) + 1));
 
 	Locker clocker(creature);
 
