295a9a99ee7fc32e0f2df9c17e3e9c86fc2d73a0
Victor Popovici <theanswer@swgemu.com>
2015-09-03 18:38:44 +0200

    [fixed] stability issue
    Change-Id: Idd72b4e0f8c495001ed5543c81ef43056b594886

--- MMOCoreORB/src/server/zone/objects/player/components/PlayerZoneComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/player/components/PlayerZoneComponent.cpp
@@ -35,7 +35,8 @@ void PlayerZoneComponent::notifyInsertToZone(SceneObject* sceneObject, Zone* new
 	if (sceneObject->isPlayerCreature() && newZone != NULL) {
 		PlayerObject* ghost = sceneObject->asCreatureObject()->getPlayerObject();
 
-		ghost->setSavedTerrainName(newZone->getZoneName());
+		if (ghost != NULL)
+			ghost->setSavedTerrainName(newZone->getZoneName());
 	}
 
 	ZoneComponent::notifyInsertToZone(sceneObject, newZone);
@@ -87,15 +88,19 @@ void PlayerZoneComponent::switchZone(SceneObject* sceneObject, const String& new
 			player->executeObjectControllerAction(STRING_HASHCODE("dismount"));
 		}
 
-		ghost->setSavedParentID(0);
+		if (ghost != NULL) {
+			ghost->setSavedParentID(0);
 
-		ghost->setTeleporting(true);
-		ghost->setOnLoadScreen(true);
-		ghost->updateLastValidatedPosition();
+			ghost->setTeleporting(true);
+			ghost->setOnLoadScreen(true);
+			ghost->updateLastValidatedPosition();
+			ghost->setClientLastMovementStamp(0);
+
+			ghost->unloadSpawnedChildren();
+		}
+
 		player->setMovementCounter(0);
-		ghost->setClientLastMovementStamp(0);
 
-		ghost->unloadSpawnedChildren();
 	}
 
 	ZoneComponent::switchZone(sceneObject, newTerrainName, newPostionX, newPositionZ, newPositionY, parentID, toggleInvisibility);
@@ -121,10 +126,13 @@ void PlayerZoneComponent::teleport(SceneObject* sceneObject, float newPositionX,
 	if (player != NULL) {
 		PlayerObject* ghost = player->getPlayerObject();
 
-		ghost->setTeleporting(true);
-		ghost->updateLastValidatedPosition();
+		if (ghost != NULL) {
+			ghost->setTeleporting(true);
+			ghost->updateLastValidatedPosition();
+			ghost->setClientLastMovementStamp(0);
+		}
+
 		player->setMovementCounter(0);
-		ghost->setClientLastMovementStamp(0);
 	}
 }
 
@@ -141,7 +149,8 @@ void PlayerZoneComponent::updateZone(SceneObject* sceneObject, bool lightUpdate,
 		CreatureObject* player = sceneObject->asCreatureObject();
 		PlayerObject* ghost = player->getPlayerObject();
 
-		ghost->setSavedParentID(0);
+		if (ghost != NULL)
+			ghost->setSavedParentID(0);
 	}
 }
 
@@ -152,7 +161,8 @@ void PlayerZoneComponent::updateZoneWithParent(SceneObject* sceneObject, SceneOb
 		CreatureObject* player = sceneObject->asCreatureObject();
 		PlayerObject* ghost = player->getPlayerObject();
 
-		ghost->setSavedParentID(sceneObject->getParentID());
+		if (ghost != NULL)
+			ghost->setSavedParentID(sceneObject->getParentID());
 	}
 }
 /*
