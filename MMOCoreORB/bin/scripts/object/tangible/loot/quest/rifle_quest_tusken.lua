957691e5540bda37a9144d01837832f24da2cff4
Ivojedi <ivojedi@swgemu.com>
2015-09-18 19:01:37 -0700

    [Fixed] tusken king rifle - mantis 6625
    Change-Id: I01366cbe0c1002c1caa61cdfb1f7409e8bd18ed7
    (cherry picked from commit fc3a12e777d10bded39c022fd83dc15eec54c3f2)

--- MMOCoreORB/bin/scripts/object/tangible/loot/quest/rifle_quest_tusken.lua
+++ MMOCoreORB/bin/scripts/object/tangible/loot/quest/rifle_quest_tusken.lua
@@ -43,6 +43,88 @@
 
 object_tangible_loot_quest_rifle_quest_tusken = object_tangible_loot_quest_shared_rifle_quest_tusken:new {
 
+	playerRaces = { "object/creature/player/bothan_male.iff",
+				"object/creature/player/bothan_female.iff",
+				"object/creature/player/human_male.iff",
+				"object/creature/player/human_female.iff",
+				"object/creature/player/ithorian_male.iff",
+				"object/creature/player/ithorian_female.iff",
+				"object/creature/player/moncal_male.iff",
+				"object/creature/player/moncal_female.iff",
+				"object/creature/player/rodian_male.iff",
+				"object/creature/player/rodian_female.iff",
+				"object/creature/player/sullustan_male.iff",
+				"object/creature/player/sullustan_female.iff",
+				"object/creature/player/trandoshan_male.iff",
+				"object/creature/player/trandoshan_female.iff",
+				"object/creature/player/twilek_male.iff",
+				"object/creature/player/twilek_female.iff",
+				"object/creature/player/wookiee_male.iff",
+				"object/creature/player/wookiee_female.iff",
+				"object/creature/player/zabrak_male.iff",
+				"object/creature/player/zabrak_female.iff" },
+
+	-- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
+	-- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
+	attackType = RANGEDATTACK,
+
+	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, FORCE, LIGHTSABER
+	damageType = ENERGY,
+
+	-- NONE, LIGHT, MEDIUM, HEAVY
+	armorPiercing = NONE,
+
+	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
+	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
+	-- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, combat_meleespecialize_onehandlightsaber
+	xpType = "combat_rangedspecialize_rifle",
+
+	certificationsRequired = { "cert_rifle_tusken" },
+
+	creatureAccuracyModifiers = { "rifle_accuracy" },
+
+	creatureAimModifiers = { "rifle_aim", "aim" },
+
+	defenderDefenseModifiers = { "ranged_defense" },
+
+	defenderSecondaryDefenseModifiers = { "block" },
+
+	speedModifiers = { "rifle_speed" },
+
+	damageModifiers = { },
+
+
+	-- The values below are the default values.  To be used for blue frog objects primarily
+	healthAttackCost = 25,
+	actionAttackCost = 35,
+	mindAttackCost = 75,
+	forceCost = 0,
+
+	pointBlankRange = 0,
+	pointBlankAccuracy = -60,
+
+	idealRange = 60,
+	idealAccuracy = 20,
+
+	maxRange = 64,
+	maxRangeAccuracy = -50,
+
+	minDamage = 50,
+	maxDamage = 100,
+
+	attackSpeed = 10,
+
+	woundsRatio = 3,
+
+	numberExperimentalProperties = {1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2},
+	experimentalProperties = {"XX", "XX", "CD", "OQ", "CD", "OQ", "CD", "OQ", "CD", "OQ", "CD", "OQ", "CD", "OQ", "XX", "XX", "XX", "CD", "OQ", "CD", "OQ", "CD", "OQ", "CD", "OQ"},
+	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "expEffeciency", "exp_durability", "null", "null", "null", "expRange", "expEffeciency", "expEffeciency", "expEffeciency"},
+	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "roundsused", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "attackhealthcost", "attackactioncost", "attackmindcost"},
+	experimentalMin = {0, 0, 50, 100, 10, 3, 30, 750, -60, -50, 60, 0, 25, 35, 75},
+	experimentalMax = {0, 0, 100, 150, 7, 8, 65, 1500, -60, -50, 60, 20, 15, 20, 45},
+	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_loot_quest_rifle_quest_tusken, "object/tangible/loot/quest/rifle_quest_tusken.iff")
