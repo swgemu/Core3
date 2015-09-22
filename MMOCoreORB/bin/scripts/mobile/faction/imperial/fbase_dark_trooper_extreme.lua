b786e48f39b8eff4435ec044506b297e97ebaf58
Nee <nee@swgemu.com>
2015-09-22 03:31:04 +0000

    [modified] spawns at Faction-bases to be more accurate emulation of original Pre-cu SWG 'Publish 14.1' era, Mantis 6157 (phase 2b)
    Change-Id: I3653211d73c48be29a2fa7855bffd56d3de5298b

--- MMOCoreORB/bin/scripts/mobile/faction/imperial/fbase_dark_trooper_extreme.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/imperial/fbase_dark_trooper_extreme.lua
@@ -1,6 +1,6 @@
 fbase_dark_trooper_extreme = Creature:new {
 	objectName = "@mob/creature_names:fbase_dark_trooper_extreme",
-	randomNameType = NAME_DARKTROOPER,
+	randomNameType = NAME_DARKTROOPER_TAG,
 	socialGroup = "imperial",
 	faction = "imperial",
 	level = 200,
