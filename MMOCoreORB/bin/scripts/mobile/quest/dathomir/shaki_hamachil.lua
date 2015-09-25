e04590d3aef5e06dad5730cd0602bb74e26763b8
Bill Negrelli <bnegrelli@gmail.com>
2015-09-25 14:10:12 +0000

    [Added] Shaki Hamachil quest line (Dathomir)
    Change-Id: I6dff52b7d9385eef9af039b390bc09a436368e5f

--- MMOCoreORB/bin/scripts/mobile/quest/dathomir/shaki_hamachil.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/dathomir/shaki_hamachil.lua
@@ -23,13 +23,13 @@ shaki_hamachil = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = 128,
+	optionsBitmask = 264,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_shaki_hamachil.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "shaki_hamachil_mission_giver_convotemplate",
 	attacks = {
 	}
 }
---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/dathomir/shaki_hamachil.lua
@@ -0,0 +1,69 @@
+shaki_hamachil_missions =
+{
+	{
+		missionType = "retrieve",
+			primarySpawns =
+			{
+				{ npcTemplate = "shaki_smuggler_q1", planetName = "dathomir", npcName = "smuggler" }
+			},
+			secondarySpawns = {},
+			itemSpawns =
+			{
+				{ itemTemplate = "object/tangible/mission/quest_item/shaki_hamachil_q1_needed.iff", itemName = "" }
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 100 },
+				{ rewardType = "faction",faction = "imperial", amount = 5 }
+			}
+	},
+	{
+		missionType = "deliver",
+			primarySpawns =
+			{
+				{ npcTemplate = "shaki_imperial_q2", planetName = "dathomir", npcName = "an Imperial Second Lieutenant" }
+			},
+			secondarySpawns = {},
+			itemSpawns =
+			{
+				{ itemTemplate = "object/tangible/mission/quest_item/shaki_hamachil_q2_needed.iff", itemName = "" }
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 150 },
+				{ rewardType = "faction",faction = "imperial",amount = 5 }
+
+			}
+		}
+
+}
+
+
+
+npcMapShakiHamachil =
+	{
+		{ spawnData = { planetName = "dathomir", npcTemplate = "shaki_hamachil", x = -101.049, z = 18, y = -1542.62, direction = 0, cellID = 0, position = STAND },
+			npcNumber = 1,
+			stfFile = "@static_npc/dathomir/dathomir_researchoutpost_shaki_hamachil",
+			missions = shaki_hamachil_missions
+		}
+	}
+
+ShakiHamachil = ThemeParkLogic:new {
+	numberOfActs = 1,
+	npcMap = npcMapShakiHamachil,
+	className = "ShakiHamachil",
+	screenPlayState = "shaki_hamachil_quest",
+	distance = 1200,
+	faction = FACTIONIMPERIAL
+}
+
+registerScreenPlay("ShakiHamachil", true)
+
+shaki_hamachil_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = ShakiHamachil
+}
+
+shaki_hamachil_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = ShakiHamachil
+}
