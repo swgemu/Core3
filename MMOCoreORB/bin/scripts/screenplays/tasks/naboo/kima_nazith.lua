aa4571d846e409c500e55117e867d746ef11a5a1
Bill Negrelli <bnegrelli@gmail.com>
2015-10-07 20:14:55 +0000

    Mantis #6613: Added Kima Nazith questline (Deja Peak, Naboo)
    Change-Id: I6270bf63c26e6a80bec69a8be5f4101d82fea312

---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/kima_nazith.lua
@@ -0,0 +1,87 @@
+kima_nazith_missions =
+	{
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "gungan_mercenary", planetName = "naboo", npcName = "Benbul Leedee" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "imperial_trooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "imperial_trooper", planetName = "naboo", npcName = "" },
+			},
+			itemSpawns = {},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 25 },
+			}
+		},
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "kima_trandoshan_slavemaster", planetName = "naboo", npcName = "Hrodius Goragg" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "imperial_trooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+			},
+			itemSpawns = {},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 50 },
+			}
+		},
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "lesser_prophet_of_the_dark_side", planetName = "naboo", npcName = "Kunn Kuroosh" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+				{ npcTemplate = "stormtrooper", planetName = "naboo", npcName = "" },
+			},
+			itemSpawns = {},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 75 },
+			}
+		},
+	}
+
+npcMapKimaNazith =
+	{
+		{
+			spawnData = { planetName = "naboo", npcTemplate = "kima_nazith", x = 4972, z = 360, y = -1582, direction = -178, cellID = 0, position = STAND },
+			
+			npcNumber = 1,
+			stfFile = "@static_npc/naboo/kima_nazith",
+			missions = kima_nazith_missions
+		}
+	}
+
+KimaNazith = ThemeParkLogic:new {
+	numberOfActs = 1,
+	npcMap = npcMapKimaNazith,
+	className = "KimaNazith",
+	screenPlayState = "kima_nazith_quest",
+	distance = 600,
+	faction = FACTIONREBEL,
+}
+
+registerScreenPlay("KimaNazith", true)
+
+kima_nazith_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = KimaNazith
+}
+kima_nazith_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = KimaNazith
+}
