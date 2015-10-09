28cbd58cf18a4b728550ee779546f650eb3e9eda
Bill Negrelli <bnegrelli@gmail.com>
2015-10-09 16:03:49 +0000

    Mantis #6613: Added Kima Nazith questline (Deja Peak, Naboo)
    Change-Id: I45cd0a439f2fe71dd7ff9436145c49bf4e631245

---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/kima_nazith.lua
@@ -0,0 +1,36 @@
+kima_nazith = Creature:new {
+	objectName = "",
+	customName = "Kima Nazith",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 100,
+	chanceHit = 1,
+	damageMin = 645,
+	damageMax = 1000,
+	baseXp = 9429,
+	baseHAM = 24000,
+	baseHAMmax = 30000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 264,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_commoner_naboo_human_female_03.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "kima_nazith_mission_giver_convotemplate",
+	attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(kima_nazith, "kima_nazith")
