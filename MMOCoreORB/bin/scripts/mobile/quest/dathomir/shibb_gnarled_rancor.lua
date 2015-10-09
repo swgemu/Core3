6b3e8a43b63fc31bf251cfa7ac56b9a3ed01e777
Bill Negrelli <bnegrelli@gmail.com>
2015-10-09 15:23:45 +0000

    [fixed] Shibb Nisshil questline Mantis #6645
    Change-Id: I96cedabe1d07ee89c64cdc5e3e646590d0d18071

---
+++ MMOCoreORB/bin/scripts/mobile/quest/dathomir/shibb_gnarled_rancor.lua
@@ -0,0 +1,42 @@
+shibb_gnarled_rancor = Creature:new {
+	objectName = "@mob/creature_names:thug",
+	randomNameType = NAME_GENERIC_TAG,
+	socialGroup = "brigand",
+	faction = "",
+	level = 17,
+	chanceHit = 0.32,
+	damageMin = 180,
+	damageMax = 190,
+	baseXp = 1257,
+	baseHAM = 3500,
+	baseHAMmax = 4300,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER + STALKER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+
+	templates = {"object/mobile/rancor_hue.iff"},
+	controlDeviceTemplate = "object/intangible/pet/rancor_hue.iff",
+	scale = 1.1,
+	lootGroups = {},
+	weapons = {},
+	attacks = {
+		{"stunattack",""},
+		{"intimidationattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(shibb_gnarled_rancor, "shibb_gnarled_rancor")
+
