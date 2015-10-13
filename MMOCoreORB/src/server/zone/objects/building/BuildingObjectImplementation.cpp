f88073c1547423ab040110734c55e5794120ade5
Dustin Niehoff <dannuic@gmail.com>
2015-10-13 05:09:33 -0600

    [wip] AI restructuring
    status: Testable version, feature complete
    	full Creature tree implemented
    	pet trees implemented
    	all old trees implemented
    	completely removed old template loading
    features: Added blackboard variant type to store arbitrary data
    	  Modular trees implemented
    	  Running chain (state saving) implemented
    
    Change-Id: Idce01797d20aa649fb17e8b6078b0b2fde6a1643

--- MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
@@ -592,12 +592,12 @@ void BuildingObjectImplementation::destroyObjectFromDatabase(
 
 		if (child == NULL)
 			continue;
-          
+
           	Locker locker(child);
 
 		if (child->isAiAgent()) {
 			AiAgent* ai = child->asAiAgent();
-                  
+
 			ai->setRespawnTimer(0);
 		}
 
@@ -1651,7 +1651,7 @@ Vector<Reference<MeshData*> > BuildingObjectImplementation::getTransformedMeshDa
 				const CellPortal* portal = tmpl->getPortal(i);
 				const MeshData* mesh = pl->getPortalGeometry(portal->getGeometryIndex());
 
-				data.emplace(std::move(MeshData::makeCopyNegateZ(mesh, fullTransform)));
+				data.emplace(MeshData::makeCopyNegateZ(mesh, fullTransform));
 			}
 
 #ifdef RENDER_EXTERNAL_FLOOR_MESHES_ONLY
