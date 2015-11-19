248ebaf1fcd277fa8f8368bc772bc8c48308281a
Bill Negrelli <bnegrelli@gmail.com>
2015-11-19 17:32:34 -0500

    Mantis #6613: Added Kima Nazith questline (Deja Peak, Naboo)
    Change-Id: I610823a047b2612da8f5d673dfbcb1e0c43eb387

---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/benbul_leedee.lua
@@ -0,0 +1,45 @@
+benbul_leedee = Creature:new {
+	objectName = "",
+	customName = "Benbul Leedee",
+	socialGroup = "gungan",
+	faction = "gungan",
+	level = 20,
+	chanceHit = 0.31,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 831,
+	baseHAM = 2400,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,15,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/gungan_male.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4000000},
+				{group = "gungan_common", chance = 2000000},
+				{group = "tailor_components", chance = 2000000},
+				{group = "loot_kit_parts", chance = 2000000}
+			}
+		}
+	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "",
+	attacks = merge(brawlermid,marksmanmid)
+}
+
+CreatureTemplates:addCreatureTemplate(benbul_leedee, "benbul_leedee")
---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/kunn_kuroosh.lua
@@ -0,0 +1,53 @@
+kunn_kuroosh = Creature:new {
+	objectName = "",
+	customName = "Kunn Kuroosh",
+	socialGroup = "imperial",
+	faction = "imperial",
+	level = 25,
+	chanceHit = 0.36,
+	damageMin = 240,
+	damageMax = 250,
+	baseXp = 2637,
+	baseHAM = 7200,
+	baseHAMmax = 8800,
+	armor = 0,
+	resists = {20,20,10,25,35,10,10,10,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {	
+			"object/mobile/dressed_dark_jedi_human_male_01.iff" 
+		},
+		lootGroups = {
+		{
+			groups = {
+				{group = "color_crystals", chance = 300000},
+				{group = "junk", chance = 6300000},
+				{group = "rifles", chance = 600000},
+				{group = "holocron_dark", chance = 150000},
+				{group = "holocron_light", chance = 150000},
+				{group = "carbines", chance = 600000},
+				{group = "pistols", chance = 600000},
+				{group = "clothing_attachments", chance = 150000},
+				{group = "armor_attachments", chance = 150000},
+				{group = "wearables_common", chance = 1000000}
+			}
+		}
+	},
+	weapons = {"dark_jedi_weapons_gen2"},
+	conversationTemplate = "",
+	attacks = merge(lightsabermaster,forcewielder)
+}
+
+CreatureTemplates:addCreatureTemplate(kunn_kuroosh, "kunn_kuroosh")
