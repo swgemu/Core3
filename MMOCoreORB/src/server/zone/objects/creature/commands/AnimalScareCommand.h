b9bf3460a1392cae2246a528a9f99e8bb7671e78
Nee <nee@swgemu.com>
2015-11-26 09:43:22 +0000

    [WIP] [fixed] Jedi Powers 'animal scare' command (Mantis #5248)
    Change-Id: I9ba810d92c5a6224719f1f0a795ff20ccffb505b

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
@@ -6,12 +6,14 @@
 #define ANIMALSCARECOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/AiAgent.h"
 
-class AnimalScareCommand : public QueueCommand {
+class AnimalScareCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalScareCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -22,6 +24,50 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		if (targetObject == NULL || !targetObject->isCreatureObject()) {
+				return INVALIDTARGET;
+		}
+
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+
+		if (object == NULL || object->isPlayerCreature())
+				return INVALIDTARGET;
+
+		CreatureObject* targetCreature = cast<CreatureObject*>( object.get());
+
+		if (targetCreature == NULL || targetCreature->isDead() || targetCreature->isNonPlayerCreatureObject() || targetCreature->isDroidSpecies() || targetCreature->isWalkerSpecies() || !targetCreature->isAttackableBy(creature))
+				return INVALIDTARGET;
+
+		int res = doCombatAction(creature, target);
+
+		if (res == SUCCESS) {
+
+			// Commence fleeing action...
+
+			ManagedReference<CreatureObject*> creatureTarget = targetObject.castTo<CreatureObject*>();
+
+			if (creatureTarget != NULL) {
+				Locker clocker(creatureTarget, creature);
+
+				ManagedReference<AiAgent*> target = cast<AiMoveEvent*>(creature);
+
+				Locker locker(target);
+
+				CombatManager::instance()->attemptPeace(target);
+
+				target->setOblivious() && target->clearCombatState() && target->setMovementCounter(100);
+
+				CombatManager::instance()->broadcastCombatSpam(creature, creatureTarget, NULL, 0, "cbt_spam", combatSpam + "_hit", 1);
+			}
+
+		}
 
 		return SUCCESS;
 	}
