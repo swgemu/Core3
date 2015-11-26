188d2af4d1dfa3d773238e433cda678a727e98f0
Nee <nee@swgemu.com>
2015-11-26 17:31:47 +0000

    [WIP] [fixed] Jedi Powers 'animal scare' command (Mantis #5248)
    Change-Id: I9ba810d92c5a6224719f1f0a795ff20ccffb505b

--- MMOCoreORB/bin/scripts/commands/animalScare.lua
+++ MMOCoreORB/bin/scripts/commands/animalScare.lua
@@ -43,6 +43,21 @@
 
 AnimalScareCommand = {
         name = "animalscare",
+
+	damageMultiplier = 0,
+	speed = 4.0,
+	forceCost = 200,
+	accuracySkillMod = "animalscare_accuracy",
+	attackType = FORCEATTACK,
+
+	animationCRC = hashCode("mind_trick_1"),
+
+	combatSpam = "animalscare",
+
+	poolsToDamage = NO_ATTRIBUTE,
+
+	range = 32
+
 }
 
 AddCommand(AnimalScareCommand)
