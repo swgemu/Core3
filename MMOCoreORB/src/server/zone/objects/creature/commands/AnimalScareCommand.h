ec9e8ac34aec946bda12fb695f0e2be2ee02807a
Nee <nee@swgemu.com>
2015-11-26 19:24:43 +0000

    [WIP] [fixed] Jedi Powers 'animal scare' command (Mantis #5248)
    Change-Id: I9ba810d92c5a6224719f1f0a795ff20ccffb505b

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
@@ -6,12 +6,15 @@
 #define ANIMALSCARECOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/AiAgent.h"
+#include "server/zone/objects/creature/ai/events/AiMoveEvent.h"
 
-class AnimalScareCommand : public QueueCommand {
+class AnimalScareCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalScareCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -22,6 +25,39 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		if (targetObject == NULL || !targetObject->isCreatureObject() || targetObject->isPlayerCreature() || targetObject->isDroidObject() || !targetObject->isCreature()) {
+			return INVALIDTARGET;
+		}
+
+		int res = doCombatAction(creature, target);
+
+		if (res == SUCCESS) {
+
+			// Commence fleeing action...
+
+			ManagedReference<CreatureObject*> creatureTarget = targetObject.castTo<CreatureObject*>();
+
+			if (creatureTarget != NULL) {
+				Locker clocker(creatureTarget, creature);
+
+				AiAgent* aiAgent = cast<AiAgent*>(creatureTarget.get());
+
+				CombatManager::instance()->attemptPeace(creatureTarget);
+
+				AiMoveEvent* aiEvent = new AiMoveEvent(aiAgent, false, true);
+				aiEvent->schedule();
+
+				CombatManager::instance()->broadcastCombatSpam(creature, creatureTarget, NULL, 0, "cbt_spam", combatSpam + "_hit", 1);
+			}
+
+		}
 
 		return SUCCESS;
 	}
