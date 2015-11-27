7cf04bf2b274ac6c302aeb9e3392642673dd9738
Nee <nee@swgemu.com>
2015-11-27 09:57:52 +0000

    [WIP] [fixed] Jedi Powers 'animal scare' command (Mantis #5248)
    Change-Id: I9ba810d92c5a6224719f1f0a795ff20ccffb505b

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalScareCommand.h
@@ -1,17 +1,22 @@
 /*
+
 				Copyright <SWGEmu>
 		See file COPYING for copying conditions.*/
 
 #ifndef ANIMALSCARECOMMAND_H_
 #define ANIMALSCARECOMMAND_H_
 
-#include "server/zone/objects/scene/SceneObject.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/Creature.h"
+#include "server/zone/managers/combat/CombatManager.h"
+#include "server/zone/objects/creature/ai/AiAgent.h"
+#include "server/zone/objects/creature/ai/events/AiMoveEvent.h"
 
-class AnimalScareCommand : public QueueCommand {
+class AnimalScareCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalScareCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -22,6 +27,45 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		if (targetObject == NULL || !targetObject->isCreature()) {
+			creature->sendSystemMessage("@jedi_spam:animal_scare_fail"); //"You fail to scare your target."
+			return INVALIDTARGET;
+		}
+
+		int res = doCombatAction(creature, target);
+
+		if (res == SUCCESS) {
+
+		// Commence fleeing action...
+
+			ManagedReference<Creature*> creatureTarget = targetObject.castTo<Creature*>();
+
+			Locker clocker(creatureTarget, creature);
+
+			AiAgent* aiAgent = cast<AiAgent*>(creatureTarget.get());
+
+			AiMoveEvent* aiEvent = new AiMoveEvent(aiAgent);
+			aiEvent->schedule(AiAgent::FLEEING);
+
+			creatureTarget->dropFromDefenderLists(creature);
+			creatureTarget->clearQueueActions();
+			creatureTarget->setState(CreatureState::COMBATATTITUDEEVASIVE);
+			creatureTarget->isFleeing();
+			creature->doCombatAnimation(creatureTarget, STRING_HASHCODE("mind_trick_1"), 1, 0);
+			creature->sendSystemMessage("@jedi_spam:animal_scare_success"); //"You successfully scare your target."
+
+			CombatManager::instance()->broadcastCombatSpam(creature, creatureTarget, NULL, 0, "cbt_spam", combatSpam + "_hit", 1);
+
+			return res;
+
+			}
 
 		return SUCCESS;
 	}
