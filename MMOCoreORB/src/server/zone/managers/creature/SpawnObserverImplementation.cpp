cef450fac81b442058c0f5fb83119323ed614603
Victor Popovici <theanswer@swgemu.com>
2015-12-02 11:11:12 +0100

    [fixed] stability issue
    Change-Id: Ia687c949353ac23c0dee445eabfd1968e502e734

--- MMOCoreORB/src/server/zone/managers/creature/SpawnObserverImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/SpawnObserverImplementation.cpp
@@ -3,16 +3,26 @@
 #include "server/zone/objects/creature/ai/AiAgent.h"
 
 void SpawnObserverImplementation::despawnSpawns() {
+	Vector<ManagedReference<AiAgent* > > agents;
+
 	for (int i = 0; i < spawnedCreatures.size(); ++i) {
 		ManagedReference<CreatureObject*> obj = spawnedCreatures.get(i);
 
 		if (obj != NULL && obj->isAiAgent()) {
 			AiAgent* aiObj = cast<AiAgent*>(obj.get());
-
-			Locker locker(aiObj);
-			aiObj->setDespawnOnNoPlayerInRange(true);
+			agents.add(aiObj);
 		}
 	}
 
 	spawnedCreatures.removeAll();
+
+	EXECUTE_TASK_1(agents, {
+			for (int i = 0; i < agents_p.size(); ++i) {
+				AiAgent* agent = agents_p.get(i);
+
+				Locker locker(agent);
+
+				agent->setDespawnOnNoPlayerInRange(true);
+			}
+	});
 }
