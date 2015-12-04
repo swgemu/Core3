7ae062545953e77dc0616e95a4a7fae95b73b3b4
Nee <nee@swgemu.com>
2015-12-04 11:17:20 +0000

    [WIP] [fixed] the Jedi Powers 'Animal Attack' command should now function (Mantis #5246)
    Change-Id: I15d73aad36f03d7aac97523a754dd58ca18ec792

--- MMOCoreORB/bin/scripts/commands/animalAttack.lua
+++ MMOCoreORB/bin/scripts/commands/animalAttack.lua
@@ -43,6 +43,21 @@
 
 AnimalAttackCommand = {
         name = "animalattack",
+
+	damageMultiplier = 0,
+	speed = 4.0,
+	forceCost = 150,
+	accuracySkillMod = "animalattack_accuracy",
+	attackType = FORCEATTACK,
+
+	animationCRC = hashCode("mind_trick_1"),
+
+	combatSpam = "animalattack",
+
+	poolsToDamage = NO_ATTRIBUTE,
+
+	range = 32
+
 }
 
 AddCommand(AnimalAttackCommand)
--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
@@ -6,12 +6,20 @@
 #define ANIMALATTACKCOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/tangible/TangibleObject.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/AiAgent.h"
+#include "server/zone/objects/creature/ai/events/AiMoveEvent.h"
+#include "server/zone/objects/creature/ai/events/AiLoadTask.h"
+#include "server/zone/objects/scene/ObserverEventType.h"
+#include "server/zone/managers/combat/CombatManager.h"
+#include "server/zone/objects/creature/commands/QueueCommand.h"
 
-class AnimalAttackCommand : public QueueCommand {
+class AnimalAttackCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalAttackCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -22,6 +30,67 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		if (targetObject == NULL || !targetObject->isCreature()) {
+			return INVALIDTARGET;
+		}
+
+		CreatureObject* targetCreature = cast<CreatureObject*>(targetObject.get());
+
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
+
+		if (!targetCreature->isAttackableBy(creature))
+			return INVALIDTARGET;
+
+		int res = doCombatAction(creature, target);
+
+		CombatManager* combatManager = CombatManager::instance();
+
+		if (res == SUCCESS) {
+
+			// Commence controlling action...
+
+			ManagedReference<AiAgent*> creatureTarget = targetObject.castTo<AiAgent*>();
+
+			if (creatureTarget != NULL) {
+				Locker clocker(creatureTarget, creature);
+
+				AiAgent* aiAgent = cast<AiAgent*>(creatureTarget.get());
+
+				AiMoveEvent* aiEvent = new AiMoveEvent(aiAgent);
+				aiEvent->schedule();
+
+				CombatManager::instance()->forcePeace(creatureTarget);
+
+				AiLoadTask* aiTask = new AiLoadTask(aiAgent);
+				aiTask->schedule();
+
+				creature->getMainDefender();
+				creatureTarget->setDefender(creature->getMainDefender());
+
+				Reference<CreatureObject*> defender = server->getZoneServer()->getObject(target, true).castTo<CreatureObject*>();
+				if (defender == NULL || !defender->isAttackableBy(creatureTarget) ) {
+					return GENERALERROR;
+				} else {
+					creatureTarget->enqueueAttack(QueueCommand::FRONT);
+				}
+
+				creature->sendSystemMessage("@jedi_spam:animal_attack_success"); //"You successfully enrage your target."
+
+			}
+		}
+
+		else {
+			creature->sendSystemMessage("@jedi_spam:animal_attack_fail"); //"You fail to enrage your target."
+
+		}
 
 		return SUCCESS;
 	}
