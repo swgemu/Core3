24dec7fac05a61035eb0b9fa3bd00c8e7a36a6a0
Nee <nee@swgemu.com>
2015-12-04 23:40:32 +0000

    [WIP] [fixed] the Jedi Powers 'Animal Attack' command should now function (Mantis #5246)
    Change-Id: I15d73aad36f03d7aac97523a754dd58ca18ec792

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalAttackCommand.h
@@ -5,13 +5,17 @@
 #ifndef ANIMALATTACKCOMMAND_H_
 #define ANIMALATTACKCOMMAND_H_
 
-#include "server/zone/objects/scene/SceneObject.h"
+//#include "server/zone/objects/scene/SceneObject.h"
+//#include "server/zone/objects/tangible/TangibleObject.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/Creature.h"
+#include "server/zone/managers/combat/CombatManager.h"
 
-class AnimalAttackCommand : public QueueCommand {
+class AnimalAttackCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalAttackCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -22,6 +26,41 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		Creature* targetCreature = cast<Creature*>(targetObject.get());
+
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
+
+		// Commence controlling action...
+
+		TangibleObject* defender = cast<TangibleObject*>(creature->getMainDefender());
+
+		if (defender == NULL || defender == targetCreature) {
+			creature->sendSystemMessage("@jedi_spam:animal_attack_fail"); //"You fail to enrage your target."
+			return GENERALERROR;
+		}
+
+		{
+			Locker clocker (defender, creature);
+			if (!defender->isAttackableBy(targetCreature)) {
+				creature->sendSystemMessage("@jedi_spam:animal_attack_fail"); //"You fail to enrage your target."
+				return GENERALERROR;
+			}
+		}
+
+		Locker clocker(targetCreature, creature);
+
+		CombatManager::instance()->forcePeace(targetCreature);
+		targetCreature->setDefender(defender);
+
+		creature->sendSystemMessage("@jedi_spam:animal_attack_success"); //"You successfully enrage your target."
 
 		return SUCCESS;
 	}
