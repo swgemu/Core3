d32b339ddba5002d28c409205aff20da76d7e99a
Nee <nee@swgemu.com>
2015-12-12 03:05:52 +0000

    [adjusted] various NPC mob 'loot tables' to be more accurate emulation, Mantis #6706 (phase 3, Dathomir)
    Change-Id: I7ef9f700d4c5cf3329a7d4925b26b1023da9159e

--- MMOCoreORB/bin/scripts/mobile/dathomir/singing_mountain_clan_outcast.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/singing_mountain_clan_outcast.lua
@@ -39,8 +39,8 @@ singing_mountain_clan_outcast = Creature:new {
 				{group = "rifles", chance = 1000000},
 				{group = "pistols", chance = 1000000},
 				{group = "carbines", chance = 1000000},
-				{group = "wearables_common", chance = 500000},
-				{group = "wearables_uncommon", chance = 500000}
+				{group = "wearables_uncommon", chance = 500000},
+				{group = "tailor_components", chance = 500000}
 			}
 		}
 	},
