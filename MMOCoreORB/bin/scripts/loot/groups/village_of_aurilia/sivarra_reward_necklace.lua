b207008806bab86380d00220fcb0cd40d5bf7d2d
Nee <nee@swgemu.com>
2016-01-01 20:25:32 +0000

    [fixed] Village reward 'ice pendant' to generate properly (Mantis #6668)
    Change-Id: I05477f7ab825fab6a9c84ba8bdbd0e9cf3c29a35

---
+++ MMOCoreORB/bin/scripts/loot/groups/village_of_aurilia/sivarra_reward_necklace.lua
@@ -0,0 +1,10 @@
+sivarra_reward_necklace = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "village_medic_puzzle_ice_pendant", weight = 10000000},
+	}
+}
+
+addLootGroupTemplate("sivarra_reward_necklace", sivarra_reward_necklace)
