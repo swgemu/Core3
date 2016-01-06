497f0bfdc1eec1d9f3801ac89badfb6f9e456b89
Nee <nee@swgemu.com>
2016-01-06 09:31:27 +0000

    [WIP] [fixed] Jedi skill 'Channel Force' should no longer cause permanant negative modifiers if player uses it in continuous succession (Mantis #6780)
    Change-Id: Ica97e8c130cddc48e3f6e2383a9b910424d660a9

--- MMOCoreORB/src/server/zone/objects/player/events/ChannelForceRegenTask.h
+++ MMOCoreORB/src/server/zone/objects/player/events/ChannelForceRegenTask.h
@@ -28,12 +28,14 @@ public:
 
 		if(creature != NULL) {
 			const static int amountOfTicks = 10; // How many ticks will it take to get back to normal HAM.
-			int hamBack = round((float) forceBonus / (float) amountOfTicks);
+			int hamDiff = round((float) forceBonus + (float) amountOfTicks);
+			int hamBack = (counter == 9) ? ((int)forceBonus % amountOfTicks) : ((int)forceBonus / amountOfTicks);
+
+			int maxHealth = creature->getMaxHAM(CreatureAttribute::HEALTH);
+			int maxAction = creature->getMaxHAM(CreatureAttribute::ACTION);
+			int maxMind = creature->getMaxHAM(CreatureAttribute::MIND);
 
 			if (counter < amountOfTicks) {
-				int maxHealth = creature->getMaxHAM(CreatureAttribute::HEALTH);
-				int maxAction = creature->getMaxHAM(CreatureAttribute::ACTION);
-				int maxMind = creature->getMaxHAM(CreatureAttribute::MIND);
 
 				creature->setMaxHAM(CreatureAttribute::HEALTH, maxHealth + hamBack, true);
 				creature->setMaxHAM(CreatureAttribute::ACTION, maxAction + hamBack, true);
@@ -43,8 +45,16 @@ public:
 
 				this->reschedule(6000); // Reschedule in 6 seconds...
 			}
-			else {
+			else if (counter >= 9) {
+				int currentHealth = creature->getMaxHAM(CreatureAttribute::HEALTH);
+				int currentAction = creature->getMaxHAM(CreatureAttribute::ACTION);
+				int currentMind = creature->getMaxHAM(CreatureAttribute::MIND);
 
+				creature->setMaxHAM(CreatureAttribute::HEALTH, currentHealth + hamDiff, true);
+				creature->setMaxHAM(CreatureAttribute::ACTION, currentAction + hamDiff, true);
+				creature->setMaxHAM(CreatureAttribute::MIND, currentMind + hamDiff, true);
+			}
+			else {
 				creature->removePendingTask("channelForceRegenTask");
 			}
 		}
