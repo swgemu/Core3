f20d0213f3a617a61462281d30e144e13f77d2a3
Miztahh <miztah@swgemu.com>
2016-01-08 04:09:17 +0000

    [fixed] Menus on all GCW base terminals and added proper checks before allowing terminal use [added] Proper string responses to errors and status changes to terminals [fixed] SUI text on Security Terminal to use the proper GCW base text and added proper repair procedure [changed] Override Terminal to more accurately match the bio engineer minigame from live [changed] Power Regulator terminal to use a consistent set of switch rules that change during each vulnerability [fixed] Bug causing GCW bases to be un-enterable after a server restart even though the base's turrets are destroyed [fixed] Cleaned up GCWManager a bit more
    Change-Id: I13ab954cad7bad9e931679ee4e512a1c90854915

--- MMOCoreORB/src/server/zone/objects/tangible/terminal/components/SecurityTerminalMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/terminal/components/SecurityTerminalMenuComponent.cpp
@@ -26,11 +26,6 @@ void SecurityTerminalMenuComponent::fillObjectMenuResponse(SceneObject* sceneObj
 	if (building == NULL || player->isDead() || player->isIncapacitated())
 			return;
 
-	if (player->getFaction() == 0) {
-		player->sendSystemMessage("@faction_recruiter:must_be_declared_use"); // Your faction affiliation must be delcared in order to use that item.
-		return;
-	}
-
 	Zone* zone = building->getZone();
 
 	if (zone == NULL)
@@ -41,21 +36,21 @@ void SecurityTerminalMenuComponent::fillObjectMenuResponse(SceneObject* sceneObj
 	if (gcwMan == NULL)
 		return;
 
-	if (!gcwMan->canUseTerminals(player, building, sceneObject))
+	if (!gcwMan->isBaseVulnerable(building))
 		return;
 
-	if (gcwMan->isUplinkJammed(building) && !gcwMan->isSecurityTermSliced(building) && player->getFaction() != building->getFaction()) {
-		if (gcwMan->isTerminalDamaged(cast<TangibleObject*>(sceneObject)))
-			menuResponse->addRadialMenuItem(229, 3, "@ui:repair");
-	}
-
+	if (gcwMan->isTerminalDamaged(cast<TangibleObject*>(sceneObject)))
+		menuResponse->addRadialMenuItem(20, 3, "@ui:repair");
+	else
+		menuResponse->addRadialMenuItem(20, 3, "@slicing/slicing:slice");
 }
 
 int SecurityTerminalMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) {
-	if (sceneObject == NULL || !sceneObject->isTangibleObject() || player == NULL || player->isDead() || player->isIncapacitated())
+	if (sceneObject == NULL || !sceneObject->isTangibleObject() || player == NULL || player->isDead() || player->isIncapacitated() || selectedID != 20)
 		return 1;
 
 	ManagedReference<BuildingObject*> building = cast<BuildingObject*>(sceneObject->getParentRecursively(SceneObjectType::FACTIONBUILDING).get().get());
+	ManagedReference<TangibleObject*> securityTerminal = cast<TangibleObject*>(sceneObject);
 
 	if (building == NULL)
 		return 1;
@@ -70,18 +65,45 @@ int SecurityTerminalMenuComponent::handleObjectMenuSelect(SceneObject* sceneObje
 	if (gcwMan == NULL)
 		return 1;
 
-	if (!gcwMan->canUseTerminals(player, building, sceneObject))
+	if (!gcwMan->isBaseVulnerable(building))
 		return 1;
 
-	ManagedReference<TangibleObject*> tano = cast<TangibleObject*>(sceneObject);
+	// Most of the string rows for these errors did not exist in 14.1, pulled string text from a different patch
+	if (!gcwMan->areOpposingFactions(player->getFaction(), building->getFaction())) {
+		player->sendSystemMessage("@faction/faction_hq/faction_hq_response:no_tamper"); // You are not an enemy of this structure. Why would you want to tamper?
+		return 1;
+	} else if (gcwMan->isSecurityTermSliced(building)) {
+		player->sendSystemMessage("The security terminal has already been sliced!");
+		return 1;
+	} else if (!gcwMan->isUplinkJammed(building))	{
+		player->sendSystemMessage("@faction/faction_hq/faction_hq_response:other_objectives"); // Other objectives must be disabled prior to gaining access to this one.
+		return 1;
+	} else if (player->isInCombat()) {
+		player->sendSystemMessage("You cannot slice the terminal while you are in combat!");
+		return 1;
+	} else if (securityTerminal->getParentID() != player->getParentID()) {
+		player->sendSystemMessage("You cannot slice the terminal if you are not even in the same room!");
+		return 1;
+	} else if (securityTerminal->getDistanceTo(player) > 15) {
+		player->sendSystemMessage("You are too far away from the terminal to continue slicing!");
+		return 1;
+	} else if (!player->hasSkill("combat_smuggler_slicing_01")) {
+		player->sendSystemMessage("Only a smuggler with terminal slicing knowledge could expect to disable this security terminal!");
+		return 1;
+	}
 
-	if (player->getFaction() != building->getFaction()) {
-		if (selectedID == 229) {
-			if (player->hasSkill("combat_smuggler_slicing_01"))
-				gcwMan->repairTerminal(player, tano);
-			else
-				player->sendSystemMessage("Only an experienced Smuggler with Slicing experience could expect to repair the Security Terminal");
+	if (gcwMan->isTerminalDamaged(securityTerminal)) {
+		player->sendSystemMessage("You begin repairing the damage done by a prior slicing attempt...");
+		gcwMan->repairTerminal(player, securityTerminal);
+	} else {
+		if (player->containsActiveSession(SessionFacadeType::SLICING)) {
+			player->sendSystemMessage("@slicing/slicing:already_slicing");
+			return 1;
 		}
+
+		ManagedReference<SlicingSession*> session = new SlicingSession(player);
+		session->setBaseSlice(true);
+		session->initalizeSlicingMenu(player, securityTerminal);
 	}
 
 	return 0;
