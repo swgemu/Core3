19605094a6fa1c5ad50057feea1d88852d6e6a63
Greg SLomin <gregslomin@gmail.com>
2016-01-09 15:41:39 +0000

    [refactor] Refactoring jedi buff commands to use JediQueueCommand instead of QueueCommand in preparation for Visibility Changes Added overridableCRCs and blockingCRCs to JediQueueCommand to facilitate buff reapplication for different ranks Reverted accidental changes to forceRun1/2/3.lua Refactored remaining self buffs
    Change-Id: I93912353d367c3ea68a97fc4e2032464ee0c7de5

--- MMOCoreORB/bin/scripts/commands/forceSpeed2.lua
+++ MMOCoreORB/bin/scripts/commands/forceSpeed2.lua
@@ -43,6 +43,10 @@
 
 ForceSpeed2Command = {
         name = "forcespeed2",
+        forceCost = 300,
+        duration = 360,
+        --animationCRC = hashCode()
+        clientEffect = "clienteffect/pl_force_speed_self.cef"
 }
 
 AddCommand(ForceSpeed2Command)
