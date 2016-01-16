f9b674d998894a84cc206c7227f6121d57a43e6c
Greg Slomin <gregslomin@gmail.com>
2016-01-16 04:27:55 -0600

    [added] Basic Assist command implementation
    Change-Id: Ib42cbff63e60fc888bfbcaca30927e411fb8e733

--- MMOCoreORB/src/server/zone/objects/creature/commands/AssistCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AssistCommand.h
@@ -6,6 +6,7 @@
 #define ASSISTCOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/managers/objectcontroller/ObjectController.h"
 
 class AssistCommand : public QueueCommand {
 public:
@@ -22,6 +23,20 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+          
+		if (targetObject == NULL || !targetObject->isTangibleObject() || targetObject == creature)
+			return INVALIDTARGET;
+
+		CreatureObject *targetCreo = targetObject->asCreatureObject();
+
+		if(targetCreo == NULL)
+			return INVALIDTARGET;
+
+		creature->setTargetID(targetCreo->getTargetID(), false); // This should allow people to use heals and buffs on an assisted target
+
+		creature->enqueueCommand(STRING_HASHCODE("attack"), 1, creature->getTargetID(), ""); // Should we limit the amount of times this can be enqueued?
 
 		return SUCCESS;
 	}
