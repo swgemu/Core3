f13ab0e1cc6faae0311f0d3d678f959dd93a60ac
Nee <nee@swgemu.com>
2016-01-18 09:10:31 +0000

    [fixed] missing stats & effects on Chef food 'dustcrepe' (Mantis #6832)
    Change-Id: I92faf93e939329ad335ca8b1b141eb217d53ed4c

--- MMOCoreORB/src/server/zone/objects/tangible/consumable/ConsumableImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/consumable/ConsumableImplementation.cpp
@@ -265,9 +265,17 @@ int ConsumableImplementation::handleObjectMenuSelect(CreatureObject* player, byt
 			//Tilla till reduces food stomach filling by a percentage
 			int currentfilling = ghost->getFoodFilling();
 			ghost->setFoodFilling(round(currentfilling * (100 - nutrition) / 100.0f), true);
+		} else if (effect == "slow_dot" && player->getDamageOverTimeList() != NULL) {
+			player->getDamageOverTimeList()->multiplyAllDOTDurations ((100 - nutrition) / 100.f);
+			player->sendSystemMessage("@combat_effects:slow_dot_done"); // The remaining duration of DOTs affecting you have been reduced by %DI%.
 		}
+
+		break;
 	}
-	}
+
+	default:
+		break;
+        }
 
 	if (buff != NULL) {
 		Locker locker(buff);
@@ -286,7 +294,6 @@ int ConsumableImplementation::handleObjectMenuSelect(CreatureObject* player, byt
 	player->sendSystemMessage(stringId);//player->sendSystemMessage("base_player", "prose_consume_item", objectID);
 
 	// Play the client effect sound depending on species/gender.
-
 	// Get the species.
 	int species = player->getSpecies();
 
@@ -314,7 +321,6 @@ int ConsumableImplementation::handleObjectMenuSelect(CreatureObject* player, byt
 		break;
 
 	}
-
 	//Consume a charge from the item, destroy it if it reaches 0 charges remaining.
 	//useCharge(player);
 	decreaseUseCount();
