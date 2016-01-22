c33b14f40ff865478fe174f87a40015e1f0ee75d
Bledsoe <bnegrelli@gmail.com>
2016-01-21 21:19:02 -0500

    [Added] Melios Purl questline (Bestine MedCenter)
    Change-Id: I5b7458a2e002f7d7e017b1a16857409da663cdae

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/melios_purl_outfit.lua
@@ -0,0 +1,13 @@
+melios_purl_outfit = {
+	{ 
+		{objectTemplate = "object/tangible/wearables/shirt/shirt_s07.iff", customizationVariables = {{"/private/index_color_1", 3}} },
+		{objectTemplate = "object/tangible/wearables/pants/pants_s12.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/shoes/shoes_s03.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/wearables/vest/vest_s05.iff", customizationVariables = {{"/private/index_color_1", 3}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_utility_belt.iff", customizationVariables = {} },
+		{objectTemplate = "object/tangible/hair/human/hair_human_male_s26.iff", customizationVariables = {{"/private/index_color_1", 11}} },
+	}
+	 
+}
+
+addOutfitGroup("melios_purl_outfit", melios_purl_outfit)
---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/melios_purl.lua
@@ -0,0 +1,39 @@
+melios_purl = Creature:new {
+	objectName = "@theme_park_name:melious_purl",
+	customName = "Melios Purl",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 8,
+	chanceHit = 0.28,
+	damageMin = 90,
+	damageMax = 110,
+	baseXp = 356,
+	baseHAM = 810,
+	baseHAMmax = 990,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 236,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/tatooine_npc/melios_purl.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "melios_purl_mission_giver_convotemplate",
+	outfit = "melios_purl_outfit",
+	scale = .85,
+	attacks = {
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(melios_purl, "melios_purl")
