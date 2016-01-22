f99c1bad37e4f1f5a98c4de0ebbc4dce780d80e1
Bledsoe <bnegrelli@gmail.com>
2016-01-21 21:19:02 -0500

    [Added] Melios Purl questline (Bestine MedCenter)
    Change-Id: I5b7458a2e002f7d7e017b1a16857409da663cdae

---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/melios_womp_rat.lua
@@ -0,0 +1,46 @@
+melios_womp_rat = Creature:new {
+	objectName = "@mob/creature_names:mutant_womprat",
+	socialGroup = "rat",
+	faction = "",
+	level = 13,
+	chanceHit = 0.29,
+	damageMin = 130,
+	damageMax = 140,
+	baseXp = 609,
+	baseHAM = 1000,
+	baseHAMmax = 1200,
+	armor = 0,
+	resists = {110,115,0,0,0,0,115,-1,-1},
+	meatType = "meat_wild",
+	meatAmount = 6,
+	hideType = "hide_leathery",
+	hideAmount = 5,
+	boneType = "bone_mammal",
+	boneAmount = 4,
+	milk = 0,
+	tamingChance = 0.25,
+	ferocity = 3,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + STALKER,
+	optionsBitmask = 128,
+	diet = CARNIVORE,
+
+	templates = {"object/mobile/womp_rat.iff"},
+	controlDeviceTemplate = "object/intangible/pet/womp_rat_hue.iff",
+	scale = 2.15,
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	lootGroups = {
+		{
+			groups = {
+				{ group = "task_loot_melios_purl_q2", chance = 10000000},			},
+ 			lootChance = 10000000
+		},
+	},
+	attacks = {
+		{"stunattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(melios_womp_rat, "melios_womp_rat")
