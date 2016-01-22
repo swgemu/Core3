f875bef6a1a80654169f5386448d600f5b20706e
Bledsoe <bnegrelli@gmail.com>
2016-01-22 15:14:01 -0500

    [Added] Melios Purl questline (Bestine Med Center)
    Change-Id: I382ab71696a7e6e40da12469daa435c35715cb84

---
+++ MMOCoreORB/bin/scripts/loot/groups/task_loot/task_loot_melios_purl_q2.lua
@@ -0,0 +1,10 @@
+task_loot_melios_purl_q2 = {
+  description = "",
+  minimumLevel = 0,
+  maximumLevel = 0,
+  lootItems = {
+    {itemTemplate = "melios_purl_q2", weight = 10000000}
+  }
+}
+
+addLootGroupTemplate("task_loot_melios_purl_q2", task_loot_melios_purl_q2)
---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/aroy_sekun.lua
@@ -0,0 +1,40 @@
+aroy_sekun = Creature:new {
+	objectName = "",
+	customName = "Aroy Sekun",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 7,
+	chanceHit = 0.26,
+	damageMin = 55,
+	damageMax = 65,
+	baseXp = 187,
+	baseHAM = 270,
+	baseHAMmax = 330,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {
+					"object/mobile/dressed_quest_farmer.iff",
+					"object/mobile/dressed_commoner_naboo_human_male_02.iff",
+					"object/mobile/dressed_commoner_fat_human_male_01.iff"
+		},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "melios_purl_mission_target_convotemplate",
+	attacks = brawlermaster
+}
+
+CreatureTemplates:addCreatureTemplate(aroy_sekun, "aroy_sekun")
