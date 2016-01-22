8a5355663202516834d6d9178454ad6e90635a81
Bledsoe <bnegrelli@gmail.com>
2016-01-22 15:14:01 -0500

    [Added] Melios Purl questline (Bestine Med Center)
    Change-Id: I382ab71696a7e6e40da12469daa435c35715cb84

---
+++ MMOCoreORB/bin/scripts/loot/items/task_loot/melios_purl_q2.lua
@@ -0,0 +1,15 @@
+melios_purl_q2 = {
+  minimumLevel = 0,
+  maximumLevel = -1,
+  customObjectName = "",
+  directObjectTemplate = "object/tangible/mission/quest_item/melios_purl_q2_needed.iff",
+  craftingValues = {
+  },
+  customizationStringNames = {},
+  customizationValues = {},
+  skillMods = {
+
+  }
+}
+
+addLootItemTemplate("melios_purl_q2", melios_purl_q2)
