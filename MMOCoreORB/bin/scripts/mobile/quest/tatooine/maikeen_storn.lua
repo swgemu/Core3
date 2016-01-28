54a90e19c76b5f00e15e63333aae4840edcfb8e8
Bledsoe <bnegrelli@gmail.com>
2016-01-27 22:55:28 -0500

    [Added] Bestine:Melios Purl quests  http://www.swgemu.com/archive/scrapbookv51/data/20070129004902/index.htm
    Change-Id: I0f06dac01b45846fadc13743bede77a107db3a47

---
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/maikeen_storn.lua
@@ -0,0 +1,41 @@
+maikeen_storn = Creature:new {
+	objectName = "@mob/creature_names:farmer",
+	customName = "Maikeen Storn",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 4,
+	chanceHit = 0.24,
+	damageMin = 40,
+	damageMax = 45,
+	baseXp = 62,
+	baseHAM = 113,
+	baseHAMmax = 138,
+	armor = 0,
+	resists = {15,15,15,15,15,15,15,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = HERD,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_commoner_naboo_zabrak_female_02.iff",
+	},
+				
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "melios_purl_mission_target_convotemplate",
+	attacks = brawlermaster
+}
+
+CreatureTemplates:addCreatureTemplate(maikeen_storn, "maikeen_storn")
+
+
+
