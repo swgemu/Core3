ead6cba4939d7244ae236b3ec005bb74cedc4143
Bledsoe <bnegrelli@gmail.com>
2016-01-28 09:48:51 -0500

    [WIP][Added] Bestine Election:Tour Aryon convo & election routine.
    Change-Id: I3001edfbeb3c3c99a8caa7d68d2517b9676352a3

--- MMOCoreORB/bin/scripts/mobile/quest/tatooine/bestine_election/tour_aryon.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/tatooine/bestine_election/tour_aryon.lua
@@ -23,13 +23,13 @@ tour_aryon = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = 128,
+	optionsBitmask = 264,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_tatooine_tour_aryon.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "tourAryonConvoTemplate",
 	attacks = {
 	}
 }
