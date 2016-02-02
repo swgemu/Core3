547100b54307ceed5752fb875cdf763af1f78fa7
Ivojedi <ivojedi@swgemu.com>
2016-02-01 18:37:59 -0800

    [Fixed] neutral players being able to attack faction npcs - mantis 6868
    Change-Id: I247fcfd766e17bbfc886681bcab61ae3a11bfaf5

--- MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
@@ -3142,17 +3142,20 @@ bool AiAgentImplementation::isAttackableBy(CreatureObject* object) {
 
 	unsigned int targetFaction = object->getFaction();
 
-	if (targetFaction != 0 && getFaction() != 0) {
-		if (targetFaction == getFaction()) {
+	if (getFaction() != 0) {
+		if (targetFaction != 0) {
+			if (targetFaction == getFaction()) {
+				return false;
+			}
+
+			PlayerObject* ghost = object->getPlayerObject();
+
+			if (ghost != NULL && ghost->getFactionStatus() == FactionStatus::ONLEAVE) {
+				return false;
+			}
+		} else if (object->isPlayerCreature()) {
 			return false;
 		}
-
-		PlayerObject* ghost = object->getPlayerObject();
-
-		if (ghost != NULL && ghost->getFactionStatus() == FactionStatus::ONLEAVE) {
-			return false;
-		}
-
 	}
 
 	if (object->isAiAgent()) {
