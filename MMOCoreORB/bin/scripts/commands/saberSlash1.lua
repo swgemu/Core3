ad2465a7f74a014fb22013d6949402525d4a5531
Greg Slomin <gregslomin@gmail.com>
2016-02-04 06:41:55 -0600

    [fixed] SaberSlash1/2 bleeds [refactor] Changed CombatQueueCommand::dotEffects to a Vector from a VectorMap [refactor] Changed dotEffect primary/secondary percentage to a float from a uint8 Change-Id: I1dc946f4f4411178397fdbb5577227e2b083e40d

--- MMOCoreORB/bin/scripts/commands/saberSlash1.lua
+++ MMOCoreORB/bin/scripts/commands/saberSlash1.lua
@@ -72,7 +72,9 @@ SaberSlash1Command = {
 		0,
 		25,
 		50, 
-		30
+		30,
+		8.33,
+		8.33
 	  ),
 
 	  	DotEffect( 
@@ -83,7 +85,9 @@ SaberSlash1Command = {
 		0,
 		25,
 		50, 
-		30
+		30,
+		8.33,
+		8.33
 	  ),
 	},
 
@@ -95,7 +99,9 @@ SaberSlash1Command = {
 		{}, 
 		100, 
 		0, 
-		0 
+		0,
+		8.33,
+		8.33
 	  )
 	},
 
