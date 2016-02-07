f642fc80379d04827b5cfb8b2aa58ca0ee44ef22
Nee <nee@swgemu.com>
2016-02-07 14:15:41 +0000

    [fixed] Rifleman attack-special 'SniperShot' to be more accurate emulation (Mantis #6881) -- PART 1 -- (Part 2 will require complete rewrite of CombatManager::StartCombat function, specifically the 'isIncapacitated return false' portion)
    Change-Id: I22a1a8b4cbb0153e407688a189bfb315c6a6164f

--- MMOCoreORB/src/server/zone/objects/creature/commands/SniperShotCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SniperShotCommand.h
@@ -22,12 +22,12 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
-			
+
 		if (!creature->isPlayerCreature())
 			return GENERALERROR;
-		
+			
 		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
-		
+
 		if (creature == targetObject || targetObject == NULL || !targetObject->isPlayerCreature())
 			return INVALIDTARGET;
 
@@ -44,20 +44,25 @@ public:
 
 		WeaponObject* weapon = creature->getWeapon();
 
+		if (!player->isIncapacitated() && !player->isDead()) {
+			creature->sendSystemMessage("@error_message:target_not_incapacitated");  //You cannot perform the death blow. Your target is not incapacitated.
+			return GENERALERROR;
+		}  else if (player->isDead()) {
+			StringIdChatParameter params("error_message", "prose_target_already_dead");
+			params.setTT(player->getDisplayedName());
+			creature->sendSystemMessage(params); //But %TT is already dead!
+			return GENERALERROR;
+		}
+
 		int maxRange = weapon->getMaxRange();
 
 		if (!player->isInRange(creature, maxRange + player->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
-				
-		if (player->isAttackableBy(creature) && player->isIncapacitated()) {
-			playerManager->killPlayer(creature, player, 1);			
-		}  else if (!player->isIncapacitated()) {
-			creature->sendSystemMessage("@error_message:target_not_incapacitated");  //You cannot perform the death blow. Your target is not incapacitated.
-		}  else if (player->isDead()) {
-			return GENERALERROR;
-		}
 
- 		return doCombatAction(creature, target);
+ 		if (player->isAttackableBy(creature) && player->isIncapacitated())
+ 			playerManager->killPlayer(creature, player, 1);
+
+ 		return doCombatAction(creature,target);
 
 	}
 
