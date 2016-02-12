c47597169b234a6f17b9d4cab8efbef8275a4545
Anakis <esri.seska@gmail.com>
2016-02-12 04:04:29 +0000

    [Added] Chat room invite command. [Added] Chat room uninvite command. [Added] Chat room kick command. [Fixed] Chat room titles are now checked for profanity.
    Change-Id: I5c8c9ee756f67545a601516be660e744d289c0e2

---
+++ MMOCoreORB/src/server/zone/packets/chat/ChatOnInviteToRoom.h
@@ -0,0 +1,38 @@
+/*
+ * ChatOnInviteToRoom.h
+ *
+ *  Created on: Feb 12, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATONINVITETOROOM_H_
+#define CHATONINVITETOROOM_H_
+
+#include "engine/engine.h"
+
+class ChatOnInviteToRoom : public BaseMessage {
+public:
+	ChatOnInviteToRoom(CreatureObject* inviter, const String& invitee, const String& roomPath, int error, int requestID) : BaseMessage() {
+		String galaxy = inviter->getZoneServer()->getGalaxyName();
+
+		insertShort(0x05);
+		insertInt(0x493FE74A);  //Opcode
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); //Galaxy name
+		insertAscii(inviter->getFirstName()); //Inviter's first name
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); // Galaxy name
+		insertAscii(invitee); //Invitee's first name
+
+		insertAscii(roomPath); //Full room path
+
+		insertInt(error); ///Result code
+		insertInt(requestID); //Request ID
+
+	}
+
+};
+
+#endif /*CHATONINVITETOROOM_H_*/
