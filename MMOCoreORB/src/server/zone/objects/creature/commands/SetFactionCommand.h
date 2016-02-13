b97e07f01ce985d36b6cac1e0e809577e38ea98a
Ivojedi <ivojedi@swgemu.com>
2016-02-13 15:08:56 -0800

    [Added] optional arg to /setfaction admin command to set faction rank
    Change-Id: I6c0d0b743d7c38323fc92d75a4a58955f92231f6

--- MMOCoreORB/src/server/zone/objects/creature/commands/SetFactionCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SetFactionCommand.h
@@ -45,40 +45,36 @@ public:
 		ManagedReference<CreatureObject*> pobj = cast<CreatureObject*>( obj.get());
 		ManagedReference<PlayerObject*> targetPlayerObject = NULL;
 
-
-
 		if (pobj != NULL)
 			targetPlayerObject = pobj->getPlayerObject();
-
-
 
 		//First, check if they passed a name with the command.
 		UnicodeTokenizer tokenizer(arguments);
 		tokenizer.setDelimeter(" ");
 
 		// if no parameters are given, just send the flags
-		if (!tokenizer.hasMoreTokens()){
+		if (!tokenizer.hasMoreTokens()) {
 			StringBuffer msg;
 			msg << "PvPStatusbitmask = " << String::valueOf(pvpStatus) << endl;
 			msg << "Optionsbitmask = " << String::valueOf(optionsBitmask) << endl;
 			msg <<  "Faction = " << String::valueOf(intFaction) << endl;
 
 
-			if(targetPlayerObject != NULL)
+			if (targetPlayerObject != NULL)
 				msg << "Faction Status: " << String::valueOf(targetPlayerObject->getFactionStatus());
 
 			creature->sendSystemMessage(msg.toString());
 			return SUCCESS;
 		}
 
-
 		String faction;
 		tokenizer.getStringToken(faction);
 
-		if(!tokenizer.hasMoreTokens()){
-			creature->sendSystemMessage("SYNTAX: /setfaction <name> [imperial | rebel | neutral] [onleave | covert | overt ]");
+		if (!tokenizer.hasMoreTokens()) {
+			creature->sendSystemMessage("SYNTAX: /setfaction <name> [imperial | rebel | neutral] [onleave | covert | overt ] [rank]");
 			return INVALIDPARAMETERS;
 		}
+
 		Locker _lock(tano,creature);
 
 		if (faction == "neutral") {
@@ -90,28 +86,15 @@ public:
 		}
 
 		if (faction == "imperial" || faction == "rebel" || faction == "hutt") {
-			if (pobj != NULL && faction.hashCode() != tano->getFaction()) {
-				pobj->setFactionRank(0);
-			}
-
 			tano->setFaction(faction.hashCode());
 		}
-
-		if (targetPlayerObject != NULL) { // Cap off points to new caps
-			targetPlayerObject->increaseFactionStanding("imperial", 0);
-			targetPlayerObject->increaseFactionStanding("rebel", 0);
-		}
-
 
 		if (tokenizer.hasMoreTokens()) {
 			//The next argument should be whether they are overt, onleave, or covert
 			String status;
 			tokenizer.getStringToken(status);
 
-
-
 			if (targetPlayerObject != NULL) {
-
 				if ( status == "overt") {
 					targetPlayerObject->setFactionStatus(FactionStatus::OVERT);
 				} else  if (status == "covert"){
@@ -133,6 +116,27 @@ public:
 			tano->broadcastPvpStatusBitmask();
 		}
 
+		if (tokenizer.hasMoreTokens()) {
+			int rank = tokenizer.getIntToken();
+
+			if (rank < 0)
+				rank = 0;
+			else if (rank > 15)
+				rank = 15;
+
+			if (pobj != NULL)
+				pobj->setFactionRank(rank);
+
+		} else {
+			if (pobj != NULL && faction.hashCode() != intFaction) {
+				pobj->setFactionRank(0);
+			}
+		}
+
+		if (targetPlayerObject != NULL) { // Cap off points to new caps
+			targetPlayerObject->increaseFactionStanding("imperial", 0);
+			targetPlayerObject->increaseFactionStanding("rebel", 0);
+		}
 
 		return SUCCESS;
 	}
