d496633cf9d24e3a71369603d2a74508858d75bf
Greg Slomin <gregslomin@gmail.com>
2016-02-16 20:36:48 -0600

    [added] LambdaObserver and LambdaObserverFunction class to use with existing observer system [added] std=c++11 to Makefile.am - You will need a C++11 compiler [added] CREATUREREVIVED observer type and notify functions [fixed] Feign death removal on character load [refactor] Changed some prose packages to use DisplayedName instead of their objectID [refactor] Removed SCHEDULE_TASK macros and replaced them with TaskManager::scheduleTask lambdas
    Change-Id: Ib25cdeabcda7bb78c9a792fed66d27753bca8234

---
+++ MMOCoreORB/src/server/utils/LambdaObserverFunction.h
@@ -0,0 +1,30 @@
+/*
+ 				Copyright <SWGEmu>
+		See file COPYING for copying conditions.
+*/
+
+#ifndef LAMBDAOBSERVERFUNCTION_H_
+#define LAMBDAOBSERVERFUNCTION_H_
+
+#include "engine/util/Observable.h"
+#include <functional>
+
+  /* Usage:
+ * registerObserver(new LambdaObserver(
+ * new LambdaObserverFunction([(captured variables)](uint32 eventType, Observable* observable, ManagedObject* arg1, uint64 arg2) ->int {
+ *  (Code goes here)
+ *  return (resultCode);
+ * })));
+ */
+  
+class LambdaObserverFunction : public Object {
+	std::function<int(uint32, Observable*, ManagedObject*, uint64)> func;
+public:
+
+  int notifyObserverEvent(uint32 eventType, Observable* obs, ManagedObject* arg1, uint64 arg2) {
+	  return func(eventType, obs, arg1, arg2);
+  }
+  LambdaObserverFunction(std::function<int(uint32, Observable*, ManagedObject*, uint64)> f) : func(f) { }
+};
+
+#endif
