db04ad648b1cb5b8bec8857d2a876ef3c4b05400
Greg Slomin <gregslomin@gmail.com>
2016-02-18 16:45:37 -0600

    [added] Ability to use healstate and healenhance without arguments (#3650)
    Change-Id: I2f6be49e149be8477216d23dcf05438c6192a9e2

--- MMOCoreORB/src/server/zone/objects/creature/commands/HealStateCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/HealStateCommand.h
@@ -6,6 +6,7 @@
 #define HEALSTATECOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/chat/StringIdChatParameter.h"
 #include "server/zone/objects/tangible/pharmaceutical/StatePack.h"
 #include "server/zone/objects/tangible/pharmaceutical/RangedStimPack.h"
 #include "server/zone/ZoneServer.h"
@@ -19,6 +20,7 @@
 class HealStateCommand : public QueueCommand {
 	float mindCost;
 	float range;
+	Vector<uint64> healableStates;
 public:
 
 	HealStateCommand(const String& name, ZoneProcessServer* server)
@@ -26,6 +28,10 @@ public:
 
 		mindCost = 20;
 		range = 6;
+		healableStates.add(CreatureState::STUNNED);
+		healableStates.add(CreatureState::DIZZY);
+		healableStates.add(CreatureState::BLINDED);
+		healableStates.add(CreatureState::INTIMIDATED);
 	}
 
 	void deactivateStateTreatment(CreatureObject* creature) const {
@@ -206,21 +212,44 @@ public:
 
 		parseModifier(arguments.toString(), state, objectId);
 
-		if (state == CreatureState::INVALID) {
-			creature->sendSystemMessage("@healing_response:healing_response_70"); //You must specify a valid state type.
-			return GENERALERROR;
-		}
-
 		SceneObject* inventory = creature->getSlottedObject("inventory");
 
 		ManagedReference<StatePack*> statePack = NULL;
 
-		if (inventory != NULL) {
-			statePack = inventory->getContainerObject(objectId).castTo<StatePack*>();
-		}
+		if(state != CreatureState::INVALID || objectId != 0) {
+			if (inventory != NULL) {
+				statePack = inventory->getContainerObject(objectId).castTo<StatePack*>();
+			}
 
-		if (statePack == NULL)
-			statePack = findStatePack(creature, state);
+			if (statePack == NULL)
+				statePack = findStatePack(creature, state);
+		}else {
+			uint64 targetStateBitmask = creatureTarget->getStateBitmask();
+			for(int i=0; i<healableStates.size(); i++) {
+
+				uint64 healableState = healableStates.get(i);
+
+				if(!(targetStateBitmask & healableState))
+					continue;
+
+
+				state = healableState;
+				statePack = findStatePack(creature, healableState);
+
+				if(statePack != NULL) {
+					break;
+				}
+			}
+
+			//if state is INVALID they had no healable states
+			//if it is valid but statePack is NULL they had no valid medicine for *any* state and will error in canPerformSkill
+			if(state == CreatureState::INVALID) {
+				StringIdChatParameter stringId("healing", "no_state_to_heal"); // %TT has no state that you can heal.
+				stringId.setTT(creatureTarget->getDisplayedName());
+				creature->sendSystemMessage(stringId);
+				return GENERALERROR;
+			}
+		}
 
 		int mindCostNew = creature->calculateCostAdjustment(CreatureAttribute::FOCUS, mindCost);
 
