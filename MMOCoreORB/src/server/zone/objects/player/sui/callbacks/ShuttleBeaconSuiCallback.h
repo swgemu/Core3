c1552dc2254ff360c726b7f3522dc0cb5bd296e4
Miztahh <miztah@swgemu.com>
2016-02-19 00:12:54 +0000

    [added] Shuttle Beacon event perk
    Change-Id: Iaee770898f879b7afb017ab6f289f4f42322c5d6

---
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/ShuttleBeaconSuiCallback.h
@@ -0,0 +1,44 @@
+#ifndef SHUTTLEBEACONSUICALLBACK_H_
+#define SHUTTLEBEACONSUICALLBACK_H_
+
+
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/tangible/eventperk/ShuttleBeacon.h"
+
+class ShuttleBeaconSuiCallback : public SuiCallback {
+public:
+	ShuttleBeaconSuiCallback(ZoneServer* server)
+		: SuiCallback(server) {
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, uint32 eventIndex, Vector<UnicodeString>* args) {
+		bool cancelPressed = (eventIndex == 1);
+
+		if (!suiBox->isListBox() || cancelPressed)
+			return;
+
+		SuiListBox* listBox = cast<SuiListBox*>(suiBox);
+
+		ManagedReference<SceneObject*> beacon = listBox->getUsingObject();
+
+		if (beacon == NULL)
+			return;
+
+		ManagedReference<ShuttleBeacon*> shuttleBeacon = cast<ShuttleBeacon*>(beacon.get());
+
+		if (shuttleBeacon == NULL)
+			return;
+
+		if (args->size() < 1)
+			return;
+
+		int index = Integer::valueOf(args->get(0).toString());
+
+		if (index == -1)
+			return;
+
+		shuttleBeacon->spawnShuttle(player, index);
+	}
+};
+
+#endif /* SHUTTLEBEACONSUICALLBACK_H_ */
