fe987c3b68c609d7ef7b15b99547416cd52133ed
Miztahh <miztah@swgemu.com>
2016-02-19 00:12:54 +0000

    [added] Shuttle Beacon event perk
    Change-Id: Iaee770898f879b7afb017ab6f289f4f42322c5d6

--- MMOCoreORB/bin/scripts/screenplays/events/eventPerkData.lua
+++ MMOCoreORB/bin/scripts/screenplays/events/eventPerkData.lua
@@ -79,7 +79,7 @@ eventPerkGamesSuppliesDeeds = {
 	{ template = "lottery_droid_deed", cost = 25000, displayName = "@event_perk:pro_lottery_droid_game" },
 	--{ template = "race_droid_deed", cost = 10000, displayName = "@event_perk:pro_race_droid_game" },
 	--{ template = "scavenger_droid_deed", cost = 25000, displayName = "@event_perk:pro_scavenger_droid_game" },
-	--{ template = "shuttle_beacon", cost = 25000, displayName = "@event_perk:pro_shuttle_beacon_game" },
+	{ template = "shuttle_beacon", cost = 25000, displayName = "@event_perk:pro_shuttle_beacon_game" },
 	{ template = "radiologically_shielded_chest_deed", cost = 15000, displayName = "@event_perk:pro_radiological_chest_game" },
 	{ template = "skeleton_chest_deed", cost = 15000, displayName = "@event_perk:pro_skeleton_chest_deed" },
 	{ template = "sparking_treasure_box_chest_deed", cost = 15000, displayName = "@event_perk:pro_sparking_chest_deed" },
