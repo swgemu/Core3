149b7a94b71dff339f714bcddbee57b3f7dc6132
Nee Shadowfire <nee@swgemu.com>
2016-03-24 13:37:02 +0000

    [changed] 'Force Choke' and it's damage & DoT amounts (Mantis #6748)
    Change-Id: I3fb67d10b1c5923898d508365c364cefcd734529

--- MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTime.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTime.cpp
@@ -157,12 +157,6 @@ uint32 DamageOverTime::initDot(CreatureObject* victim, CreatureObject* attacker)
 		break;
 	case CommandEffect::FORCECHOKE:
 		nextTick.addMiliTime(6000);
-		strength = (float)strength * (1.f - (System::random(25) / 100.f));
-
-		if (victim->isPlayerCreature() && attacker->isPlayerCreature()) {
-			strength /= 4;
-		}
-
 		break;
 	}
 
@@ -350,18 +344,18 @@ uint32 DamageOverTime::doForceChokeTick(CreatureObject* victim, CreatureObject* 
 	Reference<CreatureObject*> attackerRef = attacker;
 	Reference<CreatureObject*> victimRef = victim;
 
-	EXECUTE_TASK_3(victimRef, attackerRef, damage, {
+	EXECUTE_TASK_4(victimRef, attackerRef, attribute, damage, {
 			Locker locker(victimRef_p);
 
 			Locker crossLocker(attackerRef_p, victimRef_p);
 
-			victimRef_p->inflictDamage(attackerRef_p, CreatureAttribute::HEALTH, damage_p, true);
-			victimRef_p->inflictDamage(attackerRef_p, CreatureAttribute::ACTION, damage_p, true);
-			victimRef_p->inflictDamage(attackerRef_p, CreatureAttribute::MIND, damage_p, true);
+			victimRef_p->inflictDamage(attackerRef_p, attribute_p, damage_p, true);
 			if (victimRef_p->hasAttackDelay())
 				victimRef_p->removeAttackDelay();
 
 			victimRef_p->playEffect("clienteffect/pl_force_choke.cef", "");
+			victimRef_p->sendSystemMessage("@combat_effects:choke_single");
+			victimRef_p->showFlyText("combat_effects", "choke", 0xFF, 0, 0);
 	});
 
 	return damage;
