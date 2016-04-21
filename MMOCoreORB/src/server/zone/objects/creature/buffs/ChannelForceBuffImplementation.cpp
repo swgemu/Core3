bfb769d0ad6367c8b6ffbb8314b2f521f56cdc77
Katherine 'Valkyra' <valkyra@swgemu.com>
2016-04-20 20:04:46 -0400

    [fixed] Converted Channel Force task to be within a buff object to fix #6780
    Change-Id: I8f51c2ceebf2864121c29bda784d7744e2c6f825

---
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/ChannelForceBuffImplementation.cpp
@@ -0,0 +1,53 @@
+#include "server/zone/objects/creature/buffs/ChannelForceBuff.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/creature/buffs/ChannelForceBuffTickEvent.h"
+#include "server/zone/managers/player/PlayerManager.h"
+#include "server/zone/ZoneServer.h"
+#include "templates/params/creature/CreatureAttribute.h"
+
+void ChannelForceBuffImplementation::initializeTransientMembers() {
+	BuffImplementation::initializeTransientMembers();
+}
+
+void ChannelForceBuffImplementation::activate(bool applyModifiers) {
+	if(creature.get() != NULL) {
+
+		// Do the subtraction of max ham.
+		creature.get()->addMaxHAM(CreatureAttribute::HEALTH, -cfTick, true);
+		creature.get()->addMaxHAM(CreatureAttribute::ACTION, -cfTick, true);
+		creature.get()->addMaxHAM(CreatureAttribute::MIND, -cfTick, true);
+
+		// Generic buff activate.
+		BuffImplementation::activate(false);
+
+		doHamTick();
+	}
+}
+
+void ChannelForceBuffImplementation::deactivate(bool removeModifiers) {
+	if(creature.get() != NULL) {
+		BuffImplementation::deactivate(false);
+		if (amountOfTicks > 0) { // Probably got here prematurely, like a death.
+			PlayerManager* pMan = creature.get()->getZoneServer()->getPlayerManager();
+			if (pMan != NULL) {
+				pMan->fixHAM(creature.get());
+				amountOfTicks = 0;
+			}
+		}
+	}
+}
+
+void ChannelForceBuffImplementation::doHamTick() {
+	Locker locker(creature.get());
+	Locker lockerX(_this.getReferenceUnsafeStaticCast(),creature.get());
+
+	creature.get()->addMaxHAM(CreatureAttribute::HEALTH, cfTick / 20, true);
+	creature.get()->addMaxHAM(CreatureAttribute::ACTION, cfTick / 20, true);
+	creature.get()->addMaxHAM(CreatureAttribute::MIND, cfTick / 20, true);
+	amountOfTicks--;
+
+	if (amountOfTicks > 0) {
+		ChannelForceBuffTickEvent* cfbEvent = new ChannelForceBuffTickEvent(_this.getReferenceUnsafeStaticCast());
+		cfbEvent->schedule(8000); // 8 Seconds.
+	}
+}
