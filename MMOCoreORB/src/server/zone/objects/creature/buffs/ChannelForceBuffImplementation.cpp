6e19641a48532d315528288d29b60c4ef8a14070
Katherine 'Valkyra' <valkyra@swgemu.com>
2016-04-20 20:04:46 -0400

    [fixed] Converted Channel Force task to be within a buff object to fix #6780
    Change-Id: I8f51c2ceebf2864121c29bda784d7744e2c6f825

---
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/ChannelForceBuffImplementation.cpp
@@ -0,0 +1,62 @@
+#include "server/zone/objects/creature/buffs/ChannelForceBuff.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/creature/buffs/ChannelForceBuffTickEvent.h"
+#include "server/zone/managers/player/PlayerManager.h"
+#include "server/zone/ZoneServer.h"
+#include "templates/params/creature/CreatureAttribute.h"
+
+void ChannelForceBuffImplementation::initializeTransientMembers() {
+	BuffImplementation::initializeTransientMembers();
+
+	if (cfBuffEvent == NULL) {
+		cfBuffEvent = new ChannelForceBuffTickEvent(_this.getReferenceUnsafeStaticCast());
+	}
+}
+
+void ChannelForceBuffImplementation::activate(bool applyModifiers) {
+		// Generic buff activate.
+		BuffImplementation::activate(applyModifiers);
+
+		if (cfBuffEvent != NULL) {
+			cfBuffEvent->cancel();
+                } else {
+                  	cfBuffEvent = new ChannelForceBuffTickEvent(_this.getReferenceUnsafeStaticCast());
+                }
+
+		cfBuffEvent->schedule(8000);
+}
+
+void ChannelForceBuffImplementation::deactivate(bool removeModifiers) {
+	BuffImplementation::deactivate(removeModifiers);
+	if (cfBuffEvent != NULL) {
+		cfBuffEvent->cancel();
+		cfBuffEvent = NULL;
+	}
+}
+
+void ChannelForceBuffImplementation::doHamTick() {
+
+	// Grab what the attribute modifiers have.
+	VectorMap<byte, int>* attributeModifiers = getAttributeModifiers();
+
+  	int divisor = 20; // Dividing the attribute modifier by this amount.
+  	int secondsToUpdate = 8; // In seconds.
+
+  	// Locks.
+  	ManagedReference<CreatureObject*> creo = creature.get();
+
+	if (creo == NULL)
+		return;
+
+  	Locker crossLock(creo, _this.getReferenceUnsafeStaticCast());
+
+	for (int i = 0; i < attributeModifiers->size(); ++i) {
+		byte attribute = attributeModifiers->elementAt(i).getKey();
+		int modifier = attributeModifiers->elementAt(i).getValue();
+		int healAmount = MAX(-modifier / divisor, -modifier);
+		creo->addMaxHAM(attribute, healAmount, true);
+		setAttributeModifier(attribute, modifier + healAmount);
+	}
+
+	cfBuffEvent->reschedule(secondsToUpdate * 1000);
+}
