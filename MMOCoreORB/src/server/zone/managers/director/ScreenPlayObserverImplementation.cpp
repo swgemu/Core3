fcf47a3034945ce623146300913b96befb834e60
Greg Slomin <gregslomin@gmail.com>
2016-05-22 15:32:42 -0500

    [fixed] doRecovery using 2D distance for autoattack range [fixed] assert that fails to compile on clang/OSX
    Change-Id: I8edc77d3e0ef1d4e30f1cee893ad9b7fa1df166e

--- MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
@@ -29,7 +29,8 @@ int ScreenPlayObserverImplementation::notifyObserverEvent(uint32 eventType, Obse
 		}
 
 		if (!lua_isnumber(lua->getLuaState(), -1)) {
-			assert(printf("ScreenPlayObserver %s:%s didnt return a valid value in an observer handler\n", play.toCharArray(), key.toCharArray()) && 0);
+			printf("ScreenPlayObserver %s:%s didnt return a valid value in an observer handler\n", play.toCharArray(), key.toCharArray())
+			assert(false);
 		}
 
 		ret = lua->getIntParameter(lua->getLuaState());
