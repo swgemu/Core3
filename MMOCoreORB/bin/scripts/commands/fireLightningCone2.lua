d8934e8808da40358601fd32be4789bf388d0718
Greg Slomin <gregslomin@gmail.com>
2016-06-06 22:32:34 -0500

    [fixed] light lightning canon animations (#7050)
    Change-Id: Ieeb697f4ee330b372abcd70f41ef964eafda7456

--- MMOCoreORB/bin/scripts/commands/fireLightningCone2.lua
+++ MMOCoreORB/bin/scripts/commands/fireLightningCone2.lua
@@ -50,7 +50,7 @@ FireLightningCone2Command = {
 	actionCostMultiplier = 1,
 	mindCostMultiplier = 1,
 
-	animation = "fire_acid_rifle_single_2", 
+	animation = "fire_lightning_rifle_single_2",
 	animType = GENERATE_INTENSITY,
 
 	combatSpam = "firelightningcone2",
