9c3e207a9bde0f8ff5cea99e96f4d440debd8479
Nee Shadowfire <nee@swgemu.com>
2016-06-13 19:38:58 +0000

    [fixed] event perk chests from allowing NO-TRADE items to be placed (Mantis #7084)
    Change-Id: I0718735b71eb6be94848316ac6c6bba6b8c2af78

--- MMOCoreORB/src/server/zone/objects/tangible/components/PlaceableLootContainerComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/PlaceableLootContainerComponent.cpp
@@ -30,13 +30,8 @@ int PlaceableLootContainerComponent::canAddObject(SceneObject* sceneObject, Scen
 
 	if(sceneObject->getContainerObjectsSize() >= 0){
 		errorDescription = "@event_perk:chest_can_not_add"; //You do not have permission to add items to this container.
-		 return TransferErrorCode::INVALIDTYPE;
+		return TransferErrorCode::CANTADD;
 	}
 
 	return 1;
 }
-
-
-
-
-
