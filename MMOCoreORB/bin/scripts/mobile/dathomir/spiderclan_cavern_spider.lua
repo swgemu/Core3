918d42888e080f0cc33dfe056f9faa79ed472544
Nee Shadowfire <nee@swgemu.com>
2016-06-30 14:12:50 +0000

    [fixed] some Spiderclan Cave spawns from incorrectly fighting with eachother which caused unnecessary AI loops
    Change-Id: I04c518a07715f79f2cdab177aef04773341c0a15

---
+++ MMOCoreORB/bin/scripts/mobile/dathomir/spiderclan_cavern_spider.lua
@@ -0,0 +1,39 @@
+spiderclan_cavern_spider = Creature:new {
+  objectName = "@mob/creature_names:cave_gaping_spider",
+  socialGroup = "spider_nightsister",
+  faction = "spider_nightsister",
+  level = 27,
+  chanceHit = 0.36,
+  damageMin = 250,
+  damageMax = 260,
+  baseXp = 2822,
+  baseHAM = 7700,
+  baseHAMmax = 9400,
+  armor = 0,
+  resists = {130,130,15,-1,15,15,-1,-1,-1},
+  meatType = "meat_insect",
+  meatAmount = 35,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+  creatureBitmask = PACK + KILLER + STALKER,
+  optionsBitmask = AIENABLED,
+  diet = CARNIVORE,
+
+  templates = {"object/mobile/gaping_spider.iff"},
+  scale = 1.1,
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "",
+  attacks = {
+    {"mildpoison",""},
+    {"intimidationattack",""}
+  }
+}
+
+CreatureTemplates:addCreatureTemplate(spiderclan_cavern_spider, "spiderclan_cavern_spider")
