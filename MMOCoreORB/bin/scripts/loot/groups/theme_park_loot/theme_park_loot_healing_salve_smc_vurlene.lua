1daab29d0508af38f735f6fd9f0ecdaee163f18b
Nee Shadowfire <nee@swgemu.com>
2016-07-12 13:32:17 +0000

    [added] the SMC themepark to Dathomir (Mantis #6900)
    Change-Id: I1795728831ab7c1557a4f5a9baabf917748a864b

---
+++ MMOCoreORB/bin/scripts/loot/groups/theme_park_loot/theme_park_loot_healing_salve_smc_vurlene.lua
@@ -0,0 +1,10 @@
+theme_park_loot_healing_salve_smc_vurlene = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "healing_salve_smc_vurlene", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("theme_park_loot_healing_salve_smc_vurlene", theme_park_loot_healing_salve_smc_vurlene)
---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/theme_park_naboo_holy_man.lua
@@ -0,0 +1,38 @@
+theme_park_naboo_holy_man = Creature:new {
+  objectName = "@mob/creature_names:naboo_holy_man",
+  randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+  socialGroup = "naboo",
+  faction = "naboo",
+  level = 5,
+  chanceHit = 0.250000,
+  damageMin = 45,
+  damageMax = 50,
+  baseXp = 85,
+  baseHAM = 135,
+  baseHAMmax = 165,
+  armor = 0,
+  resists = {0,0,0,0,0,0,0,-1,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0.000000,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = HERD,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = HERBIVORE,
+
+  templates = {"object/mobile/gungan_male.iff"},
+  lootGroups = {},
+  weapons = {"rebel_weapons_light"},
+  conversationTemplate = "theme_park_singing_mountain_clan_mission_target_convotemplate",
+  reactionStf = "@npc_reaction/fancy",
+  attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_naboo_holy_man, "theme_park_naboo_holy_man")
