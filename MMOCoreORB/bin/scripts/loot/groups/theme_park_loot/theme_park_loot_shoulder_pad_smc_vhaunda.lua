418327c67fa0dd0616b27633f2eae6cfbc89257a
Nee Shadowfire <nee@swgemu.com>
2016-07-12 13:32:17 +0000

    [added] the SMC themepark to Dathomir (Mantis #6900)
    Change-Id: I1795728831ab7c1557a4f5a9baabf917748a864b

---
+++ MMOCoreORB/bin/scripts/loot/groups/theme_park_loot/theme_park_loot_shoulder_pad_smc_vhaunda.lua
@@ -0,0 +1,10 @@
+theme_park_loot_shoulder_pad_smc_vhaunda = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "shoulder_pad_smc_vhaunda", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("theme_park_loot_shoulder_pad_smc_vhaunda", theme_park_loot_shoulder_pad_smc_vhaunda)
