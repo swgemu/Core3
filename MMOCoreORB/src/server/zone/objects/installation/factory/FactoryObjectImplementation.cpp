5a75221ffa4fed83744203664b11c7c37d71acb7
Nee Shadowfire <nee@swgemu.com>
2016-07-23 06:50:54 +0000

    [changed] factory runtimes in relation to schematic 'complexity' (Mantis #7148)
    Change-Id: I10c8cb77ba6d014c12d01f33c547d0fbfc829a5c

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -428,7 +428,7 @@ bool FactoryObjectImplementation::startFactory() {
 			return false;
 	}
 
-	timer = ((int)schematic->getComplexity()) * 2;
+	timer = ((int)schematic->getComplexity()) * 8;
 
 	if(!populateSchematicBlueprint(schematic))
 		return false;
