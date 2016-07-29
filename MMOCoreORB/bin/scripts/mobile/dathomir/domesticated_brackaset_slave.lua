9c8f35dd6914de936a021c318bbdb8238bd3c424
Nee Shadowfire <nee@swgemu.com>
2016-07-29 14:06:15 +0000

    [FIXED] creature-pet's regular AOE attacks should now function again (Mantis #7175) -- [removed] a bugged 'SpiderClan' spawn that shouldn't be there anyway (Mantis #7126) --- [changed] lair template for a couple of 'theaters', so the NPCs won't fight their own pets and cause constant unnecessary AI activations
    Change-Id: I790155671cd79270b21ca8c13f72e0c87e9310df

---
+++ MMOCoreORB/bin/scripts/mobile/dathomir/domesticated_brackaset_slave.lua
@@ -0,0 +1,45 @@
+domesticated_brackaset_slave = Creature:new {
+	objectName = "@mob/creature_names:domesticated_brackaset",
+	socialGroup = "slave",
+	faction = "",
+	level = 23,
+	chanceHit = 0.35,
+	damageMin = 170,
+	damageMax = 180,
+	baseXp = 2006,
+	baseHAM = 8550,
+	baseHAMmax = 8550,
+	armor = 0,
+	resists = {115,115,0,0,-1,0,0,-1,-1},
+	meatType = "meat_domesticated",
+	meatAmount = 100,
+	hideType = "hide_leathery",
+	hideAmount = 90,
+	boneType = "bone_mammal",
+	boneAmount = 80,
+	milkType = "milk_domesticated",
+	milk = 50,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/brackaset_hue.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "brackaset_common", chance = 10000000}
+			},
+			lootChance = 1460000
+		}
+	},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"intimidationattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(domesticated_brackaset_slave, "domesticated_brackaset_slave")
