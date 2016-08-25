64a16a7cbb5d629a828665458ae61e72e9e3d3ea
Nee Shadowfire <nee@swgemu.com>
2016-08-25 22:17:54 +0000

    [added] 'Brennis Doore' npc quest task to NABOO (Mantis #7161, phase 3)
    Change-Id: I43a6deb8e9ec63b36c732327df4b441b74b1d3e7

---
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/brennis_doore.lua
@@ -0,0 +1,88 @@
+brennis_doore_missions =
+{
+	{
+		missionType = "deliver",
+		primarySpawns =
+		{
+			{ npcTemplate = "grandmother_quest_brennis", planetName = "naboo", npcName = "Mia Magansitty" }
+		},
+		secondarySpawns =
+		{
+
+		},
+		itemSpawns =
+		{
+			{ itemTemplate = "object/tangible/mission/quest_item/brennis_doore_q1_needed.iff", itemName = "" }
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 50 }
+		}
+	},
+	{
+		missionType = "deliver",
+		primarySpawns =
+		{
+			{ npcTemplate = "ole_friend_quest_brennis", planetName = "naboo", npcName = "Temir Tonnalo" }
+		},
+		secondarySpawns =
+		{
+
+		},
+		itemSpawns =
+		{
+			{ itemTemplate = "object/tangible/mission/quest_item/brennis_doore_q2_needed.iff", itemName = "" }
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 75 }
+		}
+	},
+	{
+		missionType = "deliver",
+		primarySpawns =
+		{
+			{ npcTemplate = "imperial_rep_quest_brennis", planetName = "naboo", npcName = "Rille Nuder" }
+		},
+		secondarySpawns =
+		{
+
+		},
+		itemSpawns =
+		{
+			{ itemTemplate = "object/tangible/mission/quest_item/brennis_doore_q3_needed.iff", itemName = "" }
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 100 }
+		}
+	}
+}
+
+npcMapBrennisDoore =
+{
+	{
+		spawnData = { planetName = "naboo", npcTemplate = "brennis_doore", x = 1740.0, z = 12.0, y = 2657.0, direction = 138, cellID = 0, position = STAND },
+		npcNumber = 1,
+		stfFile = "@static_npc/naboo/brennis_doore",
+		missions = brennis_doore_missions
+	},
+}
+
+BrennisDoore = ThemeParkLogic:new {
+	numberOfActs = 1,
+	npcMap = npcMapBrennisDoore,
+	permissionMap = {},
+	className = "BrennisDoore",
+	screenPlayState = "brennis_doore_task",
+	distance = 1000,
+}
+
+registerScreenPlay("BrennisDoore", true)
+
+brennis_doore_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = BrennisDoore
+}
+brennis_doore_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = BrennisDoore
+}
