82bc4be6e81950053f0f744b9c99c2afb745e059
Nee Shadowfire <nee@swgemu.com>
2016-08-27 00:46:46 +0000

    [Added] 'Brennis Doore' npc quest task to NABOO (Mantis #7161, phase 3)
    Change-Id: I43a6deb8e9ec63b36c732327df4b441b74b1d3e7

---
+++ MMOCoreORB/bin/scripts/mobile/quest/naboo/ole_friend_quest_brennis.lua
@@ -0,0 +1,40 @@
+ole_friend_quest_brennis = Creature:new {
+	objectName = "@mob/creature_names:criminal",
+	socialGroup = "thug",
+	faction = "thug",
+	level = 7,
+	chanceHit = 0.26,
+	damageMin = 55,
+	damageMax = 65,
+	baseXp = 187,
+	baseHAM = 270,
+	baseHAMmax = 330,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = AIENABLED + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_criminal_thug_human_male_01.iff",
+		"object/mobile/dressed_goon_twk_male_01.iff",
+		"object/mobile/dressed_villain_trandoshan_male_01.iff",
+		"object/mobile/dressed_criminal_thug_bothan_male_01.iff"
+	},
+	lootGroups = {},
+	weapons = {"pirate_weapons_light"},
+	conversationTemplate = "brennis_doore_mission_target_convotemplate",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(ole_friend_quest_brennis, "ole_friend_quest_brennis")
