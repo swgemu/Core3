c3600051983d23cd1448a2749dc94e4febe9dd6b
ValkyraSWG <valkyra@cox.net>
2016-08-31 19:40:56 -0400

    [added] Reworked ForceShrine code from C++ to Lua systems, with finding trainer code inserted into DirectorManager for Lua usage. [added] Jedi Trials base framework with 2 trials completed, shrine will not select a non-implemented trial.
    Change-Id: Ia837c1e08e92f7ca99ff107981272bbd67c7d0d9

---
+++ MMOCoreORB/bin/scripts/screenplays/jedi/padawan/trials/trial_artist.lua
@@ -0,0 +1,47 @@
+local ObjectManager = require("managers.object.object_manager")
+local JediPadawanTrial = require("screenplays.jedi.padawan.jedi_padawan_trial")
+
+FIRST_MESSAGE = "@jedi_trials:artist_01 "
+SECOND_MESSAGE = "@jedi_trials:artist_02 "
+SCREEN_PLAY_NAME = "JediPadawanTrialArtistScreenPlay"
+MOBILE = "sathme_forr"
+WAYPOINT_DESCRIPTION = "@jedi_trials:artist_03"
+ZONE_NAME = "naboo"
+ZONE_REGION = "Kaadara"
+ZONE_COORDINATES = {x = 4983, y = 6634}
+
+JediPadawanTrialArtistScreenPlay = JediPadawanTrial:new {
+	numberOfActs = 1,
+
+	screenplayName = SCREEN_PLAY_NAME,
+}
+
+registerScreenPlay(SCREEN_PLAY_NAME, false)
+
+function JediPadawanTrialArtistScreenPlay:start(pCreature)
+	JediPadawanTrial:sendSuiNotification(pCreature, FIRST_MESSAGE, SECOND_MESSAGE, SCREEN_PLAY_NAME, ZONE_NAME, ZONE_REGION)
+	CreatureObject(pCreature):setScreenPlayState(1, "JediPadawanTrial")
+end
+
+function JediPadawanTrialArtistScreenPlay:notifyOkPressed(pCreature, sui, cancelPressed, arg0)
+	JediPadawanTrial:initializeTrial(pCreature, SCREEN_PLAY_NAME, MOBILE, WAYPOINT_DESCRIPTION, ZONE_NAME, ZONE_COORDINATES)
+end
+
+function JediPadawanTrialArtistScreenPlay:startPart2(pCreature)
+	local pNpc = spawnMobile(ZONE_NAME, "trials_ileeb_isonocra", 60, 5996, getTerrainHeight(pCreature, 5996, 5885), 5885, getRandomNumber(360) - 180, 0)
+
+	if (pNpc ~= nil) then
+		local ghost = CreatureObject(pCreature):getPlayerObject()
+		PlayerObject(ghost):addWaypoint(ZONE_NAME, "Jedi Trials", WAYPOINT_DESCRIPTION, -7152, 4549, WAYPOINT_COLOR_BLUE, true, true, 0)
+	end
+end
+
+function JediPadawanTrialArtistScreenPlay:despawnNPC02(pCreature)
+
+	if (pCreature ~= nil) then
+		CreatureObject(pCreature):destroyObjectFromWorld()
+	end
+
+end
+
+return JediPadawanTrialArtistScreenPlay
