a7cd4ea3d02721e36b05d7b375339b9e8c0c7614
ValkyraSWG <valkyra@cox.net>
2016-09-08 12:45:32 -0400

    [added] ShowFlyText packet documentation notes [added] Ability to set the size of the flytext ingame via ShowFlyText float in packet [changed] ShowFlyText in SceneObject now can send either broadcasted or private flytext, broadcasted by default. The former has a scale of 1.0, which will change flytext ingame to proper size.
    Change-Id: I1702b34073589adfe9aaa06c78a3c81dd6d3e808

--- MMOCoreORB/src/server/zone/packets/object/ShowFlyText.h
+++ MMOCoreORB/src/server/zone/packets/object/ShowFlyText.h
@@ -16,18 +16,26 @@ namespace object {
 
 class ShowFlyText : public ObjectControllerMessage {
 public:
-	ShowFlyText(SceneObject* creo, const String& file, const String& aux, uint8 red, uint8 green, uint8 blue)
+	ShowFlyText(SceneObject* creo, const String& file, const String& aux, uint8 red, uint8 green, uint8 blue, float size)
 			: ObjectControllerMessage(creo->getObjectID(), 0x1B, 0x1BD) {
-		insertLong(creo->getObjectID());
+		insertLong(creo->getObjectID()); // Target object ID
+
+		insertAscii(file.toCharArray()); // StringId file
+		insertInt(0); // Spacer
+		insertAscii(aux.toCharArray()); // StringId entry
+		insertFloat(size); // Scale, 1.0 for broadcasted, 0 for none.
+		insertByte(red); // R
+		insertByte(green); // G
+		insertByte(blue); // B
+
+		 /* enum Flags
+		  * {
+		  * 0x0001, -- Only shown on target.
+		  * 0x0002, -- show in chat box?
+		  * 0x0004, -- ???
+		  */
+		insertByte(5); // FLAGS. 5 = Displayed on target, possible bitmask of 1+4?
 		
-		insertAscii(file.toCharArray());
-		insertInt(0);		
-		insertAscii(aux.toCharArray());
-		insertInt(0);
-		insertByte(red);
-		insertByte(green);
-		insertByte(blue);
-		insertByte(5);// unk
 	}
 
 };
