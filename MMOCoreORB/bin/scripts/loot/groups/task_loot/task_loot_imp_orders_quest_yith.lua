48281497c33c6f4d763f000db1b45fa0b049992b
Nee Shadowfire <nee@swgemu.com>
2016-09-09 09:19:06 +0000

    [added] Yith Seenath missions to YAVIN-4 (Mantis #7176, phase 1)
    Change-Id: I0f284e0ae7199814f272d094bbcae5740267f155

---
+++ MMOCoreORB/bin/scripts/loot/groups/task_loot/task_loot_imp_orders_quest_yith.lua
@@ -0,0 +1,10 @@
+task_loot_imp_orders_quest_yith = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "imp_orders_quest_yith", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("task_loot_imp_orders_quest_yith", task_loot_imp_orders_quest_yith)
