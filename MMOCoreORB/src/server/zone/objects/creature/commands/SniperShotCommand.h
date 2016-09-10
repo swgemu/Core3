c9c2614887a7cc48245a4b07557e988e6cf0fd65
Nee Shadowfire <nee@swgemu.com>
2016-09-10 07:02:25 +0000

    [FIXED] Rifleman attack-special 'SniperShot' (Mantis #6881)
    Change-Id: I22a1a8b4cbb0153e407688a189bfb315c6a6164f

--- MMOCoreORB/src/server/zone/objects/creature/commands/SniperShotCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SniperShotCommand.h
@@ -6,6 +6,7 @@
 #define SNIPERSHOTCOMMAND_H_
 
 #include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/managers/combat/CombatManager.h"
 #include "CombatQueueCommand.h"
 
 class SniperShotCommand : public CombatQueueCommand {
@@ -22,12 +23,12 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
-			
+
 		if (!creature->isPlayerCreature())
 			return GENERALERROR;
-		
+
 		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
-		
+
 		if (creature == targetObject || targetObject == NULL || !targetObject->isPlayerCreature())
 			return INVALIDTARGET;
 
@@ -37,30 +38,51 @@ public:
 
 		PlayerManager* playerManager = server->getPlayerManager();
 
+		WeaponObject* weapon = creature->getWeapon();
+
+		int maxRange = weapon->getMaxRange();
+		if (!player->isInRange(creature, maxRange + player->getTemplateRadius() + creature->getTemplateRadius()))
+			return TOOFAR;
+
 		if (!CollisionManager::checkLineOfSight(creature, player)) {
 			creature->sendSystemMessage("@container_error_message:container18"); //You can't see that object.  You may have to move closer to it.
 			return GENERALERROR;
 		}
 
-		WeaponObject* weapon = creature->getWeapon();
-
-		int maxRange = weapon->getMaxRange();
-
-		if(!checkDistance(creature, player, maxRange))
-			return TOOFAR;
-				
-		if (player->isAttackableBy(creature) && (player->isIncapacitated() && player->isFeigningDeath() == false)) {
-			playerManager->killPlayer(creature, player, 1);			
-		}  else if (!player->isIncapacitated() || player->isFeigningDeath()) {
-			creature->sendSystemMessage("@error_message:target_not_incapacitated");  //You cannot perform the death blow. Your target is not incapacitated.
-		}  else if (player->isDead()) {
+		if (player->isDead()) {
+			StringIdChatParameter params("error_message", "prose_target_already_dead");
+			params.setTT(player->getDisplayedName());
+			creature->sendSystemMessage(params); //But %TT is already dead!
 			return GENERALERROR;
 		}
 
- 		return doCombatAction(creature, target);
+		if (player->isAttackableBy(creature) && player->isFeigningDeath() == false) {
 
+			if (player->isIncapacitated()) {
+
+				if (CombatManager::instance()->applySpecialAttackCost(creature, creature->getWeapon(), CreatureAttackData("", this, target))) {
+					CombatManager::instance()->broadcastCombatSpam(creature, player, NULL, 50, "cbt_spam", combatSpam + "_hit", 1);
+					playerManager->killPlayer(creature, player, 1, true);
+					player->updatePostures(false);
+					creature->doCombatAnimation(player, STRING_HASHCODE ("fire_1_special_single_medium_face"), 0x01, 0xFF);
+					return SUCCESS;
+				} else {
+					return INSUFFICIENTHAM;
+				}
+
+			} else {
+				doCombatAction(creature, target);
+				if (player->isIncapacitated()) {
+					playerManager->killPlayer(creature, player, 1, true);
+					return SUCCESS;
+				} else if ((!player->isIncapacitated() || player->isFeigningDeath()) && doCombatAction(creature, target) == SUCCESS) {
+					creature->sendSystemMessage("@error_message:target_not_incapacitated");  //You cannot perform the death blow. Your target is not incapacitated.
+					return GENERALERROR;
+				}
+			}
+		}
+		return SUCCESS;
 	}
-
 };
 
 #endif //SNIPERSHOTCOMMAND_H_
