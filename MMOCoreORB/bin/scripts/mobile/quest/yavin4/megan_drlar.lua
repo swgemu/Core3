336135cba117dd35330485ae22aa8d66b7f4aa62
Nee Shadowfire <nee@swgemu.com>
2016-09-13 06:58:47 +0000

    [added] Megan Dr'lar's quests on YAVIN-4 (Mantis #7182, phase 2)
    Change-Id: I8aa9ccd7dd74eef348ccc5ad93d5565cac3d6dd5

--- MMOCoreORB/bin/scripts/mobile/quest/yavin4/megan_drlar.lua
+++ MMOCoreORB/bin/scripts/mobile/quest/yavin4/megan_drlar.lua
@@ -20,26 +20,17 @@ megan_drlar = Creature:new {
 	milk = 0,
 	tamingChance = 0.000000,
 	ferocity = 0,
-	pvpBitmask = ATTACKABLE,
+	pvpBitmask = NONE,
 	creatureBitmask = NONE,
+	optionsBitmask = AIENABLED + CONVERSABLE,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/megan_drlar.iff"},
-	lootGroups = {
-		{
-			groups = {
-				{group = "junk", chance = 5000000},
-				{group = "rifles", chance = 1000000},
-				{group = "pistols", chance = 1000000},
-				{group = "melee_weapons", chance = 1000000},
-				{group = "carbines", chance = 1000000},
-				{group = "wearables_common", chance = 1000000}
-			}
-		}
-	},
+	lootGroups = {},
 	weapons = {},
 	reactionStf = "@npc_reaction/slang",
-	attacks = brawlermaster
+	conversationTemplate = "megan_drlar_mission_giver_convotemplate",
+	attacks = {}
 }
 
 CreatureTemplates:addCreatureTemplate(megan_drlar, "megan_drlar")
