c944f7f4b744f042116850d8067470d8dc125f88
Nee Shadowfire <nee@swgemu.com>
2016-09-15 20:48:59 +0000

    [added] the 'Jazeen Thurmm' missions to YAVIN-4 (Mantis #7182, phase 1)
    Change-Id: I08a7c7dbee7c45c01219b8ad27dcae325649ada5

---
+++ MMOCoreORB/bin/scripts/mobile/quest/yavin4/survivor_quest_jazeen.lua
@@ -0,0 +1,35 @@
+survivor_quest_jazeen = Creature:new {
+	objectName = "@mob/creature_names:crafting_contractor",
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 4,
+	chanceHit = 0.24,
+	damageMin = 40,
+	damageMax = 45,
+	baseXp = 62,
+	baseHAM = 113,
+	baseHAMmax = 138,
+	armor = 0,
+	resists = {15,15,15,15,15,15,15,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = HERD,
+	optionsBitmask = AIENABLED + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_artisan_trainer_02.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "jazeen_thurmm_mission_target_convotemplate",
+	attacks = merge(brawlermaster, teraskasinovice)
+}
+
+CreatureTemplates:addCreatureTemplate(survivor_quest_jazeen, "survivor_quest_jazeen")
