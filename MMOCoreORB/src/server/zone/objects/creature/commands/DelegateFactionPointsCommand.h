5925926732ad322807d29c0ff55a8bdbdeba6fb2
Nee Shadowfire <nee@swgemu.com>
2016-09-16 00:52:43 +0000

    [fixed] you can no longer /delegate to players with CAPPED faction points
    Change-Id: Ifb17a69acd22adabd505f99550ee60392596eac5

--- MMOCoreORB/src/server/zone/objects/creature/commands/DelegateFactionPointsCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/DelegateFactionPointsCommand.h
@@ -128,13 +128,16 @@ public:
 		int delegateRatioFrom = FactionManager::instance()->getRankDelegateRatioFrom(creature->getFactionRank());
 		int delegateRatioTo = FactionManager::instance()->getRankDelegateRatioTo(creature->getFactionRank());
 
+		uint32 targetsRank = targetCreature->getFactionRank();
+		int targetsCap = FactionManager::instance()->getFactionPointsCap(targetsRank);
+
 		int currentFactionPoints = delegator->getFactionStanding(faction);
+		int targetsCurrentPoints = targetPlayerObject->getFactionStanding(faction);
 
 		if (currentFactionPoints < 200) {
-			StringIdChatParameter param("faction_recruiter", "not_enough_standing_spend");
+			StringIdChatParameter param("faction_recruiter", "not_enough_standing_spend"); //"You do not have enough faction standing to spend. You must maintain at least %DI to remain part of the %TO faction."
 			param.setDI(200);
 			param.setTO(faction);
-
 			creature->sendSystemMessage(param);
 			return GENERALERROR;
 		}
@@ -158,6 +161,9 @@ public:
 		tipAmount = (uint32) tokenizer.getIntToken();
 
 		if (tipAmount == 0)
+			return GENERALERROR;
+		else if (((targetsCurrentPoints + tipAmount) > targetsCap) || (targetsCurrentPoints == targetsCap))
+			creature->sendSystemMessage("That player has reached the faction points limit for their current rank.");
 			return GENERALERROR;
 
 		return doDelegate(creature, targetCreature, tipAmount);
