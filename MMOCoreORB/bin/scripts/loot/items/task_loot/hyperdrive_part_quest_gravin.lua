6bf55d80fd326c5a48d7eab241fbd70eb15998aa
Nee Shadowfire <nee@swgemu.com>
2016-09-23 19:04:17 +0000

    [Added] the 'Gravin Attal' missions to TALUS Imp Outpost (Mantis #7172, phase 4)
    Change-Id: I3a81c6f826967f6ccec70db7b5a47480e6db8dc2

---
+++ MMOCoreORB/bin/scripts/loot/items/task_loot/hyperdrive_part_quest_gravin.lua
@@ -0,0 +1,11 @@
+hyperdrive_part_quest_gravin = {
+	minimumLevel = 0,
+	maximumLevel = 0,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/mission/quest_item/gravin_attal_q2_needed.iff",
+	craftingValues = {},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("hyperdrive_part_quest_gravin", hyperdrive_part_quest_gravin)
