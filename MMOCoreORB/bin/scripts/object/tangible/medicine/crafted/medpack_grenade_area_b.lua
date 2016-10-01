f6d87499fdee9262e34926b1408b6dd01371e622
Nee Shadowfire <nee@swgemu.com>
2016-10-01 04:00:29 +0000

    [fixed] some healing stimpacks that weren't included in a recent related change
    Change-Id: Id42ab6c73a207c4bb8eedb3527bd330b2f0d5b8a

--- MMOCoreORB/bin/scripts/object/tangible/medicine/crafted/medpack_grenade_area_b.lua
+++ MMOCoreORB/bin/scripts/object/tangible/medicine/crafted/medpack_grenade_area_b.lua
@@ -49,8 +49,9 @@ object_tangible_medicine_crafted_medpack_grenade_area_b = object_tangible_medici
 	rangeMod = 0.4,
 	area = 10,
 	range = 10,
-	effectiveness = 100,
+	effectiveness = 200,
 	medicineClass = STIM_B,
+	attributes = {0, 3},
 
 	numberExperimentalProperties = {1, 1, 2, 2, 2, 2, 2, 1},
 	experimentalProperties = {"XX", "XX", "OQ", "PE", "OQ", "UT", "OQ", "PE", "CD", "OQ", "CD", "OQ", "XX"},
