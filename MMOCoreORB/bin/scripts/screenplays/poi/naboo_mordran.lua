96eb1f757abd290a98b7f7a7e337327f497f9b9b
Nee Shadowfire <nee@swgemu.com>
2016-10-19 03:39:10 +0000

    [modified] a few spawn points for perimeter npcs at the Mordran POI (Mantis #7228)
    Change-Id: I9f7053667123a1e198a2dca96f61784a2bc7373b

--- MMOCoreORB/bin/scripts/screenplays/poi/naboo_mordran.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/naboo_mordran.lua
@@ -17,13 +17,17 @@ end
 function NabooMordranScreenPlay:spawnMobiles()
 
 		--mordran respawns between 15-20 minutes
-	spawnMobile("naboo", "mordran", 1200, -1969, 6.4, -5295, 45, 0)
+	local pNpc = spawnMobile("naboo", "mordran", 1200, -1969, 6.4, -5295, 45, 0)
 	spawnMobile("naboo", "outcast_gungan", 300, getRandomNumber(32) + -1985, 6.4, getRandomNumber(12) + -5302, 45, 0)
 	spawnMobile("naboo", "outcast_gungan", 300, getRandomNumber(32) + -1985, 6.4, getRandomNumber(12) + -5302, 45, 0)
 	spawnMobile("naboo", "outcast_gungan", 300, getRandomNumber(32) + -1985, 6.4, getRandomNumber(12) + -5302, 45, 0)
-	spawnMobile("naboo", "gungan_reject", 360, getRandomNumber(8) + -1971, 6.4, getRandomNumber(12) + -5302, 45, 0)
-	spawnMobile("naboo", "gungan_reject", 360, getRandomNumber(8) + -1971, 6.4, getRandomNumber(12) + -5302, 45, 0)
-	spawnMobile("naboo", "exiled_gungan", 360, getRandomNumber(8) + -1971, 6.4, getRandomNumber(12) + -5302, 45, 0)
-	spawnMobile("naboo", "exiled_gungan", 360, getRandomNumber(8) + -1971, 6.4, getRandomNumber(12) + -5302, 45, 0)
-	
+	pNpc = spawnMobile("naboo", "gungan_reject", 360, getRandomNumber(8) + -1974, 6.4, getRandomNumber(12) + -5302, 45, 0)
+	AiAgent(pNpc):setAiTemplate("idlewander")
+	pNpc = spawnMobile("naboo", "gungan_reject", 360, getRandomNumber(8) + -1974, 6.4, getRandomNumber(12) + -5302, 45, 0)
+	AiAgent(pNpc):setAiTemplate("idlewander")
+	pNpc = spawnMobile("naboo", "exiled_gungan", 360, getRandomNumber(8) + -1974, 6.4, getRandomNumber(12) + -5302, 45, 0)
+	AiAgent(pNpc):setAiTemplate("idlewander")
+	pNpc = spawnMobile("naboo", "exiled_gungan", 360, getRandomNumber(8) + -1974, 6.4, getRandomNumber(12) + -5302, 45, 0)
+	AiAgent(pNpc):setAiTemplate("idlewander")
+
 end
