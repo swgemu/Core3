b0e682a60bf87d032199232ff3b7ace618e0b3c0
Miztah <miztah@swgemu.com>
2016-11-01 23:20:28 -0400

    [changed] Village raiders should now move towards and into the village after spawning, initiating combat with potential targets on the way [fixed] Not being able to turn in Sivarra's phase 4 healing quest [fixed] Thugs counting towards Sivarra's quest when they shouldnt [fixed] Village turrets should not cycle targets until their current one is dead, allowing some raiders to make it into the village [fixed] The med droid next to Sivarra not giving a status on her quest in phase 4 [changed] Spawned raiders will now despawn after 20 minutes if not killed
    Change-Id: I5ab4bd67f266a0d53ca5be85434ba5ce9c6ae8af

--- MMOCoreORB/bin/scripts/ai/actions/gettarget.lua
+++ MMOCoreORB/bin/scripts/ai/actions/gettarget.lua
@@ -4,10 +4,10 @@ function GetTargetCreaturePet:doAction(pAgent)
 	if (pAgent ~= nil) then
 		--print("1")
 		local agent = AiAgent(pAgent)
-		
+
 		local command = agent:getLastCommand()
 		if (command ~= PET_ATTACK and command ~= PET_GUARD and command ~= PET_SPECIAL_ATTACK1 and command ~= PET_SPECIAL_ATTACK2 and command ~= PET_PATROL) then
-			return BEHAVIOR_FAILURE	
+			return BEHAVIOR_FAILURE
 		end
 
 		if (command == PET_GUARD) then
@@ -86,10 +86,10 @@ function GetTargetDroidPet:doAction(pAgent)
 	if (pAgent ~= nil) then
 		--print("1")
 		local agent = AiAgent(pAgent)
-		
+
 		local command = agent:getLastCommand()
 		if (command ~= PET_ATTACK and command ~= PET_GUARD and command ~= PET_PATROL ) then
-			return BEHAVIOR_FAILURE	
+			return BEHAVIOR_FAILURE
 		end
 
 		if (command == PET_GUARD) then
@@ -168,10 +168,10 @@ function GetTargetFactionPet:doAction(pAgent)
 	if (pAgent ~= nil) then
 		--print("1")
 		local agent = AiAgent(pAgent)
-		
+
 		local command = agent:getLastCommand()
 		if (command ~= PET_ATTACK and command ~= PET_GUARD and command ~= PET_PATROL) then
-			return BEHAVIOR_FAILURE	
+			return BEHAVIOR_FAILURE
 		end
 
 		if (command == PET_GUARD) then
@@ -243,4 +243,44 @@ function GetTargetFactionPet:terminate(pAgent)
 		if agent:getBehaviorStatus() == BEHAVIOR_FAILURE and agent:getFollowState() ~= PATROLLING then agent:restoreFollowObject() end
 	end
 	return 0
+end
+
+function GetTargetVillageRaider:doAction(pAgent)
+	if (pAgent ~= nil) then
+		local agent = AiAgent(pAgent)
+		local creature = CreatureObject(pAgent)
+
+		local pTarget = agent:getTargetFromMap()
+
+		if pTarget ~= nil and pTarget ~= agent:getFollowObject() then
+			if agent:validateTarget(pTarget) then
+				agent:setFollowObject(pTarget)
+				agent:setDefender(pTarget)
+				return BEHAVIOR_SUCCESS
+			end
+		elseif pTarget ~= nil and agent:validateFollow() then
+			agent:setDefender(pTarget)
+			return BEHAVIOR_SUCCESS
+		end
+
+		pTarget = agent:getTargetFromDefenders()
+
+		if pTarget ~= nil and pTarget ~= agent:getFollowObject() then
+			if agent:validateTarget(pTarget) then
+				agent:setFollowObject(pTarget)
+				agent:setDefender(pTarget)
+				return BEHAVIOR_SUCCESS
+			end
+		elseif pTarget ~= nil and agent:validateFollow() then
+			agent:setDefender(pTarget)
+			return BEHAVIOR_SUCCESS
+		end
+
+		if (agent:isInCombat()) then
+			agent:clearCombatState(true)
+			agent:setOblivious()
+		end
+	end
+
+	return BEHAVIOR_FAILURE
 end
