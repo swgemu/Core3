629eea770e59588715194eb74dcd9797043c391b
Loshult <loshult@swgemu.com>
2016-11-08 14:24:48 +0000

    [Added] Correct contraband scan greeting message if a rebel initiates the scan. [Modified] Each NPC that scans for contraband now only scans the player 12.5% of the time. [Added] Dismount of player at start of contraband scan and notification through system message about it. [Added] Contraband scans are now not initiated if the scanner or the player is dead, feign death, incapacitated or in combat.
    Change-Id: I1169f5a6f1e4414f994ba0fe06f03b9ba54cbb0e

--- MMOCoreORB/src/server/zone/managers/gcw/tasks/ContrabandScanTask.h
+++ MMOCoreORB/src/server/zone/managers/gcw/tasks/ContrabandScanTask.h
@@ -12,20 +12,72 @@
 #include "server/zone/objects/creature/ai/AiAgent.h"
 
 class ContrabandScanTask : public Task {
+	const unsigned int SCANINITIATECHANCE = 8; // 1/8 chance to initiate scan - 12.5 % chance.
+	const unsigned int INITIATEDELAY = 500; // 0.5 s delay.
+	const unsigned int PLAYERSCANCOOLDOWN = 30 * 1000; // 30 s cooldown
+
 	ManagedWeakReference<AiAgent*> weakScanner;
 	ManagedWeakReference<CreatureObject*> weakPlayer;
+
+	enum ScanState {
+		SCANCHANCE,
+		INITIATESCAN
+	};
+
+	ScanState scanState;
+
+	String getFactionStringId(AiAgent* scanner, const String& imperial, const String& rebel) {
+		const String stringId = "@imperial_presence/contraband_search:";
+		if (scanner->getFaction() == Factions::FACTIONIMPERIAL) {
+			return stringId + imperial;
+		} else {
+			return stringId + rebel;
+		}
+	}
+
+	bool scanPrerequisitesMet(AiAgent* scanner, CreatureObject* player) {
+		return scanner != NULL && player != NULL && !scanner->isDead() && !player->isDead() && !player->isFeigningDeath()
+				&& !player->isIncapacitated() && !scanner->isInCombat() && !player->isInCombat();
+	}
+
+	void scanChance(CreatureObject* player) {
+		if (System::random(SCANINITIATECHANCE) >= SCANINITIATECHANCE - 1) {
+			this->reschedule(INITIATEDELAY);
+			scanState = INITIATESCAN;
+			player->updateCooldownTimer("crackdown_scan", PLAYERSCANCOOLDOWN);
+		}
+	}
+
+	void initiateScan(Zone* zone, AiAgent* scanner, CreatureObject* player) {
+		StringIdChatParameter dismoutFaction;
+		dismoutFaction.setStringId(getFactionStringId(scanner, "dismount_imperial", "dismount_rebel"));
+		player->sendSystemMessage(dismoutFaction);
+
+		if (player->isRidingMount()) {
+			player->dismount();
+
+			StringIdChatParameter dismount;
+			dismount.setStringId("@imperial_presence/contraband_search:dismount");
+			player->sendSystemMessage(dismount);
+		}
+
+		StringIdChatParameter chat;
+		chat.setStringId(getFactionStringId(scanner, "scan_greeting_imperial", "scan_greeting_rebel"));
+		zone->getZoneServer()->getChatManager()->broadcastChatMessage(scanner, chat, player->getObjectID(), 0, 0);
+	}
 
 public:
 	ContrabandScanTask(AiAgent* scanner, CreatureObject* player){
 		weakScanner = scanner;
 		weakPlayer = player;
+		scanState = SCANCHANCE;
 	}
 
 	void run() {
 		ManagedReference<AiAgent*> scanner = weakScanner.get();
 		ManagedReference<CreatureObject*> player = weakPlayer.get();
 
-		if (scanner == NULL || player == NULL){
+		if (!scanPrerequisitesMet(scanner, player)){
 			return;
 		}
 
@@ -38,13 +90,16 @@ public:
 			return;
 		}
 
-		StringIdChatParameter chat;
-		chat.setStringId("@imperial_presence/contraband_search:scan_greeting_imperial");
-		zone->getZoneServer()->getChatManager()->broadcastChatMessage(scanner, chat, player->getObjectID(), 0, 0);
-
-		StringIdChatParameter systemMessage;
-		systemMessage.setStringId("@imperial_presence/contraband_search:contraband_scan_imperial");
-		player->sendSystemMessage(systemMessage);
+		switch (scanState) {
+		case SCANCHANCE:
+			scanChance(player);
+			break;
+		case INITIATESCAN:
+			initiateScan(zone, scanner, player);
+			break;
+		default:
+			break;
+		}
 	}
 };
 
