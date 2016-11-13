e6f4f4d6ae3c40b844c6c959fc165eaa8cd256ec
Miztah <miztah@swgemu.com>
2016-11-13 00:36:16 -0500

    [fixed] Destroying a player's force crystal prior to reaching the village should now reset the player back to the old man stage - mantis 7165 [fixed] Dageerin phase 3 not properly failing when the player logs in after a phase change
    Change-Id: I09aa2be6b0ccb7fe6fd3e95bdc5dd3c5cc880941

--- MMOCoreORB/bin/scripts/managers/jedi/village_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/village_jedi_manager.lua
@@ -18,102 +18,107 @@ VillageJediManager = JediManager:new {
 -- Handling of the useItem event.
 -- @param pSceneObject pointer to the item object.
 -- @param itemType the type of item that is used.
--- @param pCreatureObject pointer to the creature object that used the item.
-function VillageJediManager:useItem(pSceneObject, itemType, pCreatureObject)
-	if (pSceneObject == nil or pCreatureObject == nil) then
+-- @param pPlayer pointer to the creature object that used the item.
+function VillageJediManager:useItem(pSceneObject, itemType, pPlayer)
+	if (pSceneObject == nil or pPlayer == nil) then
 		return
 	end
 
 	Logger:log("useItem called with item type " .. itemType, LT_INFO)
 	if itemType == ITEMHOLOCRON then
-		VillageJediManagerHolocron.useHolocron(pSceneObject, pCreatureObject)
+		VillageJediManagerHolocron.useHolocron(pSceneObject, pPlayer)
 	end
 	if itemType == ITEMWAYPOINTDATAPAD then
-		SithShadowEncounter:useWaypointDatapad(pSceneObject, pCreatureObject)
+		SithShadowEncounter:useWaypointDatapad(pSceneObject, pPlayer)
 	end
 	if itemType == ITEMTHEATERDATAPAD then
-		SithShadowIntroTheater:useTheaterDatapad(pSceneObject, pCreatureObject)
+		SithShadowIntroTheater:useTheaterDatapad(pSceneObject, pPlayer)
 	end
 end
 
 -- Handling of the checkForceStatus command.
--- @param pCreatureObject pointer to the creature object of the player who performed the command
-function VillageJediManager:checkForceStatusCommand(pCreatureObject)
-	if (pCreatureObject == nil) then
+-- @param pPlayer pointer to the creature object of the player who performed the command
+function VillageJediManager:checkForceStatusCommand(pPlayer)
+	if (pPlayer == nil) then
 		return
 	end
 
-	Glowing:checkForceStatusCommand(pCreatureObject)
+	Glowing:checkForceStatusCommand(pPlayer)
 end
 
 -- Handling of the onPlayerLoggedIn event. The progression of the player will be checked and observers will be registered.
--- @param pCreatureObject pointer to the creature object of the player who logged in.
-function VillageJediManager:onPlayerLoggedIn(pCreatureObject)
-	if (pCreatureObject == nil) then
+-- @param pPlayer pointer to the creature object of the player who logged in.
+function VillageJediManager:onPlayerLoggedIn(pPlayer)
+	if (pPlayer == nil) then
 		return
 	end
 
-	Glowing:onPlayerLoggedIn(pCreatureObject)
+	Glowing:onPlayerLoggedIn(pPlayer)
 
-	if (FsIntro:isOnIntro(pCreatureObject)) then
-		FsIntro:onLoggedIn(pCreatureObject)
+	if (FsIntro:isOnIntro(pPlayer)) then
+		FsIntro:onLoggedIn(pPlayer)
 	end
 
-	if (FsOutro:isOnOutro(pCreatureObject)) then
-		FsOutro:onLoggedIn(pCreatureObject)
+	if (FsOutro:isOnOutro(pPlayer)) then
+		FsOutro:onLoggedIn(pPlayer)
 	end
 
-	if (QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN) and not QuestManager.hasCompletedQuest(pCreatureObject, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN) and not VillageCommunityCrafting:isOnActiveCrafterList(pCreatureObject)) then
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN)
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_01)
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_02)
+	if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN) and not QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN) and not VillageCommunityCrafting:isOnActiveCrafterList(pPlayer)) then
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_MAIN)
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_01)
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_2_CRAFT_DEFENSES_02)
 	end
 
-	if (QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN) and not QuestManager.hasCompletedQuest(pCreatureObject, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN) and not VillageCommunityCrafting:isOnActiveCrafterList(pCreatureObject)) then
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN)
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_01)
-		QuestManager.resetQuest(pCreatureObject, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_02)
+	if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN) and not QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN) and not VillageCommunityCrafting:isOnActiveCrafterList(pPlayer)) then
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_MAIN)
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_01)
+		QuestManager.resetQuest(pPlayer, QuestManager.quests.FS_PHASE_3_CRAFT_SHIELDS_02)
 	end
 
-	if (not VillageCommunityCrafting:isOnActiveCrafterList(pCreatureObject)) then
-		VillageCommunityCrafting:removeSchematics(pCreatureObject, 2)
-		VillageCommunityCrafting:removeSchematics(pCreatureObject, 3)
+	if (not VillageCommunityCrafting:isOnActiveCrafterList(pPlayer)) then
+		VillageCommunityCrafting:removeSchematics(pPlayer, 2)
+		VillageCommunityCrafting:removeSchematics(pPlayer, 3)
 	end
 
 	-- Any quests below are run from township because they are not a standard task
 	local currentPhase = VillageJediManagerTownship:getCurrentPhase()
 	if (currentPhase ~= 1) then
-		if (QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) or
-			QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) or
-			QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)) then
-			FsMedicPuzzle:doPhaseChange(pCreatureObject)
+		if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) or
+			QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) or
+			QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)) then
+			FsMedicPuzzle:doPhaseChange(pPlayer)
 		end
-		if (QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_CRAFT_PUZZLE_QUEST_00) and not
-			QuestManager.hasCompletedQuest(pCreatureObject, QuestManager.quests.FS_CRAFT_PUZZLE_QUEST_00)) then
-			FsCrafting1:doPhaseChangeFail(pCreatureObject)
+		if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_CRAFT_PUZZLE_QUEST_00) and not
+			QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_CRAFT_PUZZLE_QUEST_00)) then
+			FsCrafting1:doPhaseChangeFail(pPlayer)
 		end
 	end
 	if (currentPhase ~= 2) then
-		if (QuestManager.hasActiveQuest(pCreatureObject, QuestManager.quests.FS_QUESTS_SAD_TASKS)) then
-			FsSad:doPhaseChangeFail(pCreatureObject)
+		if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_QUESTS_SAD_TASKS)) then
+			FsSad:doPhaseChangeFail(pPlayer)
+		end
+	end
+	if (currentPhase ~= 3) then
+		if (QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_QUESTS_SAD2_TASKS)) then
+			FsSad2:doPhaseChangeFail(pPlayer)
 		end
 	end
 	if (currentPhase ~= 4) then
-		FsVillageDefense:doPhaseChangeFail(pCreatureObject)
+		FsVillageDefense:doPhaseChangeFail(pPlayer)
 	end
 end
 
-function VillageJediManager:onPlayerLoggedOut(pCreatureObject)
-	if (pCreatureObject == nil) then
+function VillageJediManager:onPlayerLoggedOut(pPlayer)
+	if (pPlayer == nil) then
 		return
 	end
 
-	if (FsIntro:isOnIntro(pCreatureObject)) then
-		FsIntro:onLoggedOut(pCreatureObject)
+	if (FsIntro:isOnIntro(pPlayer)) then
+		FsIntro:onLoggedOut(pPlayer)
 	end
 
-	if (FsOutro:isOnOutro(pCreatureObject)) then
-		FsOutro:onLoggedOut(pCreatureObject)
+	if (FsOutro:isOnOutro(pPlayer)) then
+		FsOutro:onLoggedOut(pPlayer)
 	end
 end
 
@@ -158,20 +163,20 @@ function VillageJediManager:canSurrenderSkill(pPlayer, skillName)
 end
 
 -- Handling of the onFSTreesCompleted event.
--- @param pCreatureObject pointer to the creature object of the player
-function VillageJediManager:onFSTreeCompleted(pCreatureObject, branch)
-	if (pCreatureObject == nil) then
+-- @param pPlayer pointer to the creature object of the player
+function VillageJediManager:onFSTreeCompleted(pPlayer, branch)
+	if (pPlayer == nil) then
 		return
 	end
 
 	-- Remove the "_04" from the end of the skill...
 	local branchSub = string.sub(branch, 0, (string.len(branch) - 3))
 
-	CreatureObject(pCreatureObject):setScreenPlayState(4, "VillageUnlockScreenPlay:" .. branchSub)
+	CreatureObject(pPlayer):setScreenPlayState(4, "VillageUnlockScreenPlay:" .. branchSub)
 
-	if (ExperienceConverter:getMasteredBranches(pCreatureObject) >= NUMBEROFTREESTOMASTER) then
-		VillageJediManagerCommon.setJediProgressionScreenPlayState(pCreatureObject, VILLAGE_JEDI_PROGRESSION_COMPLETED_VILLAGE)
-		FsOutro:startOldMan(pCreatureObject)
+	if (ExperienceConverter:getMasteredBranches(pPlayer) >= NUMBEROFTREESTOMASTER) then
+		VillageJediManagerCommon.setJediProgressionScreenPlayState(pPlayer, VILLAGE_JEDI_PROGRESSION_COMPLETED_VILLAGE)
+		FsOutro:startOldMan(pPlayer)
 	end
 end
 
