687a47c8769c45bf427016b2d41db7b26701dfc7
Nee Shadowfire <nee@swgemu.com>
2016-11-18 01:03:07 +0000

    [added] the mission for 'Joz Jodhul' on CORELLIA (Mantis #7209, phase 4)
    Change-Id: I6f8cc422ac4b6d0b7fc1f68abdf51cf70a682b8d

---
+++ MMOCoreORB/bin/scripts/mobile/quest/corellia/rat_quest_joz.lua
@@ -0,0 +1,36 @@
+rat_quest_joz = Creature:new {
+	objectName = "@mob/creature_names:scavenger_rat",
+	socialGroup = "self",
+	faction = "",
+	level = 8,
+	chanceHit = 0.27,
+	damageMin = 70,
+	damageMax = 75,
+	baseXp = 187,
+	baseHAM = 405,
+	baseHAMmax = 495,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "meat_wild",
+	meatAmount = 4,
+	hideType = "hide_leathery",
+	hideAmount = 3,
+	boneType = "bone_mammal",
+	boneAmount = 2,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	optionsBitmask = AIENABLED,
+	diet = CARNIVORE,
+
+	templates = {"object/mobile/womp_rat.iff"},
+	scale = 1.25,
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(rat_quest_joz, "rat_quest_joz")
