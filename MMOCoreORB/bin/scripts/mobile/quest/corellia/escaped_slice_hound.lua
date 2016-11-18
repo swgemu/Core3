a9d400225ff1315f93d93eb54cd7f32b42983b8f
Nee Shadowfire <nee@swgemu.com>
2016-11-18 06:33:36 +0000

    [added] NPC 'Gilkur Budz' remaining 2 missions on Corellia
    Change-Id: I55f1fe184faf25d1541c1cf7e327aab6c37237e3

---
+++ MMOCoreORB/bin/scripts/mobile/quest/corellia/escaped_slice_hound.lua
@@ -0,0 +1,38 @@
+escaped_slice_hound = Creature:new {
+	objectName = "@mob/creature_names:slice_hound",
+	socialGroup = "slice_hound",
+	faction = "",
+	level = 9,
+	chanceHit = 0.27,
+	damageMin = 120,
+	damageMax = 130,
+	baseXp = 292,
+	baseHAM = 405,
+	baseHAMmax = 495,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "meat_carnivore",
+	meatAmount = 65,
+	hideType = "hide_bristley",
+	hideAmount = 35,
+	boneType = "bone_mammal",
+	boneAmount = 30,
+	milk = 0,
+	tamingChance = 0.25,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE + AGGRESSIVE + ENEMY,
+	creatureBitmask = PACK + HERD,
+	optionsBitmask = AIENABLED,
+	diet = CARNIVORE,
+
+	templates = {"object/mobile/corellian_slice_hound.iff"},
+	controlDeviceTemplate = "object/intangible/pet/corellian_slice_hound_hue.iff",
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"intimidationattack",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(escaped_slice_hound, "escaped_slice_hound")
