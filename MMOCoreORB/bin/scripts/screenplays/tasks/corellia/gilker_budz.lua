ec7ab4afdcf2c89dd5a0ec1f1cff1f924f7a7382
Nee Shadowfire <nee@swgemu.com>
2016-11-18 06:33:36 +0000

    [added] NPC 'Gilkur Budz' remaining 2 missions on Corellia
    Change-Id: I55f1fe184faf25d1541c1cf7e327aab6c37237e3

--- MMOCoreORB/bin/scripts/screenplays/tasks/corellia/gilker_budz.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/corellia/gilker_budz.lua
@@ -1,43 +1,108 @@
-gilker_budz_missions =
+poacherHideout =
 	{
+		type = "destructible",
+		buildingTemplate = "object/building/military/pirate_warehouse.iff",
+		terminal = { template = "object/tangible/terminal/terminal_destructible_building.iff", vectorCellID = 3, x = 0, z = 0.3, y = -6.0 },
+		childNpcs =
 		{
-			missionType = "retrieve",
-			primarySpawns =
-			{
-				{ npcTemplate = "sneeroos", npcName = "Snee-Roos" }
-			},
-			secondarySpawns =	
-			{
-				{ npcTemplate = "thug", npcName = "a thug" }
-			},
-			itemSpawns =
-			{
-				{ itemTemplate = "object/tangible/mission/quest_item/gilker_budz_q1_needed.iff", itemName = "Datadisc" }
-			},
-			rewards =
-			{
-				{ rewardType = "credits", amount = 250 }
-			}
+			{ npcTemplate = "poacher", npcName = "Poacher Leader", vectorCellID = 3, x = 0, z = 0.3, y = -3.0 }
 		}
 	}
 
-npcMapGilkerBudz =
+gilker_budz_missions =
+{
 	{
+		missionType = "retrieve",
+		primarySpawns =
 		{
-			spawnData = { npcTemplate = "gilker_budz", x = -3336.21, z = 72.4231, y = 2926.19, direction = 277.144, cellID = 0, position = STAND },
-			worldPosition = { x = -3336, y = 2926 },
-			npcNumber = 1,
-			stfFile = "@static_npc/corellia/gilker_budz",
-			missions = gilker_budz_missions
+			{ npcTemplate = "sneeroos", npcName = "Snee-Roos" }
+		},
+		secondarySpawns =
+		{
+			{ npcTemplate = "thug", npcName = "a thug" }
+		},
+		itemSpawns =
+		{
+			{ itemTemplate = "object/tangible/mission/quest_item/gilker_budz_q1_needed.iff", itemName = "Datadisc" }
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 250 }
+		}
+	},
+	{
+		missionType = "destroy",
+		buildingSpawn = poacherHideout,
+		primarySpawns =
+		{
+			{ npcTemplate = "poacher", npcName = "a Poacher pelt-skinner" }
+		},
+		secondarySpawns =
+		{
+			{ npcTemplate = "poacher", npcName = "" },
+			{ npcTemplate = "poacher", npcName = "" },
+			{ npcTemplate = "poacher", npcName = "" },
+			{ npcTemplate = "poacher", npcName = "" },
+			{ npcTemplate = "poacher", npcName = "" }
+		},
+		staticObjects =
+			{
+				{ objectTemplate = "object/tangible/furniture/decorative/foodcart.iff", objectName = "Gronda Meat Storage" },
+				{ objectTemplate = "object/tangible/furniture/decorative/tanning_hide_s01.iff", objectName = "Gronda Hide" },
+				{ objectTemplate = "object/tangible/furniture/decorative/tanning_hide_s02.iff", objectName = "Gronda Pelt" },
+				{ objectTemplate = "object/static/structure/general/streetlamp_small_style_02_on.iff", objectName = "" }
+			},
+		itemSpawns =
+		{
+
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 500 }
+		}
+	},
+	{
+		missionType = "assassinate",
+		primarySpawns =
+		{
+			{ npcTemplate = "grassland_slice_hound", npcName = "a released slice hound" }
+		},
+		secondarySpawns =
+		{
+			{ npcTemplate = "vicious_slice_hound", npcName = "a vicious slice hound" },
+			{ npcTemplate = "vicious_slice_hound", npcName = "a vicious slice hound" },
+			{ npcTemplate = "vicious_slice_hound", npcName = "a vicious slice hound" },
+			{ npcTemplate = "vicious_slice_hound", npcName = "a vicious slice hound" },
+			{ npcTemplate = "vicious_slice_hound", npcName = "a vicious slice hound" }
+		},
+		itemSpawns =
+		{
+
+		},
+		rewards =
+		{
+			{ rewardType = "credits", amount = 750 }
 		}
 	}
+}
+
+npcMapGilkerBudz =
+{
+	{
+		spawnData = { npcTemplate = "gilker_budz", x = -3336.21, z = 72.4231, y = 2926.19, direction = 277.144, cellID = 0, position = STAND },
+		worldPosition = { x = -3336, y = 2926 },
+		npcNumber = 1,
+		stfFile = "@static_npc/corellia/gilker_budz",
+		missions = gilker_budz_missions
+	}
+}
 
 GilkerBudz = ThemeParkLogic:new {
 	npcMap = npcMapGilkerBudz,
 	className = "GilkerBudz",
 	screenPlayState = "gilker_budz_quest",
 	planetName = "corellia",
-	distance = 800
+	distance = 1100
 }
 
 registerScreenPlay("GilkerBudz", true)
