4e33f9f507b224d86f1645391e913e039a9ecd56
Nee Shadowfire <nee@swgemu.com>
2016-11-19 02:47:13 +0000

    [Fixed] correct template for Village outro 'Mellichae' cyber arm  rare loot drop
    Change-Id: Ia43184d21851217a8291ecf2ac02117486bbc2d8

---
+++ MMOCoreORB/bin/scripts/loot/items/mellichae_cybernetic_arm.lua
@@ -0,0 +1,12 @@
+mellichae_cybernetic_arm = {
+    minimumLevel = 0,
+    maximumLevel = -1,
+    customObjectName = "",
+    directObjectTemplate = "object/tangible/item/quest/force_sensitive/fs_mellichae_cyborg_arm.iff",
+    craftingValues = {},
+    skillMods = {},
+    customizationStringNames = {},
+    customizationValues = {}
+}
+
+addLootItemTemplate("mellichae_cybernetic_arm", mellichae_cybernetic_arm)
