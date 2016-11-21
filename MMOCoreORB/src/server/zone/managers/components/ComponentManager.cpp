d0299c80bd870eea09f34d43b1b21075a000289b
ValkyraSWG <valkyra@cox.net>
2016-11-20 22:31:27 -0500

    [wip] Jedi Padawan Trials and Force Shrine menu conversion.
    Change-Id: Ia837c1e08e92f7ca99ff107981272bbd67c7d0d9

--- MMOCoreORB/src/server/zone/managers/components/ComponentManager.cpp
+++ MMOCoreORB/src/server/zone/managers/components/ComponentManager.cpp
@@ -51,7 +51,6 @@
 #include "server/zone/objects/tangible/components/HolocronMenuComponent.h"
 #include "server/zone/objects/tangible/components/WaypointDatapadMenuComponent.h"
 #include "server/zone/objects/tangible/components/ForceCrystalMenuComponent.h"
-#include "server/zone/objects/tangible/components/ForceShrineMenuComponent.h"
 #include "server/zone/objects/tangible/components/RobeObjectMenuComponent.h"
 #include "server/zone/objects/tangible/components/generic/ArtCrateMenuComponent.h"
 #include "server/zone/objects/tangible/components/WeaponObjectMenuComponent.h"
@@ -268,7 +267,6 @@ ComponentManager::ComponentManager() {
 	components.put("LightsaberObjectMenuComponent", new LightsaberObjectMenuComponent());
 	components.put("RobeObjectMenuComponent", new RobeObjectMenuComponent());
 	components.put("ForceCrystalMenuComponent", new ForceCrystalMenuComponent());
-	components.put("ForceShrineMenuComponent", new ForceShrineMenuComponent());
 	components.put("SaberInventoryContainerComponent", new SaberInventoryContainerComponent());
 
 	components.put("VehicleCustomKitObjectMenuComponent", new VehicleCustomKitObjectMenuComponent());
