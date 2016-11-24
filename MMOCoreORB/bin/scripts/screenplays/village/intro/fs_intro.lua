a1644ec98ccc13ebedb901f3c3d0934fd2251956
Ivojedi <ivojedi@swgemu.com>
2016-11-24 05:13:57 -0800

    [Fixed] stability issue
    Change-Id: Idb7cdcc8752e1f6437d94e109c59e380d0b64417

--- MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
@@ -62,16 +62,18 @@ function FsIntro:startStepDelay(pPlayer, step)
 	if (step == 1) then
 		local oldManVisits = tonumber(readScreenPlayData(pPlayer, "VillageJediProgression", "FsIntroOldManVisits"))
 
-		if (oldManVisits == 2) then
-			stepDelay = stepDelay + (2 * 24 * 60 * 60 * 1000)
-		elseif (oldManVisits == 3) then
-			stepDelay = stepDelay + (7 * 24 * 60 * 60 * 1000)
-		elseif (oldManVisits == 4) then
-			stepDelay = stepDelay + (14 * 24 * 60 * 60 * 1000)
-		elseif (oldManVisits == 5) then
-			stepDelay = stepDelay + (30 * 24 * 60 * 60 * 1000)
-		elseif (oldManVisits >= 6) then
-			stepDelay = stepDelay + (60 * 24 * 60 * 60 * 1000)
+		if oldManVisits ~= nil then
+			if (oldManVisits == 2) then
+				stepDelay = stepDelay + (2 * 24 * 60 * 60 * 1000)
+			elseif (oldManVisits == 3) then
+				stepDelay = stepDelay + (7 * 24 * 60 * 60 * 1000)
+			elseif (oldManVisits == 4) then
+				stepDelay = stepDelay + (14 * 24 * 60 * 60 * 1000)
+			elseif (oldManVisits == 5) then
+				stepDelay = stepDelay + (30 * 24 * 60 * 60 * 1000)
+			elseif (oldManVisits >= 6) then
+				stepDelay = stepDelay + (60 * 24 * 60 * 60 * 1000)
+			end
 		end
 	end
 
