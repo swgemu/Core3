7c30b12caa52b82e10a688bf3fe6f56befe3bd8a
Miztah <miztah@swgemu.com>
2016-12-14 00:27:21 -0500

    [fixed] Typo in junk dealer code
    Change-Id: I3ea0248d6ca03323908595ceac2c21943219bcdb

--- MMOCoreORB/bin/scripts/screenplays/junk_dealer/junk_dealer.lua
+++ MMOCoreORB/bin/scripts/screenplays/junk_dealer/junk_dealer.lua
@@ -62,7 +62,7 @@ function JunkDealer:getEligibleJunk(pPlayer, dealerType)
 		if pItem ~= nil then
 			local tano = TangibleObject(pItem)
 			local sceno = SceneObject(pItem)
-			if tano:getJunkDealerNeeded() & dealerNum > 0 and tano:getCraftersName() == "" and not tano:isBroken() and not tano:isSliced() and not tano:isNoTrade() and sceno:getContainerObjectsSize() == 0 then
+			if tano:getJunkDealerNeeded() and dealerNum > 0 and tano:getCraftersName() == "" and not tano:isBroken() and not tano:isSliced() and not tano:isNoTrade() and sceno:getContainerObjectsSize() == 0 then
 				local name = sceno:getDisplayedName()
 				local value = tano:getJunkValue()
 				local textTable = {"[" .. value .. "] " .. name, sceno:getObjectID()}
