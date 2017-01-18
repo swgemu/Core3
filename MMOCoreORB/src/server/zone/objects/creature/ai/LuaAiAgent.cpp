eefafd5562bb49c07817202651e4c47ff85c5d2c
Victor Popovici <theanswer@swgemu.com>
2017-01-18 01:29:25 +0000

    Insert the description of the change.
    Change-Id: I349910f546dee2ecbed65e4db3769db459ad72cb

--- MMOCoreORB/src/server/zone/objects/creature/ai/LuaAiAgent.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/ai/LuaAiAgent.cpp
@@ -338,6 +338,8 @@ int LuaAiAgent::setLevel(lua_State* L) {
 int LuaAiAgent::setWait(lua_State* L) {
 	float seconds = lua_tonumber(L, -1);
 
+  	Locker locker(realObject);
+  
 	realObject->setWait((int)(seconds*1000));
 
 	return 0;
