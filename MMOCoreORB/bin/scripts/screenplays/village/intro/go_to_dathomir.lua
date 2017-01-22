3cc3627133ce239963e22845a5423e999189c1d3
Miztah <miztah@swgemu.com>
2017-01-21 22:43:13 -0500

    [fixed] Players not being able to converse with Paemos after completing the force sensitive intro quests
    Change-Id: Ib0ceafa7c05d3a2c5a3d42978f789483b7a13837

--- MMOCoreORB/bin/scripts/screenplays/village/intro/go_to_dathomir.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/intro/go_to_dathomir.lua
@@ -42,6 +42,7 @@ function GoToDathomir:onSuccessfulSpawn(pPlayer)
 	end
 
 	QuestManager.activateQuest(pPlayer, QuestManager.quests.FS_VILLAGE_ELDER)
+	VillageJediManagerCommon.setJediProgressionScreenPlayState(pPlayer, VILLAGE_JEDI_PROGRESSION_HAS_VILLAGE_ACCESS)
 	CreatureObject(pPlayer):sendSystemMessage("@quest/force_sensitive/intro:force_sensitive")
 
 	if (not PlayerObject(pGhost):isJedi()) then
