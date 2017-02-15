e5fc7a3003763b1e906ba31038fa9247e8f485ff
Ivojedi <ivojedi@swgemu.com>
2017-02-15 02:59:20 -0800

    [Added] eyes of mesra to loot - mantis 7286
    Change-Id: Ic96ea41c58c6a3138a6ff8132f2a9f4391bd32b6

---
+++ MMOCoreORB/bin/scripts/loot/items/junk/magic_eight_ball.lua
@@ -0,0 +1,15 @@
+magic_eight_ball = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/magic_eight_ball/magic_eight_ball.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {},
+	junkDealerTypeNeeded = JUNKGENERIC,
+	junkMinValue = 20,
+	junkMaxValue = 40
+}
+
+addLootItemTemplate("magic_eight_ball", magic_eight_ball)
