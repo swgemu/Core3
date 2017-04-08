67ff3f49e24f077234d3fc26e3ec71553faab58a
Victor Popovici <theanswer@swgemu.com>
2017-04-08 12:22:42 +0200

    [changed] removed old max/min macros
    Change-Id: I204a6f4302fa682fc682d04dc116164fd3cb739b

--- MMOCoreORB/src/server/zone/managers/faction/FactionManager.cpp
+++ MMOCoreORB/src/server/zone/managers/faction/FactionManager.cpp
@@ -205,7 +205,7 @@ int FactionManager::getFactionPointsCap(int rank) {
 	if (rank >= factionRanks.getCount())
 		return -1;
 
-	return MAX(1000, getRankCost(rank) * 20);
+	return Math::max(1000, getRankCost(rank) * 20);
 }
 
 bool FactionManager::isFaction(const String& faction) {
