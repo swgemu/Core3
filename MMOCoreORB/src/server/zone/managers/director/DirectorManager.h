19e67fefea60133005f981fd28216ddbe187a33d
Miztah <miztah@swgemu.com>
2017-04-21 16:45:28 -0400

    [added] Snoop functionality to list pending player lua events
    Change-Id: I11b6897109152cfb724de7ed73b54d6dd6fd306a

--- MMOCoreORB/src/server/zone/managers/director/DirectorManager.h
+++ MMOCoreORB/src/server/zone/managers/director/DirectorManager.h
@@ -10,12 +10,12 @@
 
 #include "DirectorSharedMemory.h"
 #include "server/zone/managers/director/QuestStatus.h"
+#include "server/zone/managers/director/ScreenPlayTask.h"
 #include "server/zone/managers/director/QuestVectorMap.h"
 
+#include "system/util/SynchronizedSortedVector.h"
 #include "system/util/SynchronizedHashTable.h"
 #include "system/util/SynchronizedVectorMap.h"
-
-class ScreenPlayTask;
 
 namespace server {
 namespace zone {
@@ -50,6 +50,7 @@ namespace server {
   namespace managers {
    namespace director {
    class PersistentEvent;
+   class ScreenPlayTask;
 
 	class DirectorManager : public Singleton<DirectorManager>, public Object, public Logger, public ReadWriteLock {
 		ThreadLocal<Lua*> localLua;
@@ -58,6 +59,7 @@ namespace server {
 		VectorMap<String, bool> screenPlays;
 		SynchronizedVectorMap<String, Reference<QuestStatus*> > questStatuses;
 		SynchronizedVectorMap<String, Reference<QuestVectorMap*> > questVectorMaps;
+		SynchronizedSortedVector<Reference<ScreenPlayTask*> > screenplayTasks;
 
 #ifdef WITH_STM
 		TransactionalReference<DirectorSharedMemory* > sharedMemory;
@@ -99,6 +101,7 @@ namespace server {
 		QuestVectorMap* createQuestVectorMap(const String& keyString);
 		void removeQuestVectorMap(const String& keyString);
 
+		Vector<Reference<ScreenPlayTask*> > getPlayerEvents(CreatureObject* player);
 		String getStringSharedMemory(const String& key);
 
 		virtual Lua* getLuaInstance();
