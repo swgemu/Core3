eaa565e206ddc8594ddc33719f92edd60c7496b7
Mack <stanton510@googlemail.com>
2017-05-20 10:13:46 +0100

    [changed] Generic Businessman quest gives expired ticket as sellable junk loot item now
    Change-Id: I76401662d05e0a5d8aaa73ab3a4ff423137e9c26

---
+++ MMOCoreORB/bin/scripts/loot/groups/generic_quests/generic_quests_businessman_expired_ticket.lua
@@ -0,0 +1,10 @@
+generic_quests_businessman_expired_ticket = {
+  description = "",
+  minimumLevel = 0,
+  maximumLevel = 0,
+  lootItems = {
+    {itemTemplate = "expired_ticket", weight = 10000000}
+  }
+}
+
+addLootGroupTemplate("generic_quests_businessman_expired_ticket", generic_quests_businessman_expired_ticket)
