e4e8a8c4b53c6255c0912923c9d34bd3fc293533
Nee Shadowfire <nee@swgemu.com>
2017-06-06 02:53:20 +0000

    [Fixed] convo error for Rebel assassinate Corvette 'Pashna' ticketgiver npc if player, after already receiving the ticket, returns to ask AGAIN where to proceed (Mantis 7481) -- [fixed] wrong faction on 'Battle Droids' aboard the REBEL Corvette (Mantis 7481)-- [fixed] a couple of  mis-positioned interior spawns at the 'Rebel outpost' on Rori -- [fixed] a mis-positioned cave spawn in the 'NS Stronghold' on Dathomir
    Change-Id: I7ee9020a99fa1a3ca8230aaa2d8bd25e2f8c0385

--- MMOCoreORB/bin/scripts/mobile/dungeon/corellian_corvette/rebel/imperial_battle_droid.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/corellian_corvette/rebel/imperial_battle_droid.lua
@@ -1,7 +1,7 @@
 imperial_battle_droid = Creature:new {
 	objectName = "@mob/creature_names:imperial_battle_droid",
-	socialGroup = "rebel",
-	faction = "rebel",
+	socialGroup = "imperial",
+	faction = "imperial",
 	level = 134,
 	chanceHit = 5.5,
 	damageMin = 795,
