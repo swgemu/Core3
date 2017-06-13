0943961bb7c9c2148d3105dd679fda63681cc523
Nee Shadowfire <nee@swgemu.com>
2017-06-13 19:56:52 +0000

    [changed] system message for  certain cases when  'isHealableBy' isn't true  with the more appropriate string version of the same message
    Change-Id: Ibe4e3212d33ce6aa0b06e9e9549209a379e94539

--- MMOCoreORB/src/server/zone/objects/creature/commands/DiagnoseCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/DiagnoseCommand.h
@@ -50,7 +50,7 @@ public:
 		}
 
 		if (!creatureTarget->isHealableBy(creature)) {
-			creature->sendSystemMessage("@healing:pvp_no_help"); //It would be unwise to help such a patient.
+			creature->sendSystemMessage("@healing_response:unwise_to_help"); //It would be unwise to help such a patient.
 			return GENERALERROR;
 		}
 
