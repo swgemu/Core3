70e507e741b6a7a38758da892e3a6a058e357b19
TheAnswer <victorpopovici@gmail.com>
2017-06-26 00:36:21 +0200

    [fixed] some static objects not parsing the collision flags properly from templates that derive from buildings
    Change-Id: Id2d5c35002c30ff635632db2a67720a6d95cd14a

--- MMOCoreORB/src/templates/SharedStaticObjectTemplate.h
+++ MMOCoreORB/src/templates/SharedStaticObjectTemplate.h
@@ -10,7 +10,9 @@
 
 #include "templates/SharedObjectTemplate.h"
 
-class SharedStaticObjectTemplate : public SharedObjectTemplate {
+#include "building/SharedBuildingObjectTemplate.h"
+
+class SharedStaticObjectTemplate : public SharedBuildingObjectTemplate {
 protected:
 
 
@@ -31,11 +33,13 @@ public:
 		uint32 nextType = iffStream->getNextFormType();
 
 		if (nextType != 'STAT') {
-			//Logger::console.error("expecting SHOT got " + String::hexvalueOf((int)nextType));
+			//Logger::console.error("expecting STAT got " + String::hexvalueOf((int)nextType));
 
-			SharedObjectTemplate::readObject(iffStream);
+			SharedBuildingObjectTemplate::readObject(iffStream);
 
 			return;
+		} else {
+			Logger::console.error("opening STAT");
 		}
 
 		iffStream->openForm('STAT');
@@ -48,14 +52,10 @@ public:
 			derv = iffStream->getNextFormType();
 		}
 
-		/*while (derv != 0) {
-					if (derv != '
-				}*/
-
 		iffStream->openForm(derv);
 
 		try {
-			//parseFileData(iffStream);
+			parseFileData(iffStream);
 		} catch (Exception& e) {
 			String msg;
 			msg += "exception caught parsing file data ->";
