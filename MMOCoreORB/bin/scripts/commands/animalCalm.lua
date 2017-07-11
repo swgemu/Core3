965444102fa0a7dbd5b40863d12dffb859a4c8e7
Nee Shadowfire <nee@swgemu.com>
2017-07-11 03:42:32 +0000

    [WIP] - [fixed] Jedi Powers 'animal calm' command (Mantis #5247)
    Change-Id: I4c6003310a9436b24df10e05a68ef077610bbbee

--- MMOCoreORB/bin/scripts/commands/animalCalm.lua
+++ MMOCoreORB/bin/scripts/commands/animalCalm.lua
@@ -44,7 +44,17 @@
 AnimalCalmCommand = {
         name = "animalcalm",
         visMod = 25,
+		damageMultiplier = 0,
+		speed = 4.0,
+		forceCost = 50,
+		accuracySkillMod = "mindblast_accuracy",
+		attackType = FORCEATTACK,
+
+		combatSpam = "mindblast1",
+
+		poolsToDamage = NO_ATTRIBUTE,
+
+		range = 32
 }
 
 AddCommand(AnimalCalmCommand)
-
--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalCalmCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalCalmCommand.h
@@ -6,12 +6,15 @@
 #define ANIMALCALMCOMMAND_H_
 
 #include "server/zone/objects/creature/commands/JediQueueCommand.h"
+#include "ForcePowersQueueCommand.h"
+#include "server/zone/objects/creature/ai/Creature.h"
+#include "server/zone/managers/combat/CombatManager.h"
 
-class AnimalCalmCommand : public JediQueueCommand {
+class AnimalCalmCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalCalmCommand(const String& name, ZoneProcessServer* server)
-		: JediQueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
@@ -23,9 +26,51 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-		return SUCCESS;
-	}
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
 
+		ManagedReference<SceneObject*> targetObject = server->getZoneServer()->getObject(target);
+
+		Creature* targetCreature = cast<Creature*>(targetObject.get());
+
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
+
+		int res = doCombatAction(creature, target);
+
+		if (res == SUCCESS) {
+
+			ManagedReference<Creature*> creatureTarget = targetObject.castTo<Creature*>();
+
+			// Commence calming action...
+
+			Locker clocker(creatureTarget, creature);
+
+			if (targetObject == NULL || !targetObject->isCreature()) {
+				creature->sendSystemMessage("@error_message:target_not_creature"); //"The selected action cannot be performed because your target is not a creature."
+				return GENERALERROR;
+			}
+
+			if (creatureTarget->getMainDefender() == creature) {
+				creatureTarget->removeDefender(creature);
+				//CombatManager::instance()->forcePeace(creatureTarget);
+			}
+
+			creature->doCombatAnimation(creatureTarget, STRING_HASHCODE("mind_trick_1"), 1, 0);
+			creature->sendSystemMessage("@jedi_spam:calm_target"); //"You successfully calm down your target."
+
+			CombatManager::instance()->broadcastCombatSpam(creature, targetCreature, NULL, 0, "cbt_spam", combatSpam + "_hit", 1);
+
+			return SUCCESS;
+
+		} else {
+			creature->sendSystemMessage("@jedi_spam:fail_calm_target"); //"You fail to calm your target."
+			CombatManager::instance()->broadcastCombatSpam(creature, targetCreature, NULL, 0, "cbt_spam", combatSpam + "_miss", 1);
+		}
+
+		return res;
+	}
 };
 
 #endif //ANIMALCALMCOMMAND_H_
