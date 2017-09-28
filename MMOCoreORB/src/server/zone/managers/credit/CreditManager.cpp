e175bc3acc20e55d987e810ba6eecd551a6d0c36
Greg Slomin <gregslomin@gmail.com>
2017-09-28 04:03:07 +0000

    [fixed] stability issue
    Change-Id: Ie8a14451f17371b98e9b2651cf91fc91694dd6ca

--- MMOCoreORB/src/server/zone/managers/credit/CreditManager.cpp
+++ MMOCoreORB/src/server/zone/managers/credit/CreditManager.cpp
@@ -8,11 +8,17 @@
 void CreditManager::addBankCredits(uint64 creatureID, int amount, bool notifyClient) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
 
+	if (obj == NULL)
+		return;
+
 	Locker locker(obj);
 	obj->setBankCredits(obj->getBankCredits()+amount, notifyClient);
 }
 void CreditManager::addCashCredits(uint64 creatureID, int amount, bool notifyClient) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
+
+	if (obj == NULL)
+		return;
 
 	Locker locker(obj);
 	obj->setCashCredits(obj->getCashCredits()+amount, notifyClient);
@@ -20,6 +26,9 @@ void CreditManager::addCashCredits(uint64 creatureID, int amount, bool notifyCli
 
 bool CreditManager::subtractBankCredits(uint64 creatureID, int amount, bool notifyClient) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
+
+	if (obj == NULL)
+		return false;
 
 	Locker locker(obj);
 	int newCredits = obj->getBankCredits() - amount;
@@ -34,6 +43,10 @@ bool CreditManager::subtractBankCredits(uint64 creatureID, int amount, bool noti
 
 bool CreditManager::subtractCashCredits(uint64 creatureID, int amount, bool notifyClient) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
+
+	if (obj == NULL)
+		return false;
+
 	Locker locker(obj);
 
 	int newCredits = obj->getCashCredits() - amount;
@@ -49,6 +62,9 @@ bool CreditManager::subtractCashCredits(uint64 creatureID, int amount, bool noti
 bool CreditManager::verifyBankCredits(uint64 creatureID, int amount) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
 
+	if (obj == NULL)
+		return false;
+
 	if (amount < 0)
 		return false;
 
@@ -60,6 +76,9 @@ bool CreditManager::verifyBankCredits(uint64 creatureID, int amount) {
 
 bool CreditManager::verifyCashCredits(uint64 creatureID, int amount) {
 	ManagedReference<CreditObject*> obj = CreditManager::getCreditObject(creatureID);
+
+	if (obj == NULL)
+		return false;
 
 	if (amount < 0)
 		return false;
@@ -79,6 +98,8 @@ ManagedReference<CreditObject*> CreditManager::getCreditObject(uint64 creoID) {
 
 	if (creditObj == NULL) {
 		ManagedReference<CreatureObject*> creo = Core::getObjectBroker()->lookUp(creoID).castTo<CreatureObject*>();
+		if (creo == NULL)
+			return NULL;
 
 		Locker locker(creo);
 		creditObj = creo->getCreditObject();
