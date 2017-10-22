ac4ea6e6e1de9212682716fb8c9a6c481d05ce3d
Victor Popovici <theanswer@swgemu.com>
2017-10-22 11:25:36 +0200

    [fixed] another check for boundaries in get height
    Change-Id: I60a0ccc955e0118bcc5ff20a0181a9b0de2a59d0

--- MMOCoreORB/src/server/zone/managers/planet/PlanetManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/planet/PlanetManagerImplementation.cpp
@@ -960,10 +960,12 @@ bool PlanetManagerImplementation::isInObjectsNoBuildZone(float x, float y, float
 bool PlanetManagerImplementation::isSpawningPermittedAt(float x, float y, float margin) {
 	SortedVector<ActiveArea*> activeAreas;
 
-	Vector3 targetPos(x, y, zone->getHeight(x, y));
+	Vector3 targetPos(x, y, 0);
 
 	if (!zone->isWithinBoundaries(targetPos))
 		return false;
+
+	targetPos.setZ(zone->getHeight(x, y));
 
 	zone->getInRangeActiveAreas(x, y, &activeAreas, true);
 	zone->getInRangeActiveAreas(x, y, margin + 64.f, &activeAreas, true);
