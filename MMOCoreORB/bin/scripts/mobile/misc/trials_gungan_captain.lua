6705d916f7ec6bc3717179e32de0726fbb162669
Miztah <miztah@swgemu.com>
2017-12-03 17:06:22 -0500

    [fixed] Architect trial npc aggroing if player has negative gungan faction
    Change-Id: Idb99aa8b2b1d34ededf61e2810a34c943586d333

---
+++ MMOCoreORB/bin/scripts/mobile/misc/trials_gungan_captain.lua
@@ -0,0 +1,46 @@
+trials_gungan_captain = Creature:new {
+	objectName = "@mob/creature_names:gungan_captain",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "gungan",
+	faction = "",
+	level = 21,
+	chanceHit = 0.34,
+	damageMin = 200,
+	damageMax = 210,
+	baseXp = 2006,
+	baseHAM = 5400,
+	baseHAMmax = 6600,
+	armor = 0,
+	resists = {15,15,15,-1,30,15,15,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/gungan_male.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4000000},
+				{group = "gungan_common", chance = 2000000},
+				{group = "tailor_components", chance = 2000000},
+				{group = "loot_kit_parts", chance = 2000000}
+			}
+		}
+	},
+	weapons = {"rebel_weapons_medium"},
+	conversationTemplate = "",
+	attacks = merge(brawlermaster,marksmanmaster)
+}
+
+CreatureTemplates:addCreatureTemplate(trials_gungan_captain, "trials_gungan_captain")
