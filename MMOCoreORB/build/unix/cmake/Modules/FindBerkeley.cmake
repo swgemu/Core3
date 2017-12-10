89d81f51874adda33667c469af6d9e07691f11d6
Victor Popovici <victorpopovici@gmail.com>
2017-12-10 20:35:32 +0100

    [updated] berkeley cmake search
    Change-Id: Ifbf67358268af548fd97bbb173fddffd7da43ab0

--- MMOCoreORB/build/unix/cmake/Modules/FindBerkeley.cmake
+++ MMOCoreORB/build/unix/cmake/Modules/FindBerkeley.cmake
@@ -9,7 +9,7 @@
 # DB_ROOT_DIR - set to BerkeleyDB's root directory
 # DB_VERSION - set to required BerkeleyDB version, e.g. 47
 
-set(DB_ROOT_DIR /usr/local/BerkeleyDB.5.3)
+#set(DB_ROOT_DIR /usr/local/BerkeleyDB.5.3)
 set(DB_VERSION 5.3)
 
 # This module defines:
@@ -18,12 +18,56 @@ set(DB_VERSION 5.3)
 # DB_LIBRARIES - Libraries needed to use Berkeley DB
 
 #find_path(DB_INCLUDE_DIR db.h db_cxx.h
- #         PATHS ${DB_ROOT_DIR}
-  #        PATH_SUFFIXES include
-   #       )
+#          PATHS ${DB_ROOT_DIR}
+#          PATH_SUFFIXES include
+#          )
           
-set(DB_INCLUDE_DIR /usr/local/BerkeleyDB.5.3/include)
-set(DB_LIBRARIES /usr/local/BerkeleyDB.5.3/lib)
+#set(DB_INCLUDE_DIR /usr/local/BerkeleyDB.5.3/include)
+#set(DB_LIBRARIES /usr/local/BerkeleyDB.5.3/lib)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DB DEFAULT_MSG DB_INCLUDE_DIR DB_LIBRARIES)
+#include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(DB DEFAULT_MSG DB_INCLUDE_DIR DB_LIBRARIES)
+
+
+set(DB_VERSION 5.3)
+
+IF (DB_INCLUDE_DIR)
+    # Already in cache, be silent
+    SET(DB_FIND_QUIETLY TRUE)
+ENDIF (DB_INCLUDE_DIR)
+
+FIND_PATH(DB_INCLUDE_DIR db.h
+        /opt/local/include/db53/
+        /usr/local/BerkeleyDB.5.3/include
+        /usr/include/
+        NO_DEFAULT_PATH
+        )
+
+FIND_LIBRARY(DB_LIBRARIES
+        NAMES db-5.3
+        PATHS /opt/local/lib/db53 /usr/local/BerkeleyDB.5.3/lib /usr/lib/x86_64-linux-gnu/
+        )
+
+IF (DB_INCLUDE_DIR AND DB_LIBRARIES)
+    SET(DB_FOUND TRUE)
+ELSE ()
+    SET(DB_FOUND FALSE)
+    SET(DB_LIBRARIES)
+    SET(DB_INCLUDE_DIR)
+ENDIF ()
+
+IF (DB_FOUND)
+    IF (NOT DB_FIND_QUIETLY)
+        MESSAGE(STATUS "Found berkeley db includes: ${DB_INCLUDE_DIR}")
+        MESSAGE(STATUS "Found berkeley db library: ${DB_LIBRARIES}")
+    ENDIF (NOT DB_FIND_QUIETLY)
+ELSE (DB_FOUND)
+    IF (DB_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could NOT find berkeley db 5.3 library")
+    ENDIF (DB_FIND_REQUIRED)
+ENDIF (DB_FOUND)
+
+MARK_AS_ADVANCED(
+        DB_LIBRARIES
+        DB_INCLUDE_DIR
+)
