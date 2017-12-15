260d6fea2df4db80b96c4b80c4ff9007a8b40090
Victor Popovici <victorpopovici@gmail.com>
2017-12-15 21:41:08 +0100

    [fixed] silencing some cmake messages [fixed] starting idlc with better suited jvm args
    Change-Id: I1438be12c09185c2005e24d94ecbb486209bc0b7

--- MMOCoreORB/cmake/Modules/FindBerkeley.cmake
+++ MMOCoreORB/cmake/Modules/FindBerkeley.cmake
@@ -1,40 +1,24 @@
-# Copyright 2010, Dimitri Kaparis <kaparis.dimitri at gmail dot com>
-
 # CMake module to find Berkeley DB
-
-# For MSVC, only static libraries are used (with 's' suffix)
 
 # This module uses:
 #
-# DB_ROOT_DIR - set to BerkeleyDB's root directory
 # DB_VERSION - set to required BerkeleyDB version, e.g. 47
-
-#set(DB_ROOT_DIR /usr/local/BerkeleyDB.5.3)
-set(DB_VERSION 5.3)
 
 # This module defines:
 # DB_FOUND - True if BerkleyDB is found
 # DB_INCLUDE_DIR - BerkeleyDB's include directory
 # DB_LIBRARIES - Libraries needed to use Berkeley DB
 
-#find_path(DB_INCLUDE_DIR db.h db_cxx.h
-#          PATHS ${DB_ROOT_DIR}
-#          PATH_SUFFIXES include
-#          )
-          
-#set(DB_INCLUDE_DIR /usr/local/BerkeleyDB.5.3/include)
-#set(DB_LIBRARIES /usr/local/BerkeleyDB.5.3/lib)
-
-#include(FindPackageHandleStandardArgs)
-#find_package_handle_standard_args(DB DEFAULT_MSG DB_INCLUDE_DIR DB_LIBRARIES)
-
-
 set(DB_VERSION 5.3)
 
 IF (DB_INCLUDE_DIR)
-    # Already in cache, be silent
-    SET(DB_FIND_QUIETLY TRUE)
+    SET(DB_FIND_QUIETLY_INCLUDE TRUE)
 ENDIF (DB_INCLUDE_DIR)
+
+IF (DB_LIBRARIES)
+    SET(DB_FIND_QUIETLY_LIB TRUE)
+ENDIF (DB_LIBRARIES)
+
 
 FIND_PATH(DB_INCLUDE_DIR db.h
         /opt/local/include/db53/
@@ -57,10 +41,13 @@ ELSE ()
 ENDIF ()
 
 IF (DB_FOUND)
-    IF (NOT DB_FIND_QUIETLY)
+    IF (NOT DB_FIND_QUIETLY_INCLUDE)
         MESSAGE(STATUS "Found berkeley db includes: ${DB_INCLUDE_DIR}")
+    ENDIF (NOT DB_FIND_QUIETLY_INCLUDE)
+
+    IF (NOT DB_FIND_QUIETLY_LIB)
         MESSAGE(STATUS "Found berkeley db library: ${DB_LIBRARIES}")
-    ENDIF (NOT DB_FIND_QUIETLY)
+    ENDIF (NOT DB_FIND_QUIETLY_LIB)
 ELSE (DB_FOUND)
     IF (Berkeley_FIND_REQUIRED)
         MESSAGE(FATAL_ERROR "Could NOT find berkeley db 5.3 library")
