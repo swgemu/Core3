d3237cc9a7940ae67a68a35b7ec03b37fb57d55e
Miztah <miztah@swgemu.com>
2017-12-24 17:08:27 -0500

    [added] Generic screenplay player login/logout trigger functions
    Change-Id: I80e07500d019b3d507237f3662ba4df30df30257

---
+++ MMOCoreORB/bin/scripts/screenplays/playerTriggers.lua
@@ -0,0 +1,13 @@
+PlayerTriggers = { }
+
+function PlayerTriggers:playerLoggedIn(pPlayer)
+	if (pPlayer == nil) then
+		return
+	end
+end
+
+function PlayerTriggers:playerLoggedOut(pPlayer)
+	if (pPlayer == nil) then
+		return
+	end
+end
