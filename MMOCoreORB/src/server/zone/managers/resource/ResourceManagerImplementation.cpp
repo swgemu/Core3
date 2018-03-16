80017b0d716693d0225fcf71f74a24cf35e946e3
Miztah <miztah@swgemu.com>
2018-03-16 22:24:15 +0000

    [fixed] Issue with removing power from a player's inventory
    Change-Id: Icfda5bb14d98cdc50d79caf9785d24994f7d83ee

--- MMOCoreORB/src/server/zone/managers/resource/ResourceManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/resource/ResourceManagerImplementation.cpp
@@ -326,7 +326,7 @@ void ResourceManagerImplementation::removePowerFromPlayer(CreatureObject* player
 
 	uint32 containerPower = 0;
 
-	for (int i = 0; i < inventory->getContainerObjectsSize() && power > 0; ++i) {
+	for (int i = inventory->getContainerObjectsSize() - 1; i >= 0 && power > 0; --i) {
 		ManagedReference<SceneObject*> obj = inventory->getContainerObject(i);
 
 		if (obj == NULL || !obj->isResourceContainer())
