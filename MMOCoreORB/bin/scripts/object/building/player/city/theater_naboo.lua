c4af612d1e7fdbacc6aa6224dfacf69f7bb41161
Farelli <onabfarell@gmail.com>
2018-05-22 11:00:05 -0400

    [Added] Ejection points for Corellia, Naboo, & Tatooine theaters
    Change-Id: I0ebb40056323b6fa3e99b14a69ecd4053f4bf914

--- MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
+++ MMOCoreORB/bin/scripts/object/building/player/city/theater_naboo.lua
@@ -55,6 +55,7 @@ object_building_player_city_theater_naboo = object_building_player_city_shared_t
 		{"private_med_battle_fatigue", 5},
 		{"private_safe_logout", 1}
 	},
+	ejectDistance = 1.5,
 	cityRankRequired = 4,
 	zoneComponent = "StructureZoneComponent",
 	childObjects = {
