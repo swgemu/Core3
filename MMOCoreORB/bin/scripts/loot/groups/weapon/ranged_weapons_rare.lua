2ffa4e30e14e9cc78f52a02db872025ea7a9505a
Smoki <mcbanting@gmail.com>
2018-07-22 02:46:36 -0400

    [Added] Crafting components loot items and groups (Part.2)(Mantis #7645)
    Change-Id: I6005f241ed7a2b2c3028444b0058f6a038c9410d

---
+++ MMOCoreORB/bin/scripts/loot/groups/weapon/ranged_weapons_rare.lua
@@ -0,0 +1,17 @@
+ranged_weapons_rare = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = -1,
+	lootItems = {
+		{itemTemplate = "pistol_republic_blaster", weight = 1250000},
+		{itemTemplate = "pistol_launcher", weight = 1250000},
+		{itemTemplate = "pistol_scatter", weight = 1250000},
+		{itemTemplate = "pistol_dx2", weight = 1250000},
+		{itemTemplate = "carbine_elite", weight = 1250000},
+		{itemTemplate = "rifle_flame_thrower", weight = 1250000},
+		{itemTemplate = "rifle_t21", weight = 1250000},
+		{itemTemplate = "rifle_lightning", weight = 1250000}
+	}
+}
+
+addLootGroupTemplate("ranged_weapons_rare", ranged_weapons_rare)
