1a509968a0b17977a9455ac672fe933c3029307d
Miztah <miztah@swgemu.com>
2018-08-10 00:47:42 +0000

    [changed] GCW base terminals now spawn at random locations when the vulnerability window begins and despawn at the end of the window [adjusted] HQ terminal positioning
    Change-Id: I55b254848f98a39411bb76c3f7d4ea72e0a32ba5

--- MMOCoreORB/bin/scripts/object/building/faction_perk/hq/hq_s04_imp.lua
+++ MMOCoreORB/bin/scripts/object/building/faction_perk/hq/hq_s04_imp.lua
@@ -99,11 +99,7 @@ object_building_faction_perk_hq_hq_s04_imp = object_building_faction_perk_hq_sha
 		{ templateFile = "object/installation/faction_perk/turret/tower_lg.iff", x = 18, z = 0, y = -5,  ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1 },
 		{ templateFile = "object/installation/faction_perk/turret/tower_lg.iff", x = 0, z = 0, y = -13,  ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1 },
 
-		{templateFile = "object/tangible/terminal/terminal_hq_imperial.iff", x = .38, z = .25, y = 1.75, ox = 0, oy = 0, oz = 0, ow = 1, cellid = 2, containmentType = -1 },
-		{templateFile = "object/tangible/hq_destructible/power_regulator.iff", x = 14, z=-13.8, y=-4, ox = 0, oy =1, oz = 0, ow =0, cellid = 10, containmentType = -1 },
-		{templateFile = "object/tangible/hq_destructible/override_terminal.iff", x = 12.75, z = -13.75, y = 10.2, ox = 0, oy = .7, oz = 0, ow =.7, cellid = 9, containmentType = -1 },
-		{templateFile = "object/tangible/hq_destructible/security_terminal.iff", x = -5.5, z = -13.75, y = 7.5, ox = 0, oy = 1, oz = 0, ow = 0, cellid = 7, containmentType = -1 },
-		{templateFile = "object/tangible/hq_destructible/uplink_terminal.iff", x = 6.5, z = -6.75, y = -10, ow =1, ox =0, oz = 0, oy =0, cellid = 5, containmentType = -1},
+		{templateFile = "object/tangible/terminal/terminal_hq_imperial.iff", x = 0, z = 0.25, y = 1.86, ox = 0, oy = 0, oz = 0, ow = 1, cellid = 2, containmentType = -1 },
 
 		{templateFile="object/tangible/terminal/terminal_mission.iff", x=18.25, z=-13.75, y=12.75, ow=-.69, ox=0, oz=0, oy=.71, cellid=9, containmentType=-1},
 		{templateFile="object/tangible/terminal/terminal_bank.iff", x=-7, z=-13.75, y=-3.3, ow=.7, ox=0, oz=0, oy=.66, cellid=7, containmentType=-1},
