5fb4798b2671f4230e467e4409ef6f25267b38e0
Miztah <miztah@swgemu.com>
2018-08-21 23:29:40 +0000

    [fixed] Council leader now has half the normal cooldown when using the light side demote function
    Change-Id: I2931f097e69ecf697f269158d356949321039e56

--- MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
@@ -2382,7 +2382,8 @@ void FrsManagerImplementation::sendVoteDemoteSui(CreatureObject* player, SceneOb
 		return;
 	}
 
-	if (managerData->hasDemotedRecently(player->getObjectID(), requestDemotionDuration)) {
+	// Council leader has half the normal cooldown
+	if (managerData->hasDemotedRecently(player->getObjectID(), (playerTier == 5) ? (requestDemotionDuration / 2) : requestDemotionDuration)) {
 		uint64 miliDiff = managerData->getDemoteDuration(player->getObjectID());
 		uint64 timeLeft = requestDemotionDuration - miliDiff;
 
@@ -2477,7 +2478,8 @@ void FrsManagerImplementation::handleVoteDemoteSui(CreatureObject* player, Scene
 		return;
 	}
 
-	if (managerData->hasDemotedRecently(player->getObjectID(), requestDemotionDuration)) {
+	// Council leader has half the normal cooldown
+	if (managerData->hasDemotedRecently(player->getObjectID(), (playerTier == 5) ? (requestDemotionDuration / 2) : requestDemotionDuration)) {
 		uint64 miliDiff = managerData->getDemoteDuration(player->getObjectID());
 		uint64 timeLeft = requestDemotionDuration - miliDiff;
 
