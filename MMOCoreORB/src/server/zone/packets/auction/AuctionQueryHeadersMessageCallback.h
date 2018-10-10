0322caaeb6102d33c081b887928e9314d57ba538
Miztah <miztah@swgemu.com>
2018-10-10 03:27:27 +0000

    [added] Auction window text filtering and min/max price filtering functionality (needs updated client ui file)
    Change-Id: Icafdfcfef91319c714dcb22adac25487ee8677fa

--- MMOCoreORB/src/server/zone/packets/auction/AuctionQueryHeadersMessageCallback.h
+++ MMOCoreORB/src/server/zone/packets/auction/AuctionQueryHeadersMessageCallback.h
@@ -16,13 +16,19 @@ class AuctionQueryHeadersMessageCallback : public MessageCallback {
 	int counter;
 	int screen;
 	uint32 category;
+	int itemType;
+	UnicodeString filterText;
+	int unk1;
+	int minPrice;
+	int maxPrice;
+	bool includeEntranceFee;
 	uint64 vendorID;
-	char unk1;
+	bool isVendor;
 	int offset;
 
 public:
 	AuctionQueryHeadersMessageCallback(ZoneClientSession* client, ZoneProcessServer* server) :
-			MessageCallback(client, server), extent(0), counter(0), screen(0), category(0), vendorID(0), unk1(0), offset(0) {
+			MessageCallback(client, server), extent(0), counter(0), screen(0), category(0), itemType(0), unk1(0), minPrice(0), maxPrice(0), includeEntranceFee(0), vendorID(0), isVendor(0), offset(0) {
 
 	}
 
@@ -34,9 +40,16 @@ public:
 		// 2 - all items, 3 - my sales, 4 - my bids, 5 - available items,
 		// 7 - for sale (vendor), 9 - offers to vendor
 		category = message->parseInt();  // Bitmask
-		message->shiftOffset(21);
+
+		itemType = message->parseInt();
+		message->parseUnicode(filterText);
+		unk1 = message->parseInt();
+		minPrice = message->parseInt();
+		maxPrice = message->parseInt();
+		includeEntranceFee = message->parseByte();
+
 		vendorID = message->parseLong();
-		unk1 = message->parseByte(); //Becomes one when using a vendor.
+		isVendor = message->parseByte(); //Becomes one when using a vendor.
 		offset = message->parseShort();
 
 	}
@@ -52,7 +65,7 @@ public:
 		AuctionManager* auctionManager = server->getZoneServer()->getAuctionManager();
 
 		if (auctionManager != NULL)
-			auctionManager->getData(player, extent, vendorID, screen, category, counter, offset);
+			auctionManager->getData(player, extent, vendorID, screen, category, filterText, minPrice, maxPrice, includeEntranceFee, counter, offset);
 	}
 };
 
