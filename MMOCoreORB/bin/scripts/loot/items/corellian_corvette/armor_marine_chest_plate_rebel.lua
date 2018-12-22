3355fed164228cbf228d6be2e9bedf6a7a12fa11
Farelli <onabfarell@gmail.com>
2018-12-22 11:22:39 -0500

    [Fixed] Rebel marine armor chest plate corvette loot attributes (Mantis #7594) [Fixed] Heat vulnerability on marine and stormtrooper armor
    Change-Id: If09e46f537bb1834b071a2aee9c667daec42f466

--- MMOCoreORB/bin/scripts/loot/items/corellian_corvette/armor_marine_chest_plate_rebel.lua
+++ MMOCoreORB/bin/scripts/loot/items/corellian_corvette/armor_marine_chest_plate_rebel.lua
@@ -6,6 +6,13 @@ armor_marine_chest_plate_rebel = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/wearables/armor/marine/armor_marine_chest_plate_rebel.iff",
 	craftingValues = {
+		{"armor_rating",1,1,0},
+		{"armor_special_type",0,0,0},
+		{"armor_effectiveness",30,30,0},
+		{"armor_integrity",45000,45000,0},
+		{"armor_health_encumbrance",150,150,0},
+		{"armor_action_encumbrance",49,49,0},
+		{"armor_mind_encumbrance",19,19,0},
 	},
 	customizationStringNames = {},
 	customizationValues = {}
