666ef335f08eca9994ce669bc0f32eb56aa63796
Miztah <miztah@swgemu.com>
2019-01-30 03:23:41 +0000

    [added] Dress group functionality to allow generic template lists to be used across different mobiles
    Change-Id: I61c4ec170fb79329b0747c826ed21790ad6a11fa

---
+++ MMOCoreORB/bin/scripts/mobile/dressgroup/dark_jedi.lua
@@ -0,0 +1,10 @@
+dark_jedi = {
+	"object/mobile/dressed_dark_jedi_human_male_01.iff",
+	"object/mobile/dressed_dark_jedi_human_female_01.iff",
+	"object/mobile/dressed_dark_side_savage.iff",
+	"object/mobile/dressed_forsaken_force_drifter.iff",
+	"object/mobile/dressed_force_trained_archaist.iff",
+	"object/mobile/dressed_force_sensitive_crypt_crawler.iff"
+}
+
+addDressGroup("dark_jedi", dark_jedi)
