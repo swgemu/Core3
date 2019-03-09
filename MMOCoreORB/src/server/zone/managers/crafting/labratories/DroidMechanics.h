686f684f8f34fb3cd50de557df4e3fa8887c375d
Farelli <onabfarell@gmail.com>
2019-03-09 08:58:02 -0500

    [Fixed] Droid combat rating cap (mantis 8027)
    Change-Id: I84005296458f309ba1debcff0d5db3ebd43300ca

--- MMOCoreORB/src/server/zone/managers/crafting/labratories/DroidMechanics.h
+++ MMOCoreORB/src/server/zone/managers/crafting/labratories/DroidMechanics.h
@@ -64,6 +64,8 @@ public:
 	static float determineMinDamage(int droidType, int rating) {
 		if (rating == 0)
 			return 1;
+		if (rating > 600)
+			rating = 600;
 		if(droidType == DroidObject::R_SERIES)
 			return ((int)((rating/600.0) * 80.0)) + 80;
 		if(droidType == DroidObject::LE_REPAIR)
@@ -75,6 +77,8 @@ public:
 	static float determineMaxDamage(int droidType, int rating) {
 		if (rating == 0)
 			return 1;
+		if (rating > 600)
+			rating = 600;
 		if(droidType == DroidObject::R_SERIES)
 			return ((int)((rating/600.0) * 85.0)) + 85;
 		if(droidType == DroidObject::LE_REPAIR)
