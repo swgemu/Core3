c5ef29ae35be78c956bf6de96e24212aa750de3b
Karl 'lordkator' Bunch <lordkator@swgemu.com>
2019-04-09 12:47:47 +0000

    [added] event terminal usage log
    Change-Id: I66351d2fd10a180d7a393959a0f2920692952a5d

--- MMOCoreORB/bin/scripts/screenplays/events/buffTerminalMenuComponent.lua
+++ MMOCoreORB/bin/scripts/screenplays/events/buffTerminalMenuComponent.lua
@@ -6,6 +6,23 @@ function buffTerminalMenuComponent:fillObjectMenuResponse(pSceneObject, pMenuRes
 	response:addRadialMenuItem(21, 3, "Clear Wounds")
 end
 
+function buffTerminalMenuComponent:logUsage(pPlayer, what)
+	local creature = CreatureObject(pPlayer)
+	local fh = io.open("log/buffTerminalMenuComponent.log", "a+")
+	fh:write(string.format(
+	    "%s [buffTerminalMenuComponent] %s on %s (oid: %d) on %s at %s %s\n",
+		getFormattedTime(),
+	    what,
+		creature:getFirstName(),
+		creature:getObjectID(),
+		creature:getZoneName(),
+		math.floor(creature:getWorldPositionX()),
+		math.floor(creature:getWorldPositionY())
+	))
+	fh:flush()
+	fh:close()
+end
+
 function buffTerminalMenuComponent:handleObjectMenuSelect(pSceneObject, pPlayer, selectedID)
 	if CreatureObject(pPlayer):isInCombat() or CreatureObject(pPlayer):isIncapacitated() or CreatureObject(pPlayer):isDead() then
 		return 0
@@ -17,12 +34,14 @@ function buffTerminalMenuComponent:handleObjectMenuSelect(pSceneObject, pPlayer,
 
 	if selectedID == 20 then
 		CreatureObject(pPlayer):enhanceCharacter()
+		buffTerminalMenuComponent:logUsage(pPlayer, "enhanceCharacter")
 	elseif selectedID == 21 then
 		for i = 0, 8 do
 			CreatureObject(pPlayer):setWounds(i, 0)
 		end
 
 		CreatureObject(pPlayer):setShockWounds(0)
+		buffTerminalMenuComponent:logUsage(pPlayer, "clearWounds")
 	end
 
 	return 0
