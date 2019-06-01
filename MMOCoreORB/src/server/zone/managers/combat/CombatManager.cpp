6db8ae950ec9b783af67a412b57e2c38dbc014b0
Hakry <Hakry18@gmail.com>
2019-06-01 11:10:08 -0400

    [fixed] Force Absorb to be based off of damage instead of ability cost - Mantis #6810 [fixed] Force Absorb to absorb force from Force Drain attacks - Mantis #6810
    Change-Id: I6e28dd47e6a880242edc3067fdd214a11a5edbae

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -2294,9 +2294,10 @@ int CombatManager::getArmorReduction(TangibleObject* attacker, WeaponObject* wea
 
 		// Force Absorb
 		if (defender->getSkillMod("force_absorb") > 0 && defender->isPlayerCreature()) {
-			int forceCost = data.getForceCost();
-			defender->notifyObservers(ObserverEventType::FORCEABSORB, attacker, forceCost);
-			hitList->setForceAbsorb(forceCost);
+			float absorbDam = damage * 0.4f;
+
+			defender->notifyObservers(ObserverEventType::FORCEABSORB, attacker, absorbDam);
+			hitList->setForceAbsorb(absorbDam);
 		}
 	}
 
