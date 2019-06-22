5e4d286c7e5d6dd066c7013e6301bb67bc40cedd
Miztah <miztah@swgemu.com>
2019-06-22 04:59:23 +0000

    [fixed] Some active area shape issues
    Change-Id: I9d03a8a210b43005c0c24d1f128a82d1b58e3589

--- MMOCoreORB/src/server/zone/managers/creature/SpawnAreaMap.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/SpawnAreaMap.cpp
@@ -29,7 +29,7 @@ void SpawnAreaMap::loadMap(Zone* z) {
 	lua->deinit();
 
 	delete lua;
-	lua = NULL;
+	lua = nullptr;
 }
 
 void SpawnAreaMap::loadRegions() {
@@ -88,32 +88,55 @@ void SpawnAreaMap::readAreaObject(LuaObject& areaObj) {
 	float innerRadius = 0;
 	float outerRadius = 0;
 
-    LuaObject areaShapeObject = areaObj.getObjectAt(4);
-    if (areaShapeObject.isValidTable()) {
-        if (areaShapeObject.getIntAt(1) == CIRCLE) {
-            radius = areaShapeObject.getFloatAt(2);
-        } else if (areaShapeObject.getIntAt(1) == RECTANGLE) {
-            x2 = areaShapeObject.getFloatAt(2);
-            y2 = areaShapeObject.getFloatAt(3);
-        } else if (areaShapeObject.getIntAt(1) == RING) {
-        	innerRadius = areaShapeObject.getFloatAt(2);
-        	outerRadius = areaShapeObject.getFloatAt(3);
-        }
-        areaShapeObject.pop();
-    } else {
-    	areaShapeObject.pop();
-        radius = areaObj.getFloatAt(4);
-        x2 = 0;
-        y2 = 0;
-    }
+	LuaObject areaShapeObject = areaObj.getObjectAt(4);
+	if (!areaShapeObject.isValidTable()) {
+		error("Invalid area shape table for spawn region " + name);
+		return;
+	}
 
-    if (radius == 0 && x2 == 0 && y2 == 0 && innerRadius == 0 && outerRadius == 0)
+	int areaType = areaShapeObject.getIntAt(1);
+
+	if (areaType == CIRCLE) {
+		radius = areaShapeObject.getFloatAt(2);
+
+		if (radius <= 0 && !(tier & WORLDSPAWNAREA)) {
+			error("Invalid radius of " + String::valueOf(radius) + " must be > 0 for circular spawn region " + name);
+			return;
+		}
+	} else if (areaType == RECTANGLE) {
+		x2 = areaShapeObject.getFloatAt(2);
+		y2 = areaShapeObject.getFloatAt(3);
+		int rectWidth = x2 - x;
+		int rectHeight = y2 - y;
+
+		if (!(tier & WORLDSPAWNAREA) && (rectWidth <= 0 || rectHeight <= 0)) {
+			error("Invalid corner coordinates for rectangular spawn region " + name + ", total height: " + String::valueOf(rectHeight) + ", total width: " + String::valueOf(rectWidth));
+			return;
+		}
+	} else if (areaType == RING) {
+		innerRadius = areaShapeObject.getFloatAt(2);
+		outerRadius = areaShapeObject.getFloatAt(3);
+
+		if (!(tier & WORLDSPAWNAREA)) {
+			if (innerRadius <= 0) {
+				error("Invalid inner radius of " + String::valueOf(innerRadius) + " must be > 0 for ring spawn region " + name);
+				return;
+			} else if (outerRadius <= 0) {
+				error("Invalid outer radius of " + String::valueOf(outerRadius) + " must be > 0 for ring spawn region " + name);
+				return;
+			}
+		}
+	}
+
+	areaShapeObject.pop();
+
+	if (radius == 0 && x2 == 0 && y2 == 0 && innerRadius == 0 && outerRadius == 0)
 		return;
 
 	static const uint32 crc = STRING_HASHCODE("object/spawn_area.iff");
 
 	ManagedReference<SpawnArea*> area = dynamic_cast<SpawnArea*>(ObjectManager::instance()->createObject(crc, 0, "spawnareas"));
-	if (area == NULL)
+	if (area == nullptr)
 		return;
 
 	Locker objLocker(area);
@@ -122,7 +145,7 @@ void SpawnAreaMap::readAreaObject(LuaObject& areaObj) {
 
 	area->setObjectName(nameID, false);
 
-	if (x2 > 0 && y2 > 0) {
+	if (areaType == RECTANGLE) {
 		ManagedReference<RectangularAreaShape*> rectangularAreaShape = new RectangularAreaShape();
 		Locker shapeLocker(rectangularAreaShape);
 		rectangularAreaShape->setDimensions(x, y, x2, y2);
@@ -130,13 +153,13 @@ void SpawnAreaMap::readAreaObject(LuaObject& areaObj) {
 		float centerY = y + ((y2 - y) / 2);
 		rectangularAreaShape->setAreaCenter(centerX, centerY);
 		area->setAreaShape(rectangularAreaShape);
-	} else if (radius > 0) {
+	} else if (areaType == CIRCLE) {
 		ManagedReference<CircularAreaShape*> circularAreaShape = new CircularAreaShape();
 		Locker shapeLocker(circularAreaShape);
 		circularAreaShape->setAreaCenter(x, y);
 		circularAreaShape->setRadius(radius);
 		area->setAreaShape(circularAreaShape);
-	} else if (innerRadius > 0 && outerRadius > 0) {
+	} else if (areaType == RING) {
 		ManagedReference<RingAreaShape*> ringAreaShape = new RingAreaShape();
 		Locker shapeLocker(ringAreaShape);
 		ringAreaShape->setAreaCenter(x, y);
@@ -202,7 +225,7 @@ void SpawnAreaMap::unloadMap() {
 	for (int i = 0; i < size(); i++) {
 		SpawnArea* area = get(i);
 
-		if (area != NULL) {
+		if (area != nullptr) {
 			Locker locker(area);
 			area->destroyObjectFromWorld(false);
 		}
