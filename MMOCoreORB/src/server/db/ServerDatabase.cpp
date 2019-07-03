5346ff36f4ea73761d895a7c89ceaf8bdf79672e
Karl 'lordkator' Bunch <lordkator@swgemu.com>
2019-07-03 10:41:05 +0000

    [fixed] memory leak
    Change-Id: Id5b9113a262cc7c233e35486206888cdde21f182

--- MMOCoreORB/src/server/db/ServerDatabase.cpp
+++ MMOCoreORB/src/server/db/ServerDatabase.cpp
@@ -38,7 +38,7 @@ ServerDatabase::ServerDatabase(ConfigManager* configManager) {
 
 		String createTable = "CREATE TABLE `db_metadata` AS SELECT 1000 as `schema_version`;";
 		try {
-			instance()->executeQuery(createTable);
+			Reference<ResultSet*> result = instance()->executeQuery(createTable);
 			updateDatabaseSchema();
 		} catch (Exception& e) {
 			error("Failed to create db_metadata table, please manually create in mysql: " + createTable);
@@ -66,8 +66,10 @@ void ServerDatabase::alterDatabase(int nextSchemaVersion, const String& alterSql
 	String updateVersionSql = "UPDATE `db_metadata` SET `schema_version` = " + String::valueOf(nextSchemaVersion);
 
 	try {
-		instance()->executeQuery(alterSql);
-		instance()->executeQuery(updateVersionSql);
+		Reference<ResultSet*> result = instance()->executeQuery(alterSql);
+
+		if (result != nullptr)
+			result = instance()->executeQuery(updateVersionSql);
 		dbSchemaVersion = nextSchemaVersion;
 		info("Upgraded mysql database schema to version " + String::valueOf(dbSchemaVersion), true);
 	} catch (Exception& e) {
