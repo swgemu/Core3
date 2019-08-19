60e06475e3a00364883cbc8292779560c4ac7855
Victor Popovici <victorpopovici@gmail.com>
2019-08-19 07:11:42 +0000

    [fixed] stability issue
    Change-Id: Ic7ad0eb9f2adbba712a92ae3b90ed6a1d71a2fc2

--- MMOCoreORB/src/server/zone/managers/auction/AuctionManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/auction/AuctionManagerImplementation.cpp
@@ -39,7 +39,7 @@ void AuctionManagerImplementation::initialize() {
 
 	Locker locker(_this.getReferenceUnsafeStaticCast());
 
-	Core::getTaskManager()->initializeCustomQueue("AuctionSearchQueue", ConfigManager::instance()->getMaxAuctionSearchJobs(), false);
+	Core::getTaskManager()->initializeCustomQueue("AuctionSearchQueue", ConfigManager::instance()->getMaxAuctionSearchJobs(), true);
 
 	auctionMap = new AuctionsMap();
 
