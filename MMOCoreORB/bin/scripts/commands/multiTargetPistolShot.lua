9a1f728149d0a3b94dc598d50f55ecbdb5fcf5a4
Nicholas Rafalski <nicholas.rafalski@gmail.com>
2019-10-27 15:08:38 +0000

    [fixed] MultiTargetPistolShot damange/speed multipliers and AOE (#8047)
    Change-Id: I01a7318ab11b7104ce1129d794b420e8c9bd4ef3

--- MMOCoreORB/bin/scripts/commands/multiTargetPistolShot.lua
+++ MMOCoreORB/bin/scripts/commands/multiTargetPistolShot.lua
@@ -44,20 +44,19 @@
 MultiTargetPistolShotCommand = {
         name = "multitargetpistolshot",
 
-	damageMultiplier = 4.0,
-	speedMultiplier = 2.0,
+	damageMultiplier = 4.2,
+	speedMultiplier = 3.5,
 	healthCostMultiplier = 0.5,
 	actionCostMultiplier = 1.25,
 	mindCostMultiplier = 0.5,
 	accuracyBonus = 50,
 
-        --no documentation on specific coneAngle, leaving it alone
-	coneAngle = 30,
-	coneAction = true,
-
+	areaAction = true,
+        areaRange = 32,
+        
 	poolsToDamage = RANDOM_ATTRIBUTE,
 
-	animation = "fire_5_special_single", 
+	animation = "fire_5_special_single",
 	animType = GENERATE_RANGED,
 
 	combatSpam = "pistolmultishot",
