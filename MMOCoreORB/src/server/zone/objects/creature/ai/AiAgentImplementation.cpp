747b5fac096634ccbedb896c8924d8f7327962dd
Miztah <miztah@swgemu.com>
2020-01-15 01:38:32 +0000

    [fixed] Stability issue
    Change-Id: I9aefe7528a9932f55506ca6f8c8ac5d53858c1ee

--- MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
@@ -666,7 +666,7 @@ void AiAgentImplementation::initializeTransientMembers() {
 	CreatureObjectImplementation::initializeTransientMembers();
 
 	// Fix for pets created prior to ai update
-	if (defaultWeapon == nullptr) {
+	if (petDeed != nullptr && defaultWeapon == nullptr) {
 		SharedObjectTemplate* temp = getObjectTemplate();
 		loadTemplateData(temp);
 	}
