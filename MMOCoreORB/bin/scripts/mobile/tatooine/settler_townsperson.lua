f09e76222e223441d1c2ab9a3a9623386655727f
Loshult <loshult@swgemu.com>
2020-04-19 21:08:07 +0200

    [Fixed] Mantis 8200: Settler having thug faction instead of townsperson faction.
    Change-Id: I1925bb57a92c8e8061bbba30b60362102ebaf875

---
+++ MMOCoreORB/bin/scripts/mobile/tatooine/settler_townsperson.lua
@@ -0,0 +1,49 @@
+settler_townsperson = Creature:new {
+	objectName = "@mob/creature_names:settler",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "townsperson",
+	faction = "townsperson",
+	level = 6,
+	chanceHit = 0.250000,
+	damageMin = 50,
+	damageMax = 55,
+	baseXp = 113,
+	baseHAM = 180,
+	baseHAMmax = 220,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0.000000,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_commoner_tatooine_bith_female_03.iff",
+		"object/mobile/dressed_commoner_old_human_male_02.iff",
+		"object/mobile/dressed_commoner_naboo_twilek_female_01.iff"
+	},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 4000000},
+				{group = "wearables_common", chance = 3000000},
+				{group = "loot_kit_parts", chance = 2000000},
+				{group = "tailor_components", chance = 1000000},
+			}
+		}
+	},
+	weapons = {"rebel_weapons_light"},
+	reactionStf = "@npc_reaction/townperson",
+	attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(settler_townsperson, "settler_townsperson")
