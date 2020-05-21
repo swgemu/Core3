2a38cb45bb7fe1dc4cdb9cec2135b182fb450576
phoenix <phoenix@swgemu.com>
2020-05-21 11:07:31 -0400

    [Fixed] Mantis 8231 - Missing nullptr check.
    Change-Id: Ieb9891abe01293f5e7ccac0c1f0f34c80bb70218

--- MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/scene/SceneObjectImplementation.cpp
@@ -502,6 +502,10 @@ void SceneObjectImplementation::sendDestroyTo(SceneObject* player) {
 void SceneObjectImplementation::sendAttributeListTo(CreatureObject* object) {
 	AttributeListMessage* alm = new AttributeListMessage(asSceneObject());
 
+	if (attributeListComponent == nullptr) {
+		throw Exception("nullptr attribute list component");
+	}
+
 	try {
 
 		attributeListComponent->fillAttributeList(alm, object, asSceneObject());
