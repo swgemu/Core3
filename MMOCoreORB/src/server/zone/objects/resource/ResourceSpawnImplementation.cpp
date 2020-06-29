1f93bad0682a324e32de797fa9abd59373b7c7f9
swgemu <swgemu@swgemu.swgemu.com>
2020-06-29 09:42:40 -0700

    [Added] Resource name in inventory

--- MMOCoreORB/src/server/zone/objects/resource/ResourceSpawnImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/resource/ResourceSpawnImplementation.cpp
@@ -245,7 +245,8 @@ Reference<ResourceContainer*> ResourceSpawnImplementation::createResource(int un
    		newResource->setQuantity(units);
 
    	newResource->setCustomObjectName(getFamilyName(), false);
-
+   		String resourceName = getFinalClass() + " (" + getName() + ")";
+   		newResource->setCustomObjectName(resourceName, false);
    	++containerReferenceCount;
 
    	return newResource;
