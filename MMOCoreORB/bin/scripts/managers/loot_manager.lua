23a08d9bfcf1912d84a168f8be9fe5ad3db2c543
TrigsC <trigsc@gmail.com>
2020-07-08 14:25:18 -0700

    [Updated] Loot Stuff

--- MMOCoreORB/bin/scripts/managers/loot_manager.lua
+++ MMOCoreORB/bin/scripts/managers/loot_manager.lua
@@ -41,17 +41,17 @@
 --which carries forward this exception.
 
 --Determines how often exceptional and legendary items can drop.
-yellowChance = 1000 -- 1 in 1,000
-exceptionalChance = 100000 --1 in 100,000
-legendaryChance = 1000000 --1 in 1,000,000
+yellowChance = 1000000 -- 1 in 1,0000
+exceptionalChance = 1000000000 --1 in 100,000
+legendaryChance = 1000000000000 --1 in 1,000,000
 --yellowChance = 50 --1 in 50 for testing
 --exceptionalChance = 100 --1 in 100 for testing
 --legendaryChance = 1000 --1 in 1000 for testing
 
 --Determines how much of an increase in the base stats will be applied to the object.
-yellowModifier = 1.5 
-exceptionalModifier = 2.5
-legendaryModifier = 5.0
+yellowModifier = 0.5 --1.5 
+exceptionalModifier = 1.0 --2.5
+legendaryModifier = 1.5 --5.0
 
 --The chance for random skill mods to be on looted weapons/wearables
 skillModChance = 500 -- 1 in 500
