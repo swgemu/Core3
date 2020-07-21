0602aee216c749a91ce1c42e26ecbb1f07be1c2f
TrigsC <trigsc@gmail.com>
2020-07-20 20:31:34 -0700

    [Update] Spider Venom 200 cap

--- MMOCoreORB/bin/scripts/loot/items/geonosian_lab/geo_spider_venom_rare.lua
+++ MMOCoreORB/bin/scripts/loot/items/geonosian_lab/geo_spider_venom_rare.lua
@@ -6,7 +6,7 @@ geo_spider_venom_rare = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/chemistry/spider_venom.iff",
 	craftingValues = {
-		{"power",161,300,0},
+		{"power",161,200,0},
 		{"useCount",1,11,0},
 	},
 	customizationStringNames = {},
