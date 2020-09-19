6c2d1dd5850b1f8476ec2f1604efd006f988bc83
TrigsC <trigsc@gmail.com>
2020-09-19 13:35:57 -0700

    [Update] Corellia and Tatooine Cantina Heals

--- MMOCoreORB/bin/scripts/object/building/tatooine/cantina_tatooine.lua
+++ MMOCoreORB/bin/scripts/object/building/tatooine/cantina_tatooine.lua
@@ -42,7 +42,7 @@
 
 object_building_tatooine_cantina_tatooine = object_building_tatooine_shared_cantina_tatooine:new {
 	skillMods = {
-		{"private_med_wound_mind", 20},
+		{"private_med_wound_mind", 100},
 		{"private_buff_mind", 100},
 		{"private_med_battle_fatigue", 5},
 		{"private_safe_logout", 1}
