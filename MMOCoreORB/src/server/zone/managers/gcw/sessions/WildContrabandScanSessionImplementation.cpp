b2274702dfbeb2770616356cc482cb91a434894b
Loshult <loshult@swgemu.com>
2020-09-22 09:02:28 +0200

    [Added] Task to initiate contraband scans in the wild to GCWManager. [Added] Randomly select a player in the wild to perform a scan on. [Added] Session with probe landing for wild contraband scans.
    Change-Id: I49d4b3599a07562b5d8bb2f15f3629e39a6e63c6

---
+++ MMOCoreORB/src/server/zone/managers/gcw/sessions/WildContrabandScanSessionImplementation.cpp
@@ -0,0 +1,131 @@
+/*
+ * WildContrabandScanSessionImplementation.cpp
+ *
+ *  Created on: Nov 27, 2016
+ *      Author: loshult
+ */
+
+#include "server/zone/managers/creature/CreatureManager.h"
+#include "server/zone/managers/gcw/sessions/WildContrabandScanSession.h"
+#include "server/zone/managers/gcw/tasks/WildContrabandScanTask.h"
+#include "server/zone/managers/planet/PlanetManager.h"
+#include "server/zone/objects/creature/ai/AiAgent.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/packets/scene/PlayClientEffectLocMessage.h"
+#include "server/zone/Zone.h"
+
+int WildContrabandScanSessionImplementation::initializeSession() {
+	ManagedReference<CreatureObject*> player = weakPlayer.get();
+
+	if (wildContrabandScanTask == nullptr) {
+		wildContrabandScanTask = new WildContrabandScanTask(player);
+	}
+	if (!wildContrabandScanTask->isScheduled()) {
+		wildContrabandScanTask->schedule(TASKDELAY);
+	}
+
+	player->updateCooldownTimer("crackdown_scan", CONTRABANDSCANCOOLDOWN);
+
+	if (player->getActiveSession(SessionFacadeType::WILDCONTRABANDSCAN) != nullptr) {
+		player->dropActiveSession(SessionFacadeType::WILDCONTRABANDSCAN);
+	}
+	player->addActiveSession(SessionFacadeType::WILDCONTRABANDSCAN, _this.getReferenceUnsafeStaticCast());
+
+	landingCoordinates = getLandingCoordinates(player);
+
+	return true;
+}
+
+int WildContrabandScanSessionImplementation::cancelSession() {
+	ManagedReference<CreatureObject*> player = weakPlayer.get();
+
+	if (player != nullptr) {
+		player->dropActiveSession(SessionFacadeType::WILDCONTRABANDSCAN);
+	}
+
+	if (wildContrabandScanTask != nullptr) {
+		wildContrabandScanTask->cancel();
+	}
+
+	return clearSession();
+}
+
+int WildContrabandScanSessionImplementation::clearSession() {
+	return 0;
+}
+
+void WildContrabandScanSessionImplementation::runWildContrabandScan() {
+	ManagedReference<CreatureObject*> player = weakPlayer.get();
+
+	if (!scanPrerequisitesMet(player)) {
+		cancelSession();
+	}
+
+	Locker locker(player);
+
+	ManagedReference<Zone*> zone = player->getZone();
+
+	if (zone == nullptr) {
+		cancelSession();
+	}
+
+	timeLeft--;
+
+	switch (scanState) {
+	case LANDING:
+		landProbeDroid(player);
+		scanState = CLOSINGIN;
+		break;
+	case CLOSINGIN:
+		if (timeLeft <= 0) {
+			droid = cast<AiAgent*>(player->getZone()->getCreatureManager()->spawnCreature(STRING_HASHCODE("probot"), 0, landingCoordinates.getX(),
+																							  landingCoordinates.getZ(), landingCoordinates.getY(), 0));
+			scanState = INITIATESCAN;
+		}
+		break;
+	case INITIATESCAN:
+		scanState = SCANDELAY;
+		break;
+	case SCANDELAY:
+		scanState = WAITFORSHUTTLE;
+		break;
+	case WAITFORSHUTTLE:
+		scanState = TAKEOFF;
+		break;
+	case TAKEOFF: {
+		scanState = FINISHED;
+		Locker clocker(droid, player);
+		droid->destroyObjectFromWorld(true);
+	}
+		break;
+	default:
+		break;
+	}
+
+	if (scanState != FINISHED) {
+		wildContrabandScanTask->reschedule(TASKDELAY);
+	} else {
+		cancelSession();
+	}
+}
+
+bool WildContrabandScanSessionImplementation::scanPrerequisitesMet(CreatureObject* player) {
+	return player != nullptr && player->isPlayerCreature() && !player->isDead() && !player->isFeigningDeath() && !player->isIncapacitated() && !player->isInCombat();
+}
+
+void WildContrabandScanSessionImplementation::landProbeDroid(CreatureObject* player) {
+	PlayClientEffectLoc* effect = new PlayClientEffectLoc("clienteffect/probot_delivery.cef", player->getZone()->getZoneName(), landingCoordinates.getX(),
+														  landingCoordinates.getZ(), landingCoordinates.getY(), 0, 0);
+	player->sendMessage(effect);
+	timeLeft = 3;
+}
+
+Vector3 WildContrabandScanSessionImplementation::getLandingCoordinates(CreatureObject* player) {
+	if (player->getZone() == nullptr || player->getZone()->getPlanetManager() == nullptr) {
+		return player->getPosition();
+	}
+
+	PlanetManager* planetManager = player->getZone()->getPlanetManager();
+
+	return planetManager->getInSightSpawnPoint(player, 30, 120, 15);
+}
