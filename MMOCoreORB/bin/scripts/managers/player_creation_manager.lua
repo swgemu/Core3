24c1ee1a91238d818f35441ea86c66081ec4a12d
TrigsC <trigsc@gmail.com>
2020-09-23 12:18:54 -0700

    [Update] Starting Cash

--- MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
@@ -2,10 +2,10 @@
 freeGodMode = 0;
 --How many cash credits new characters start with after creating a character (changed during test phase, normal value is 100)
 --startingCash = 100
-startingCash = 100000
+startingCash = 100
 --How many bank credits new characters start with after creating a character (changed during test phase, normal value is 1000)
 --startingBank = 1000
-startingBank = 100000
+startingBank = 1000
 --How many skill points a new characters start with
 skillPoints = 250
 
