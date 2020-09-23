950de5bd6d5c45e9e53d3287ad6e41bdd84e5467
TrigsC <trigsc@gmail.com>
2020-09-23 12:21:32 -0700

    [Update] 2 Characters

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -328,8 +328,8 @@ bool PlayerCreationManager::createCharacter(ClientCreateCharacterCallback* callb
 
 	auto client = callback->getClient();
 
-	if (client->getCharacterCount(zoneServer.get()->getGalaxyID()) >= 10) {
-		ErrorMessage* errMsg = new ErrorMessage("Create Error", "You are limited to 10 characters per galaxy.", 0x0);
+	if (client->getCharacterCount(zoneServer.get()->getGalaxyID()) >= 2) {
+		ErrorMessage* errMsg = new ErrorMessage("Create Error", "You are limited to 2 characters per galaxy.", 0x0);
 		client->sendMessage(errMsg);
 
 		return false;
