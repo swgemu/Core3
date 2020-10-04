e879dbfb079094cf2136e88ba01d1eb5f7dadd07
TrigsC <trigsc@gmail.com>
2020-10-04 13:43:08 -0700

    [Update] Ubese Helmet Fix

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ubese/armor_ubese_helmet.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ubese/armor_ubese_helmet.lua
@@ -81,7 +81,7 @@ object_tangible_wearables_armor_ubese_armor_ubese_helmet = object_tangible_weara
 				"object/mobile/vendor/zabrak_male.iff" },
 
 	-- Damage types in WeaponObject
-	vulnerability = STUN + LIGHTSABER,
+	vulnerability = HEAT + COLD + STUN + LIGHTSABER,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 23,
