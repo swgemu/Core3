a49796ed859fe1a4e6ae62a88920317028e77192
Aleksander <aleksander.lode@gmail.com>
2020-10-30 08:30:51 +0100

    [added] polearm_staff_janta to janta loot tables, Mantis #8094
    Change-Id: I8c4b9bafea4cd44d0e76ebef5a93fcfa9dfdbe3d

--- MMOCoreORB/bin/scripts/loot/groups/npc/janta_common.lua
+++ MMOCoreORB/bin/scripts/loot/groups/npc/janta_common.lua
@@ -7,6 +7,7 @@ janta_common = {
 	lootItems = {
 		{itemTemplate = "janta_blood", weight = 2500000},
 		{itemTemplate = "knife_janta", weight = 1500000},
+		{itemTemplate = "polearm_staff_janta", weight = 1500000},		
 		{itemTemplate = "janta_hides", weight = 3500000},
 		{itemTemplate = "spear_rack_schematic", weight = 500000},
 		{groupTemplate = "wearables_common", weight = 1000000},
