ac5739131aa67820c1559a8d3dda58137c30df36
Hakry <Hakry@swgemu.com>
2020-11-29 12:51:33 -0500

    [draft] Visions of the Past, Dreams of the Future Event
    Change-Id: I03775a5222975d990262511c19c496454d8256a7

---
+++ MMOCoreORB/bin/scripts/custom_scripts/loot/items/visions_of_the_past/visions_item_1.lua
@@ -0,0 +1,15 @@
+visions_item_1 = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Visions of the Past Rare Salvage",
+	directObjectTemplate = "object/tangible/mission/quest_item/singular_nak_q2_needed.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {},
+	skillMods = {
+
+	}
+}
+
+addLootItemTemplate("visions_item_1", visions_item_1)
---
+++ MMOCoreORB/bin/scripts/custom_scripts/mobile/serverobjects.lua
@@ -0,0 +1,8 @@
+includeFile("../custom_scripts/mobile/conversation.lua")
+includeFile("../custom_scripts/mobile/conversations.lua")
+
+--Visions of the Past
+includeFile("../custom_scripts/mobile/visions_of_the_past/visions_imperial_informant.lua")
+includeFile("../custom_scripts/mobile/visions_of_the_past/visions_imperial_manager.lua")
+includeFile("../custom_scripts/mobile/visions_of_the_past/visions_rebel_informant.lua")
+includeFile("../custom_scripts/mobile/visions_of_the_past/visions_rebel_manager.lua")
