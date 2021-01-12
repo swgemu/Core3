df6f3bb8149419da36ed6dfe46f6933c24b1cc41
Hakry <Hakry@swgemu.com>
2021-01-11 21:51:34 -0500

    [added] Vader and Leia chance to visit gcw controlled cities Bestine and Anchorhead enabled only for testing [added] mobiles specified for event [added] travel points to allow player_shuttle to land in city areas
    Change-Id: Id001c96ae1a3929d070bf2372f70fd39e4cbd2c2

---
+++ MMOCoreORB/bin/scripts/mobile/event/city_control_landing/darth_vader_city_control.lua
@@ -0,0 +1,36 @@
+darth_vader_city_control = Creature:new {
+	objectName = "@mob/creature_names:darth_vader",
+	socialGroup = "imperial",
+	faction = "imperial",
+	level = 300,
+	chanceHit = 30,
+	damageMin = 1645,
+	damageMax = 3000,
+	baseXp = 28549,
+	baseHAM = 385000,
+	baseHAMmax = 471000,
+	armor = 1,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = NONE,
+	optionsBitmask = AIENABLED + INVULNERABLE,
+	diet = HERBIVORE,
+  	scale = 1.25,
+
+	templates = {"object/mobile/darth_vader.iff"},
+	lootGroups = {},
+	weapons = {"darth_vader_weapons"},
+	conversationTemplate = "",
+	attacks = merge(lightsabermaster, forcepowermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(darth_vader_city_control, "darth_vader_city_control")
