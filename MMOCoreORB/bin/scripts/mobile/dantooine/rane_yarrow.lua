c40e13ab9567a401647390739cc471a8a44e09c4
Hakry <Hakry@swgemu.com>
2021-02-21 11:47:22 -0500

    [added] remaining city patrolling NPCs [added] temporary combatPatrol ai template [changed] method used for patrol point delays [fixed] various NPC to better reflect live
    Change-Id: Icbe3aaabf899b34f22640b5332f9f351d9ef75bd

--- MMOCoreORB/bin/scripts/mobile/dantooine/rane_yarrow.lua
+++ MMOCoreORB/bin/scripts/mobile/dantooine/rane_yarrow.lua
@@ -22,7 +22,7 @@ rane_yarrow = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = NONE,
-	optionsBitmask = AIENABLED,
+	optionsBitmask = AIENABLED + JTLINTERESTING,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/space_greeter_dantooine_brother_neutral.iff"},
