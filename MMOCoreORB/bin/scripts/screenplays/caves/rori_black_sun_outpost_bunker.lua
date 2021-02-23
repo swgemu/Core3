9955e210ffe057d4fd562eaa864e50fcc4658c9d
Kellen Kania <kellenkania@gmail.com>
2021-02-23 12:08:34 -0800

    [fixed] Mantis 8301: Black Sun Bunker Technician
    Change-Id: I19ebddaa1365f454a8e338493c944add0c4725c1

--- MMOCoreORB/bin/scripts/screenplays/caves/rori_black_sun_outpost_bunker.lua
+++ MMOCoreORB/bin/scripts/screenplays/caves/rori_black_sun_outpost_bunker.lua
@@ -20,16 +20,18 @@ function RoriBlackSunOutpostBunkerScreenPlay:spawnMobiles()
 	spawnMobile("rori", "black_sun_initiate", 300, 4838.6, 76.1, -1084.6, -130, 0)
 	spawnMobile("rori", "black_sun_initiate", 300, 4842.2, 76.2, -1090.2, -130, 0)
 
-	--inside
+	--inside ground level
 	spawnMobile("rori", "black_sun_smuggler", 300, 0.0, 0.3, 2.6, 177, 3566198)
 	spawnMobile("rori", "black_sun_initiate", 300, -3.7, 0.3, -4.5, -2, 3566198)
 	spawnMobile("rori", "black_sun_initiate", 300, -3.9, 0.3, -4.0, -91, 3566199)
+	--inside first level down
 	spawnMobile("rori", "black_sun_minion", 300, 19.0, -12.0, 28.7, -94, 3566202)
 	spawnMobile("rori", "black_sun_smuggler", 300, 22.9, -12.0, 35.9, -16, 3566202)
 	spawnMobile("rori", "black_sun_initiate", 300, 17.5, -12.0, 60.2, -174, 3566204)
 	spawnMobile("rori", "black_sun_smuggler", 300, -3.5, -12.0, 75.6, -101, 3566206)
 	spawnMobile("rori", "black_sun_minion", 300, 3.6, -12.0, 79.3, 178, 3566206)
 	spawnMobile("rori", "black_sun_initiate", 300, 34.7, -12.0, 73.7, -47, 3566213)
+	--inside second level down
 	spawnMobile("rori", "black_sun_initiate", 300, 33.5, -20.0, 111.7, 175, 3566214)
 	spawnMobile("rori", "black_sun_smuggler", 300, 45.8, -20.0, 112.5, 173, 3566215)
 	spawnMobile("rori", "black_sun_smuggler", 300, 46.0, -20.0, 108.6, -4, 3566215)
@@ -44,7 +46,7 @@ function RoriBlackSunOutpostBunkerScreenPlay:spawnMobiles()
 	spawnMobile("rori", "insane_miner", 600, 105.7, -20.0, 110.6, 85, 3566240)
 	spawnMobile("rori", "black_sun_minion", 300, 44.3, -20.0, 150.0, -94, 3566235)
 	--medical area where the quest npc (Tover Blackmoor) stands
-	spawnMobile("rori", "technician", 600, 44.6, -12.0, 157.6, -9, 3566235)
+	spawnMobile("rori", "technician", 600, 44.6, -20.0, 157.6, -9, 3566235)
 	--elevator shaft closet without any button to go down
 	spawnMobile("rori", "blood_thirsty_borgle", 600, -7.3, -12.0, 59.0, 79, 3566207)
 
