3701ad670f81eaec3b659433d8ff3207746d22cf
SwgSmoki <swgsmoki@gmail.com>
2021-03-16 22:59:29 +0000

    [fixed] Crafted object template customization, mantis #8167, #8317
    [refactored] Ranged weapon visible component map, #8167
    [fixed] Ranged weapon component crafting appearance selection, #8167
    [fixed] Ranged weapon visible components to match ingredients, #8167
    [fixed] Customized crafted object stringId name inheritance, #8317
    
    Change-Id: I27de18717660bb66e5e1c0708fb3757b4325d331

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/component/scope_weapon_advanced.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/component/scope_weapon_advanced.lua
@@ -3,78 +3,86 @@
 
 --This File is part of Core3.
 
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
+--This program is free software; you can redistribute
+--it and/or modify it under the terms of the GNU Lesser
 --General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
+--Foundation; either version 2 of the License,
 --or (at your option) any later version.
 
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 --See the GNU Lesser General Public License for
 --more details.
 
---You should have received a copy of the GNU Lesser General 
+--You should have received a copy of the GNU Lesser General
 --Public License along with this program; if not, write to
 --the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
+--Linking Engine3 statically or dynamically with other modules
+--is making a combined work based on Engine3.
+--Thus, the terms and conditions of the GNU Lesser General Public License
 --cover the whole combination.
 
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
+--In addition, as a special exception, the copyright holders of Engine3
+--give you permission to combine Engine3 program with free software
+--programs or libraries that are released under the GNU LGPL and with
+--code included in the standard release of Core3 under the GNU LGPL
+--license (or modified versions of such code, with unchanged license).
+--You may copy and distribute such a system following the terms of the
+--GNU LGPL for Engine3 and the licenses of the other code concerned,
+--provided that you include the source code of that other code when
 --and as the GNU LGPL requires distribution of source code.
 
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
+--Note that people who make modified versions of Engine3 are not obligated
+--to grant this special exception for their modified versions;
+--it is their choice whether to do so. The GNU Lesser General Public License
+--gives permission to release a modified version without this exception;
+--this exception also makes it possible to release a modified version
 
 
 object_draft_schematic_weapon_component_scope_weapon_advanced = object_draft_schematic_weapon_component_shared_scope_weapon_advanced:new {
 
-   templateType = DRAFTSCHEMATIC,
+		templateType = DRAFTSCHEMATIC,
 
-   customObjectName = "Advanced Weapon Scope",
+		customObjectName = "Advanced Weapon Scope",
 
-   craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 14, 
-   size = 2, 
-   factoryCrateType = "object/factory/factory_crate_weapon.iff",
-   
-   xpType = "crafting_weapons_general", 
-   xp = 60, 
+		craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
+		complexity = 14,
+		size = 2,
+		factoryCrateType = "object/factory/factory_crate_weapon.iff",
 
-   assemblySkill = "weapon_assembly", 
-   experimentingSkill = "weapon_experimentation", 
-   customizationSkill = "weapon_customization", 
+		xpType = "crafting_weapons_general",
+		xp = 60,
 
-   customizationOptions = {},
-   customizationStringNames = {},
-   customizationDefaults = {},
+		assemblySkill = "weapon_assembly",
+		experimentingSkill = "weapon_experimentation",
+		customizationSkill = "weapon_customization",
 
-   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"scope_shell", "shock_proofing_and_gasket_seals", "scope_bracket", "light_accumulators", "targeting_aid_circuitry"},
-   ingredientSlotType = {0, 0, 0, 0, 0},
-   resourceTypes = {"aluminum_chromium", "petrochem_inert_polymer", "aluminum_chromium", "crystalline_green_diamond", "copper"},
-   resourceQuantities = {18, 10, 5, 6, 3},
-   contribution = {100, 100, 100, 100, 100},
+		customizationOptions = {},
+		customizationStringNames = {},
+		customizationDefaults = {},
 
+		ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
+		ingredientTitleNames = {"scope_shell", "shock_proofing_and_gasket_seals", "scope_bracket", "light_accumulators", "targeting_aid_circuitry"},
+		ingredientSlotType = {0, 0, 0, 0, 0},
+		resourceTypes = {"aluminum_chromium", "petrochem_inert_polymer", "aluminum_chromium", "crystalline_green_diamond", "copper"},
+		resourceQuantities = {18, 10, 5, 6, 3},
+		contribution = {100, 100, 100, 100, 100},
 
-   targetTemplate = "object/tangible/component/weapon/scope_weapon_advanced.iff",
+		targetTemplate = "object/tangible/component/weapon/scope_weapon_advanced.iff",
 
-   additionalTemplates = {
-             }
-
+		additionalTemplates = 
+		{
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s01.iff",
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s02.iff",
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s03.iff",
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s04.iff",
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s05.iff",
+			"object/tangible/component/weapon/shared_scope_weapon_sm_s06.iff",
+			--"object/tangible/component/weapon/shared_scope_weapon_lg_s01.iff",
+			--"object/tangible/component/weapon/shared_scope_weapon_lg_s02.iff",
+			--"object/tangible/component/weapon/shared_scope_weapon_lg_s03.iff",
+		}
 }
 ObjectTemplates:addTemplate(object_draft_schematic_weapon_component_scope_weapon_advanced, "object/draft_schematic/weapon/component/scope_weapon_advanced.iff")
