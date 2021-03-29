5911fcad917dc634cd9f64b1814f257f59c7fb6b
Hakry <Hakry@swgemu.com>
2021-03-29 13:29:30 -0400

    [changed] When a target is added to an attackers defender list - Mantis #5621 [fixed] Defender Lists and Peace ability to function more like live [fixed] Lightsaber block animation from not firing
    Change-Id: I011e7fc9360418850af6793bb8f293b7682fcda1

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -84,69 +84,41 @@ bool CombatManager::startCombat(CreatureObject* attacker, TangibleObject* defend
 			VisibilityManager::instance()->increaseVisibility(creo, 25);
 	}
 
+	attacker->setCombatState();
+	defender->setCombatState();
+
 	attacker->setDefender(defender);
-	defender->addDefender(attacker);
 
 	return true;
 }
 
 bool CombatManager::attemptPeace(CreatureObject* attacker) const {
-	const DeltaVector<ManagedReference<SceneObject*> >* defenderList = attacker->getDefenderList();
 
-	for (int i = defenderList->size() - 1; i >= 0; --i) {
-		try {
-			ManagedReference<SceneObject*> object = defenderList->get(i);
+	attacker->removeDefenders();
+	attacker->setState(CreatureState::PEACE);
 
-			TangibleObject* defender = cast<TangibleObject*>( object.get());
+	ThreatMap* threatMap = attacker->getThreatMap();
 
-			if (defender == nullptr)
+	if (threatMap != nullptr) {
+		for (int i = 0; i < threatMap->size(); i++) {
+			CreatureObject* threatCreo = threatMap->elementAt(i).getKey();
+
+			if (threatCreo == nullptr || threatCreo == attacker) {
 				continue;
-
-			try {
-				Locker clocker(defender, attacker);
-
-				if (defender->hasDefender(attacker)) {
-
-					if (defender->isCreatureObject()) {
-						CreatureObject* creature = defender->asCreatureObject();
-
-						if (creature->getMainDefender() != attacker || creature->hasState(CreatureState::PEACE) || creature->isDead() || attacker->isDead() || !creature->isInRange(attacker, 128.f)) {
-							attacker->removeDefender(defender);
-							defender->removeDefender(attacker);
-						}
-					} else {
-						attacker->removeDefender(defender);
-						defender->removeDefender(attacker);
-					}
-				} else {
-					attacker->removeDefender(defender);
-				}
-
-				clocker.release();
-
-			} catch (Exception& e) {
-				error(e.getMessage());
-				e.printStackTrace();
 			}
-		} catch (ArrayIndexOutOfBoundsException& exc) {
 
+			if ((attacker->isInRange(threatCreo, 128.f) && threatCreo->getMainDefender() == attacker)) {
+				continue;
+			}
+
+			attacker->clearCombatState(false);
+		}
+
+		if (threatMap->size() == 0) {
+			attacker->clearCombatState(false);
 		}
 	}
-
-	if (defenderList->size() != 0) {
-		debug("defenderList not empty, trying to set Peace State");
-
-		attacker->setState(CreatureState::PEACE);
-
-		return false;
-	} else {
-		attacker->clearCombatState(false);
-
-		// clearCombatState() (rightfully) does not automatically set peace, so set it
-		attacker->setState(CreatureState::PEACE);
-
-		return true;
-	}
+	return true;
 }
 
 void CombatManager::forcePeace(CreatureObject* attacker) const {
@@ -289,9 +261,6 @@ int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* 
 	if (!tano->isAttackableBy(attacker))
 		return 0;
 
-	attacker->addDefender(tano);
-	tano->addDefender(attacker);
-
 	if (tano->isCreatureObject()) {
 		CreatureObject* defender = tano->asCreatureObject();
 
@@ -335,8 +304,9 @@ int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* 
 }
 
 int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* weapon, CreatureObject* defender, const CreatureAttackData& data, bool* shouldGcwCrackdownTef, bool* shouldGcwTef, bool* shouldBhTef) const {
-	if (defender->isEntertaining())
+	if (defender->isEntertaining()) {
 		defender->stopEntertaining();
+	}
 
 	int hitVal = HIT;
 	uint8 hitLocation = 0;
@@ -380,7 +350,8 @@ int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* 
 		damageMultiplier = 0.0f;
 		break;}
 	case RICOCHET:
-		doLightsaberBlock(attacker, weapon, defender, damage);
+		//doLightsaberBlock(attacker, weapon, defender, damage);
+		broadcastCombatAction(attacker, defender, weapon, data, 0, hitVal, 0);
 		checkForTefs(attacker, defender, shouldGcwCrackdownTef, shouldGcwTef, shouldBhTef);
 		damageMultiplier = 0.0f;
 		return 0;
@@ -440,9 +411,6 @@ int CombatManager::doTargetCombatAction(TangibleObject* attacker, WeaponObject* 
 
 	if (defenderObject->isEntertaining())
 		defenderObject->stopEntertaining();
-
-	attacker->addDefender(defenderObject);
-	defenderObject->addDefender(attacker);
 
 	float damageMultiplier = data.getDamageMultiplier();
 
@@ -2718,10 +2686,12 @@ Reference<SortedVector<ManagedReference<TangibleObject*> >* > CombatManager::get
 				if (!(weapon->isThrownWeapon()) && !(data.isSplashDamage()) && !(weapon->isHeavyWeapon())) {
 					if (CollisionManager::checkLineOfSight(object, attacker)) {
 						defenders->put(tano);
+						attacker->addDefender(tano);
 					}
 				} else {
 					if (CollisionManager::checkLineOfSight(object, defenderObject)) {
 						defenders->put(tano);
+						attacker->addDefender(tano);
 					}
 				}
 			} catch (Exception& e) {
