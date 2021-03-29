29f7b187ed86aee688272abe9338bebc84545d76
Hakry <Hakry@swgemu.com>
2021-03-29 13:29:30 -0400

    [changed] When a target is added to an attackers defender list - Mantis #5621 [fixed] Defender Lists and Peace ability to function more like live [fixed] Lightsaber block animation from not firing
    Change-Id: I011e7fc9360418850af6793bb8f293b7682fcda1

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -638,6 +638,24 @@ int CreatureManagerImplementation::notifyDestruction(TangibleObject* destructor,
 		}
 	}
 
+	ThreatMap* destructorThreatMap = destructor->getThreatMap();
+
+	if (destructorThreatMap != nullptr) {
+		for (int i = 0; i < destructorThreatMap->size(); i++) {
+			CreatureObject* destructedCreo = destructorThreatMap->elementAt(i).getKey();
+
+			if (destructedCreo == destructedObject) {
+				destructorThreatMap->remove(i);
+			}
+		}
+	}
+
+	const DeltaVector<ManagedReference<SceneObject*> >* defenderList = destructor->getDefenderList();
+
+	if (defenderList->size() == 0) {
+		destructor->clearCombatState(false);
+	}
+
 	// now we can safely lock destructor again
 	if (destructedObject != destructor)
 		destructor->wlock(destructedObject);
