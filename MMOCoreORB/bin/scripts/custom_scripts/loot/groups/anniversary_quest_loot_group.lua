0f192f264ffbc637178c56792d2d113821b28d1c
Fastburn <fastburn@swgemu.com>
2021-04-30 13:46:52 +0000

    [draft] Basilisk Anniversary Event
    Change-Id: Icd9f830dff9fece5ccacab0b7f67181f9f72f698

---
+++ MMOCoreORB/bin/scripts/custom_scripts/loot/groups/anniversary_quest_loot_group.lua
@@ -0,0 +1,10 @@
+anniversary_quest_loot_group = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "anniversary_item_1", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("anniversary_quest_loot_group", anniversary_quest_loot_group)
---
+++ MMOCoreORB/bin/scripts/custom_scripts/mobile/serverobjects.lua
@@ -0,0 +1,7 @@
+includeFile("../custom_scripts/mobile/conversation.lua")
+includeFile("../custom_scripts/mobile/conversations.lua")
+
+--Basilisk Anniversary
+includeFile("../custom_scripts/mobile/basilisk_anniversary/anniversary_grandmaster_baker.lua")
+includeFile("../custom_scripts/mobile/basilisk_anniversary/anniversary_information_broker.lua")
+includeFile("../custom_scripts/mobile/basilisk_anniversary/anniversary_disaster_of_desserts.lua")
