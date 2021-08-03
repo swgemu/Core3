b82a9284a107275595ed832b6fa3b1ed465ff959
Hakry <Hakry@swgemu.com>
2021-08-02 22:25:02 -0400

    [refactor] Fishing Manager Implementation [added] Fishing Manager lua for loading configuration and options [added] Chance to catch loot while fishing in heavy vegetation areas [fixed] various fishing calculations
    Change-Id: I29cc0e5fb3306b0f7b84b123952a779aed79e98d

---
+++ MMOCoreORB/bin/scripts/managers/fishing_manager.lua
@@ -0,0 +1,33 @@
+-- Set false to disable fishing
+fishingEnabled = true
+
+fishTypes = {"blackfish", "blowfish", "bluefish", "faa", "laa", "ray", "striped"}
+
+fishColors = {
+	{"corellia", 61},
+	{"dantooine", 51},
+	{"dathomir", 21},
+	{"endor", 32},
+	{"lok", 8},
+	{"naboo", 14},
+	{"rori", 55},
+	{"talus", 0},
+	{"tatooine", 7},
+	{"yavin4", 41},
+}
+
+fishLengths = {
+	{"blackfish", 25},
+	{"blowfish", 15},
+	{"bluefish", 30},
+	{"faa", 30},
+	{"laa", 30},
+	{"ray", 35},
+	{"striped", 40},
+ }
+
+fishingActions = {"None", "Tug Up", "Tug Right", "Tug Left", "Small Reel", "Stop Fishing"}
+
+fishingStates = {"Nothing", "Waiting.....", "Snagged", "Nibble...", "BITE!", "CAUGHT SOMETHING!"}
+
+baitStatus = {"Fresh", "Soggy", "Mush"}
