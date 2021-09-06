7f176165236b21bfe3686fbf1e95d00ae9dafe1f
Hakry <Hakry@swgemu.com>
2021-09-05 22:20:04 -0400

    [event] Legendary Labor Day
    Change-Id: I64365a7af387c6609227ac54c06e2100fc3e118a

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -48,16 +48,18 @@ onlineCharactersPerAccount = 2 --How many characters are allowed online from a s
 allowSameAccountPvpRatingCredit = 0
 
 --Blue frog / GM buff values
-performanceBuff = 1000
-medicalBuff = 1900
+--performanceBuff = 1000
+--medicalBuff = 1900
+performanceBuff = 2000
+medicalBuff = 3500
 performanceDuration = 7200 -- in seconds
 medicalDuration = 7200 -- in seconds
 
 --Sets the experience multiplier while grouped
-groupExpMultiplier = 1.2
+groupExpMultiplier = 10.0
 
 --Sets a global experience multiplier
-globalExpMultiplier = 1.0
+globalExpMultiplier = 5.0
 
 --Sets the base number of control devices of each type that a player can have in their datapad at once
 --For creature pets, A Creature Handler will have the base number + their stored pets skill mod as limit
