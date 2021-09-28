8b59b26358f718c51ed2ac0dd4179884ac28b8b0
Hakry <Hakry@swgemu.com>
2021-09-28 14:42:58 -0400

    [fixed] Improper weapons/skills with some NPC templates

--- MMOCoreORB/bin/scripts/mobile/weapon/groups/commando_ranged.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/commando_ranged.lua
@@ -1,4 +1,7 @@
 commando_ranged = {
+	"object/weapon/ranged/rifle/rifle_flame_thrower.iff",
+	"object/weapon/ranged/rifle/rifle_acid_beam.iff",
+	"object/weapon/ranged/pistol/pistol_launcher.iff",
 	"object/weapon/ranged/rifle/rifle_t21.iff",
 	"object/weapon/ranged/rifle/rifle_sg82.iff",
 	"object/weapon/ranged/rifle/rifle_e11.iff"
--- MMOCoreORB/bin/scripts/mobile/weapon/groups/dark_jedi_weapons_gen4.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/dark_jedi_weapons_gen4.lua
@@ -2,11 +2,10 @@ dark_jedi_weapons_gen4 = {
 	"object/weapon/melee/polearm/crafted_saber/sword_lightsaber_polearm_gen4.iff",
 	"object/weapon/melee/sword/crafted_saber/sword_lightsaber_one_handed_gen4.iff",
 	"object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_gen4.iff"
-	
 }
 
 addWeapon("dark_jedi_weapons_gen4", dark_jedi_weapons_gen4)
---To facilitate the correctness to Weapons groups 
+--To facilitate the correctness to Weapons groups
 --please include comments under here that point to your source
 --such as links to screenshots from wiki's that show pre-cu creature holding specific weapons
 --or links to cannon wiki pages showing a weapon belonging to a specific group in the Star Wars Universe
