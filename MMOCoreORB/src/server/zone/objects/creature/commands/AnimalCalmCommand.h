88a67c365c440072770d74263073d5f27ffd9003
Hakry <Hakry@swgemu.com>
2021-10-04 17:48:42 -0400

    [added] Animal Attack, Animal Scare and Animal Calm abilities

--- MMOCoreORB/src/server/zone/objects/creature/commands/AnimalCalmCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/AnimalCalmCommand.h
@@ -5,27 +5,82 @@
 #ifndef ANIMALCALMCOMMAND_H_
 #define ANIMALCALMCOMMAND_H_
 
-#include "server/zone/objects/creature/commands/JediQueueCommand.h"
+#include "ForcePowersQueueCommand.h"
 
-class AnimalCalmCommand : public JediQueueCommand {
+class AnimalCalmCommand : public ForcePowersQueueCommand {
 public:
 
 	AnimalCalmCommand(const String& name, ZoneProcessServer* server)
-		: JediQueueCommand(name, server) {
+		: ForcePowersQueueCommand(name, server) {
 
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-		return SUCCESS;
-	}
+		if (isWearingArmor(creature)) {
+			return NOJEDIARMOR;
+		}
 
+		ZoneServer* zoneServer = creature->getZoneServer();
+
+		if (zoneServer == nullptr)
+			return GENERALERROR;
+
+		ManagedReference<SceneObject*> targetObject = zoneServer->getObject(target);
+
+		StringIdChatParameter param;
+		param.setStringId("@error_message:target_not_creature");
+
+		if (targetObject == nullptr)
+			return GENERALERROR;
+
+		if (!targetObject->isCreatureObject()) {
+			creature->sendSystemMessage(param);
+			return GENERALERROR;
+		}
+
+		CreatureObject* tarCreo = targetObject->asCreatureObject();
+
+		if (tarCreo == nullptr || !tarCreo->isAiAgent() || tarCreo->isNonPlayerCreatureObject()) {
+			creature->sendSystemMessage(param);
+			return GENERALERROR;
+		}
+
+		AiAgent* agent = tarCreo->asAiAgent();
+
+		if (agent == nullptr)
+			return GENERALERROR;
+
+		int result = doCombatAction(creature, target);
+
+		if (result == SUCCESS) {
+			int playerLevel = creature->getLevel() * 2;
+			int targetLevel = tarCreo->getLevel();
+			int failCalc = (targetLevel - playerLevel + System::random(250));
+
+			if (agent->getCreatureBitmask() & CreatureFlag::AGGRESSIVE)
+				failCalc += 75;
+
+			if (failCalc < 300) {
+				Locker alock(agent, creature);
+
+				CombatManager::instance()->forcePeace(agent);
+
+				param.setStringId("@jedi_spam:calm_target");
+			} else {
+				param.setStringId("@jedi_spam:fail_calm_target");
+			}
+
+			creature->sendSystemMessage(param);
+		}
+
+		return result;
+	}
 };
 
 #endif //ANIMALCALMCOMMAND_H_
