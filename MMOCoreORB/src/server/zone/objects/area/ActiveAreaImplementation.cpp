112ff968a1dadc4501c753d89b69d6466a90fe5e
Hakry <Hakry@swgemu.com>
2021-10-07 16:31:29 -0400

    [added] region debugging enabled with /lfg bitmask
    Change-Id: If8071aeed77e7e3aad91c616ecfe1650d988cb76

--- MMOCoreORB/src/server/zone/objects/area/ActiveAreaImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/area/ActiveAreaImplementation.cpp
@@ -8,6 +8,8 @@
 #include "server/zone/objects/area/ActiveArea.h"
 #include "events/ActiveAreaEvent.h"
 #include "server/zone/objects/area/areashapes/AreaShape.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/objects/area/SpawnArea.h"
 
 bool ActiveAreaImplementation::containsPoint(float px, float py, uint64 cellid) const {
 	if (cellObjectID != 0 && cellObjectID != cellid)
@@ -52,6 +54,48 @@ void ActiveAreaImplementation::notifyEnter(SceneObject* obj) {
 	if (cellObjectID == 0 || cellObjectID == obj->getParentID())
 		notifyObservers(ObserverEventType::ENTEREDAREA, obj);
 
+	if (obj->isPlayerCreature()) {
+		CreatureObject* creo = obj->asCreatureObject();
+
+		if (creo != nullptr) {
+			PlayerObject* ghost = creo->getPlayerObject();
+
+			if (ghost != nullptr && ghost->getCharacterBitmask() & PlayerObject::LFG) {
+				StringBuffer enterMsg;
+				String name = getObjectNameStringIdName();
+				enterMsg << "?Entering Active Area:  " << name << "  -  ";
+
+				String shapeString = areaShape == nullptr ? " City; " : areaShape->isRectangularAreaShape() ? " rectangle;  " : areaShape->isCircularAreaShape() ? " circle;  "
+					: areaShape->isRingAreaShape() ? " ring;  " : " none;  ";
+
+				enterMsg << " Area shape = " << shapeString;
+				enterMsg << " Radius = " << getRadius() << ";  ";
+
+				Vector3 playerCoords = creo->getWorldPosition();
+				enterMsg << " Player Coords: X = " << playerCoords.getX() <<  "  Z = " << playerCoords.getZ() <<"  Y = " << playerCoords.getY() << ";  ";
+
+				Vector3 coords = getWorldPosition();
+				enterMsg << " Area Coords - X = " << coords.getX() <<  "  Z = " << coords.getZ() <<"  Y = " << coords.getY() << ";  ";
+
+				if (!isNoSpawnArea()) {
+					ManagedReference<ActiveArea*> area = _this.getReferenceUnsafeStaticCast();
+
+					if (area != nullptr) {
+						SpawnArea* spawnArea = area.castTo<SpawnArea*>();
+
+						if (spawnArea != nullptr) {
+							int tier = spawnArea->getTier();
+
+							enterMsg << " Spawn Area:  Tier - " << tier << ";  ";
+						}
+					}
+				}
+
+				creo->sendSystemMessage(enterMsg.toString());
+			}
+		}
+	}
+
 	if (obj->isPlayerCreature() && attachedScenery.size() > 0) {
 		ManagedReference<SceneObject*> sceno = obj;
 		Vector<ManagedReference<SceneObject* > > scene = attachedScenery;
@@ -70,6 +114,47 @@ void ActiveAreaImplementation::notifyEnter(SceneObject* obj) {
 void ActiveAreaImplementation::notifyExit(SceneObject* obj) {
 	if (cellObjectID == 0 || cellObjectID != obj->getParentID())
 		notifyObservers(ObserverEventType::EXITEDAREA, obj);
+
+	if (obj->isPlayerCreature()) {
+		CreatureObject* creo = obj->asCreatureObject();
+		if (creo != nullptr) {
+			PlayerObject* ghost = creo->getPlayerObject();
+
+			if (ghost != nullptr && ghost->getCharacterBitmask() & PlayerObject::LFG) {
+				StringBuffer exitMsg;
+				String name = getObjectNameStringIdName();
+				exitMsg << "?Exiting Active Area:  " << name << "  -  ";
+
+				String shapeString = areaShape == nullptr ? " City; " : areaShape->isRectangularAreaShape() ? " rectangle;  " : areaShape->isCircularAreaShape() ? " circle;  "
+					: areaShape->isRingAreaShape() ? " ring;  " : " none;  ";
+
+				exitMsg << " Area shape = " << shapeString;
+				exitMsg << " Radius = " << getRadius() << ";  ";
+
+				Vector3 playerCoords = creo->getWorldPosition();
+				exitMsg << " Player Coords: X = " << playerCoords.getX() <<  "  Z = " << playerCoords.getZ() <<"  Y = " << playerCoords.getY() << ";  ";
+
+				Vector3 coords = getWorldPosition();
+				exitMsg << " Area Coords - X = " << coords.getX() <<  "  Z = " << coords.getZ() <<"  Y = " << coords.getY() << ";  ";
+
+				if (!isNoSpawnArea()) {
+					ManagedReference<ActiveArea*> area = _this.getReferenceUnsafeStaticCast();
+
+					if (area != nullptr) {
+						SpawnArea* spawnArea = area.castTo<SpawnArea*>();
+
+						if (spawnArea != nullptr) {
+							int tier = spawnArea->getTier();
+
+							exitMsg << " Spawn Area:  Tier - " << tier << ";  ";
+						}
+					}
+				}
+
+				creo->sendSystemMessage(exitMsg.toString());
+			}
+		}
+	}
 
 	if (obj->isPlayerCreature() && attachedScenery.size() > 0) {
 		ManagedReference<SceneObject*> sceno = obj;
