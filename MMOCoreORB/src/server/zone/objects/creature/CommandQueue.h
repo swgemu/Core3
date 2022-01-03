ad8e2cfe3bb2b7db75d468004b08b4b70c5a4422
Hakry <Hakry@swgemu.com>
2022-01-03 13:13:32 -0500

    [refactor] CommandQueue with proper state handling and CommandQueue class to handle enquing, running and removing commands on creature objects [fixed] Auto attack not properly delaying next attack [added] Observer for combat queue commands being added to queue
    Note: Thank you to LordKator and TheAnswer for their assistance and insight
    
    Change-Id: I69628ff528125b045c909d1a503c97a8063f80b9

---
+++ MMOCoreORB/src/server/zone/objects/creature/CommandQueue.h
@@ -0,0 +1,107 @@
+/*
+		Copyright <SWGEmu>
+	See file COPYING for copying conditions.*/
+
+/*
+* CommandQueue.h
+*
+* Created on: Jan. 1, 2022
+* Author: Hakry
+*/
+
+#ifndef COMMANDQUEUE_H_
+#define COMMANDQUEUE_H_
+
+#include "engine/engine.h"
+#include "server/zone/objects/creature/variables/CommandQueueActionVector.h"
+
+//#define DEBUG_QUEUE
+
+class CommandQueueTask;
+
+class CommandQueue : public Object {
+	mutable Mutex queueMutex;
+
+	enum State {
+		NONE, WAITING, RUNNING, DELAY
+	};
+
+	WeakReference<CreatureObject*> weakCreature;
+	CommandQueueActionVector queueVector;
+	Reference<CommandQueueTask*> queueTask;
+	State state = NONE;
+	static const int DEFAULTTIME = 100;
+#ifdef DEBUG_QUEUE
+	uint64 runNumber = 0;
+	Time lastRun;
+#endif // DEBUG_QUEUE
+
+private:
+	void removeAction(CommandQueueAction* actionToDelete);
+	CommandReference<CommandQueueAction*> getNextAction();
+
+public:
+	CommandQueue(CreatureObject* creature);
+
+	~CommandQueue();
+
+	void run();
+	int handleRunningState();
+
+	bool checkForImmediateActions(CreatureObject* creature);
+	void enqueueCommand(unsigned int actionCRC, unsigned int actionCount, uint64 targetID, const UnicodeString& arguments, int priority, int compareCounter);
+	void sendCommand(uint32 crc, const UnicodeString& args, uint64 targetID, int priority);
+	void clearQueueAction(unsigned int actioncntr, float timer, unsigned int tab1, unsigned int tab2);
+	void clearQueueActions(bool combatOnly);
+
+	int getQueueSize() const {
+		return queueVector.size();
+	}
+
+	void deleteQueueAction(uint32 actionCount);
+
+	String toString() const;
+
+	String toStringData() const {
+		return toString();
+	}
+};
+
+class CommandQueueTask : public Task {
+	private:
+		WeakReference<CommandQueue*> weakQueue;
+
+	public:
+		CommandQueueTask(CommandQueue* queue) {
+			weakQueue = queue;
+		}
+
+	void run() {
+
+		auto commandQueue = weakQueue.get();
+
+		if (commandQueue == nullptr) {
+#ifdef DEBUG_QUEUE
+			Logger::console.info(true) << __PRETTY_FUNCTION__ << ":" << __LINE__ << " weakQueue.get() == nullptr!";
+#endif // DEBUG_QUEUE
+			return;
+		}
+
+		try {
+#ifdef DEBUG_QUEUE
+			Logger::console.info(true) << "######################################## Task Start";
+#endif // DEBUG_QUEUE
+			commandQueue->run();
+#ifdef DEBUG_QUEUE
+			Logger::console.info(true) << "######################################## Task Complete\n";
+#endif // DEBUG_QUEUE
+
+		} catch (Exception& e) {
+			e.printStackTrace();
+		} catch (...) {
+			throw;
+		}
+	}
+};
+
+#endif /* COMMANDQUEUE_H_ */
