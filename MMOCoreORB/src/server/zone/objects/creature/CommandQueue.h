9a3b256c9e299d52862021035b6225a07fdb9bab
Hakry <Hakry@swgemu.com>
2022-01-03 13:13:32 -0500

    [refactor] CommandQueue with proper state handling and CommandQueue class to handle enquing, running and removing commands on creature objects"
    Change-Id: I69628ff528125b045c909d1a503c97a8063f80b9

---
+++ MMOCoreORB/src/server/zone/objects/creature/CommandQueue.h
@@ -0,0 +1,40 @@
+/*
+* CommandQueue.h
+*
+* Created on: Jan. 1, 2022
+* Author: Hakry
+*/
+
+#ifndef COMMANDQUEUE_H_
+#define COMMANDQUEUE_H_
+
+#include "engine/engine.h"
+#include "server/zone/objects/creature/variables/CommandQueueActionVector.h"
+
+class CommandQueue {
+	enum State {
+		NONE, WAITING, RUNNING, DELAYED
+	};
+
+	ManagedWeakReference<CreatureObject*> weakCreature;
+	CommandQueueActionVector queueVector;
+	State state = NONE;
+
+public:
+	void initialize(CreatureObject* creature);
+	int run();
+	void handleRunningState();
+
+	void enqueueCommand(unsigned int actionCRC, unsigned int actionCount, uint64 targetID, const UnicodeString& arguments, int priority, int compareCounter);
+	void sendCommand(uint32 crc, const UnicodeString& args, uint64 targetID, int priority);
+	void clearQueueAction(unsigned int actioncntr, float timer, unsigned int tab1, unsigned int tab2);
+	void clearQueueActions(bool combatOnly);
+
+	int getQueueSize() const {
+		return queueVector.size();
+	}
+
+	void deleteQueueAction(uint32 actionCount);
+};
+
+#endif /* COMMANDQUEUE_H_ */
