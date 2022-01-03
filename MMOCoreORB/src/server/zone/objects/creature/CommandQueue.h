9e7edb70815c9fdc9cf4cd3ef28fbae1d80988c5
Hakry <Hakry@swgemu.com>
2022-01-03 13:13:32 -0500

    [refactor] CommandQueue with proper state handling and CommandQueue class to handle enquing, running and removing commands on creature objects [fixed] Auto attack not properly delaying next attack [added] Observer for combat queue commands being added to queue
    Change-Id: I69628ff528125b045c909d1a503c97a8063f80b9

---
+++ MMOCoreORB/src/server/zone/objects/creature/CommandQueue.h
@@ -0,0 +1,42 @@
+/*
+* CommandQueue.h
+*
+* Created on: Jan. 1, 2022
+* Author: Hakry
+*/
+
+#ifndef COMMANDQUEUE_H_
+#define COMMANDQUEUE_H_
+
+#include "engine/engine.h"
+#include "server/zone/objects/creature/variables/CommandQueueActionVector.h"
+
+class CommandQueue {
+	enum State {
+		NONE, WAITING, RUNNING, DELAY
+	};
+
+	WeakReference<CreatureObject*> weakCreature;
+	CommandQueueActionVector queueVector;
+	State state = NONE;
+	static const int DELAYCHECK = 250;
+
+public:
+	void initialize(CreatureObject* creature);
+	void run();
+	int handleRunningState();
+	void removeAction(CommandQueueAction* actionToDelete);
+
+	void enqueueCommand(unsigned int actionCRC, unsigned int actionCount, uint64 targetID, const UnicodeString& arguments, int priority, int compareCounter);
+	void sendCommand(uint32 crc, const UnicodeString& args, uint64 targetID, int priority);
+	void clearQueueAction(unsigned int actioncntr, float timer, unsigned int tab1, unsigned int tab2);
+	void clearQueueActions(bool combatOnly);
+
+	int getQueueSize() const {
+		return queueVector.size();
+	}
+
+	void deleteQueueAction(uint32 actionCount);
+};
+
+#endif /* COMMANDQUEUE_H_ */
