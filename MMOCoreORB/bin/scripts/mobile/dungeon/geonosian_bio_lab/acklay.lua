e7800e0217ec117c00809d85c246af6280772a36
Miztah <miztah@swgemu.com>
2022-02-01 01:39:29 +0000

    [added] Max leash range to acklay
    Change-Id: I97049908c1083bee45ebe84f7b411fb8262d263e

--- MMOCoreORB/bin/scripts/mobile/dungeon/geonosian_bio_lab/acklay.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/geonosian_bio_lab/acklay.lua
@@ -26,6 +26,7 @@ acklay = Creature:new {
 	creatureBitmask = PACK + KILLER,
 	optionsBitmask = AIENABLED,
 	diet = CARNIVORE,
+	customAiMap = "geoLabAcklay",
 
 	templates = {"object/mobile/acklay_hue.iff"},
 	lootGroups = {
