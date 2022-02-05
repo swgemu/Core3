c560899dad5a7c93ddfddce3c697a9cd382e728d
Hakry <Hakry@swgemu.com>
2022-02-04 19:29:42 -0500

    [fixed] Ewok villages to no longer patrol
    Change-Id: Idcbe0d7821d5488763398a14817fe661f8a2ff8c

--- MMOCoreORB/bin/scripts/screenplays/poi/endor_dulok_village_north.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/endor_dulok_village_north.lua
@@ -1,58 +1,72 @@
 EndorDulokVillageNorthScreenPlay = ScreenPlay:new {
 	numberOfActs = 1,
+	planet = "endor",
+
+	mobiles = {
+		{"donkuwah_chieftain",300, -1194.2, 25.0, 2988.4, 26, 0},
+		{"enraged_donkuwah",300, -1199.9, 25.0, 2981.6, 175, 0},
+		{"enraged_donkuwah",300, -1198.1, 25.0, 2980.0, -104, 0},
+		{"eerie_donkuwah_spiritmaster",300, -1178.1, 17.0, 2964.4, -142, 0},
+		{"haggard_donkuwah_battlelord",300, -1171.0, 17.0, 2957.4, -83, 0},
+		{"vile_donkuwah_battlelord",300, -1158.6, 17.0, 2953.2, -113, 0},
+		{"donkuwah_battlelord",300, -1171.1, 12.0, 2927.1, -98, 0},
+		{"donkuwah_battlelord",300, -1175.2, 12.0, 2928.6, 157, 0},
+		{"donkuwah_battlelord",300, -1181.3, 12.0, 2922.7, 31, 0},
+		{"donkuwah_battlelord",300, -1169.1, 12.0, 2920.7, -28, 0},
+		{"donkuwah_shaman",300, -1158.3, 12.0, 2933.3, -76, 0},
+		{"donkuwah_spiritmaster",300, -1181.2, 12.0, 2944.2, 171, 0},
+		{"foul_donkuwah_laborer",300, -1199.8, 12.0, 2925.2, -78, 0},
+		{"foul_donkuwah_laborer",300, -1202.0, 12.0, 2928.5, -168, 0},
+		{"foul_donkuwah_laborer",300, -1205.0, 12.0, 2925.8, 102, 0},
+		{"crafty_donkuwah_scout",300, -1205.9, 12.0, 2909.7, 158, 0},
+		{"beguiling_donkuwah_scout",300, -1213.1, 12.0, 2906.6, -165, 0},
+		{"tricky_donkuwah_scout",300, -1207.5, 12.0, 2911.2, 174, 0},
+		{"donkuwah_scout",300, -1252.9, 12.0, 2929.5, -119, 0},
+		{"donkuwah_scout",300, -1256.2, 12.0, 2930.9, -149, 0},
+		{"donkuwah_scout",300, -1253.7, 12.0, 2925.6, -85, 0},
+		{"donkuwah_laborer",300, -1238.1, 12.0, 2942.6, -127, 0},
+		{"donkuwah_laborer",300, -1241.5, 12.0, 2944.6, -172, 0},
+		{"foul_donkuwah_laborer",300, -1251.1, 12.0, 2950.8, -123, 0},
+		{"donkuwah_cub",300, -1270.2, 12.0, 2960.4, 38, 0},
+		{"donkuwah_cub",300, -1270.8, 12.0, 2962.6, 27, 0},
+		{"donkuwah_cub",300, -1270.2, 12.0, 2964.8, 93, 0},
+		{"donkuwah_cub",300, -1266.7, 12.0, 2963.3, -102, 0},
+		{"donkuwah_tribesman",300, -1259.0, 12.0, 2954.0, -38, 0},
+		{"donkuwah_tribesman",300, -1262.9, 12.0, 2956.3, 49, 0},
+		{"shaggy_donkuwah_youth",300, -1253.1, 12.0, 2973.1, -128, 0},
+		{"bewitching_donkuwah_shaman",300, -1261.7, 12.0, 2978.3, -136, 0},
+		{"grungy_donkuwah_laborer",300, -1273.0, 12.3, 2978.1, -160, 0},
+		{"grungy_donkuwah_laborer",300, -1275.9, 12.1, 2974.6, 117, 0},
+		{"gnarled_donkuwah_spiritmaster",300, -1244.5, 15.1, 2974.0, 15, 0},
+		{"vile_donkuwah_battlelord",300, -1246.4, 18.0, 2988.9, 125, 0},
+		{"vile_donkuwah_battlelord",300, -1231.0, 18.0, 2982.0, -76, 0},
+		{"vile_donkuwah_battlelord",300, -1231.5, 18.0, 2985.6, -105, 0},
+		{"frenzied_donkuwah",300, -1228.9, 18.0, 2990.7, -22, 0},
+		{"frenzied_donkuwah",300, -1230.3, 18.0, 2993.7, 108, 0},
+		{"frenzied_donkuwah",300, -1227.4, 18.0, 2993.4, -125, 0},
+		{"vicious_donkuwah_battlelord",300, -1221.6, 18, 2986.1, 27,  0},
+	}
 
 }
 
 registerScreenPlay("EndorDulokVillageNorthScreenPlay", true)
 
 function EndorDulokVillageNorthScreenPlay:start()
-	if (isZoneEnabled("endor")) then
+	if (isZoneEnabled(self.planet)) then
 		self:spawnMobiles()
 	end
 end
 
 function EndorDulokVillageNorthScreenPlay:spawnMobiles()
-    	spawnMobile("endor", "donkuwah_chieftain",300, -1194.2, 25.0, 2988.4, 26, 0)
-	spawnMobile("endor", "enraged_donkuwah",300, -1199.9, 25.0, 2981.6, 175, 0)
-	spawnMobile("endor", "enraged_donkuwah",300, -1198.1, 25.0, 2980.0, -104, 0)
-	spawnMobile("endor", "eerie_donkuwah_spiritmaster",300, -1178.1, 17.0, 2964.4, -142, 0)
-	spawnMobile("endor", "haggard_donkuwah_battlelord",300, -1171.0, 17.0, 2957.4, -83, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, -1158.6, 17.0, 2953.2, -113, 0)
-	spawnMobile("endor", "donkuwah_battlelord",300, -1171.1, 12.0, 2927.1, -98, 0)
-	spawnMobile("endor", "donkuwah_battlelord",300, -1175.2, 12.0, 2928.6, 157, 0)
-	spawnMobile("endor", "donkuwah_battlelord",300, -1181.3, 12.0, 2922.7, 31, 0)
-	spawnMobile("endor", "donkuwah_battlelord",300, -1169.1, 12.0, 2920.7, -28, 0)
-	spawnMobile("endor", "donkuwah_shaman",300, -1158.3, 12.0, 2933.3, -76, 0)
-	spawnMobile("endor", "donkuwah_spiritmaster",300, -1181.2, 12.0, 2944.2, 171, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, -1199.8, 12.0, 2925.2, -78, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, -1202.0, 12.0, 2928.5, -168, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, -1205.0, 12.0, 2925.8, 102, 0)
-	spawnMobile("endor", "crafty_donkuwah_scout",300, -1205.9, 12.0, 2909.7, 158, 0)
-	spawnMobile("endor", "beguiling_donkuwah_scout",300, -1213.1, 12.0, 2906.6, -165, 0)
-	spawnMobile("endor", "tricky_donkuwah_scout",300, -1207.5, 12.0, 2911.2, 174, 0)
-	spawnMobile("endor", "donkuwah_scout",300, -1252.9, 12.0, 2929.5, -119, 0)
-	spawnMobile("endor", "donkuwah_scout",300, -1256.2, 12.0, 2930.9, -149, 0)
-	spawnMobile("endor", "donkuwah_scout",300, -1253.7, 12.0, 2925.6, -85, 0)
-	spawnMobile("endor", "donkuwah_laborer",300, -1238.1, 12.0, 2942.6, -127, 0)
-	spawnMobile("endor", "donkuwah_laborer",300, -1241.5, 12.0, 2944.6, -172, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, -1251.1, 12.0, 2950.8, -123, 0)
-	spawnMobile("endor", "donkuwah_cub",300, -1270.2, 12.0, 2960.4, 38, 0)
-	spawnMobile("endor", "donkuwah_cub",300, -1270.8, 12.0, 2962.6, 27, 0)
-	spawnMobile("endor", "donkuwah_cub",300, -1270.2, 12.0, 2964.8, 93, 0)
-	spawnMobile("endor", "donkuwah_cub",300, -1266.7, 12.0, 2963.3, -102, 0)
-	spawnMobile("endor", "donkuwah_tribesman",300, -1259.0, 12.0, 2954.0, -38, 0)
-	spawnMobile("endor", "donkuwah_tribesman",300, -1262.9, 12.0, 2956.3, 49, 0)
-	spawnMobile("endor", "shaggy_donkuwah_youth",300, -1253.1, 12.0, 2973.1, -128, 0)
-	spawnMobile("endor", "bewitching_donkuwah_shaman",300, -1261.7, 12.0, 2978.3, -136, 0)
-	spawnMobile("endor", "grungy_donkuwah_laborer",300, -1273.0, 12.3, 2978.1, -160, 0)
-	spawnMobile("endor", "grungy_donkuwah_laborer",300, -1275.9, 12.1, 2974.6, 117, 0)
-	spawnMobile("endor", "gnarled_donkuwah_spiritmaster",300, -1244.5, 15.1, 2974.0, 15, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, -1246.4, 18.0, 2988.9, 125, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, -1231.0, 18.0, 2982.0, -76, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, -1231.5, 18.0, 2985.6, -105, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, -1228.9, 18.0, 2990.7, -22, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, -1230.3, 18.0, 2993.7, 108, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, -1227.4, 18.0, 2993.4, -125, 0)
-	spawnMobile("endor", "vicious_donkuwah_battlelord",300, -1221.6, 18, 2986.1, 27,  0)
+	local mobiles = self.mobiles
 
+	for i = 1, #mobiles do
+		local mob = mobiles[i]
+
+		local pMobile = spawnMobile(self.planet, mob[1], mob[2], mob[3], mob[4], mob[5], mob[6], mob[7])
+
+		if pMobile ~= nil then
+			AiAgent(pMobile):addCreatureFlag(AI_STATIONARY)
+		end
+	end
 end
--- MMOCoreORB/bin/scripts/screenplays/poi/endor_dulok_village_south.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/endor_dulok_village_south.lua
@@ -1,74 +1,86 @@
 EndorDulokVillageSouthScreenPlay = ScreenPlay:new {
 	numberOfActs = 1,
+	planet = "endor",
 
+	mobiles = {
+		{"donkuwah_scout",300, 5934.7, 235, -2514.5, -95, 0},
+		{"donkuwah_scout",300, 5934.2, 235.1, -2510, -95, 0},
+		{"donkuwah_scout",300, 5933.2, 234.7, -2505.3, -95, 0},
+
+		{"crafty_donkuwah_scout",300, 5997.3, 250, -2510.4, -148, 0},
+		{"beguiling_donkuwah_scout",300, 5993.7, 250, -2500.4, -104, 0},
+		{"tricky_donkuwah_scout",300, 5995.5, 250, -2482.3, -49, 0},
+
+		{"tricky_donkuwah_scout",300, 6075.6, 260, -2525.9, -179, 0},
+
+		{"bewitching_donkuwah_shaman",300, 6055, 262, -2492.5, -8, 0},
+		{"donkuwah_cub",300, 6050.2, 262, -2513.6, -151, 0},
+		{"donkuwah_cub",300, 6047.1, 262, -2513.7, 147, 0},
+		{"donkuwah_cub",300, 6047.3, 262, -2517.2, 34, 0},
+		{"donkuwah_cub",300, 6050.4, 262, -2517.3, -52, 0},
+		{"grungy_donkuwah_laborer",300, 6060.3, 262, -2519.2, -19, 0},
+		{"grungy_donkuwah_laborer",300, 6056.5, 262, -2518.1, 41, 0},
+		{"shaggy_donkuwah_youth",300, 6062.3, 262, -2501.1, -177, 0},
+		{"donkuwah_tribesman",300, 6045.5, 262, -2501.1, 130, 0},
+		{"donkuwah_tribesman",300, 6049.2, 262, -2500.6, -178, 0},
+
+		{"foul_donkuwah_laborer",300, 6091.4, 263, -2481.5, 29, 0},
+		{"foul_donkuwah_laborer",300, 6054.1, 262, -2502.9, -19, 0},
+		{"foul_donkuwah_laborer",300, 6077.8, 263, -2443.6, 98, 0},
+		{"foul_donkuwah_laborer",300, 6036.2, 262, -2453.4, -89, 0},
+
+		{"donkuwah_battlelord",300, 6030.4, 262, -2457.5, 85, 0},
+		{"donkuwah_battlelord",300, 6032.8, 262, -2447.9, 105, 0},
+		{"donkuwah_shaman",300, 6032.4, 262, -2468, 152, 0},
+		{"donkuwah_spiritmaster",300, 6047.4, 262, -2465.5, -65, 0},
+
+		{"eerie_donkuwah_spiritmaster",300, 6089.7, 263.1, -2475.5, -138, 0},
+		{"haggard_donkuwah_battlelord",300, 6098.4, 263.2, -2480.2, -134, 0},
+		{"vile_donkuwah_battlelord",300, 6100.2, 263, -2490, -68, 0},
+		{"vile_donkuwah_battlelord",300, 6093.9, 263, -2498.2, -45, 0},
+		{"vile_donkuwah_battlelord",300, 6100.9, 263, -2505.9, -103, 0},
+		{"vile_donkuwah_battlelord",300, 6092, 263, -2518.6, -25, 0},
+
+		{"frenzied_donkuwah",300, 6077.9, 263, -2451.1, -69, 0},
+		{"frenzied_donkuwah",300, 6084.2, 264.2, -2442.2, -77, 0},
+		{"frenzied_donkuwah",300, 6071.9, 263, -2432.8, -174, 0},
+		{"frenzied_donkuwah",300, 6062.8, 263, -2439.2, 148, 0},
+		{"frenzied_donkuwah",300, 6060.7, 263, -2427.6, -121, 0},
+
+		{"foul_donkuwah_laborer",300, 6044.4, 260, -2442.6, 140, 0},
+		{"foul_donkuwah_laborer",300, 6046, 260, -2444.5, -42, 0},
+
+		{"foul_donkuwah_laborer",300, 6075.1, 260, -2504.8, 0, 0},
+		{"foul_donkuwah_laborer",300, 6075.1, 260, -2502.7, 175, 0},
+
+		{"vile_donkuwah_battlelord",300, 6081.1, 263.6, -2463.2, 59, 0},
+		{"vile_donkuwah_battlelord",300, 6083.2, 263.8, -2464, -49, 0},
+		{"vile_donkuwah_battlelord",300, 6083.3, 263.9, -2461.6, -157, 0},
+
+		{"donkuwah_chieftain",300, 6108.6, 265, -2450.7, 60, 0},
+		{"enraged_donkuwah",300, 6098.9, 265, -2459.4, -93, 0},
+		{"enraged_donkuwah",300, 6096.3, 265, -2454.7, -160, 0},
+	}
 }
 
 registerScreenPlay("EndorDulokVillageSouthScreenPlay", true)
 
 function EndorDulokVillageSouthScreenPlay:start()
-	if (isZoneEnabled("endor")) then
+	if (isZoneEnabled(self.planet)) then
 		self:spawnMobiles()
 	end
 end
 
 function EndorDulokVillageSouthScreenPlay:spawnMobiles()
+	local mobiles = self.mobiles
 
-	spawnMobile("endor", "donkuwah_scout",300, 5934.7, 235, -2514.5, -95, 0)
-	spawnMobile("endor", "donkuwah_scout",300, 5934.2, 235.1, -2510, -95, 0)
-	spawnMobile("endor", "donkuwah_scout",300, 5933.2, 234.7, -2505.3, -95, 0)
+	for i = 1, #mobiles do
+		local mob = mobiles[i]
 
-	spawnMobile("endor", "crafty_donkuwah_scout",300, 5997.3, 250, -2510.4, -148, 0)
-	spawnMobile("endor", "beguiling_donkuwah_scout",300, 5993.7, 250, -2500.4, -104, 0)
-	spawnMobile("endor", "tricky_donkuwah_scout",300, 5995.5, 250, -2482.3, -49, 0)
+		local pMobile = spawnMobile(self.planet, mob[1], mob[2], mob[3], mob[4], mob[5], mob[6], mob[7])
 
-	spawnMobile("endor", "tricky_donkuwah_scout",300, 6075.6, 260, -2525.9, -179, 0)
-
-	spawnMobile("endor", "bewitching_donkuwah_shaman",300, 6055, 262, -2492.5, -8, 0)
-	spawnMobile("endor", "donkuwah_cub",300, 6050.2, 262, -2513.6, -151, 0)
-	spawnMobile("endor", "donkuwah_cub",300, 6047.1, 262, -2513.7, 147, 0)
-	spawnMobile("endor", "donkuwah_cub",300, 6047.3, 262, -2517.2, 34, 0)
-	spawnMobile("endor", "donkuwah_cub",300, 6050.4, 262, -2517.3, -52, 0)
-	spawnMobile("endor", "grungy_donkuwah_laborer",300, 6060.3, 262, -2519.2, -19, 0)
-	spawnMobile("endor", "grungy_donkuwah_laborer",300, 6056.5, 262, -2518.1, 41, 0)
-	spawnMobile("endor", "shaggy_donkuwah_youth",300, 6062.3, 262, -2501.1, -177, 0)
-	spawnMobile("endor", "donkuwah_tribesman",300, 6045.5, 262, -2501.1, 130, 0)
-	spawnMobile("endor", "donkuwah_tribesman",300, 6049.2, 262, -2500.6, -178, 0)
-
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6091.4, 263, -2481.5, 29, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6054.1, 262, -2502.9, -19, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6077.8, 263, -2443.6, 98, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6036.2, 262, -2453.4, -89, 0)
-
-	spawnMobile("endor", "donkuwah_battlelord",300, 6030.4, 262, -2457.5, 85, 0)
-	spawnMobile("endor", "donkuwah_battlelord",300, 6032.8, 262, -2447.9, 105, 0)
-	spawnMobile("endor", "donkuwah_shaman",300, 6032.4, 262, -2468, 152, 0)
-	spawnMobile("endor", "donkuwah_spiritmaster",300, 6047.4, 262, -2465.5, -65, 0)
-
-	spawnMobile("endor", "eerie_donkuwah_spiritmaster",300, 6089.7, 263.1, -2475.5, -138, 0)
-	spawnMobile("endor", "haggard_donkuwah_battlelord",300, 6098.4, 263.2, -2480.2, -134, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6100.2, 263, -2490, -68, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6093.9, 263, -2498.2, -45, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6100.9, 263, -2505.9, -103, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6092, 263, -2518.6, -25, 0)
-
-	spawnMobile("endor", "frenzied_donkuwah",300, 6077.9, 263, -2451.1, -69, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, 6084.2, 264.2, -2442.2, -77, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, 6071.9, 263, -2432.8, -174, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, 6062.8, 263, -2439.2, 148, 0)
-	spawnMobile("endor", "frenzied_donkuwah",300, 6060.7, 263, -2427.6, -121, 0)
-
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6044.4, 260, -2442.6, 140, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6046, 260, -2444.5, -42, 0)
-
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6075.1, 260, -2504.8, 0, 0)
-	spawnMobile("endor", "foul_donkuwah_laborer",300, 6075.1, 260, -2502.7, 175, 0)
-
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6081.1, 263.6, -2463.2, 59, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6083.2, 263.8, -2464, -49, 0)
-	spawnMobile("endor", "vile_donkuwah_battlelord",300, 6083.3, 263.9, -2461.6, -157, 0)
-
-	spawnMobile("endor", "donkuwah_chieftain",300, 6108.6, 265, -2450.7, 60, 0)
-	spawnMobile("endor", "enraged_donkuwah",300, 6098.9, 265, -2459.4, -93, 0)
-	spawnMobile("endor", "enraged_donkuwah",300, 6096.3, 265, -2454.7, -160, 0)
-
+		if pMobile ~= nil then
+			AiAgent(pMobile):addCreatureFlag(AI_STATIONARY)
+		end
+	end
 end
