9ff941a9d7b15a68eb9b074be38cbc28de973872
Smoki <swgsmoki@gmail.com>
2022-02-10 13:45:54 -0500

    [Fixed] Client position desynchronization using elevators, Mantis#8480 [fixed] Pets within range not being moved when a player uses an elevator
    Change-Id: I33a1dd602d845812bdd310a733e48ca220ab6827

--- MMOCoreORB/src/server/zone/objects/tangible/components/ElevatorMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ElevatorMenuComponent.cpp
@@ -8,60 +8,66 @@
 #include "ElevatorMenuComponent.h"
 #include "server/zone/packets/object/ObjectMenuResponse.h"
 #include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/managers/collision/CollisionManager.h"
 #include "server/zone/objects/cell/CellObject.h"
 
-void ElevatorMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, ObjectMenuResponse* menuResponse, CreatureObject* creature) const {
+void ElevatorMenuComponent::fillObjectMenuResponse(SceneObject* terminal, ObjectMenuResponse* menuResponse, CreatureObject* playerCreo) const {
 	menuResponse->addRadialMenuItem(198, 3, "@elevator_text:up");
 	menuResponse->addRadialMenuItem(199, 3, "@elevator_text:down");
 }
 
-int ElevatorMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* creature, byte selectedID) const {
-	ManagedReference<SceneObject*> parent = creature->getParent().get();
+int ElevatorMenuComponent::handleObjectMenuSelect(SceneObject* terminal, CreatureObject* playerCreo, byte selectedID) const {
+	ManagedReference<SceneObject*> parent = playerCreo->getParent().get();
 
-	if (parent == nullptr || !parent->isCellObject() || parent != sceneObject->getParent().get())
+	if (parent == nullptr || !parent->isCellObject())
 		return 0;
+
+	if (parent != terminal->getParent().get()) {
+		playerCreo->sendSystemMessage("@elevator_text:too_far"); // You are too far away to use that.
+		return 0;
+	}
 
 	CellObject* cell = parent.castTo<CellObject*>();
 
-	float x = creature->getPositionX();
-	float y = creature->getPositionY();
-	float z = sceneObject->getPositionZ();
+	float newX = playerCreo->getPositionX();
+	float newY = playerCreo->getPositionY();
+	float newZ = playerCreo->getPositionZ();
 
-	Vector<float>* floors = CollisionManager::getCellFloorCollision(x, y, cell);
+	Vector<float>* floors = CollisionManager::getCellFloorCollision(newX, newY, cell);
 	int floorCount = floors->size();
 
 	int i = 0;
 	for (; i < floorCount; ++i) {
 		float f = floors->get(i);
 
-		if (fabs(z - f) < 1.f)
+		if (fabs(newZ - f) < 1.f)
 			break; //Almost certainly the same floor.
 	}
 
 	switch (selectedID) {
 	case 198: //UP
 		if (i <= 0) {
-			creature->sendSystemMessage("You are already on the highest floor");
+			playerCreo->sendSystemMessage("@elevator_text:highest_floor"); // You are already on the highest floor.
 			delete floors;
 			floors = nullptr;
 			return 0;
 		}
 
-		z = floors->get(i - 1);
-		creature->playEffect("clienteffect/elevator_rise.cef", "");
+		newZ = floors->get(i - 1);
+		playerCreo->playEffect("clienteffect/elevator_rise.cef", "");
 		break;
 	case 199: //DOWN
 		if (i >= floorCount - 1) {
-			creature->sendSystemMessage("You are already on the lowest floor");
+			playerCreo->sendSystemMessage("@elevator_text:lowest_floor"); // You are already on the lowest floor.
 			delete floors;
 			floors = nullptr;
 			return 0;
 		}
 
-		z = floors->get(i + 1);
-		creature->playEffect("clienteffect/elevator_descend.cef", "");
+		newZ = floors->get(i + 1);
+		playerCreo->playEffect("clienteffect/elevator_descend.cef", "");
 		break;
 	default:
 		delete floors;
@@ -69,11 +75,31 @@ int ElevatorMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Crea
 		return 0;
 	}
 
-	creature->teleport(x, z, y, sceneObject->getParentID());
-
 	delete floors;
 	floors = nullptr;
 
+	playerCreo->teleport(newX, newZ, newY, parent->getObjectID());
+
+	parent->broadcastObject(playerCreo, true);
+
+	PlayerObject* ghost = playerCreo->getPlayerObject();
+
+	if (ghost == nullptr)
+		return 0;
+
+	// Pets
+	for (int i = 0; i < ghost->getActivePetsSize(); i++) {
+		ManagedReference<AiAgent*> pet = ghost->getActivePet(i);
+
+		if (pet != nullptr) {
+			if (pet->isDead() || pet->isIncapacitated() || !pet->isInRange(playerCreo, 7.f))
+				continue;
+
+			pet->teleport(newX, newZ, newY, parent->getObjectID());
+
+			parent->broadcastObject(pet, true);
+		}
+	}
+
 	return 0;
 }
-
