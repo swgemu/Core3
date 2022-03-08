4a9a66fdf4cbdbd394f74d081b53b2043e02bdab
hakry <hakry@swgemu.com>
2022-03-08 11:59:12 -0500

    [fixed] Some issues with AI not dropping target after combat breaks [fixed] Contraband scanners from continuously following a target after a scan
    Change-Id: Ia4aab93a1c7060289d00465e61db1f9ef23effbc

--- MMOCoreORB/src/server/zone/managers/creature/AiMap.h
+++ MMOCoreORB/src/server/zone/managers/creature/AiMap.h
@@ -167,6 +167,7 @@ public:
 		lua->setGlobalInt("FOLLOW_FORMATION",			AiAgent::FOLLOW_FORMATION				);
 		lua->setGlobalInt("MOVING_TO_HEAL",				AiAgent::MOVING_TO_HEAL					);
 		lua->setGlobalInt("NOTIFY_ALLY",				AiAgent::NOTIFY_ALLY					);
+		lua->setGlobalInt("CRACKDOWN_SCANNING",			AiAgent::CRACKDOWN_SCANNING				);
 
 		lua->setGlobalInt("UPRIGHT",					CreaturePosture::UPRIGHT				);
 		lua->setGlobalInt("CROUCHED",					CreaturePosture::CROUCHED				);
