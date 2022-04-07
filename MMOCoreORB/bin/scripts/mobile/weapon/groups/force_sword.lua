945f78b15873d3d7f46a18d1484098730eaef382
hakry <hakry@swgemu.com>
2022-04-06 21:30:33 -0400

    [added] missing stun batons to force_sword weapon group
    Change-Id: I453af741e9934d0fbba13ef8f30213d79c977ff4

--- MMOCoreORB/bin/scripts/mobile/weapon/groups/force_sword.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/force_sword.lua
@@ -2,7 +2,8 @@ force_sword = {
 	"object/weapon/melee/sword/sword_01.iff",
 	"object/weapon/melee/sword/sword_02.iff",
 	"object/weapon/melee/2h_sword/2h_sword_maul.iff",
-	"object/weapon/melee/sword/sword_nyax.iff"
+	"object/weapon/melee/sword/sword_nyax.iff",
+	"object/weapon/melee/baton/baton_stun.iff"
 }
 
 addWeapon("force_sword", force_sword)
--- MMOCoreORB/bin/scripts/mobile/weapon/groups/force_sword_ranged.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/force_sword_ranged.lua
@@ -2,7 +2,8 @@ force_sword_ranged = {
 	"object/weapon/melee/sword/sword_01_ranged.iff",
 	"object/weapon/melee/sword/sword_02_ranged.iff",
 	"object/weapon/melee/2h_sword/2h_sword_maul_ranged.iff",
-	"object/weapon/melee/sword/sword_nyax_ranged.iff"
+	"object/weapon/melee/sword/sword_nyax_ranged.iff",
+	"object/weapon/melee/baton/baton_stun.iff"
 }
 
 addWeapon("force_sword_ranged", force_sword_ranged)
