b3fbd793c55f5db3c8ff664a1b059c129bea2342
hakry <hakry@swgemu.com>
2022-04-07 12:11:05 -0400

    [changed] ScreenPlayObserver to log error instead of fatal when notifyObserver event
    Change-Id: I6d1e77d7012be3ffdd15af5af5cf2459d74e9057

--- MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/director/ScreenPlayObserverImplementation.cpp
@@ -21,19 +21,19 @@ int ScreenPlayObserverImplementation::notifyObserverEvent(uint32 eventType, Obse
 		startScreenPlay.callFunction();
 
 		if (lua_gettop(lua->getLuaState()) == 0) {
-			Logger::console.fatal() << "ScreenPlayObserverImplementation::notifyObserverEvent didnt return a value from " << play << ":" << key;
+			Logger::console.error() << "ScreenPlayObserverImplementation::notifyObserverEvent didn't return a value from " << play << ":" << key;
 
 			return 1;
 		}
 
 		if (!lua_isnumber(lua->getLuaState(), -1)) {
-			Logger::console.fatal() <<  "ScreenPlayObserver " << play << ":" << key << "didnt return a valid value in an observer handler";
+			Logger::console.error() <<  "ScreenPlayObserverImplementation::notifyObserverEvent didn't return a valid value in an observer handler " << play << ":" << key;
 		}
 
 		ret = lua->getIntParameter(lua->getLuaState());
 
 	} catch (const LuaPanicException& panic) {
-		Logger::console.error() << "Panic exception: " << panic.getMessage() << " while trying to run SceenPlayObserver: " << play << ":" << key;
+		Logger::console.error() << "ScreenPlayObserverImplementation -- Panic exception while trying to run ScreenPlayObserver: " << play << ":" << key << " " << panic.getMessage();
 	}
 
 	//1 remove observer, 0 keep observer
