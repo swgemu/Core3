73706210db1aa6995cce1b6e4e4c008819945b22
Miztah <miztah@swgemu.com>
2022-05-25 02:24:42 +0000

    [wip] coa3
    Change-Id: Ifc2ab85d0c26179c9794fa087a1f55cdca7e5c49

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeBadgeCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeBadgeCommand.h
@@ -21,6 +21,47 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		if (arguments.isEmpty() && target == 0) {
+			creature->sendSystemMessage("Syntax: /revokeBadge badgeID");
+			return GENERALERROR;
+		}
+
+		ZoneServer* zoneServer = server->getZoneServer();
+
+		if (zoneServer == nullptr)
+			return GENERALERROR;
+
+		ManagedReference<SceneObject*> targetObject = zoneServer->getObject(target);
+
+		if (targetObject == nullptr || !targetObject->isPlayerCreature())
+			return GENERALERROR;
+
+		StringTokenizer args(arguments.toString());
+
+		int badgeId = args.getIntToken();
+
+		if (badgeId <= 0) {
+			creature->sendSystemMessage("Improper Badge ID.");
+			return GENERALERROR;
+		}
+
+		ManagedReference<CreatureObject*> player = targetObject->asCreatureObject();
+
+		if (player == nullptr)
+			return GENERALERROR;
+
+		PlayerObject* ghost = player->getPlayerObject();
+
+		if (ghost == nullptr)
+			return GENERALERROR;
+
+		Locker clock(ghost, creature);
+
+		ghost->revokeBadge(badgeId);
+
+		player->sendSystemMessage("Badge #" + String::valueOf(badgeId) + " has been revoked by " + creature->getDisplayedName() + ".");
+		creature->sendSystemMessage("Revoked badge " + String::valueOf(badgeId) + " from " + player->getDisplayedName() + ".");
+
 		return SUCCESS;
 	}
 
