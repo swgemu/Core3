38f998a309d22e4ee630cc9aa01e83d089bd1ea8
hakry <hakry@swgemu.com>
2022-07-08 16:19:40 -0400

    [fixed] Medium naboo house sign direction
    Change-Id: Ia81379367f61e1aa75a5c5515619eefcc344bedb

--- MMOCoreORB/bin/scripts/object/building/player/player_house_naboo_medium_style_01.lua
+++ MMOCoreORB/bin/scripts/object/building/player/player_house_naboo_medium_style_01.lua
@@ -53,11 +53,11 @@ object_building_player_player_house_naboo_medium_style_01 = object_building_play
 		{"private_safe_logout", 1}
 	},
 	childObjects = {
-		{templateFile = "object/tangible/sign/player/house_address_naboo.iff", x = -0.5, z = 2.25, y = -11.5, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
+		{templateFile = "object/tangible/sign/player/house_address_naboo.iff", x = -0.5, z = 2.25, y = -11.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
 		{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 9.89, z = 1.1, y = -10.75, ox = 0, oy = 0, oz = 0, ow = 1, cellid = 1, containmentType = -1},
 	},
 	shopSigns = {
-		{templateFile = "object/tangible/sign/player/house_address_naboo.iff", x = -0.5, z = 2.25, y = -11.5, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
+		{templateFile = "object/tangible/sign/player/house_address_naboo.iff", x = -0.5, z = 2.25, y = -11.5, ox = 0, oy = -0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
 		{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = 3.62, z = 0.5, y = -15.62, ox = 0, oy = 1, oz = 0, ow =  0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_01", suiItem = "@player_structure:shop_sign1"},
 		{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = 3.62, z = 0.5, y = -15.62, ox = 0, oy = 1, oz = 0, ow =  0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_02", suiItem = "@player_structure:shop_sign2"},
 		{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = 3.62, z = 0.5, y = -15.62, ox = 0, oy = 1, oz = 0, ow =  0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_03", suiItem = "@player_structure:shop_sign3"},
