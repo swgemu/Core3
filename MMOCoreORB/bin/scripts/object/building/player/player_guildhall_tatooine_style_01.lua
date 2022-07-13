7d4e2c2cdcbf451238df432aa556b89b0267456b
hakry <hakry@swgemu.com>
2022-07-13 01:00:19 -0400

    [fixed] Tatooine guild hall style 1 merchant sign locations - Mantis #8203
    Change-Id: I5dc8477d7c478c324cf2341a8c50ac7ac5ca18a7

--- MMOCoreORB/bin/scripts/object/building/player/player_guildhall_tatooine_style_01.lua
+++ MMOCoreORB/bin/scripts/object/building/player/player_guildhall_tatooine_style_01.lua
@@ -64,10 +64,10 @@ object_building_player_player_guildhall_tatooine_style_01 = object_building_play
 	},
 	shopSigns = {
 		{templateFile = "object/tangible/sign/player/house_address_tatooine.iff", x = 4.15, z = 1.25, y = 21.7, ox = 0, oy = 1, oz = 0, ow = 0, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
-		{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = -7.52, z = 0.5, y = 13.51, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_01", suiItem = "@player_structure:shop_sign1"},
-		{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = -7.52, z = 0.5, y = 13.51, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_02", suiItem = "@player_structure:shop_sign2"},
-		{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = -7.52, z = 0.5, y = 13.51, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_03", suiItem = "@player_structure:shop_sign3"},
-		{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = -7.52, z = 0.5, y = 13.51, ox = 0, oy = 0, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_04", suiItem = "@player_structure:shop_sign4"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = 9.1, z = 0, y = 22.25, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_01", suiItem = "@player_structure:shop_sign1"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = 9.1, z = 0, y = 22.25, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_02", suiItem = "@player_structure:shop_sign2"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = 9.1, z = 0, y = 22.25, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_03", suiItem = "@player_structure:shop_sign3"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = 9.1, z = 0, y = 22.25, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_04", suiItem = "@player_structure:shop_sign4"},
 	},
 }
 
