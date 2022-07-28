e68c7ea3e0704dfa33db88ed4269cc6d1817847f
Halyn <cneumi@hotmail.com>
2022-07-28 14:04:26 -0500

    [refactored] Quest tasks now load columns dynamically.
    Change-Id: Ib35e9cf965965b9ecea8cc8bcdfb1e875e7d9087

--- MMOCoreORB/src/server/zone/managers/player/LuaQuestTask.h
+++ MMOCoreORB/src/server/zone/managers/player/LuaQuestTask.h
@@ -18,12 +18,48 @@ namespace player {
 		~LuaQuestTask();
 
 		int _setObject(lua_State* L);
+		int getPrerequisiteTasks(lua_State* L);
+		int getExclusionTasks(lua_State* L);
 		int getAttachScript(lua_State* L);
+		int getJournalEntryTitle(lua_State* L);
+		int getJournalEntryDescription(lua_State* L);
+		int getTasksOnComplete(lua_State* L);
+		int getTasksOnFail(lua_State* L);
+		int getTaskName(lua_State* L);
+		int getIsVisible(lua_State* L);
+		int getShowSystemMessages(lua_State* L);
+		int getAllowRepeats(lua_State* L);
+		int getMusicOnActivate(lua_State* L);
+		int getTarget(lua_State* L);
+		int getParameter(lua_State* L);
+		int getGrantQuestOnComplete(lua_State* L);
+		int getGrantQuestOnFail(lua_State* L);
+		int getExperienceType(lua_State* L);
+		int getExperienceAmount(lua_State* L);
+		int getFactionName(lua_State* L);
+		int getFactionAmount(lua_State* L);
+		int getBankCredits(lua_State* L);
+		int getItem(lua_State* L);
 		int getPlanetName(lua_State* L);
 		int getLocationX(lua_State* L);
 		int getLocationY(lua_State* L);
 		int getLocationZ(lua_State* L);
 		int getRadius(lua_State* L);
+		int getSignalName(lua_State* L);
+		int getMinTime(lua_State* L);
+		int getMaxTime(lua_State* L);
+		int getCreatureType(lua_State* L);
+		int getTargetServerTemplate(lua_State* L);
+		int getSocialGroup(lua_State* L);
+		int getRequiredRegion(lua_State* L);
+		int getCount(lua_State* L);
+		int getMinDistance(lua_State* L);
+		int getMaxDistance(lua_State* L);
+		int getDirection(lua_State* L);
+		int getRewardCredits(lua_State* L);
+		int getLootItemName(lua_State* L);
+		int getLootItemsRequired(lua_State* L);
+		int getLootDropPercent(lua_State* L);
 
 	private:
 		Reference<QuestTask*> realObject;
