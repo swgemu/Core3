1dcbdd87a23934ecd0cc85add247751e35ccb550
hakry <hakry@swgemu.com>
2022-10-26 21:03:11 -0400

    [modified] notifyDespawn to cancel AI think events when agent is despawned [added] think event mutex lock [removed] activate movement being called when notifyDissapear is called for AiAgents
    Change-Id: I25dc3a55bce8ab97f93b73746febd52251de8925

--- MMOCoreORB/bin/scripts/screenplays/tests/ai_test.lua
+++ MMOCoreORB/bin/scripts/screenplays/tests/ai_test.lua
@@ -3,7 +3,7 @@ AITest = ScreenPlay:new {
 	planet = "creature_test",
 
 	location = {0, 0, 0},
-
+	numberOfSpawns = 5,
 }
 
 registerScreenPlay("AITest", false)
@@ -18,13 +18,41 @@ end
 
 function AITest:spawnMobiles()
 	local loc = self.location
-	local x = loc[1]
-	local y = loc[3]
+	local x = loc[1] + getRandomNumber(10)
+	local y = loc[3] + getRandomNumber(10)
+	local z = getWorldFloor(x, y, self.planet)
+	local spawnNum = self.numberOfSpawns
+
+	for i = 1, spawnNum, 1 do
+		local pMobile = spawnMobile(self.planet, "relentless_purbole", -1, x, z, y, 0, 0)
+
+		if (pMobile ~= nil) then
+			--AiAgent(pMobile):addCreatureFlag(AI_STATIONARY)
+
+			createObserver(OBJECTDESTRUCTION, "AITest", "notifyKilled", pMobile)
+		end
+	end
+end
+
+function AITest:spawnMobile()
+	local loc = self.location
+	local x = loc[1] + getRandomNumber(10)
+	local y = loc[3] + getRandomNumber(10)
 	local z = getWorldFloor(x, y, self.planet)
 
-	local pMobile = spawnMobile(self.planet, "gondula_worker", -1, x, z, y, 0, 0)
+	local pMobile = spawnMobile(self.planet, "relentless_purbole", -1, x, z, y, 0, 0)
 
 	if (pMobile ~= nil) then
-		AiAgent(pMobile):addCreatureFlag(AI_STATIONARY)
+		--AiAgent(pMobile):addCreatureFlag(AI_STATIONARY)
+
+		createObserver(OBJECTDESTRUCTION, "AITest", "notifyKilled", pMobile)
 	end
+end
+
+function AITest:notifyKilled(pAgent, pKiller)
+	if (pAgent ~= nil) then
+		self:spawnMobile()
+	end
+
+	return 1
 end
