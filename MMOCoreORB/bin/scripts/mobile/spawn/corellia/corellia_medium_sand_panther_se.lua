3f5cf80b246a1c553b0e5410fc8e33901f1ac75f
hakry <hakry@swgemu.com>
2022-12-23 14:49:14 -0500

    [added] corellia spawn groups - Final
    Change-Id: I6d7b767d7686f352b63db932f193a23259a1dceb

---
+++ MMOCoreORB/bin/scripts/mobile/spawn/corellia/corellia_medium_sand_panther_se.lua
@@ -0,0 +1,78 @@
+corellia_medium_sand_panther_se = {
+	lairSpawns = {
+		{
+			lairTemplateName = "corellia_razor_cat_cub_hunt_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 1,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 50,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_razor_cat_pride_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 150,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 50,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_grassland_slice_hound_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 65,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_grassland_slice_hound_pack_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 65,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_humbaba_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 35,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_humbaba_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 35,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_paralope_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 30,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+		{
+			lairTemplateName = "corellia_paralope_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 30,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 40,
+			size = 15,
+		},
+	}
+}
+
+addSpawnGroup("corellia_medium_sand_panther_se", corellia_medium_sand_panther_se);
---
+++ MMOCoreORB/bin/scripts/mobile/spawn/corellia/corellia_northeastern_dark_adept_hard.lua
@@ -0,0 +1,24 @@
+corellia_northeastern_dark_adept_hard = {
+	lairSpawns = {
+		{
+			lairTemplateName = "global_dark_adept_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 35,
+			maxDifficulty = 40,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+		{
+			lairTemplateName = "corellia_beldonna_base_neutral_large_theater",
+			spawnLimit = -1,
+			minDifficulty = 35,
+			maxDifficulty = 40,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+	}
+}
+
+addSpawnGroup("corellia_northeastern_dark_adept_hard", corellia_northeastern_dark_adept_hard);
