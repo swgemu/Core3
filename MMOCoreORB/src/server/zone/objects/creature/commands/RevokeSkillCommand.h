ae2de58e4a93da2afb3193ff02a89bbae49f2f52
hakry <hakry@swgemu.com>
2022-12-24 08:25:43 -0500

    [added] revoke skill command for support staff
    Change-Id: Idf7e6f009e195d764668812f8558a32bf1eaa876

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
@@ -5,25 +5,51 @@
 #ifndef REVOKESKILLCOMMAND_H_
 #define REVOKESKILLCOMMAND_H_
 
+#include "server/zone/managers/skill/SkillManager.h"
+
 class RevokeSkillCommand : public QueueCommand {
 public:
-
-	RevokeSkillCommand(const String& name, ZoneProcessServer* server)
-		: QueueCommand(name, server) {
-
+	RevokeSkillCommand(const String& name, ZoneProcessServer* server) : QueueCommand(name, server) {
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+
+		if (object == nullptr || !object->isPlayerCreature())
+			return INVALIDTARGET;
+
+		CreatureObject* targetCreature = object->asCreatureObject();
+
+		if (targetCreature == nullptr)
+			return GENERALERROR;
+
+		String skillString = arguments.toString();
+
+		if (!targetCreature->hasSkill(skillString)) {
+			creature->sendSystemMessage(targetCreature->getFirstName() + " does not have the skill: " + skillString);
+			return GENERALERROR;
+		}
+
+		Locker clocker(targetCreature, creature);
+
+		SkillManager* skillManager = SkillManager::instance();
+
+		if (skillManager == nullptr)
+			return GENERALERROR;
+
+		if (skillManager->surrenderSkill(skillString, targetCreature, true)) {
+			creature->sendSystemMessage("Successfully revoked skill: " + skillString + " from player " + targetCreature->getFirstName());
+			targetCreature->sendSystemMessage("Skill: " + skillString + " has been revoked.");
+		}
+
 		return SUCCESS;
 	}
-
 };
 
-#endif //REVOKESKILLCOMMAND_H_
+#endif // REVOKESKILLCOMMAND_H_
