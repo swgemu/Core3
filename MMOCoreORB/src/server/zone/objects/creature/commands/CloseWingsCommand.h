55331a38431709026aabe033b5a35f9cc67ccb31
hakry <hakry@swgemu.com>
2023-01-16 01:04:29 -0500

    [added] open and close wings commands
    Change-Id: I430ae3234f13da46d17593eb2297b2fbb1c40fa7

--- MMOCoreORB/src/server/zone/objects/creature/commands/CloseWingsCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/CloseWingsCommand.h
@@ -5,24 +5,45 @@
 #ifndef CLOSEWINGS_H_
 #define CLOSEWINGS_H_
 
-#include "CombatQueueCommand.h"
+#include "QueueCommand.h"
 
-class CloseWingsCommand : public CombatQueueCommand {
+class CloseWingsCommand : public QueueCommand {
 public:
 
-	CloseWingsCommand(const String& name, ZoneProcessServer* server)
-		: CombatQueueCommand(name, server) {
+	CloseWingsCommand(const String& name, ZoneProcessServer* server) : QueueCommand(name, server) {
 	}
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
-
 		if (!checkStateMask(creature))
 			return INVALIDSTATE;
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
-		return doCombatAction(creature, target);
+		if (!creature->hasState(CreatureState::PILOTINGSHIP) && !creature->hasState(CreatureState::PILOTINGPOBSHIP)) {
+			return INVALIDSTATE;
+		}
+
+		SceneObject* rootParent = creature->getRootParent();
+
+		if (rootParent == nullptr || !rootParent->isShipObject())
+			return GENERALERROR;
+
+		ManagedReference<ShipObject*> ship = rootParent->asShipObject();
+
+		if (ship == nullptr)
+			return GENERALERROR;
+
+		uint32 optionsBitmask = ship->getOptionsBitmask();
+
+		if (!(optionsBitmask & OptionBitmask::WINGS_OPEN))
+			return GENERALERROR;
+
+		Locker lock(ship);
+
+		ship->clearOptionBit(OptionBitmask::WINGS_OPEN);
+
+		return SUCCESS;
 	}
 
 };
--- MMOCoreORB/src/templates/params/OptionBitmask.h
+++ MMOCoreORB/src/templates/params/OptionBitmask.h
@@ -8,39 +8,36 @@
 class OptionBitmask {
 public:
 	enum {
-		NONE           = 0x00000000,
-		ACTIVATED      = 0x00000001, // Used to turn structures on/off (harvesters)
-		VENDOR         = 0x00000002, // Vendor flag in creatures
-		INSURED        = 0x00000004,
-		CONVERSE       = 0x00000008, // Converse flag in creatures
-		UNKNOWN1       = 0x00000010,
-		YELLOW         = 0x00000020, // Yellow items in tangibles -- should be set whenever an item is modified (pup, SEA, BE, etc) and probably also legendary/exceptional loot
-		FACTIONAGGRO   = 0x00000040, // Sets color of mobs if they have a pvp faction and can aggro a player
-		AIENABLED      = 0x00000080, // Default bitmask for mobiles
-		INVULNERABLE   = 0x00000100, // Shows no information about condition or HAM
-		DISABLED       = 0x00000200, // Used to add (disabled) text to vehicles client-side. TODO: Change vehicle disable code to use this
-		UNKNOWN3       = 0x00000400,
-		INTERESTING    = 0x00000800, // Info Icon 1, denotes a quest giver
-		VEHICLE        = 0x00001000, // Vehicles will only appear with this bit enabled
-		HASSERIAL      = 0x00002000, // Has a serial number
-		UNKNOWN13      = 0x00004000,
-		JTLINTERESTING = 0x00008000, // Info Icon 2, denotes a JTL quest giver
-		UNKNOWN14      = 0x00010000,
-		UNKNOWN15      = 0x00020000,
-		UNKNOWN16      = 0x00040000,
-		UNKNOWN17      = 0x00080000,
-		UNKNOWN18      = 0x00100000,
-		UNKNOWN19      = 0x00200000,
-		UNKNOWN20      = 0x00400000,
-		UNKNOWN21      = 0x00800000,
-		UNKNOWN22      = 0x01000000,
-		UNKNOWN23      = 0x02000000,
-		UNKNOWN24      = 0x04000000,
-		UNKNOWN25      = 0x08000000,
-		UNKNOWN26      = 0x10000000,
-		UNKNOWN27      = 0x20000000,
-		UNKNOWN28      = 0x40000000,
-		UNKNOWN29      = 0x80000000
+		NONE                 = 0x00000000,
+		ACTIVATED            = 0x00000001, // Used to turn structures on/off (harvesters)
+		VENDOR               = 0x00000002, // Vendor flag in creatures
+		INSURED              = 0x00000004,
+		CONVERSE             = 0x00000008, // Converse flag in creatures
+		HIBERNATING          = 0x00000010,
+		YELLOW               = 0x00000020, // Yellow items in tangibles -- should be set whenever an item is modified (pup, SEA, BE, etc) and probably also legendary/exceptional loot
+		FACTIONAGGRO         = 0x00000040, // Sets color of mobs if they have a pvp faction and can aggro a player
+		AIENABLED            = 0x00000080, // Default bitmask for mobiles
+		INVULNERABLE         = 0x00000100, // Shows no information about condition or HAM
+		DISABLED             = 0x00000200, // Used to add (disabled) text to vehicles client-side. TODO: Change vehicle disable code to use this
+		UNINSURABLE          = 0x00000400,
+		INTERESTING          = 0x00000800, // Info Icon 1, denotes a quest giver
+		VEHICLE              = 0x00001000, // Vehicles will only appear with this bit enabled
+		HASSERIAL            = 0x00002000, // Has a serial number
+		WINGS_OPEN           = 0x00004000,
+		JTLINTERESTING       = 0x00008000, // Info Icon 2, denotes a JTL quest giver
+		DOCKING              = 0x00010000,
+		DESTROYING           = 0x00020000,
+		COMMABLE             = 0x00040000,
+		DOCKABLE             = 0x00080000,
+		EJECT                = 0x00100000,
+		INSPECTABLE          = 0x00200000,
+		TRANSFERABLE         = 0x00400000,
+		INFLIGHT_TUTORIAL    = 0x00800000,
+		SPACE_COMBAT_MUS     = 0x01000000,
+		ENCOUNTER_LOCKED     = 0x02000000,
+		SPAWNED_CREATURE     = 0x04000000,
+		HOLIDAY_INTERESTING  = 0x08000000,
+		LOCKED               = 0x10000000,
 	};
 };
 
