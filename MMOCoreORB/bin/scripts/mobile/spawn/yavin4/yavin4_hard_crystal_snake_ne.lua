5263a46f7865fbe62259be3aa0f62348b933851d
hakry <hakry@swgemu.com>
2023-01-30 00:20:06 -0500

    [added] yavin4 spawn groups
    Change-Id: I78429dc1b6cfb59dea1ca64a54d83197faa3235b

---
+++ MMOCoreORB/bin/scripts/mobile/spawn/yavin4/yavin4_hard_crystal_snake_ne.lua
@@ -0,0 +1,24 @@
+yavin4_hard_crystal_snake_ne = {
+	lairSpawns = {
+		{
+			lairTemplateName = "yavin4_kliknik_lair_neutral_medium",
+			spawnLimit = -1,
+			minDifficulty = 110,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 60,
+			size = 15,
+		},
+		{
+			lairTemplateName = "yavin4_kliknik_pack2_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 56,
+			maxDifficulty = 500,
+			numberToSpawn = 15,
+			weighting = 60,
+			size = 15,
+		},
+	}
+}
+
+addSpawnGroup("yavin4_hard_crystal_snake_ne", yavin4_hard_crystal_snake_ne);
---
+++ MMOCoreORB/bin/scripts/mobile/spawn/yavin4/yavin4_southeastern_hard.lua
@@ -0,0 +1,51 @@
+yavin4_southeastern_hard = {
+	lairSpawns = {
+		{
+			lairTemplateName = "yavin4_hutt_expedition_force_camp_hutt_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 85,
+			maxDifficulty = 90,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+		{
+			lairTemplateName = "yavin4_stranded_imperial_base_imperial_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 85,
+			maxDifficulty = 90,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+		{
+			lairTemplateName = "yavin4_stranded_imperial_patrol_imperial_medium_theater",
+			spawnLimit = -1,
+			minDifficulty = 85,
+			maxDifficulty = 90,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+		{
+			lairTemplateName = "yavin4_tybis_herd_neutral_none",
+			spawnLimit = -1,
+			minDifficulty = 85,
+			maxDifficulty = 90,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+		{
+			lairTemplateName = "yavin4_tybis_male_neutral_medium_boss_01",
+			spawnLimit = -1,
+			minDifficulty = 85,
+			maxDifficulty = 90,
+			numberToSpawn = 15,
+			weighting = 15,
+			size = 25,
+		},
+	}
+}
+
+addSpawnGroup("yavin4_southeastern_hard", yavin4_southeastern_hard);
