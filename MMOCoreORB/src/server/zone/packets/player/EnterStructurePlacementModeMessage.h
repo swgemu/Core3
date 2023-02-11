cb9a43a4763e605bf0fa0e1402f341cc0a371ab9
hakry <hakry@swgemu.com>
2023-02-11 17:50:50 -0500

    [added] Additional checks for structure placement - Mantis #8454 and #8761
    Change-Id: I081c2e95f226f29199a2927552d55a9670a8e25d

--- MMOCoreORB/src/server/zone/packets/player/EnterStructurePlacementModeMessage.h
+++ MMOCoreORB/src/server/zone/packets/player/EnterStructurePlacementModeMessage.h
@@ -12,7 +12,7 @@ public:
 	EnterStructurePlacementModeMessage(uint64 deedid, String crcpath) : BaseMessage() {
 		insertShort(0x03);
 		insertInt(0xE8A54DC1);  // CRC
-        
+
 		insertLong(deedid); //Deed ID
 		insertAscii(crcpath); //path to the object.
 	}
