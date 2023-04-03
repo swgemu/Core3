68806a3cbdb00406bc2d48a20bb2c41491e2b479
hakry <hakry@swgemu.com>
2023-04-03 09:54:07 -0400

    [fixed] death watch and black sun templates to have thrown weapons
    Change-Id: Idbd6c0242189acbf962b6260f47c1e5019699c83

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_ghost.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_ghost.lua
@@ -45,13 +45,14 @@ death_watch_ghost = Creature:new {
 	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
 	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
 	primaryWeapon = "pirate_weapons_heavy",
-	secondaryWeapon = "unarmed",
+	secondaryWeapon = "none",
 	conversationTemplate = "",
-	
+	thrownWeapon = "thrown_weapons",
+
 	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
 	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
 	primaryAttacks = merge(bountyhuntermaster,marksmanmaster,brawlermaster,pikemanmaster,fencermaster,swordsmanmaster),
-	secondaryAttacks = { }
+	secondaryAttacks = {}
 }
 
 CreatureTemplates:addCreatureTemplate(death_watch_ghost, "death_watch_ghost")
