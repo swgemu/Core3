fd672288f227dba950807dcbf53ad0df2fdca660
Karl 'lordkator' Bunch <lordkator@swgemu.com>
2023-06-17 14:14:43 +0000

    [fixed] test suite warning while compiling
    Change-Id: I38a744bf0570c50bb2ee2fb09d05c7300392f82c

--- MMOCoreORB/utils/googletest-release-1.13.0/googletest/include/gtest/gtest-printers.h
+++ MMOCoreORB/utils/googletest-release-1.13.0/googletest/include/gtest/gtest-printers.h
@@ -529,7 +529,8 @@ int AppropriateResolution(FloatType val) {
     } else if (val >= 0.0001) {
       mulfor6 = 1e9;
     }
-    if (static_cast<int32_t>(val * mulfor6 + 0.5) / mulfor6 == val) return 6;
+    FloatType roundedValue = static_cast<FloatType>(static_cast<int32_t>(val * mulfor6 + 0.5)) / mulfor6;
+    if (roundedValue == val) return 6;
   } else if (val < 1e10) {
     FloatType divfor6 = 1.0;
     if (val >= 1e9) {  // 1,000,000,000 to 9,999,999,999
@@ -541,7 +542,8 @@ int AppropriateResolution(FloatType val) {
     } else if (val >= 1e6) {  // 1,000,000 to 9,999,999
       divfor6 = 10;
     }
-    if (static_cast<int32_t>(val / divfor6 + 0.5) * divfor6 == val) return 6;
+    FloatType roundedValue = static_cast<FloatType>(static_cast<int32_t>(val / divfor6 + 0.5)) * divfor6;
+    if (roundedValue == val) return 6;
   }
   return full;
 }
