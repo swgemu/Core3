18d479e99ec3591825cc1b964d3de04f1e03a7d3
Halyn <cneumi@hotmail.com>
2023-07-16 20:11:57 -0500

    [added] starship texture kits
    Change-Id: I028c116b01cb743ebb6404f194994e6a77903f4b

--- MMOCoreORB/bin/scripts/object/tangible/ship/crafted/chassis/texture_kit_s01.lua
+++ MMOCoreORB/bin/scripts/object/tangible/ship/crafted/chassis/texture_kit_s01.lua
@@ -43,6 +43,7 @@
 
 object_tangible_ship_crafted_chassis_texture_kit_s01 = object_tangible_ship_crafted_chassis_shared_texture_kit_s01:new {
 
+	objectMenuComponent = "StarshipTextureKitObjectMenuComponent",
 
 	numberExperimentalProperties = {1, 1},
 	experimentalProperties = {"XX", "XX"},
