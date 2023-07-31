e0fc5e8a9de9c8cd6e2584fcd808b326e1113d3b
hakry <hakry@swgemu.com>
2023-07-31 04:32:38 -0400

    [fixed] not enough credits message after trade is completed - Mantis #8901 [modified] begin trade and handling of trade sessions
    Change-Id: Ia140f1cff777ed32d40c871c1eb92b10b858efdf

--- MMOCoreORB/src/server/zone/packets/trade/BeginTradeMessage.h
+++ MMOCoreORB/src/server/zone/packets/trade/BeginTradeMessage.h
@@ -7,8 +7,8 @@
 
 class BeginTradeMessage : public BaseMessage {
 public:
-	BeginTradeMessage(uint64 playerID) : BaseMessage(14) {
-		insertShort(1);
+	BeginTradeMessage(uint64 playerID) : BaseMessage(0x0E) {
+		insertShort(0x02);
 		insertInt(0x325932D8);
 		insertLong(playerID);
 	}
