e8adefd5fdf1e2edbe7a3d64db373577ade74d3e
hakry <hakry@swgemu.com>
2023-08-11 21:23:25 -0400

    [fixed] calculation for adding and removing private skill multiplier buff mods - Mantis #8881
    Change-Id: I802eb8d2dfe3788acfc06086cdea3b396d83d434

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun2Command.h
@@ -10,16 +10,14 @@
 
 class ForceRun2Command : public JediQueueCommand {
 public:
-
-	ForceRun2Command(const String& name, ZoneProcessServer* server)
-	: JediQueueCommand(name, server) {
+	ForceRun2Command(const String& name, ZoneProcessServer* server) : JediQueueCommand(name, server) {
 		// BuffCRC's, first one is used.
 		buffCRC = BuffCRC::JEDI_FORCE_RUN_2;
 
-        // If these are active they will block buff use
+		// If these are active they will block buff use
 		blockingCRCs.add(BuffCRC::JEDI_FORCE_RUN_1);
 		blockingCRCs.add(BuffCRC::JEDI_FORCE_RUN_3);
-        
+
 		skillMods.put("force_run", 2);
 		skillMods.put("slope_move", 66);
 	}
