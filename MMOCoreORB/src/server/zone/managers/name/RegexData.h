07f57199d4e87fadbed7ec1d64442b5c8458bac4
hakry <hakry@swgemu.com>
2023-11-17 19:30:45 -0500

    [added] name manager unit tests
    Change-Id: Ib1505b030eb480899023b249954a7491bca88d3c

---
+++ MMOCoreORB/src/server/zone/managers/name/RegexData.h
@@ -0,0 +1,37 @@
+#ifndef REGEXDATA
+#define REGEXDATA
+
+#include "engine/engine.h"
+#include <regex>
+
+class RegexData : public Object {
+
+protected:
+	std::regex* regexEntry;
+	String phrase;
+	int type;
+
+public:
+	RegexData(String filter, int filterType) : Object() {
+		regexEntry = new std::regex(filter.toCharArray(), std::regex_constants::icase);
+		type = filterType;
+		phrase = filter;
+	}
+
+	~RegexData() {
+	}
+
+	std::regex* getRegexEntry() const {
+		return regexEntry;
+	}
+
+	int getFilterType() const {
+		return type;
+	}
+
+	String getRegexPhrase() const {
+		return phrase;
+	}
+};
+
+#endif /* REGEXDATA */
