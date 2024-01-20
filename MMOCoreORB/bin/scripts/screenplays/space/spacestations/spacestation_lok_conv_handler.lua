748bb54fe47bf5b95cf3e17e3876c60cc454d272
hakry <hakry@swgemu.com>
2024-01-20 08:12:17 -0500

    [wip][added] repair option to space stations [added] ship decay upon repair
    Change-Id: I3ca63c30a11a1999d4f222d466382437fe14820e

--- MMOCoreORB/bin/scripts/screenplays/space/spacestations/spacestation_lok_conv_handler.lua
+++ MMOCoreORB/bin/scripts/screenplays/space/spacestations/spacestation_lok_conv_handler.lua
@@ -3,6 +3,12 @@ require("utils.helpers")
 
 SpacestationLokConvoHandler = Object:new {}
 
+function SpacestationLokConvoHandler:getInitialScreen(pPlayer, pNpc, pConvTemplate)
+	local convoTemplate = LuaConversationTemplate(pConvTemplate)
+
+	return convoTemplate:getScreen("spacestation_lok_greeting")
+end
+
 function SpacestationLokConvoHandler:getNextConversationScreen(pConvTemplate, pPlayer, selectedOption)
 	if (pPlayer == nil or pConvTemplate == nil) then
 		return
@@ -56,8 +62,54 @@ function SpacestationLokConvoHandler:runScreenHandlers(pConvTemplate, pPlayer, p
 		return
 	end
 
+	-- Only show request repairs if the player has enough credits for the smallest repair
+	if (screenID == "spacestation_lok_greeting" and SpaceStationScreenPlay:hasCreditsForRepair(pPlayer, pShip, 0.25)) then
+		pClonedConvo:addOption("@conversation/station_lok:s_6c8ffad8", "spacestation_lok_repair") --I need repairs.
+	end
+
+	-- Handle Landing
 	if (screenID == "spacestation_lok_land_nym_complete") then
 		createEvent(1000, "SpaceStationScreenPlay", "landShip", pPlayer, "nym")
+	end
+
+	-- Handle Repair Options
+	if (screenID == "spacestation_lok_repair") then
+		-- We already checked if player has enough credits for at least 25% repair, add that option
+		pClonedConvo:addOption("@conversation/station_lok:s_bf151ca2", "repair_small") -- No, just fix a little.
+
+		if (SpaceStationScreenPlay:hasCreditsForRepair(pPlayer, pShip, 0.50)) then
+			pClonedConvo:addOption("@conversation/station_lok:s_c80840ca", "repair_half") -- Just fix half of it.
+		end
+		if (SpaceStationScreenPlay:hasCreditsForRepair(pPlayer, pShip, 0.75)) then
+			pClonedConvo:addOption("@conversation/station_lok:s_2d1962b", "repair_most") -- Repair most of it.
+		end
+		if (SpaceStationScreenPlay:hasCreditsForRepair(pPlayer, pShip, 1.00)) then
+			pClonedConvo:addOption("@conversation/station_lok:s_445607d0", "repair_full") -- Repair all of it.
+		end
+
+		pClonedConvo:addOption("@conversation/station_lok:s_169df3bb", "spacestation_lok_repair_deny") -- Never mind.
+	end
+
+	-- Repair selection
+	if (screenID == "repair_small") then
+		pClonedConvo:setDialogTextDI(SpaceStationScreenPlay:getRepairCost(pShip, 0.25))
+	elseif (screenID == "repair_half") then
+		pClonedConvo:setDialogTextDI(SpaceStationScreenPlay:getRepairCost(pShip, 0.50))
+	elseif (screenID == "repair_most") then
+		pClonedConvo:setDialogTextDI(SpaceStationScreenPlay:getRepairCost(pShip, 0.75))
+	elseif (screenID == "repair_full") then
+		pClonedConvo:setDialogTextDI(SpaceStationScreenPlay:getRepairCost(pShip, 1.0))
+	end
+
+	-- Handle Repairing Ship
+	if (screenID == "accept_repair_25") then
+		SpaceStationScreenPlay:repairShip(pPlayer, pShip, 0.25, pNpc)
+	elseif (screenID == "accept_repair_50") then
+		SpaceStationScreenPlay:repairShip(pPlayer, pShip, 0.50, pNpc)
+	elseif (screenID == "accept_repair_75") then
+		SpaceStationScreenPlay:repairShip(pPlayer, pShip, 0.75, pNpc)
+	elseif (screenID == "accept_repair_full") then
+		SpaceStationScreenPlay:repairShip(pPlayer, pShip, 1.0, pNpc)
 	end
 
 	return pScreenClone
