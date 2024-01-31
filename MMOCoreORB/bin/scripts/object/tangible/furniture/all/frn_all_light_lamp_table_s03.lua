e7e3389f056091399a36f16710e91ab478162bc3
hakry <hakry@swgemu.com>
2024-01-30 21:20:27 -0500

    [added] candle and lamp lifespan attribute and functionality - Mantis #8979
    Change-Id: I01a62f43d5e9fab4d53d198680bfc3d543cf8985

--- MMOCoreORB/bin/scripts/object/tangible/furniture/all/frn_all_light_lamp_table_s03.lua
+++ MMOCoreORB/bin/scripts/object/tangible/furniture/all/frn_all_light_lamp_table_s03.lua
@@ -42,7 +42,7 @@
 
 
 object_tangible_furniture_all_frn_all_light_lamp_table_s03 = object_tangible_furniture_all_shared_frn_all_light_lamp_table_s03:new {
-
+	gameObjectType = 8253,
 
 	numberExperimentalProperties = {1, 1, 1, 2},
 	experimentalProperties = {"XX", "XX", "XX", "DR", "OQ"},
