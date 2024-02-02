03d714db1a01d6b75ea272d61d81a9ee75356e92
hakry <hakry@swgemu.com>
2024-02-02 11:44:34 -0500

    [wip] affector roads for prodecural terrain
    Change-Id: I28ec9f3decdb671b8ef9bbbf372b97bf21d7e3fa

--- MMOCoreORB/src/terrain/layer/affectors/AffectorRoad.h
+++ MMOCoreORB/src/terrain/layer/affectors/AffectorRoad.h
@@ -11,25 +11,27 @@
 #include "../ProceduralRule.h"
 #include "../Road.h"
 #include "../Hdta.h"
+#include "../../TerrainGenerator.h"
 
 class AffectorRoad : public ProceduralRule<'AROA'>, public AffectorProceduralRule {
 	Road road;
 	Hdta hdta;
 
-	int var1;
+	int coordinateCount;
 
 	Vector<Point2D*> positions;
 
-	float var2;
-	int var3;
+	float width;
+	int familyID;
+
 	int featheringType;
 	float featheringAmount;
-	int var6;
-	float var7;
+	int featheringShader;
+	float featheringShaderDistance;
 
 public:
-	AffectorRoad() : var1(0), var2(0), var3(0), featheringType(0), featheringAmount(0), var6(0), var7(0) {
-
+	AffectorRoad() : coordinateCount(0), width(0.f), familyID(0), featheringType(0), featheringAmount(0), featheringShader(0), featheringShaderDistance(0.5f) {
+		affectorType = HEIGHTROAD;
 	}
 
 	~AffectorRoad() {
@@ -75,26 +77,79 @@ public:
 
 		iffStream->openChunk('DATA');
 
-		var1 = iffStream->getInt();
+		coordinateCount = iffStream->getInt();
 
-		for (int i = 0; i < var1; i++) {
+		for (int i = 0; i < coordinateCount; i++) {
 			Point2D* pos = new Point2D();
 			pos->x = iffStream->getFloat();
 			pos->y = iffStream->getFloat();
 
+			Logger::console.info(true) << "Loading Road Position - X: " << pos->x << " Y: " << pos->y;
+
 			positions.add(pos);
 		}
 
-		var2 = iffStream->getFloat();
-		var3 = iffStream->getInt();
+		width = iffStream->getFloat();
+		familyID = iffStream->getInt();
 		featheringType = iffStream->getInt();
-		featheringAmount = iffStream->getFloat();
-		var6 = iffStream->getInt();
-		var7 = iffStream->getFloat();
+		featheringAmount = Math::clamp(0.f, iffStream->getFloat(), 1.f);
+		featheringShader = iffStream->getInt();
+		featheringShaderDistance = Math::clamp(0.f, iffStream->getFloat(), 1.f);
+
+		Logger::console.info(true) << "AffectorRoad -- coordinateCount: " << coordinateCount << " width: " << width << " familyID: " << familyID << " featheringType: " << featheringType << " featheringAmount: " << featheringAmount << " featheringShader: " << featheringShader << " featheringShaderDistance: " << featheringShaderDistance;
 
 		iffStream->closeChunk('DATA');
 
 		iffStream->closeForm('DATA');
+	}
+
+	void process(float x, float y, float transformValue, float& baseValue, TerrainGenerator* terrainGenerator) {
+		Logger::console.info(true) << "AffectorRoad -- process function called for location X: " << x << " Y: " << y << " transformValue: " << transformValue << " baseValue: " << baseValue;
+		Logger::console.info(true) << "Road Info -- coordinateCount: " << positions.size() << " width: " << width << " familyID: " << familyID << " featheringType: " << featheringType << " featheringAmount: " << featheringAmount << " featheringShader: " << featheringShader << " featheringShaderDistance: " << featheringShaderDistance;
+
+
+		if (transformValue == 0 || positions.size() == 0)
+			return;
+
+		//float currentHeight = terrainGenerator->get
+
+		Logger::console.info(true) << "Processing baseValue for new height";
+
+		/*	Process the RoadData
+			Result we will be returning should be the height for the road using the x, y and the base values passed into the function
+		*/
+
+		//Math::linearInterpolate(1.f, maxFactor, (speedRatio - optimalFactor) / (1.f - optimalFactor))
+
+		float distSquared = 0.f;
+		float nearestX = 0.f;
+		float nearestY = 0.f;
+
+		for (int i = 0; i < positions.size(); ++i) {
+			Point2D* pos = positions.get(i);
+
+			if (pos == nullptr)
+				continue;
+
+			float deltaX = pos->getX() - x;
+			float deltaY = pos->getY() - y;
+			float pointDistSq = (deltaX * deltaX + deltaY * deltaY);
+
+			if (distSquared == 0.f || pointDistSq < distSquared) {
+				distSquared = pointDistSq;
+
+				nearestX = pos->getX();
+				nearestY = pos->getY();
+			}
+		}
+
+		Logger::console.info(true) << "Nearest Point Distance Squared: " << distSquared << "   Location  X: " << nearestX << " Y: " << nearestY;
+
+
+	}
+
+	bool isEnabled() {
+		return informationHeader.isEnabled();
 	}
 };
 
