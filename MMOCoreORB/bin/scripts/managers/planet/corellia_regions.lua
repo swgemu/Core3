22bc23c872f6e7e58caf0ed524a6690184a58987
hakry <hakry@swgemu.com>
2024-04-26 11:40:26 -0400

    [fixed] vreni island medium spawn area to no longer extend past zone maximum
    Change-Id: I2948b5da70ca966f1d01c9ba4eadd91116747afa

--- MMOCoreORB/bin/scripts/managers/planet/corellia_regions.lua
+++ MMOCoreORB/bin/scripts/managers/planet/corellia_regions.lua
@@ -139,7 +139,7 @@ corellia_regions = {
 	{"@corellia_region_names:swamp1", 816, 4560, {CIRCLE, 1497}, SPAWNAREA, {"agrilat_swamp"}, 128},
 	{"@corellia_region_names:tyrena_easy_newbie", -5282, -2526, {RING, 622, 1000}, SPAWNAREA, {"tyrena_easy_newbie"}, 256},
 	{"@corellia_region_names:tyrena_medium_newbie", -5282, -2526, {RING, 1000, 1400}, SPAWNAREA, {"tyrena_medium_newbie"}, 256},
-	{"@corellia_region_names:vreni_island_medium_newbie", -5472, -6272, {CIRCLE, 2600}, SPAWNAREA, {"vreni_island_medium_newbie"}, 256},
+	{"@corellia_region_names:vreni_island_medium_newbie", -5472, -6272, {CIRCLE, 768}, SPAWNAREA, {"vreni_island_medium_newbie"}, 256},
 
 	-- North
 	{"@corellia_region_names:northern_beladonas_hard", 6229, 5779, {CIRCLE, 221}, SPAWNAREA, {"corellia_northern_beladonas_hard"}, 8},
