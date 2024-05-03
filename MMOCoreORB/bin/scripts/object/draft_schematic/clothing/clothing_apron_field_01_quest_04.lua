f0ba120fc50df365808fdc3a6b6558824f02945b
hakry <hakry@swgemu.com>
2024-05-03 17:06:27 -0400

    [fixed] target template for crafters apron quest schematics to use proper template - Mantis #8722
    Change-Id: I014f216923dcde441b2e5f8ebc4443e8e1cff709

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_apron_field_01_quest_04.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_apron_field_01_quest_04.lua
@@ -1,85 +1,78 @@
 --Copyright (C) 2010 <SWGEmu>
 
-
 --This File is part of Core3.
 
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
+--This program is free software; you can redistribute
+--it and/or modify it under the terms of the GNU Lesser
 --General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
+--Foundation; either version 2 of the License,
 --or (at your option) any later version.
 
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 --See the GNU Lesser General Public License for
 --more details.
 
---You should have received a copy of the GNU Lesser General 
+--You should have received a copy of the GNU Lesser General
 --Public License along with this program; if not, write to
 --the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
+--Linking Engine3 statically or dynamically with other modules
+--is making a combined work based on Engine3.
+--Thus, the terms and conditions of the GNU Lesser General Public License
 --cover the whole combination.
 
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
+--In addition, as a special exception, the copyright holders of Engine3
+--give you permission to combine Engine3 program with free software
+--programs or libraries that are released under the GNU LGPL and with
+--code included in the standard release of Core3 under the GNU LGPL
+--license (or modified versions of such code, with unchanged license).
+--You may copy and distribute such a system following the terms of the
+--GNU LGPL for Engine3 and the licenses of the other code concerned,
+--provided that you include the source code of that other code when
 --and as the GNU LGPL requires distribution of source code.
 
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
+--Note that people who make modified versions of Engine3 are not obligated
+--to grant this special exception for their modified versions;
+--it is their choice whether to do so. The GNU Lesser General Public License
+--gives permission to release a modified version without this exception;
+--this exception also makes it possible to release a modified version
 
 
 object_draft_schematic_clothing_clothing_apron_field_01_quest_04 = object_draft_schematic_clothing_shared_clothing_apron_field_01_quest_04:new {
+	templateType = DRAFTSCHEMATIC,
 
-   templateType = DRAFTSCHEMATIC,
+	craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
+	complexity = 30,
+	size = 1,
+	factoryCrateType = "object/factory/factory_crate_clothing.iff",
 
-   customObjectName = "Crafter\'s Apron",
+	xpType = "crafting_clothing_general",
+	xp = 1000,
 
-   craftingToolTab = 8, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 30, 
-   size = 1, 
-   factoryCrateType = "object/factory/factory_crate_clothing.iff",
+	assemblySkill = "clothing_assembly",
+	experimentingSkill = "clothing_experimentation",
+	customizationSkill = "clothing_customization",
 
-   xpType = "crafting_clothing_general", 
-   xp = 1000, 
+	customizationOptions = {},
+	customizationStringNames = {},
+	customizationDefaults = {},
 
-   assemblySkill = "clothing_assembly", 
-   experimentingSkill = "clothing_experimentation", 
-   customizationSkill = "clothing_customization", 
+	ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+	ingredientTitleNames = {"body", "binding_and_reinforcement", "binding_and_hardware", "colorizable_cloth_panel", "lining", "spare_hardware", "tool_kit"},
+	ingredientSlotType = {1, 0, 0, 2, 0, 0, 2},
+	resourceTypes = {"object/tangible/component/clothing/shared_leather_heavy_duty.iff", "chemical", "chemical", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "hide", "steel", "object/tangible/crafting/station/shared_structure_tool.iff"},
+	resourceQuantities = {2, 8, 3, 1, 50, 20, 1},
+	contribution = {100, 100, 100, 100, 100, 100, 100},
 
-   customizationOptions = {},
-   customizationStringNames = {},
-   customizationDefaults = {},
-   
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"body", "binding_and_reinforcement", "binding_and_hardware", "colorizable_cloth_panel", "lining", "spare_hardware", "tool_kit"},
-   ingredientSlotType = {1, 0, 0, 2, 0, 0, 2},
-   resourceTypes = {"object/tangible/component/clothing/shared_leather_heavy_duty.iff", "chemical", "chemical", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "hide", "steel", "object/tangible/crafting/station/shared_structure_tool.iff"},
-   resourceQuantities = {2, 8, 3, 1, 50, 20, 1},
-   contribution = {100, 100, 100, 100, 100, 100, 100},
+	targetTemplate = "object/tangible/wearables/apron/apron_s01_quest.iff",
 
+	additionalTemplates = {},
 
-   targetTemplate = "object/tangible/wearables/apron/apron_porcellus.iff",
-
-   additionalTemplates = {
-             },
-      
-        skillMods = {
-	    {"structure_assembly", 5},
-	    {"structure_experimentation", 5},
+	skillMods = {
+		{"structure_assembly", 5},
+		{"structure_experimentation", 5},
 	},
-
 }
 ObjectTemplates:addTemplate(object_draft_schematic_clothing_clothing_apron_field_01_quest_04, "object/draft_schematic/clothing/clothing_apron_field_01_quest_04.iff")
