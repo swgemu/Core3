8c21a8c20e48384a69214473d47bef20a183c392
Smoki <swgsmoki@gmail.com>
2024-06-27 11:39:57 -0400

    [modified] lootManager excMod bonus values to level distribution
     - excMod modifier bonus values to level distribution
     - skill mod quantity pivot to quality tier levels
     - attachment skill mods to level distribution
    
    Change-Id: Iebfa4ae963c9a580b675443297f544a66dd85819

--- MMOCoreORB/bin/scripts/managers/loot_manager.lua
+++ MMOCoreORB/bin/scripts/managers/loot_manager.lua
@@ -51,12 +51,12 @@ legendaryChance = 1000000
 
 --Multiplier values for bonus modifiers
 baseModifier = 1
-yellowModifier = 1.5
-exceptionalModifier = 2.5
-legendaryModifier = 5.0
+yellowModifier = 2
+exceptionalModifier = 8
+legendaryModifier = 9
 
 --The chance for random skill mods to be on looted weapons/wearables
-skillModChance = 500 -- 1 in 500
+skillModChance = 1000 -- 1 in 1000
 
 -- Dot Distribution chance by DOT type. They must equate to 1.0
 poisonDotChance = 0.50
@@ -70,10 +70,10 @@ mindDotChance = 0.15
 
 -- Value ranges for random dots on looted weapons (chance is set individually on the loot items)
 randomDotAttribute = {0, 8} -- See CreatureAttributes.h in src for numbers.
-randomDotStrength = {10, 200} -- Set for disease. Fire will be x1.5, poison x2.
-randomDotDuration = {30, 240} -- Set for poison. Fire will be x1.5, disease x5.
-randomDotPotency = {1, 100}
-randomDotUses = {250, 9999}
+randomDotStrength = {10, 100} -- poison x1.5, disease x0.5
+randomDotDuration = {30, 120} -- disease x4.0, fire x1.5
+randomDotPotency = {1, 50}
+randomDotUses = {250, 4999}
 
 -- Modifier applied to min/max junk values found in loot item lua
 junkValueModifier = 5;
