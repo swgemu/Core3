8e2eb56bdca329195968c40d66b9975b208964c1
hakry <hakry@swgemu.com>
2024-07-05 00:08:55 -0400

    [fixed] calculation for lair condition to better reflect level and max condition - Mantis #9034 [added] delayed random spawning for mobiles spawning from tangible lairs
    Change-Id: I5def6a31c25677f654b363fd2fb48a779510717f

---
+++ MMOCoreORB/src/server/zone/managers/creature/SpawnLairMobileTask.h
@@ -0,0 +1,58 @@
+/*
+ * SpawnLairMobileTask.h
+ *
+ * Created on: 2024-07-06
+ * Author: Hakry
+ */
+
+#ifndef SPAWNLAIRMOBILETASK_H_
+#define SPAWNLAIRMOBILETASK_H_
+
+#include "server/zone/objects/tangible/TangibleObject.h"
+#include "server/zone/managers/creature/LairObserver.h"
+
+class SpawnLairMobileTask : public Task {
+	ManagedWeakReference<TangibleObject*> lairWeak;
+	int spawnNumber;
+	String mobileString;
+
+public:
+	SpawnLairMobileTask(TangibleObject* lair, int spawnNum, String mobileStr) {
+		lairWeak = lair;
+		spawnNumber = spawnNum;
+		mobileString = mobileStr;
+	}
+
+	void run() {
+		auto lairObject = lairWeak.get();
+
+		if (lairObject == nullptr) {
+			return;
+		}
+
+		if (lairObject->getZone() == nullptr) {
+			return;
+		}
+
+		Locker lock(lairObject);
+
+		ManagedReference<LairObserver*> lairObserver = nullptr;
+		SortedVector<ManagedReference<Observer*>> observers = lairObject->getObservers(ObserverEventType::OBJECTDESTRUCTION);
+
+		for (int i = 0; i < observers.size(); i++) {
+			lairObserver = cast<LairObserver*>(observers.get(i).get());
+
+			if (lairObserver != nullptr) {
+				break;
+			}
+		}
+
+		if (lairObserver == nullptr) {
+			return;
+		}
+
+		lairObserver->spawnLairMobile(lairObject, spawnNumber, mobileString);
+	}
+};
+
+#endif /* SPAWNLAIRMOBILETASK_H_ */
