654895dec93d5b0aafb255c979d16d9e38f2e63b
hakry <hakry@swgemu.com>
2024-07-19 21:12:24 -0400

    [added] chance for creatures spawning from lairs to spawn with scout title
    Change-Id: I727049c7d1e1c48f991263cbddcb1f11c1f94f19

--- MMOCoreORB/src/server/zone/objects/mission/DestroyMissionObjectiveImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/mission/DestroyMissionObjectiveImplementation.cpp
@@ -238,6 +238,7 @@ void DestroyMissionObjectiveImplementation::spawnLair() {
 	 	lairObject->registerObserver(ObserverEventType::AIMESSAGE, lairObserver);
 	 	lairObject->registerObserver(ObserverEventType::OBJECTREMOVEDFROMZONE, lairObserver);
 		lairObject->registerObserver(ObserverEventType::NOPLAYERSINRANGE, lairObserver);
+		lairObject->registerObserver(ObserverEventType::CREATUREDESPAWNED, lairObserver);
 
 		zone->transferObject(lairObject, -1, true);
 
