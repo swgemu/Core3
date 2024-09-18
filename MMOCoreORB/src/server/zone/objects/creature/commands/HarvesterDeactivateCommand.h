19598c8d52c7436ead61ae65182ac065dff33a0b
hakry <hakry@swgemu.com>
2024-09-18 16:56:45 -0400

    [fixed] harvester activate and deactivate should again function for generators
    Change-Id: Id641f39c48df0b23611b0a07d0a4048ef2cdc7f7

--- MMOCoreORB/src/server/zone/objects/creature/commands/HarvesterDeactivateCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/HarvesterDeactivateCommand.h
@@ -29,7 +29,7 @@ public:
 
 		ManagedReference<SceneObject*> object = zoneServer->getObject(target);
 
-		if (object == nullptr || !object->isHarvesterObject()) {
+		if (object == nullptr || (!object->isHarvesterObject() && !object->isGeneratorObject())) {
 			return GENERALERROR;
 		}
 
