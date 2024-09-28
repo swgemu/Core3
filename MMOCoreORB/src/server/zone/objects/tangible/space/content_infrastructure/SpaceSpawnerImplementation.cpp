b1421ecfc810cff50c471af08703ce29cf340f79
hakry <hakry@swgemu.com>
2024-09-28 04:52:52 -0400

    [wip] spawn checking in space + space spawner
    Change-Id: I0fbedb61e21c676fadf984bf7feaeb365b4afa37

---
+++ MMOCoreORB/src/server/zone/objects/tangible/space/content_infrastructure/SpaceSpawnerImplementation.cpp
@@ -0,0 +1,56 @@
+/*
+ * SpaceSpawnerImplementation.cpp
+ *
+ * Created on: 2024-09-26
+ * Author: Hakry
+ */
+
+#include "server/zone/objects/tangible/space/content_infrastructure/SpaceSpawner.h"
+#include "server/zone/objects/tangible/TangibleObject.h"
+
+void SpaceSpawnerImplementation::notifyInsert(TreeEntry* entry) {
+	auto sceneO = static_cast<SceneObject*>(entry);
+	uint64 scnoID = sceneO->getObjectID();
+
+	if (sceneO == nullptr) {
+		return;
+	}
+
+	if ((!sceneO->isPlayerCreature() && !sceneO->isShipObject()) || sceneO->isShipAiAgent()) {
+		return;
+	}
+
+	auto tangibleObject = sceneO->asTangibleObject();
+
+	if (tangibleObject == nullptr || tangibleObject->isInvisible()) {
+		return;
+	}
+
+	numberOfPlayersInRange.increment();
+	clearDespawnEvent();
+}
+
+void SpaceSpawnerImplementation::notifyDissapear(TreeEntry* entry) {
+	auto sceneO = static_cast<SceneObject*>(entry);
+	uint64 scnoID = sceneO->getObjectID();
+
+	if (sceneO == nullptr) {
+		return;
+	}
+
+	if ((!sceneO->isPlayerCreature() && !sceneO->isShipObject()) || sceneO->isShipAiAgent()) {
+		return;
+	}
+
+	auto tangibleObject = sceneO->asTangibleObject();
+
+	if (tangibleObject == nullptr || tangibleObject->isInvisible()) {
+		return;
+	}
+
+	int val = numberOfPlayersInRange.decrement();
+
+	if (val <= 0 && despawnOnNoPlayersInRange) {
+		activateDespawnEvent();
+	}
+}
