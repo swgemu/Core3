74eb73fc75a828433c44fa81345f15498903d69d
Smoki <swgsmoki@gmail.com>
2024-11-29 12:53:41 -0500

    [added] additional object types to ShipTargetVector
    Change-Id: Ie160f5684634e6f90d99d99f6f4e67e5f51563ff

--- MMOCoreORB/src/server/zone/objects/ship/ShipDeltaVector.cpp
+++ MMOCoreORB/src/server/zone/objects/ship/ShipDeltaVector.cpp
@@ -10,6 +10,8 @@
 void ShipDeltaVector::sendMessages(ShipObject* ship) {
 	Locker lock(&deltaMutex);
 
+	auto owner = ship->getOwner().get();
+
 	for (int i = 0; i < deltaVector.size(); ++i) {
 		auto deltaID = deltaVector.elementAt(i).getKey();
 		auto message = deltaVector.elementAt(i).getValue();
@@ -23,7 +25,11 @@ void ShipDeltaVector::sendMessages(ShipObject* ship) {
 		int deltaType = getDeltaType(deltaID);
 
 		if (deltaType == DeltaType::Private) {
-			ship->sendMembersBaseMessage(message->clone());
+			if (owner != nullptr) {
+				owner->sendMessage(message->clone());
+			}
+
+			ship->sendMembersBaseMessage(message->clone(), false);
 		} else if (deltaType == DeltaType::Public) {
 			ship->broadcastMessage(message->clone(), true);
 		}
