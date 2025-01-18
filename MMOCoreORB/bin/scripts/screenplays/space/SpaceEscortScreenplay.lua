57c0bfb217db462970b4a382e06e7bf39ebfc530
hakry <hakry@swgemu.com>
2025-01-18 13:05:00 -0500

    [wip] space quest handlers by quest type
    Change-Id: I1df205b86d987f898face597e251cefd585b4ffa

---
+++ MMOCoreORB/bin/scripts/screenplays/space/SpaceEscortScreenplay.lua
@@ -0,0 +1,12 @@
+SpaceEscortScreenplay = SpaceQuestLogic:new {
+	className = "SpaceEscortScreenplay",
+}
+
+function SpaceEscortScreenplay:startQuest(pPlayer)
+	if (pPlayer == nil) then
+		Logger:log("Quest: " .. self.questName .. " Type: " .. self.QuestType .. " -- Failed to startQuest due to pPlayer being nil.", LT_ERROR)
+		return
+	end
+
+
+end
