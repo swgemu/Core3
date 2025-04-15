c3aebdc0d7cfa6bcea0fd4f5195feb92694bca1d
Halyn <cneumi@hotmail.com>
2025-04-14 20:19:06 -0500

    [fixed] astromech not always appearing on fighters [fixed] droids no longer launching in old owner's ship after being traded
    Change-Id: Ic974323a25b5c5a78c4348e2c19723827cf45c1d

--- MMOCoreORB/src/server/zone/objects/intangible/tasks/LaunchShipTask.h
+++ MMOCoreORB/src/server/zone/objects/intangible/tasks/LaunchShipTask.h
@@ -84,21 +84,16 @@ public:
 
 		pilotLock.release();
 
-		InsertPilotIntoShipTask* pilotTask = new InsertPilotIntoShipTask(player, ship);
-
-		if (pilotTask != nullptr) {
-			pilotTask->schedule(100);
+		if (ship->getShipDroidID() != 0) {
+			handleDroid(player, ship, shipControlDevice);
 		}
 
 		ship->resetDroidCommands();
 
-		if (ship->getShipDroidID() != 0) {
-			auto droidTask = new InsertAstromechIntoShipTask(ship);
+		InsertPilotIntoShipTask* pilotTask = new InsertPilotIntoShipTask(player, ship);
 
-			if (droidTask != nullptr) {
-				droidTask->schedule(150);
-			}
-
+		if (pilotTask != nullptr) {
+			pilotTask->schedule(150);
 		}
 
 		if (groupMembers.size() > 0 && (ship->isPobShip() || ship->isMultiPassengerShip())) {
@@ -121,6 +116,49 @@ public:
 			}
 		}
 	}
+
+	void handleDroid(CreatureObject* player, ShipObject* ship, ShipControlDevice* shipControlDevice) {
+		if (player == nullptr || ship == nullptr || shipControlDevice == nullptr) {
+			return;
+		}
+
+		auto zoneServer = player->getZoneServer();
+
+		if (zoneServer == nullptr) {
+			return;
+		}
+
+		ManagedReference<SceneObject*> droidControlDeviceRef = zoneServer->getObject(ship->getShipDroidID());
+
+		if (droidControlDeviceRef == nullptr || !droidControlDeviceRef->isPetControlDevice()) {
+			return;
+		}
+
+		if (!droidControlDeviceRef->isASubChildOf(player)) {
+			Locker shipLock(ship, shipControlDevice);
+
+			ship->setShipDroidID(0, true);
+			return;
+		}
+
+		auto droidControlDevice = dynamic_cast<PetControlDevice*>(droidControlDeviceRef.get());
+
+		if (droidControlDevice == nullptr)
+			return;
+
+		PetControlDeviceStoreTask* storeTask = new PetControlDeviceStoreTask(droidControlDevice, player, true);
+
+		if (storeTask != nullptr)
+			storeTask->execute();
+
+		//insert the droid into the ship
+		auto droidTask = new InsertAstromechIntoShipTask(ship);
+
+		if (droidTask != nullptr) {
+			droidTask->schedule(100);
+		}
+	}
+
 };
 
 #endif /* LAUNCHSHIPTASK_H_ */
