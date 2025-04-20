a56b4dad845c2aec2e3dace907ef4aa0e89dfc17
hakry <hakry@swgemu.com>
2025-04-20 19:14:08 -0400

    [fixed] agent vehicle types to not be healable - Mantis #9086 [fixed] agent vehicle type not able to be diseased or poisoned - Mantis #9086
    Change-Id: Ic39a3ca1c3852acd8c0d24edd7a313a399d3d5ee

--- MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
@@ -4394,6 +4394,35 @@ bool AiAgentImplementation::isAttackableBy(CreatureObject* creature) {
 	return true;
 }
 
+bool AiAgentImplementation::isHealableBy(CreatureObject* healerCreo) {
+	if (healerCreo == nullptr) {
+		return false;
+	}
+
+	if (isVehicleType()) {
+		return false;
+	}
+
+	return CreatureObjectImplementation::isHealableBy(healerCreo);
+}
+
+bool AiAgentImplementation::hasDotImmunity(uint32 dotType) const {
+	switch (dotType) {
+		case CreatureState::POISONED:
+		case CreatureState::BLEEDING:
+		case CreatureState::DISEASED:
+			if (isVehicleType()) {
+				return false;
+			}
+			break;
+		default:
+			return false;
+	}
+
+	return CreatureObjectImplementation::hasDotImmunity(dotType);
+}
+
+
 bool AiAgentImplementation::hasLoot(){
 	SceneObject* inventory = asAiAgent()->getSlottedObject("inventory");
 
