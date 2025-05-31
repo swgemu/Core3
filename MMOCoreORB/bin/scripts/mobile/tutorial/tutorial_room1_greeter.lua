6784bd847759ad1b2228ababbebd7f815713735d
Farelli <farelli@swgemu.com>
2025-05-31 11:46:12 -0400

    [WIP] New Player Tutorial
    Change-Id: I7cb4a950c72eeae95a6934c324a85b610b869c27

---
+++ MMOCoreORB/bin/scripts/mobile/tutorial/tutorial_room1_greeter.lua
@@ -0,0 +1,44 @@
+tutorial_room1_greeter = Creature:new {
+	objectName = "@newbie_tutorial/system_messages:imp_name",
+	socialGroup = "imperial",
+	faction = "imperial",
+	mobType = MOB_NPC,
+	level = 19,
+	chanceHit = 0.33,
+	damageMin = 180,
+	damageMax = 190,
+	baseXp = 1609,
+	baseHAM = 4500,
+	baseHAMmax = 5500,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = INVULNERABLE + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"imperial_officer"},
+	lootGroups = {},
+
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "unarmed",
+	secondaryWeapon = "none",
+	conversationTemplate = "tutorialRoomOneGreeterConvoTemplate",
+
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(marksmannovice,brawlernovice),
+	secondaryAttacks = { },
+}
+
+CreatureTemplates:addCreatureTemplate(tutorial_room1_greeter, "tutorial_room1_greeter")
--- MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -88,6 +88,8 @@
 #include "server/login/SWGRealmsAPI.h"
 #endif // WITH_SWGREALMS_API
 
+#include "engine/log/Logger.h"
+
 void PlayerObjectImplementation::initializeTransientMembers() {
 	playerLogLevel = ConfigManager::instance()->getPlayerLogLevel();
 
@@ -3844,6 +3846,13 @@ String PlayerObjectImplementation::getPlayedTimeString(bool verbose) const {
 }
 
 void PlayerObjectImplementation::createHelperDroid() {
+	// Do not create helper droid if config flag is false
+	bool disableHelperDroid = ConfigManager::instance()->getBool("Core3.PlayerManager.disableHelperDroid", false);
+
+	if (disableHelperDroid) {
+		return;
+	}
+
 	// Only spawn droid if character is less than 1 days old
 	if (getCharacterAgeInDays() >= 1 || isPrivileged())
 		return;
