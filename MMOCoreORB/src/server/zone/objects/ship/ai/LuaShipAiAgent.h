39314a0fdbda2cd76af68d986d06abc1dbeb7f33
hakry <hakry@swgemu.com>
2025-10-04 13:07:50 -0400

    [added] recovery space mission logic [added] getMissionOwnerID for lua ship agents [added] ship agent taunts to be sent to group members [modified] addFixedPatrolPoint to accept boolean argument to clear patrol points from agent [added] brief period of invulnerability for ship agents that have been disabled
    Change-Id: I393ec3087174efe1c465df312b47055ba9b9266e

--- MMOCoreORB/src/server/zone/objects/ship/ai/LuaShipAiAgent.h
+++ MMOCoreORB/src/server/zone/objects/ship/ai/LuaShipAiAgent.h
@@ -53,6 +53,8 @@ public:
 	int removeSpaceFactionEnemy(lua_State* L);
 	int setEscortSpeed(lua_State* L);
 	int setMissionOwner(lua_State* L);
+	int getMissionOwnerID(lua_State* L);
+	int repairShipAgent(lua_State* L);
 
 private:
 	ShipAiAgent* realObject;
