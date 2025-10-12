ad0b333f0b979c070c14f998542f836fbeb06524
Karl 'lordkator' Bunch <lordkator@swgemu.com>
2025-10-12 13:03:06 +0000

    [fixed] stability issue
    Change-Id: If77137434e694fd49c1be9e8b37ce90950d70c3d

--- MMOCoreORB/src/server/login/account/AccountManager.cpp
+++ MMOCoreORB/src/server/login/account/AccountManager.cpp
@@ -359,7 +359,6 @@ Reference<Account*> AccountManager::createAccount(const String& username, const 
 
 	return getAccount(accountID, passwordStored, true);
 }
-#endif // !WITH_SWGREALMS_API
 
 Reference<Account*> AccountManager::getAccount(uint32 accountID, bool forceSqlUpdate) {
 	static Logger logger("AccountManager");
@@ -383,32 +382,9 @@ Reference<Account*> AccountManager::getAccount(uint32 accountID, bool forceSqlUp
 
 			return nullptr;
 		}
-	} else if (!forceSqlUpdate && accObj->isSqlLoaded() && !accObj->isAccountDataStale()) {
+	} else if (!forceSqlUpdate && accObj->isSqlLoaded()) {
 		return accObj;
 	}
-
-#ifdef WITH_SWGREALMS_API
-	// Try to get account data from API
-	String errorMessage;
-	auto swgRealmsAPI = SWGRealmsAPI::instance();
-
-	if (swgRealmsAPI != nullptr && swgRealmsAPI->getAccountDataBlocking(accountID, accObj, errorMessage)) {
-		Locker locker(accObj);
-		accObj->updateFromDatabase();
-
-		return accObj;
-	}
-
-	// API failed - check failOpen setting
-	if (swgRealmsAPI != nullptr && !swgRealmsAPI->getFailOpen()) {
-		// Fail closed - do not fall back to MySQL
-		logger.error() << "SWGRealms API getAccountDataBlocking failed for accountID " << accountID << ": " << errorMessage << " (fail-closed, NOT falling back to MySQL)";
-		return nullptr;
-	}
-
-	// Fail open enabled - log error and fall back to direct SQL
-	logger.error() << "SWGRealms API getAccountDataBlocking failed for accountID " << accountID << ": " << errorMessage << " (fail-open, falling back to MySQL)";
-#endif // WITH_SWGREALMS_API
 
 	StringBuffer query;
 	query << "SELECT a.active, a.username, a.password, a.salt, a.account_id, a.station_id, UNIX_TIMESTAMP(a.created), a.admin_level, '' AS session_id FROM accounts a WHERE a.account_id = '" << accountID << "' LIMIT 1;";
@@ -423,10 +399,6 @@ Reference<Account*> AccountManager::getAccount(uint32 accountID, bool forceSqlUp
 		accObj->setSalt(result->getString(3));
 		accObj->setAccountID(accountID);
 		accObj->setStationID(result->getUnsignedInt(5));
-
-		Time ttl;
-		ttl.addMiliTime(3600 * 1000);
-		accObj->setAccountDataValidUntil(ttl);
 
 		if (!ConfigManager::instance()->getBool("Core3.AccountManager.CreatedDateFirstConnect", false)) {
 			accObj->setTimeCreated(result->getUnsignedInt(6));
@@ -447,7 +419,6 @@ Reference<Account*> AccountManager::getAccount(uint32 accountID, bool forceSqlUp
 	return nullptr;
 }
 
-#ifndef WITH_SWGREALMS_API
 Reference<Account*> AccountManager::getAccount(uint32 accountID, String& passwordStored, bool forceSqlUpdate) {
 	StringBuffer query;
 	query << "SELECT a.active, a.username, a.password, a.salt, a.account_id, a.station_id, UNIX_TIMESTAMP(a.created), a.admin_level, '' AS session_id FROM accounts a WHERE a.account_id = '" << accountID << "' LIMIT 1;";
@@ -538,9 +509,36 @@ Reference<Account*> AccountManager::getAccount(const String& accountName, bool f
 	return getAccount(query.toString(), temp, forceSqlUpdate);
 }
 #else // WITH_SWGREALMS_API
-Reference<Account*> AccountManager::getAccount(const String& accountName, bool forceSqlUpdate) {
+Reference<Account*> AccountManager::getAccount(uint32 accountID, bool forceSqlUpdate) {
 	static Logger logger("AccountManager");
 
+	Reference<Account*> accObj;
+
+	{
+		// Scope mutext ot just ObjectBroker since API can process result on separate thread
+		Locker locker(&mutex);
+
+		static uint64 databaseID = ObjectDatabaseManager::instance()->getDatabaseID("accounts");
+
+		uint64 oid = (accountID | (databaseID << 48));
+
+		accObj = Core::getObjectBroker()->lookUp(oid).castTo<Account*>();
+
+		if (accObj == nullptr) {
+			// Lazily create account object
+			accObj = dynamic_cast<Account*>(ObjectManager::instance()->createObject("Account", 3, "accounts", oid));
+
+			if (accObj == nullptr) {
+				logger.error("Error creating account object with account ID " + String::hexvalueOf((int64)oid));
+
+				return nullptr;
+			}
+		} else if (!forceSqlUpdate && accObj->isSqlLoaded() && !accObj->isAccountDataStale()) {
+			return accObj;
+		}
+	}
+
+	// Try to get account data from API
 	String errorMessage;
 	auto swgRealmsAPI = SWGRealmsAPI::instance();
 
@@ -548,6 +546,23 @@ Reference<Account*> AccountManager::getAccount(const String& accountName, bool f
 		logger.error() << "SWGRealms API instance is null";
 		return nullptr;
 	}
+
+	if (swgRealmsAPI->getAccountDataBlocking(accountID, accObj, errorMessage)) {
+		Locker locker(accObj);
+		accObj->updateFromDatabase();
+
+		return accObj;
+	}
+
+	logger.error() << "SWGRealms API getAccountDataBlocking failed for accountID " << accountID << ": " << errorMessage;
+	return nullptr;
+}
+
+Reference<Account*> AccountManager::getAccount(const String& accountName, bool forceSqlUpdate) {
+	static Logger logger("AccountManager");
+
+	String errorMessage;
+	auto swgRealmsAPI = SWGRealmsAPI::instance();
 
 	// Get account_id from username via API
 	uint32 accountID = swgRealmsAPI->getAccountID(accountName, errorMessage);
